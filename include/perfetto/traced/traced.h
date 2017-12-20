a5ff2a8e5a4db1aa74dbe04ebf94a28f74cf665c
Primiano Tucci <primiano@google.com>
2017-12-20 11:37:00 +0100

    Wire up traced binary
    Also simplify a bit the include/ folder. Not sure we'll
    ever need more than one header. That the code under
    traced contains only leaf executables and not libraries,
    one hader for the two Main(s) should be sufficient.
    Also fixes static lib generation for Mac.
    
    Change-Id: I3e1fed0f02aef61b1cf797b89855b981e0d73a5e

--- include/perfetto/traced/traced.h
+++ include/perfetto/traced/traced.h
@@ -14,8 +14,28 @@
  * limitations under the License.
  */
 
+#ifndef INCLUDE_PERFETTO_TRACED_TRACED_H_
+#define INCLUDE_PERFETTO_TRACED_TRACED_H_
+
+#include "perfetto/base/build_config.h"
+
 namespace perfetto {
 
+// TODO(primiano): The actual paths are TBD after security reviews. For the
+// moment using an abstract socket on Linux/Andriod and a linked socket on /tmp
+// for Mac.
+
+#if BUILDFLAG(OS_ANDROID) || BUILDFLAG(OS_LINUX)
+#define PERFETTO_PRODUCER_SOCK_NAME "@perfetto-producer"
+#define PERFETTO_CONSUMER_SOCK_NAME "@perfetto-consumer"
+#else
+#define PERFETTO_PRODUCER_SOCK_NAME "/tmp/perfetto-producer"
+#define PERFETTO_CONSUMER_SOCK_NAME "/tmp/perfetto-consumer"
+#endif
+
+int ServiceMain(int argc, char** argv);
 int ProbesMain(int argc, char** argv);
 
 }  // namespace perfetto
+
+#endif  // INCLUDE_PERFETTO_TRACED_TRACED_H_
