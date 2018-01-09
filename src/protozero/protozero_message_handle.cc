3dc757ba0908febbb4686268667a467707a0abfa
Lalit Maganti <lalitm@google.com>
2018-01-09 16:48:04 +0000

    perfetto: ensure that we don't finalize message when message is shared
    When a message is shared across handles and we perform a move into a
    reference from a temporary handle, we finalize the message even
    though we then make the reference handle point to this message and
    expect it to be unfinalized.
    
    Fix this subtle bug by ensuring that if we are sharing messages across
    handles that we don't finalize and add a check to make sure that the
    moved in handle is also not finalized.
    
    Test: ProtoZeroMessageHandleTest
    Change-Id: I57cd9439da8ba049a7f0967e99fea261495daf64

--- src/protozero/protozero_message_handle.cc
+++ src/protozero/protozero_message_handle.cc
@@ -44,9 +44,11 @@ ProtoZeroMessageHandleBase::ProtoZeroMessageHandleBase(
 ProtoZeroMessageHandleBase& ProtoZeroMessageHandleBase::operator=(
     ProtoZeroMessageHandleBase&& other) {
   // If the current handle was pointing to a message and is being reset to a new
-  // one, finalize the old message.
-  if (message_)
+  // one, finalize the old message. However, if the other message is the same as
+  // the one we point to, don't finalize.
+  if (message_ && message_ != other.message_)
     message_->Finalize();
+  PERFETTO_DCHECK(!other->is_finalized());
   Move(std::move(other));
   return *this;
 }
