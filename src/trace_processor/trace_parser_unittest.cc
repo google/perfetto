93b763699f28f576068dc2a53fcdce39dde04e7c
Lalit Maganti <lalitm@google.com>
2018-06-01 03:03:58 +0100

    trace_processor: add base trace loading code
    This CL sets out the foundation for the trace loading code.
    Specifically, it introduces BlobReader (an abstraction for reading data
    from an arbitary data source into a buffer) and TraceLoader.
    
    TraceLoader will use BlobReader and a class known as ProtoParser (to be
    introduced in a follow-up CL) to read and parse the trace packets and
    place them in the ColumnarTrace introduced in the previous CL
    
    Bug: 80416541
    Change-Id: I5eb725ff6676b3ecb12daba74e8f0dc766eff11e

---
+++ src/trace_processor/trace_parser_unittest.cc
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2017 The Android Open foo Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "src/trace_processor/trace_parser.h"
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+namespace perfetto {
+namespace trace_processor {
+namespace {
+
+using ::testing::_;
+using ::testing::InSequence;
+using ::testing::Invoke;
+
+TEST(TraceParser, LoadSinglePacket) {
+  // TODO(lalitm): write this test.
+}
+
+TEST(TraceParser, LoadMultiplePacket) {
+  // TODO(lalitm): write this test.
+}
+
+TEST(TraceParser, RepeatedLoadSinglePacket) {
+  // TODO(lalitm): write this test.
+}
+
+}  // namespace
+}  // namespace trace_processor
+}  // namespace perfetto
