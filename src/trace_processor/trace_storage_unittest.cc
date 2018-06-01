caed37e1d5aa9f59bae32b024db4899de0d83857
Lalit Maganti <lalitm@google.com>
2018-06-01 03:03:08 +0100

    trace_processor: initial definition of columnar trace class
    This class will form the common point which other classes will read
    from/write to. It will be shared across the three virtual tables (event, thread
    and process) and be read from.
    
    Bug: 80416541
    Change-Id: I9d431a98c2f4d7359338d55a03af09a04c01859e

---
+++ src/trace_processor/trace_storage_unittest.cc
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2017 The Android Open foo Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "src/trace_processor/trace_storage.h"
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+namespace perfetto {
+namespace trace_processor {
+namespace {
+
+using ::testing::_;
+using ::testing::InSequence;
+using ::testing::Invoke;
+
+TEST(TraceStorage, AddSliceForCpu) {
+  TraceStorage trace;
+  trace.AddSliceForCpu(2, 1000, 42, "test");
+  ASSERT_EQ(trace.start_timestamps_for_cpu(2)[0], 1000);
+}
+
+}  // namespace
+}  // namespace trace_processor
+}  // namespace perfetto
