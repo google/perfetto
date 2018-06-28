f1ba1dcd588216eb47c5ccf3251ef48b0c5d3e13
Deepanjan Roy <dproy@google.com>
2018-06-28 16:51:20 +0100

    Add tslint presubmit
    The rules are taken from internal google.tslint.json and
    tricorder.tslint.json. Did not include rules that were too g3 specific,
    or had custom implementation (because we probably need to clear with
    someone before open sourcing them and the implementations have lots of
    g3 references.)
    
    There were a small number of lint errors that are fixed with this CL.
    
    Not done yet: Adding clang-format.
    
    Change-Id: Ib1312dac51c06177d4472c5d225de64598a95360

---
+++ ui/PRESUBMIT.py
@@ -0,0 +1,49 @@
+# Copyright (C) 2018 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import subprocess
+
+def CheckChange(input, output):
+    results = []
+    results += CheckTslint(input, output)
+    return results
+
+
+def CheckChangeOnUpload(input_api, output_api):
+    return CheckChange(input_api, output_api)
+
+
+def CheckChangeOnCommit(input_api, output_api):
+    return CheckChange(input_api, output_api)
+
+
+def CheckTslint(input_api, output_api):
+    path = input_api.os_path
+    ui_path = input_api.PresubmitLocalPath();
+    node = path.join(ui_path, 'node')
+    tslint = path.join(ui_path, 'node_modules', '.bin', 'tslint')
+
+    # Some tslint rules require type information and thus need the whole
+    # project. We therefore call tslint on the whole project instead of only the
+    # changed files. It is possible to break tslint on files that was not
+    # changed by changing the type of an object.
+    if subprocess.call([node, tslint, '--project', ui_path,
+                        '--format', 'codeFrame']):
+        return [
+        output_api.PresubmitError(
+            'There were tslint errors. You may be able to fix some of them' +
+            'using \n' +
+            '$ ' + tslint + ' --project ' + ui_path + ' --fix')
+        ]
+    return []
