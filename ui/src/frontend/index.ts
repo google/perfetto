3e47c90d0655a9bcb267e478fe91a9cb8ba6f70c
michaschwab <michaschwab@google.com>
2018-07-16 14:58:51 -0400

    Handle Resize
    This simplifies the initialization of Frontend, CanvasController and
    RootVirtualContext, removing the need to pass dimensions when the DOM
    is not ready, and sets the with and height when the DOM is ready or the
    window has been resized. This also prevents unnecessary draw calls in
    the first render, since the canvas starts with a zero size and all
    tracks are deemed off canvas.
    
    Change-Id: I7c3dd9679c3c8c9a11072958f14883cdd3e13878

--- ui/src/frontend/index.ts
+++ ui/src/frontend/index.ts
@@ -28,10 +28,26 @@ import {Track} from './track';
 
 export const Frontend = {
   oninit() {
-    this.width = 1000;
-    this.height = 400;
+    this.width = 0;
+    this.height = 0;
+    this.canvasController = new CanvasController();
+  },
+  oncreate(vnode) {
+    this.onResize = () => {
+      const rect = vnode.dom.getBoundingClientRect();
+      this.width = rect.width;
+      this.height = rect.height;
+      this.canvasController.setDimensions(this.width, this.height);
+      m.redraw();
+    };
+    // Have to redraw after initialization to provide dimensions to view().
+    setTimeout(() => this.onResize());
 
-    this.canvasController = new CanvasController(this.width, this.height);
+    // Once ResizeObservers are out, we can stop accessing the window here.
+    window.addEventListener('resize', this.onResize);
+  },
+  onremove() {
+    window.removeEventListener('resize', this.onResize);
   },
   view({}) {
     const canvasTopOffset = this.canvasController.getCanvasTopOffset();
@@ -41,7 +57,14 @@ export const Frontend = {
 
     return m(
         '.frontend',
-        {style: {position: 'relative', width: this.width.toString() + 'px'}},
+        {
+          style: {
+            position: 'relative',
+            width: '100%',
+            height: 'calc(100% - 100px)',
+            overflow: 'hidden'
+          }
+        },
         m(ScrollableContainer,
           {
             width: this.width,
@@ -60,67 +83,79 @@ export const Frontend = {
             name: 'Track 1',
             trackContext: new ChildVirtualContext(
                 ctx, {y: 0, x: 0, width: this.width, height: 90}),
-            top: 0
+            top: 0,
+            width: this.width
           }),
           m(Track, {
             name: 'Track 2',
             trackContext: new ChildVirtualContext(
                 ctx, {y: 100, x: 0, width: this.width, height: 90}),
-            top: 100
+            top: 100,
+            width: this.width
           }),
           m(Track, {
             name: 'Track 3',
             trackContext: new ChildVirtualContext(
                 ctx, {y: 200, x: 0, width: this.width, height: 90}),
-            top: 200
+            top: 200,
+            width: this.width
           }),
           m(Track, {
             name: 'Track 4',
             trackContext: new ChildVirtualContext(
                 ctx, {y: 300, x: 0, width: this.width, height: 90}),
-            top: 300
+            top: 300,
+            width: this.width
           }),
           m(Track, {
             name: 'Track 5',
             trackContext: new ChildVirtualContext(
                 ctx, {y: 400, x: 0, width: this.width, height: 90}),
-            top: 400
+            top: 400,
+            width: this.width
           }),
           m(Track, {
             name: 'Track 6',
             trackContext: new ChildVirtualContext(
                 ctx, {y: 500, x: 0, width: this.width, height: 90}),
-            top: 500
+            top: 500,
+            width: this.width
           }),
           m(Track, {
             name: 'Track 7',
             trackContext: new ChildVirtualContext(
                 ctx, {y: 600, x: 0, width: this.width, height: 90}),
-            top: 600
+            top: 600,
+            width: this.width
           }),
           m(Track, {
             name: 'Track 8',
             trackContext: new ChildVirtualContext(
                 ctx, {y: 700, x: 0, width: this.width, height: 90}),
-            top: 700
+            top: 700,
+            width: this.width
           }),
           m(Track, {
             name: 'Track 9',
             trackContext: new ChildVirtualContext(
                 ctx, {y: 800, x: 0, width: this.width, height: 90}),
-            top: 800
+            top: 800,
+            width: this.width
           }),
           m(Track, {
             name: 'Track 10',
             trackContext: new ChildVirtualContext(
                 ctx, {y: 900, x: 0, width: this.width, height: 90}),
-            top: 900
+            top: 900,
+            width: this.width
           }), ), );
   },
-} as
-    m.Component<
-        {width: number, height: number},
-        {canvasController: CanvasController, width: number, height: number}>;
+} as m.Component<{width: number, height: number}, {
+  canvasController: CanvasController,
+  width: number,
+  height: number,
+  onResize: () => void
+}>;
 
 export const FrontendPage = createPage({
   view() {
