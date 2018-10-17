15e71f8e6a865bf3a9e8f74387ca97561fb18ca3
Florian Mayer <fmayer@google.com>
2018-10-17 15:11:49 +0100

    profiling: Set timeout for sockets in client.
    This makes sure that clients gracefully fail if the server locks up but
    keeps the sockets open.
    
    Change-Id: Iadfa98d5efdbad31cd03420a6b1215155c1b9dfb

--- src/profiling/memory/client.cc
+++ src/profiling/memory/client.cc
@@ -44,6 +44,8 @@
 namespace perfetto {
 namespace {
 
+constexpr struct timeval kSendTimeout = {1 /* s */, 0 /* us */};
+
 #if !PERFETTO_BUILDFLAG(PERFETTO_OS_ANDROID)
 // glibc does not define a wrapper around gettid, bionic does.
 pid_t gettid() {
@@ -64,6 +66,12 @@ std::vector<base::ScopedFile> ConnectPool(const std::string& sock_name,
     auto sock = base::CreateSocket();
     if (connect(*sock, reinterpret_cast<sockaddr*>(&addr), addr_size) == -1) {
       PERFETTO_PLOG("Failed to connect to %s", sock_name.c_str());
+      continue;
+    }
+    if (setsockopt(*sock, SOL_SOCKET, SO_SNDTIMEO,
+                   reinterpret_cast<const char*>(&kSendTimeout),
+                   sizeof(kSendTimeout)) != 0) {
+      PERFETTO_PLOG("Failed to set timeout for %s", sock_name.c_str());
       continue;
     }
     res.emplace_back(std::move(sock));
