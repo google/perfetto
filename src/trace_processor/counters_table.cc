dc38c5209303e595a5bf8a81a5c8d80adda94e5b
Isabelle Taylor <taylori@google.com>
2018-10-29 17:55:22 +0000

    trace_processor: Allow sqlite to handle string filtering
    Until we decide if/how we implement string filtering,
    allow sqlite to do it for us.
    
    Change-Id: I782616eae08e6e8e466f1f64e6ce93cd9bb928be

--- src/trace_processor/counters_table.cc
+++ src/trace_processor/counters_table.cc
@@ -65,14 +65,19 @@ std::unique_ptr<Table::Cursor> CountersTable::CreateCursor(
       new StorageCursor(std::move(it), schema_.ToColumnReporters()));
 }
 
-int CountersTable::BestIndex(const QueryConstraints&, BestIndexInfo* info) {
+int CountersTable::BestIndex(const QueryConstraints& qc, BestIndexInfo* info) {
   info->estimated_cost =
       static_cast<uint32_t>(storage_->counters().counter_count());
 
-  // We should be able to handle any constraint and any order by clause given
-  // to us.
+  // Only the string columns are handled by SQLite
   info->order_by_consumed = true;
-  std::fill(info->omit.begin(), info->omit.end(), true);
+  size_t name_index = schema_.ColumnIndexFromName("name");
+  size_t ref_type_index = schema_.ColumnIndexFromName("ref_type");
+  for (size_t i = 0; i < qc.constraints().size(); i++) {
+    info->omit[i] =
+        qc.constraints()[i].iColumn != static_cast<int>(name_index) &&
+        qc.constraints()[i].iColumn != static_cast<int>(ref_type_index);
+  }
 
   return SQLITE_OK;
 }
