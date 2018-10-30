ea99f70b87e09a20969ae251661c579a3e182756
Lalit Maganti <lalitm@google.com>
2018-10-30 12:04:26 +0000

    trace_processor: fix bug with span join
    Bug: 118665515
    Change-Id: I29bfdf61663c42e060b2b3058e32fcc1252d67a8

---
+++ test/trace_processor/slice_span_join_b118665515.sql
@@ -0,0 +1,7 @@
+create virtual table window_8 using window;
+
+create virtual table span_8 using span(sched, window_8, cpu);
+
+update window_8 set window_start=81473010031230, window_dur=19684693341, quantum=10000000 where rowid = 0;
+
+select quantum_ts as bucket, sum(dur)/cast(10000000 as float) as utilization from span_8 where cpu = 7 and utid != 0 group by quantum_ts;
