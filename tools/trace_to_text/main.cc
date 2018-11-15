0ca7fb4af54c6b9c68349cc783d14b363d024276
Florian Mayer <fmayer@google.com>
2018-11-15 21:13:42 +0000

    trace_to_text: add converter to profile proto.
    Change-Id: I95165e4323f8a7a609f1666e91a21624398ef8ee

--- tools/trace_to_text/main.cc
+++ tools/trace_to_text/main.cc
@@ -18,6 +18,7 @@
 #include <iostream>
 
 #include "perfetto/base/logging.h"
+#include "tools/trace_to_text/trace_to_profile.h"
 #include "tools/trace_to_text/trace_to_systrace.h"
 #include "tools/trace_to_text/trace_to_text.h"
 
@@ -25,7 +26,7 @@ namespace {
 
 int Usage(const char* argv0) {
   printf(
-      "Usage: %s systrace|json|text [trace.proto] "
+      "Usage: %s systrace|json|text|profile [trace.pb] "
       "[trace.txt]\n",
       argv0);
   return 1;
@@ -77,6 +78,9 @@ int main(int argc, char** argv) {
                                                     /*wrap_in_json=*/false);
   if (format == "text")
     return perfetto::trace_to_text::TraceToText(input_stream, output_stream);
+
+  if (format == "profile")
+    return perfetto::trace_to_text::TraceToProfile(input_stream, output_stream);
 
   return Usage(argv[0]);
 }
