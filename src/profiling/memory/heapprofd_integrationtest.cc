a9f9b4cfa4c1d35a4ef00b4c238e658f5a3779b9
Florian Mayer <fmayer@google.com>
2018-11-16 12:17:25 +0000

    profiling: Improve connection management.
    There can now be multiple ProfilingSessions profiling the same PID.
    For now, the ClientConfiguration of the first one wins.
    
    Test: m
    Test: flash walleye
    Test: profile system_server
    
    Change-Id: Ia8eef00753a75a449c03b87bd0abf3e75fddafdb

--- src/profiling/memory/heapprofd_integrationtest.cc
+++ src/profiling/memory/heapprofd_integrationtest.cc
@@ -43,11 +43,14 @@ class HeapprofdIntegrationTest : public ::testing::Test {
   void TearDown() override { DESTROY_TEST_SOCK(kSocketName); }
 };
 
-// TODO(fmayer): Fix out of tree integration test.
-#if PERFETTO_BUILDFLAG(PERFETTO_OS_ANDROID)
+// ASAN does not like sendmsg of the stack.
+// TODO(fmayer): Try to fix this more properly.
+#ifndef ADDRESS_SANITIZER
 #define MAYBE_EndToEnd EndToEnd
+#define MAYBE_MultiSession MultiSession
 #else
 #define MAYBE_EndToEnd DISABLED_EndToEnd
+#define MAYBE_MultiSession DISABLED_MultiSession
 #endif
 
 TEST_F(HeapprofdIntegrationTest, MAYBE_EndToEnd) {
@@ -63,14 +66,50 @@ TEST_F(HeapprofdIntegrationTest, MAYBE_EndToEnd) {
         // TODO(fmayer): Test symbolization and result of unwinding.
         // This check will only work on in-tree builds as out-of-tree
         // libunwindstack is behaving a bit weirdly.
+// TODO(fmayer): Fix out of tree integration test.
+#if PERFETTO_BUILDFLAG(PERFETTO_OS_ANDROID)
         BookkeepingRecord bookkeeping_record;
         ASSERT_TRUE(HandleUnwindingRecord(&r, &bookkeeping_record));
         bookkeeping_thread.HandleBookkeepingRecord(&bookkeeping_record);
+#endif
+        base::ignore_result(r);
+        base::ignore_result(bookkeeping_thread);
         done();
       },
       &bookkeeping_thread);
 
-  listener.ExpectPID(getpid(), {kSamplingInterval});
+  auto session = listener.ExpectPID(getpid(), {kSamplingInterval});
+  auto sock = base::UnixSocket::Listen(kSocketName, &listener, &task_runner);
+  if (!sock->is_listening()) {
+    PERFETTO_ELOG("Socket not listening.");
+    PERFETTO_CHECK(false);
+  }
+  std::thread th([kSamplingInterval] {
+    Client client(kSocketName, 1);
+    SomeFunction(&client);
+    EXPECT_EQ(client.client_config_for_testing().interval, kSamplingInterval);
+  });
+
+  task_runner.RunUntilCheckpoint("done");
+  th.join();
+}
+
+TEST_F(HeapprofdIntegrationTest, MAYBE_MultiSession) {
+  GlobalCallstackTrie callsites;
+  // TODO(fmayer): Actually test the dump.
+  BookkeepingThread bookkeeping_thread("");
+
+  base::TestTaskRunner task_runner;
+  auto done = task_runner.CreateCheckpoint("done");
+  constexpr uint64_t kSamplingInterval = 123;
+  SocketListener listener([&done](UnwindingRecord) { done(); },
+                          &bookkeeping_thread);
+
+  auto session = listener.ExpectPID(getpid(), {kSamplingInterval});
+  // Allow to get a second session, but it will still use the previous
+  // sampling rate.
+  auto session2 = listener.ExpectPID(getpid(), {kSamplingInterval + 1});
+  session = SocketListener::ProfilingSession();
   auto sock = base::UnixSocket::Listen(kSocketName, &listener, &task_runner);
   if (!sock->is_listening()) {
     PERFETTO_ELOG("Socket not listening.");
