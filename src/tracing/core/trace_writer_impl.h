a01e28ac4f1e570aed48c7378d31ec0bc6c88685
Eric Seckler <eseckler@google.com>
2019-01-08 11:21:04 +0000

    service: Scrape SMBs on flush, disable and producer disconnect
    Scrape chunks from the SMBs that have been started but haven't yet been
    committed by the producers (i.e. they are either completed or currently
    being written to). We do this in three locations:
    - When flushing, e.g. to handle unresponsive producers or producers
      that are unable to flush their active chunks.
    - When a producer disconnects, e.g. to handle crashed producers.
    - After the session was disabled, e.g. to gather data from producers
      that didn't stop their data sources in time.
    
    Bug: 73828976
    Bug: 117310221
    Change-Id: I4ed5394b6c47ea35a4999e4ff95b469337a5d273

--- src/tracing/core/trace_writer_impl.h
+++ src/tracing/core/trace_writer_impl.h
@@ -41,6 +41,8 @@ class TraceWriterImpl : public TraceWriter,
   void Flush(std::function<void()> callback = {}) override;
   WriterID writer_id() const override;
 
+  void ResetChunkForTesting() { cur_chunk_ = SharedMemoryABI::Chunk(); }
+
  private:
   TraceWriterImpl(const TraceWriterImpl&) = delete;
   TraceWriterImpl& operator=(const TraceWriterImpl&) = delete;
