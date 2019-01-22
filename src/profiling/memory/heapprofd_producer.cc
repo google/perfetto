10c19fbf3219ec00ce0972d9476169dcaa6076a1
Ryan Savitski <rsavitski@google.com>
2019-01-22 14:44:15 +0000

    heapprofd: fork mode: pass cmdline as a flag to heapprofd
    Client now reads (and normalizes) /proc/own_pid/cmdline, passing it as a
    separate flag to heapprofd. The forked heapprofd is otherwise unable to access
    the target's cmdline entry due to hidepid.
    
    Note also that this is making the client give up if it's unable to parse the
    cmdline. Previously we would still try to carry on just using the pid (so
    by-cmdline configs wouldn't work, but by-pid would).
    
    Bug: 120185246
    Change-Id: I849c4c18cd68e290077fe2633c851eb4fefd9fef

--- src/profiling/memory/heapprofd_producer.cc
+++ src/profiling/memory/heapprofd_producer.cc
@@ -100,15 +100,10 @@ HeapprofdProducer::~HeapprofdProducer() {
   }
 }
 
-void HeapprofdProducer::SetTargetProcess(pid_t target_pid) {
+void HeapprofdProducer::SetTargetProcess(pid_t target_pid,
+                                         std::string target_cmdline) {
   target_pid_ = target_pid;
-  target_cmdline_.clear();
-  if (!GetCmdlineForPID(target_pid, &target_cmdline_)) {
-    PERFETTO_ELOG(
-        "Failed to look up cmdline for target pid [%d], cmdline-based "
-        "configurations will not work. Proceeding.",
-        static_cast<int>(target_pid));
-  }
+  target_cmdline_ = target_cmdline;
 }
 
 bool HeapprofdProducer::SourceMatchesTarget(const HeapprofdConfig& cfg) {
@@ -145,7 +140,13 @@ void HeapprofdProducer::AdoptConnectedSockets(
           base::ScopedFile(fd), &weak_producer->socket_listener_,
           weak_producer->task_runner_, base::SockType::kStream);
 
-      weak_producer->socket_listener_.HandleClientConnection(std::move(socket));
+      // The forked heapprofd will not normally be able to read the target's
+      // cmdline under procfs, so pass peer's description explicitly.
+      Process process{weak_producer->target_pid_,
+                      weak_producer->target_cmdline_};
+
+      weak_producer->socket_listener_.HandleClientConnection(
+          std::move(socket), std::move(process));
     });
   }
 }
