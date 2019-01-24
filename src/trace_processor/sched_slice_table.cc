b0b53eee85d694150957fb0081b6a90f75405493
Lalit Maganti <lalitm@google.com>
2019-01-24 17:53:39 +0000

    trace_processor: surface priority and end state information in sched tables
    This allows to see why a process was descheduled and what priority it was
    running at.
    
    Bug: 122876520
    Change-Id: Ia5bb0187d1baca830ce9bdc1d0af8aad81ebe2ab

--- src/trace_processor/sched_slice_table.cc
+++ src/trace_processor/sched_slice_table.cc
@@ -34,6 +34,8 @@ StorageSchema SchedSliceTable::CreateStorageSchema() {
       .AddNumericColumn("dur", &slices.durations())
       .AddColumn<TsEndColumn>("ts_end", &slices.start_ns(), &slices.durations())
       .AddNumericColumn("utid", &slices.utids(), &slices.rows_for_utids())
+      .AddColumn<EndStateColumn>("end_state", &slices.end_state())
+      .AddNumericColumn("priority", &slices.priorities())
       .Build({"cpu", "ts"});
 }
 
@@ -85,6 +87,124 @@ uint32_t SchedSliceTable::EstimateQueryCost(const QueryConstraints& qc) {
   // If we get to this point, we do not have any special filter logic so
   // simply return the number of rows.
   return RowCount();
+}
+
+SchedSliceTable::EndStateColumn::EndStateColumn(
+    std::string col_name,
+    const std::deque<ftrace_utils::TaskState>* deque)
+    : StorageColumn(col_name, false), deque_(deque) {
+  for (uint16_t i = 0; i < state_strings_.size(); i++) {
+    state_strings_[i] = ftrace_utils::TaskState(i).ToString();
+  }
+}
+SchedSliceTable::EndStateColumn::~EndStateColumn() = default;
+
+void SchedSliceTable::EndStateColumn::ReportResult(sqlite3_context* ctx,
+                                                   uint32_t row) const {
+  const auto& state = (*deque_)[row];
+  if (state.is_valid()) {
+    PERFETTO_CHECK(state.raw_state() < state_strings_.size());
+    sqlite3_result_text(ctx, state_strings_[state.raw_state()].data(), -1,
+                        sqlite_utils::kSqliteStatic);
+  } else {
+    sqlite3_result_null(ctx);
+  }
+}
+
+void SchedSliceTable::EndStateColumn::Filter(int op,
+                                             sqlite3_value* value,
+                                             FilteredRowIndex* index) const {
+  switch (op) {
+    case SQLITE_INDEX_CONSTRAINT_ISNULL:
+    case SQLITE_INDEX_CONSTRAINT_ISNOTNULL: {
+      bool non_nulls = op == SQLITE_INDEX_CONSTRAINT_ISNOTNULL;
+      index->FilterRows([this, non_nulls](uint32_t row) {
+        const auto& state = (*deque_)[row];
+        return state.is_valid() == non_nulls;
+      });
+      break;
+    }
+    case SQLITE_INDEX_CONSTRAINT_EQ:
+    case SQLITE_INDEX_CONSTRAINT_NE:
+    case SQLITE_INDEX_CONSTRAINT_MATCH:
+      FilterOnState(op, value, index);
+      break;
+    default:
+      // TODO(lalitm): report an error to sqlite for any other constraint.
+      index->IntersectRows({});
+      break;
+  }
+}
+
+void SchedSliceTable::EndStateColumn::FilterOnState(
+    int op,
+    sqlite3_value* value,
+    FilteredRowIndex* index) const {
+  if (sqlite3_value_type(value) != SQLITE_TEXT) {
+    // TODO(lalitm): report an error to sqlite for any other constraint.
+    index->IntersectRows({});
+    return;
+  }
+
+  const char* str = reinterpret_cast<const char*>(sqlite3_value_text(value));
+  ftrace_utils::TaskState compare(str);
+  if (!compare.is_valid()) {
+    // TODO(lalitm): report an error to sqlite for any other constraint.
+    index->IntersectRows({});
+    return;
+  }
+
+  uint16_t raw_state = compare.raw_state();
+  if (op == SQLITE_INDEX_CONSTRAINT_EQ) {
+    index->FilterRows([this, raw_state](uint32_t row) {
+      const auto& state = (*deque_)[row];
+      return state.is_valid() && state.raw_state() == raw_state;
+    });
+  } else if (op == SQLITE_INDEX_CONSTRAINT_NE) {
+    index->FilterRows([this, raw_state](uint32_t row) {
+      const auto& state = (*deque_)[row];
+      return state.is_valid() && state.raw_state() != raw_state;
+    });
+  } else if (op == SQLITE_INDEX_CONSTRAINT_MATCH) {
+    index->FilterRows([this, compare](uint32_t row) {
+      const auto& state = (*deque_)[row];
+      if (!state.is_valid())
+        return false;
+      return (state.raw_state() & compare.raw_state()) == compare.raw_state();
+    });
+  } else {
+    PERFETTO_FATAL("Should never reach this state");
+  }
+}
+
+StorageColumn::Comparator SchedSliceTable::EndStateColumn::Sort(
+    const QueryConstraints::OrderBy& ob) const {
+  if (ob.desc) {
+    return [this](uint32_t f, uint32_t s) {
+      const auto& a = (*deque_)[f];
+      const auto& b = (*deque_)[s];
+      if (!a.is_valid()) {
+        return !b.is_valid() ? 0 : 1;
+      } else if (!b.is_valid()) {
+        return -1;
+      }
+      return sqlite_utils::CompareValuesAsc(a.raw_state(), b.raw_state());
+    };
+  }
+  return [this](uint32_t f, uint32_t s) {
+    const auto& a = (*deque_)[f];
+    const auto& b = (*deque_)[s];
+    if (!a.is_valid()) {
+      return !b.is_valid() ? 0 : -1;
+    } else if (!b.is_valid()) {
+      return 1;
+    }
+    return sqlite_utils::CompareValuesAsc(a.raw_state(), b.raw_state());
+  };
+}
+
+Table::ColumnType SchedSliceTable::EndStateColumn::GetType() const {
+  return Table::ColumnType::kString;
 }
 
 }  // namespace trace_processor
--- tools/diff_test_trace_processor.py
+++ tools/diff_test_trace_processor.py
@@ -98,6 +98,7 @@ def main():
         sys.stderr.write(
           "Expected did not match actual for trace {} and query {}\n"
           .format(trace_path, query_path))
+        sys.stderr.write("Expected file: {}\n".format(expected_path))
         sys.stderr.write("Commandline: {}\n".format(' '.join(cmd)))
 
         expected_lines = expected.splitlines(True)
