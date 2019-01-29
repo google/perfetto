8caa2fc10f6a8c84f5859a913066ad92fadf8510
Hector Dearman <hjd@google.com>
2019-01-29 13:56:30 +0000

    trace_processor: Push counters per upid
    Bug: 123560328
    Change-Id: I4d42937650b5e10dd8b3195895ce5d770184bf1c

--- src/trace_processor/proto_trace_parser.cc
+++ src/trace_processor/proto_trace_parser.cc
@@ -965,13 +965,15 @@ void ProtoTraceParser::ParsePrint(uint32_t,
           context_->storage->mutable_instants()->AddInstantEvent(
               timestamp, lmk_id_, 0, killed_upid, RefType::kRefUpid);
         }
-        // TODO(tilal6991): we should not add LMK events to the counters table
+        // TODO(lalitm): we should not add LMK events to the counters table
         // once the UI has support for displaying instants.
       }
-      UniqueTid utid = context_->process_tracker->UpdateThread(pid, point.tgid);
+      // This is per upid on purpose. Some counters are pushed from arbitrary
+      // threads but are really per process.
+      UniquePid upid = context_->process_tracker->UpdateProcess(point.tgid);
       StringId name_id = context_->storage->InternString(point.name);
       context_->event_tracker->PushCounter(timestamp, point.value, name_id,
-                                           utid, RefType::kRefUtid);
+                                           upid, RefType::kRefUpid);
     }
   }
   PERFETTO_DCHECK(decoder.IsEndOfBuffer());
