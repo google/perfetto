efb4bbbb26b4c7d95a2f2ef1210f330001a87f7c
Lalit Maganti <lalitm@google.com>
2019-02-22 18:52:03 +0000

    trace_processor: allow out of order columns in span join
    This CL fixes the issue where ts, dur or partition being out of order
    (i.e. not the first three cols in the table) would cause serious issues
    in the data returned.
    
    This was because there was a mismatch between the order of columns in
    the query (i.e. the order SQLite was returning columns to us) and the
    order we expect according to the schema (i.e. the order we queried
    to generate the schema).
    
    Because we forced ts and dur to be the first two columns (and partition
    to potentially be third), the columns in the schema at pos 0, 1 and 2
    would not be returned properly if it was not ts, dur and partition.
    
    Fix this by always making the queries use the order of columns in the
    schema and storing the index of ts, dur [and partition] in the table
    definition and looking it up when we want to read these fields.
    
    Change-Id: Ia0a4bc8c283e78d640c20c2a3fdec658e23c602a

---
+++ test/trace_processor/span_join_unordered_cols.sql
@@ -0,0 +1,36 @@
+create table t1(
+  a1 STRING,
+  a2 BIG INT,
+  dur BIG INT,
+  a3 BIG INT,
+  ts BIG INT PRIMARY KEY
+) without rowid;
+
+INSERT INTO t1(a1, a2, dur, a3, ts)
+VALUES
+("A", 1, 10, 100, 0),
+("B", 2, 90, 101, 10),
+("C", 3, 1, 102, 100);
+
+create table t2(
+  b1 STRING,
+  ts BIG INT,
+  b2 BIG INT,
+  part BIG INT,
+  dur BIG INT,
+  b3 BIG INT,
+  PRIMARY KEY (part, ts)
+) without rowid;
+
+INSERT INTO t2(b1, ts, b2, part, dur, b3)
+VALUES
+("A", 10, 10, 0, 90, 100),
+("B", 100, 90, 0, 10, 200),
+("C", 110, 1, 0, 5, 300),
+("A", 5, 10, 1, 45, 100),
+("B", 50, 90, 1, 40, 200),
+("C", 90, 1, 1, 100, 300);
+
+create virtual table sp using span_join(t1, t2 PARTITIONED part);
+
+select * from sp;
