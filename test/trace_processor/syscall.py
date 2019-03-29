f31a703a500602b19cd0deba5eab7937de8f0b9f
Hector Dearman <hjd@google.com>
2019-03-29 10:05:09 +0000

    trace_processor: Improve support for syscalls
    Syscall numbers are different on each platform.
    This cl introduces SyscallTracker which maps the syscall numbers
    onto the correct syscall for each platform. We use the SystemInfo
    packet to work out which platform the trace is taken from.
    
    Bug: 124099616
    Change-Id: I0e829aa6fbea1a2d59c6af11879e82e372557181

--- test/trace_processor/syscall.py
+++ test/trace_processor/syscall.py
@@ -19,11 +19,14 @@ sys.path.append(path.dirname(path.abspath(__file__)))
 import synth_common
 
 trace = synth_common.create_trace()
+trace.add_system_info(arch='aarch64')
+
 trace.add_process_tree_packet()
 trace.add_process(pid=1, ppid=0, cmdline="init")
 trace.add_process(pid=2, ppid=1, cmdline="two_thread_process")
 trace.add_process(pid=4, ppid=1, cmdline="single_thread_process")
 trace.add_thread(tid=3, tgid=2, cmdline="two_thread_process")
+
 
 trace.add_ftrace_packet(cpu=0)
 trace.add_sys_enter(ts=100, tid=1, id=0)
