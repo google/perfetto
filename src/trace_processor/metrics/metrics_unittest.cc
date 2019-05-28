6e5a4d1ce60e4298b926b4db954bea296a06d57c
Lalit Maganti <lalitm@google.com>
2019-05-28 13:49:57 +0100

    metrics: add tests for protobuilder functions
    This adds the promised tests for the metric proto builder functions
    to ensure they don't break in the face of future refactoring.
    
    Still TBD is to add a test for repeated messages but given that most
    e2e tests will be using repeated messages, this doesn't seem like the
    highest priorty item
    
    Bug: 129747127
    Change-Id: Ia7f6093260d0d68d4e9886bc5ecc0b4d1f150003

--- src/trace_processor/metrics/metrics_unittest.cc
+++ src/trace_processor/metrics/metrics_unittest.cc
@@ -20,6 +20,8 @@
 
 #include "gtest/gtest.h"
 
+#include "perfetto/common/descriptor.pbzero.h"
+
 namespace perfetto {
 namespace trace_processor {
 namespace metrics {
@@ -47,6 +49,166 @@ TEST(MetricsTest, TemplateReplace) {
 
   std::string unused;
   ASSERT_NE(TemplateReplace("{{missing}}", {{}}, &unused), 0);
+}
+
+class ProtoBuilderTest : public ::testing::Test {
+ protected:
+  template <bool repeated>
+  protozero::TypedProtoDecoder<1, repeated> DecodeSingleFieldProto(
+      const std::vector<uint8_t>& result_ser) {
+    protos::pbzero::ProtoBuilderResult::Decoder result(result_ser.data(),
+                                                       result_ser.size());
+    protozero::ConstBytes single_ser = result.single();
+    protos::pbzero::SingleBuilderResult::Decoder single(single_ser.data,
+                                                        single_ser.size);
+
+    protozero::ConstBytes proto_ser = single.protobuf();
+    return protozero::TypedProtoDecoder<1, repeated>(proto_ser.data,
+                                                     proto_ser.size);
+  }
+};
+
+TEST_F(ProtoBuilderTest, AppendLong) {
+  using FieldDescriptorProto = protos::pbzero::FieldDescriptorProto;
+
+  // Create the descriptor version of the following message:
+  // message TestProto {
+  //   optional int64 int_value = 1;
+  // }
+  ProtoDescriptor descriptor(".perfetto.protos", ".perfetto.protos.TestProto",
+                             base::nullopt);
+  descriptor.AddField(FieldDescriptor(
+      "int_value", 1, FieldDescriptorProto::TYPE_INT64, "", false));
+
+  ProtoBuilder builder(&descriptor);
+  ASSERT_TRUE(builder.AppendLong("int_value", 12345).ok());
+
+  auto result_ser = builder.SerializeToProtoBuilderResult();
+  auto proto = DecodeSingleFieldProto<false>(result_ser);
+  const protozero::Field& int_field = proto.Get(1);
+  ASSERT_EQ(int_field.as_int64(), 12345);
+}
+
+TEST_F(ProtoBuilderTest, AppendDouble) {
+  using FieldDescriptorProto = protos::pbzero::FieldDescriptorProto;
+
+  // Create the descriptor version of the following message:
+  // message TestProto {
+  //   optional double double_value = 1;
+  // }
+  ProtoDescriptor descriptor(".perfetto.protos", ".perfetto.protos.TestProto",
+                             base::nullopt);
+  descriptor.AddField(FieldDescriptor(
+      "double_value", 1, FieldDescriptorProto::TYPE_DOUBLE, "", false));
+
+  ProtoBuilder builder(&descriptor);
+  ASSERT_TRUE(builder.AppendDouble("double_value", 1.2345).ok());
+
+  auto result_ser = builder.SerializeToProtoBuilderResult();
+  auto proto = DecodeSingleFieldProto<false>(result_ser);
+  const protozero::Field& db_field = proto.Get(1);
+  ASSERT_EQ(db_field.as_double(), 1.2345);
+}
+
+TEST_F(ProtoBuilderTest, AppendString) {
+  using FieldDescriptorProto = protos::pbzero::FieldDescriptorProto;
+
+  // Create the descriptor version of the following message:
+  // message TestProto {
+  //   optional string string_value = 1;
+  // }
+  ProtoDescriptor descriptor(".perfetto.protos", ".perfetto.protos.TestProto",
+                             base::nullopt);
+  descriptor.AddField(FieldDescriptor(
+      "string_value", 1, FieldDescriptorProto::TYPE_STRING, "", false));
+
+  ProtoBuilder builder(&descriptor);
+  ASSERT_TRUE(builder.AppendString("string_value", "hello world!").ok());
+
+  auto result_ser = builder.SerializeToProtoBuilderResult();
+  auto proto = DecodeSingleFieldProto<false>(result_ser);
+  const protozero::Field& str_field = proto.Get(1);
+  ASSERT_EQ(str_field.as_std_string(), "hello world!");
+}
+
+TEST_F(ProtoBuilderTest, AppendNested) {
+  using FieldDescriptorProto = protos::pbzero::FieldDescriptorProto;
+
+  // Create the descriptor version of the following message:
+  // message TestProto {
+  //   message NestedProto {
+  //     optional int64 nested_int_value = 1;
+  //   }
+  //   optional NestedProto nested_value = 1;
+  // }
+  ProtoDescriptor nested(".perfetto.protos",
+                         ".perfetto.protos.TestProto.NestedProto",
+                         base::nullopt);
+  nested.AddField(FieldDescriptor("nested_int_value", 1,
+                                  FieldDescriptorProto::TYPE_INT64, "", false));
+
+  ProtoDescriptor descriptor(".perfetto.protos", ".perfetto.protos.TestProto",
+                             base::nullopt);
+  auto field =
+      FieldDescriptor("nested_value", 1, FieldDescriptorProto::TYPE_MESSAGE,
+                      ".perfetto.protos.TestProto.NestedProto", false);
+  field.set_resolved_type_name(".perfetto.protos.TestProto.NestedProto");
+  descriptor.AddField(field);
+
+  ProtoBuilder nest_builder(&nested);
+  ASSERT_TRUE(nest_builder.AppendLong("nested_int_value", 789).ok());
+
+  auto nest_ser = nest_builder.SerializeToProtoBuilderResult();
+
+  ProtoBuilder builder(&descriptor);
+  ASSERT_TRUE(
+      builder.AppendBytes("nested_value", nest_ser.data(), nest_ser.size())
+          .ok());
+
+  auto result_ser = builder.SerializeToProtoBuilderResult();
+  auto proto = DecodeSingleFieldProto<false>(result_ser);
+  const protozero::Field& nest_field = proto.Get(1);
+  ASSERT_EQ(nest_field.type(),
+            protozero::proto_utils::ProtoWireType::kLengthDelimited);
+
+  protozero::ConstBytes nest_bytes = nest_field.as_bytes();
+  protozero::TypedProtoDecoder<1, false> nest(nest_bytes.data, nest_bytes.size);
+
+  const protozero::Field& nest_int_field = nest.Get(1);
+  ASSERT_EQ(nest_int_field.type(),
+            protozero::proto_utils::ProtoWireType::kVarInt);
+  ASSERT_EQ(nest_int_field.as_int64(), 789);
+}
+
+TEST_F(ProtoBuilderTest, AppendRepeatedPrimitive) {
+  using FieldDescriptorProto = protos::pbzero::FieldDescriptorProto;
+
+  // Create the descriptor version of the following message:
+  // message TestProto {
+  //   repeated int64 int_value = 1;
+  // }
+  ProtoDescriptor descriptor(".perfetto.protos", ".perfetto.protos.TestProto",
+                             base::nullopt);
+  descriptor.AddField(FieldDescriptor(
+      "rep_int_value", 1, FieldDescriptorProto::TYPE_INT64, "", true));
+
+  RepeatedFieldBuilder rep_builder;
+  rep_builder.AddLong(1234);
+  rep_builder.AddLong(5678);
+
+  std::vector<uint8_t> rep_ser = rep_builder.SerializeToProtoBuilderResult();
+
+  ProtoBuilder builder(&descriptor);
+  ASSERT_TRUE(
+      builder.AppendBytes("rep_int_value", rep_ser.data(), rep_ser.size())
+          .ok());
+
+  auto result_ser = builder.SerializeToProtoBuilderResult();
+  auto proto = DecodeSingleFieldProto<true>(result_ser);
+  auto it = proto.GetRepeated(1);
+  ASSERT_EQ(it->as_int64(), 1234);
+  ASSERT_EQ((++it)->as_int64(), 5678);
+  ASSERT_FALSE(++it);
 }
 
 }  // namespace
