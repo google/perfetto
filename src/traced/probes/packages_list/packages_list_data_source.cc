fde4ae61b5a8f549c31f61a7fa6648f910ecacce
Ryan Savitski <rsavitski@google.com>
2019-06-14 16:02:52 +0100

    packages_list: allow filtering by pkg name
    Introduces a new data source config, since this source didn't have one
    before. If the new filter is non-empty, then only matching entries will
    be emitted into the trace. If the filter is empty, emit everything as
    before.
    
    Tested: manually on crosshatch-userdebug
    Bug: 134669419
    Bug: 130543265
    Change-Id: Ic484dda4e1005f295912de33914aa9bfb76d49c6

--- src/traced/probes/packages_list/packages_list_data_source.cc
+++ src/traced/probes/packages_list/packages_list_data_source.cc
@@ -22,7 +22,34 @@
 #include "perfetto/ext/tracing/core/trace_writer.h"
 #include "perfetto/trace/trace_packet.pbzero.h"
 
+using perfetto::protos::pbzero::PackagesListConfig;
+
 namespace perfetto {
+
+bool ParsePackagesListStream(protos::pbzero::PackagesList* packages_list_packet,
+                             const base::ScopedFstream& fs,
+                             const std::set<std::string>& package_name_filter) {
+  bool parsed_fully = true;
+  char line[2048];
+  while (fgets(line, sizeof(line), *fs) != nullptr) {
+    Package pkg_struct;
+    if (!ReadPackagesListLine(line, &pkg_struct)) {
+      parsed_fully = false;
+      continue;
+    }
+    if (!package_name_filter.empty() &&
+        package_name_filter.count(pkg_struct.name) == 0) {
+      continue;
+    }
+    auto* package = packages_list_packet->add_packages();
+    package->set_name(pkg_struct.name.c_str(), pkg_struct.name.size());
+    package->set_uid(pkg_struct.uid);
+    package->set_debuggable(pkg_struct.debuggable);
+    package->set_profileable_from_shell(pkg_struct.profileable_from_shell);
+    package->set_version_code(pkg_struct.version_code);
+  }
+  return parsed_fully;
+}
 
 bool ReadPackagesListLine(char* line, Package* package) {
   size_t idx = 0;
@@ -79,9 +106,15 @@ bool ReadPackagesListLine(char* line, Package* package) {
 }
 
 PackagesListDataSource::PackagesListDataSource(
+    const DataSourceConfig& ds_config,
     TracingSessionID session_id,
     std::unique_ptr<TraceWriter> writer)
-    : ProbesDataSource(session_id, kTypeId), writer_(std::move(writer)) {}
+    : ProbesDataSource(session_id, kTypeId), writer_(std::move(writer)) {
+  PackagesListConfig::Decoder cfg(ds_config.packages_list_config_raw());
+  for (auto name = cfg.package_name_filter(); name; ++name) {
+    package_name_filter_.emplace(name->as_std_string());
+  }
+}
 
 void PackagesListDataSource::Start() {
   base::ScopedFstream fs(fopen("/data/system/packages.list", "r"));
@@ -94,23 +127,10 @@ void PackagesListDataSource::Start() {
     writer_->Flush();
     return;
   }
-  bool parse_error = false;
-  char line[2048];
-  while (fgets(line, sizeof(line), *fs) != nullptr) {
-    Package pkg_struct;
-    if (ReadPackagesListLine(line, &pkg_struct)) {
-      auto* package = packages_list_packet->add_packages();
-      package->set_name(pkg_struct.name.c_str(), pkg_struct.name.size());
-      package->set_uid(pkg_struct.uid);
-      package->set_debuggable(pkg_struct.debuggable);
-      package->set_profileable_from_shell(pkg_struct.profileable_from_shell);
-      package->set_version_code(pkg_struct.version_code);
-    } else {
-      parse_error = true;
-    }
-  }
 
-  if (parse_error)
+  bool parsed_fully =
+      ParsePackagesListStream(packages_list_packet, fs, package_name_filter_);
+  if (!parsed_fully)
     packages_list_packet->set_parse_error(true);
 
   if (ferror(*fs))
