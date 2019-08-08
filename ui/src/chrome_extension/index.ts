ff5146717e4395912b4eae7d4283ae8508946db6
Nicolò Mazzucato <nicomazz@google.com>
2019-08-08 12:00:32 +0100

    Use noice-json-rpc for the extension to communicate with devtools
    The Extension code has been refactored to have better type safety using
    noice-json-rpc and the types provided by devtools-protocol.
    
    Bug: 138433107
    Change-Id: I6a14d155bca1f773d37e8c204406e3f107dfba58

--- ui/src/chrome_extension/index.ts
+++ ui/src/chrome_extension/index.ts
@@ -1,24 +1,39 @@
-// import {TraceConfig} from '../common/protos';
-import {ChromeTraceController} from './chrome_tracing_controller';
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-const chromeTraceController = new ChromeTraceController();
+import {ChromeTracingController} from './chrome_tracing_controller';
+
+let chromeTraceController: ChromeTracingController|undefined = undefined;
 
 chrome.runtime.onInstalled.addListener(() => {
   enableOnlyOnPerfettoHost();
   // Listen for messages from the perfetto ui.
-  chrome.runtime.onMessageExternal.addListener(onMessage);
+  chrome.runtime.onConnectExternal.addListener(port => {
+    chromeTraceController = new ChromeTracingController(port);
+    port.onMessage.addListener(onUIMessage);
+  });
 });
 
-function onMessage(
-    request: {method: string, traceConfig: Uint8Array},
-    _sender: chrome.runtime.MessageSender,
-    sendResponse: Function) {
-  if (request.method === 'ExtensionVersion') {
-    sendResponse({version: chrome.runtime.getManifest().version});
+function onUIMessage(
+    message: {method: string, traceConfig: Uint8Array},
+    port: chrome.runtime.Port) {
+  if (message.method === 'ExtensionVersion') {
+    port.postMessage({version: chrome.runtime.getManifest().version});
     return;
   }
-  // In the future more targets will be supported.
-  chromeTraceController.onMessage(request, _sender, sendResponse);
+  // In the future, more targets will be supported.
+  if (chromeTraceController) chromeTraceController.onMessage(message);
 }
 
 function enableOnlyOnPerfettoHost() {
