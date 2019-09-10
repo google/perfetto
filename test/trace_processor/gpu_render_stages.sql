207edd17bd4cb566fee186b6e4246676e84310c2
Adam Bodnar <abodnar@google.com>
2019-09-09 17:38:03 -0700

    Populate gpu_slices table with render_stage
    Bug: 140574674
    
    Test: build, run `trace_processor_shell my_trace`
    
    Change-Id: I256f5d890a261f33930f34f35b457f884d7fda47

--- test/trace_processor/gpu_render_stages.sql
+++ test/trace_processor/gpu_render_stages.sql
@@ -1,1 +1,5 @@
-SELECT "ts","dur","ref","ref_type","name","depth", internal_slice.arg_set_id, "flat_key", "string_value" FROM internal_slice LEFT JOIN args ON internal_slice.arg_set_id = args.arg_set_id ORDER BY "ts";
+SELECT "ts","dur","ref","ref_type","name","depth", internal_slice.arg_set_id, "flat_key",
+       "string_value", "context_id", "render_target", "submission_id", "hw_queue_id" FROM internal_slice
+INNER JOIN gpu_slice USING(slice_id)
+LEFT JOIN args ON internal_slice.arg_set_id = args.arg_set_id
+ORDER BY "ts";
