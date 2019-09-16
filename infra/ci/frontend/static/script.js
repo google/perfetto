b0330a20781942bc1c116545296d185c24a1b4a1
Florian Mayer <fmayer@google.com>
2019-09-16 11:03:10 +0100

    Add x86 ASAN to CI builds.
    Perfetto and Heapprofd actually run on x86 on cuttlefish, so this is
    an important architecture to test on.
    
    Bug: 140953289
    Change-Id: I9e67e246b9c55dfdce05ec86d2b3aa326929abfd

--- infra/ci/frontend/static/script.js
+++ infra/ci/frontend/static/script.js
@@ -22,6 +22,7 @@ const JOB_TYPES = [
   { id: 'linux-clang-x86_64-tsan', label: 'tsan' },
   { id: 'linux-clang-x86_64-msan', label: 'msan' },
   { id: 'linux-clang-x86_64-asan_lsan', label: '{a,l}san' },
+  { id: 'linux-clang-x86-asan_lsan', label: 'x86 {a,l}san' },
   { id: 'linux-clang-x86_64-libfuzzer', label: 'fuzzer' },
   { id: 'ui-clang-x86_64-debug', label: 'dbg' },
   { id: 'ui-clang-x86_64-release', label: 'rel' },
