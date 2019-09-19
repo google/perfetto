17c9bf811eba2f3dd49a4a6bd8998f9500d8de59
Isabelle Taylor <taylori@google.com>
2019-09-19 11:12:39 +0100

    perfetto-ui: Add link to bug
    Bug:111169965
    Change-Id: I45d2ef445e80ec1bce31c032bc9f92fa3fef1705

--- ui/src/frontend/topbar.ts
+++ ui/src/frontend/topbar.ts
@@ -191,14 +191,15 @@ class Omnibox implements m.ClassComponent {
                   m('i.material-icons.right', 'keyboard_arrow_right')),
                 ) :
             '',
-        commandMode ?
-            '' :
-            m('.help',
-              {
-                title:
-                    'Search functionality is in beta. Improvements coming soon!'
-              },
-              m('i.material-icons', 'help')),
+        commandMode ? '' :
+                      m('.help',
+                        {
+                          title: 'Search is in beta. Improvements coming soon!',
+                          onclick: () => {
+                            window.open('http://b/111169965', '_blank');
+                          }
+                        },
+                        m('i.material-icons', 'help')),
         m('.omnibox-results', results));
   }
 }
