846ed7a1e3c5b3ee181c0ab06fc0c732c2e586a3
Lalit Maganti <lalitm@google.com>
2019-10-24 19:14:13 +0100

    trace_processor: add benchmark for full table scans
    This CL adds a benchmark for just a column which has a small number of
    possible values but with a much larger number of rows - this situation
    happens a lot thorughout the tables (e.g. track_id in event, utid in
    sched etc.)
    
    Context: go/perfetto-tp-refactor
    Bug: 135177627
    Change-Id: I7b4e36677fc35792ab6689df99649e3604d4d95d

--- src/trace_processor/tables/macros_benchmark.cc
+++ src/trace_processor/tables/macros_benchmark.cc
@@ -24,7 +24,8 @@ namespace {
 
 #define PERFETTO_TP_TEST_TABLE(NAME, PARENT, C) \
   NAME(TestTable, "test_table")                 \
-  PERFETTO_TP_ROOT_TABLE(PARENT, C)
+  PERFETTO_TP_ROOT_TABLE(PARENT, C)             \
+  C(uint32_t, a)
 
 PERFETTO_TP_TABLE(PERFETTO_TP_TEST_TABLE);
 
@@ -59,5 +60,31 @@ static void BM_TableFilterIdColumn(benchmark::State& state) {
   }
 }
 BENCHMARK(BM_TableFilterIdColumn)
+    ->RangeMultiplier(8)
+    ->Range(1024, 2 * 1024 * 1024);
+
+static void BM_TableFilterEqMatchMany(benchmark::State& state) {
+  StringPool pool;
+  TestTable table(&pool, nullptr);
+
+  // We want the number of partitions to be significantly less than the size of
+  // the RowMap. This matches the setup of columns like track_id in the event
+  // table where there are a large number of events but a much smaller number of
+  // tracks all those events are associated with.
+  uint32_t size = static_cast<uint32_t>(state.range(0));
+  uint32_t partitions = size / 1024;
+
+  constexpr uint32_t kRandomSeed = 42;
+  std::minstd_rand0 rnd_engine(kRandomSeed);
+  for (uint32_t i = 0; i < size; ++i) {
+    TestTable::Row row(static_cast<uint32_t>(rnd_engine() % partitions));
+    table.Insert(row);
+  }
+
+  for (auto _ : state) {
+    benchmark::DoNotOptimize(table.Filter({table.a().eq(SqlValue::Long(0))}));
+  }
+}
+BENCHMARK(BM_TableFilterEqMatchMany)
     ->RangeMultiplier(8)
     ->Range(1024, 2 * 1024 * 1024);
