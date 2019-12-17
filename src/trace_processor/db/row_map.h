d405d7a94dfcb68fb9a1fdba6e10059fce1aec93
Lalit Maganti <lalitm@google.com>
2019-12-17 14:25:24 +0000

    trace_processor: reduce code on RowMap::FilterInto hot path
    Looking at the size and the two extra branches slow down this hot
    path which gets called on every id constraint. Reduce the overhead
    by removing the unnecessary branches
    
    Old:
    BM_TableFilterRootId/1024                           197 ns        197 ns    3518678
    BM_TableFilterRootId/4096                           197 ns        197 ns    3644999
    BM_TableFilterRootId/32768                          197 ns        197 ns    3660113
    BM_TableFilterRootId/262144                         199 ns        199 ns    3616380
    BM_TableFilterRootId/2097152                        196 ns        196 ns    3632720
    BM_TableFilterRootIdAndOther/1024                   215 ns        215 ns    3204181
    BM_TableFilterRootIdAndOther/4096                   214 ns        214 ns    3265619
    BM_TableFilterRootIdAndOther/32768                  216 ns        216 ns    3224383
    BM_TableFilterRootIdAndOther/262144                 216 ns        216 ns    3260170
    BM_TableFilterRootIdAndOther/2097152                215 ns        215 ns    3243621
    BM_TableFilterChildId/1024                          234 ns        234 ns    2974211
    BM_TableFilterChildId/4096                          234 ns        234 ns    2985103
    BM_TableFilterChildId/32768                         250 ns        250 ns    2941857
    BM_TableFilterChildId/262144                        236 ns        236 ns    2955799
    BM_TableFilterChildId/2097152                       237 ns        237 ns    2987954
    BM_TableFilterChildIdAndSortedInRoot/1024           721 ns        721 ns     961686
    BM_TableFilterChildIdAndSortedInRoot/4096           789 ns        789 ns     885292
    BM_TableFilterChildIdAndSortedInRoot/32768          890 ns        890 ns     779063
    BM_TableFilterChildIdAndSortedInRoot/262144        1011 ns       1011 ns     697529
    BM_TableFilterChildIdAndSortedInRoot/2097152       1191 ns       1191 ns     597745
    
    New:
    BM_TableFilterRootId/1024                           177 ns        177 ns    3926212
    BM_TableFilterRootId/4096                           178 ns        178 ns    3923877
    BM_TableFilterRootId/32768                          179 ns        179 ns    3880649
    BM_TableFilterRootId/262144                         180 ns        180 ns    3879871
    BM_TableFilterRootId/2097152                        178 ns        178 ns    3882272
    BM_TableFilterRootIdAndOther/1024                   199 ns        199 ns    3457017
    BM_TableFilterRootIdAndOther/4096                   200 ns        200 ns    3538241
    BM_TableFilterRootIdAndOther/32768                  200 ns        200 ns    3520843
    BM_TableFilterRootIdAndOther/262144                 201 ns        201 ns    3454817
    BM_TableFilterRootIdAndOther/2097152                199 ns        199 ns    3498338
    BM_TableFilterChildId/1024                          221 ns        221 ns    3162913
    BM_TableFilterChildId/4096                          227 ns        227 ns    3159299
    BM_TableFilterChildId/32768                         226 ns        226 ns    3158618
    BM_TableFilterChildId/262144                        227 ns        227 ns    3209023
    BM_TableFilterChildId/2097152                       227 ns        227 ns    3180301
    BM_TableFilterChildIdAndSortedInRoot/1024           682 ns        682 ns     996525
    BM_TableFilterChildIdAndSortedInRoot/4096           753 ns        753 ns     920648
    BM_TableFilterChildIdAndSortedInRoot/32768          853 ns        853 ns     814670
    BM_TableFilterChildIdAndSortedInRoot/262144         977 ns        977 ns     711928
    BM_TableFilterChildIdAndSortedInRoot/2097152       1159 ns       1159 ns     601261
    
    Analysis:
    There is a persistent 10% improvement in this codepath which has been verified
    to translate to a change on computing metrics on real traces.
    
    Change-Id: I426a12b0066c6a850fafa07b21bb20584cc6ce0c

--- src/trace_processor/db/row_map.h
+++ src/trace_processor/db/row_map.h
@@ -381,31 +381,13 @@ class RowMap {
   //   if (!other.Contains(idx))
   //     Remove(idx)
   void Intersect(const RowMap& other) {
-    uint32_t size = other.size();
-
-    if (size == 0u) {
-      // If other is empty, then we will also end up being empty.
-      *this = RowMap();
-      return;
-    }
-
-    if (size == 1u) {
-      // If other just has a single row, see if we also have that row. If we
-      // do, then just return that row. Otherwise, make ourselves empty.
-      uint32_t row = other.Get(0);
-      *this = Contains(row) ? RowMap::SingleRow(row) : RowMap();
-      return;
-    }
-
     if (mode_ == Mode::kRange && other.mode_ == Mode::kRange) {
       // If both RowMaps have ranges, we can just take the smallest intersection
       // of them as the new RowMap.
-      // This case is important to optimize as it comes up with sorted columns.
+      // We have this as an explicit fast path as this is very common for
+      // constraints on id and sorted columns to satisfy this condition.
       start_idx_ = std::max(start_idx_, other.start_idx_);
-      end_idx_ = std::min(end_idx_, other.end_idx_);
-
-      if (end_idx_ <= start_idx_)
-        *this = RowMap();
+      end_idx_ = std::max(start_idx_, std::min(end_idx_, other.end_idx_));
       return;
     }
 
