2fb3a2dc20184b3c0975454a9631156c5af7138f
Florian Mayer <fmayer@google.com>
2020-03-12 21:39:40 +0000

    Revert "Move enable_extra_guardrails handling to client."
    Revert submission 1253625-heapprofd-e-guardrails
    
    Reason for revert: [6/8] HeapprofdCtsTest#ProfileableAppStartupExtraGuardrails: FAILED (521ms)
    Reverted Changes:
    I9b221591c:Assert extra_guardrails fails on user.
    I2863e2c51:Move enable_extra_guardrails handling to client.
    
    Change-Id: I6bcb19003bdb969b1459ffe01499e54f27b671dd

--- src/profiling/memory/wire_protocol.h
+++ src/profiling/memory/wire_protocol.h
@@ -46,7 +46,6 @@ struct ClientConfiguration {
   uint64_t interval;
   bool block_client;
   uint64_t block_client_timeout_us;
-  bool enable_extra_guardrails;
 };
 
 // Types needed for the wire format used for communication between the client
