f059df7a43900950c242589368f08c1f39f69c86
Sami Kyostila <skyostil@google.com>
2020-04-20 10:13:32 +0100

    base: GCC 7.1 compatibility fix
    This patch works around a bug in GCC 7.1 where the compiler failes to
    generate a noexcept move constructor operator for Subprocess::Args
    ("function 'Subprocess::Subprocess(Subprocess&&)' defaulted on its
    redeclaration with an exception-specification that differs from the
    implicit exception-specification").
    
    Change-Id: I40e89e0d87f182ec1e08baa9e3aa0eda234a46ba

--- include/perfetto/ext/base/subprocess.h
+++ include/perfetto/ext/base/subprocess.h
@@ -122,6 +122,8 @@ class Subprocess {
   // Input arguments for configuring the subprocess behavior.
   struct Args {
     Args(std::initializer_list<std::string> _cmd = {}) : exec_cmd(_cmd) {}
+    Args(Args&&) noexcept;
+    Args& operator=(Args&&);
     // If non-empty this will cause an exec() when Start()/Call() are called.
     std::vector<std::string> exec_cmd;
 
