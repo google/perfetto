ba67fbcaa3a5d995c6328a7d3e0d3e2a09f4e96d
Lalit Maganti <lalitm@google.com>
2020-06-24 13:42:46 +0100

    ui: remove vsync tracks
    * vsync trakcs should really be created from metrics using the derived
      events feature of trace processor. They should also just use the
      generic counter implementation when they do that.
    * Also add the missing link between trace_controller and async tracks
      which was missing.
    
    Change-Id: I2f936435f2cc1a0622e5bdc153ffa9e127d9f07b

--- ui/src/tracks/async_slices/frontend.ts
+++ ui/src/tracks/async_slices/frontend.ts
@@ -17,10 +17,10 @@ import {Track} from '../../frontend/track';
 import {trackRegistry} from '../../frontend/track_registry';
 import {ChromeSliceTrack} from '../chrome_slices/frontend';
 
-import {SLICE_TRACK_KIND} from './common';
+import {ASYNC_SLICE_TRACK_KIND} from './common';
 
 export class AsyncSliceTrack extends ChromeSliceTrack {
-  static readonly kind = SLICE_TRACK_KIND;
+  static readonly kind = ASYNC_SLICE_TRACK_KIND;
   static create(trackState: TrackState): Track {
     return new AsyncSliceTrack(trackState);
   }
