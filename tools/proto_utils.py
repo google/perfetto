5eacf421b9c186d44759fa90e39c986a48abb500
Lalit Maganti <lalitm@google.com>
2020-06-30 13:51:23 +0100

    tp: add script to serialize test python/textproto traces
    This has been a recurring need when debugging python/textproto diff
    tests.
    
    Change-Id: Iba1ce444831a1f69ff7c90945077f4c5c22bb2ec

---
+++ tools/proto_utils.py
@@ -0,0 +1,60 @@
+# Copyright (C) 2020 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import absolute_import
+
+import os
+import subprocess
+import tempfile
+
+from google.protobuf import descriptor, descriptor_pb2, message_factory
+from google.protobuf import reflection, text_format
+
+
+def create_message_factory(descriptor_file_path, proto_type):
+  with open(descriptor_file_path, 'rb') as descriptor_file:
+    descriptor_content = descriptor_file.read()
+
+  file_desc_set_pb2 = descriptor_pb2.FileDescriptorSet()
+  file_desc_set_pb2.MergeFromString(descriptor_content)
+
+  desc_by_path = {}
+  for f_desc_pb2 in file_desc_set_pb2.file:
+    f_desc_pb2_encode = f_desc_pb2.SerializeToString()
+    f_desc = descriptor.FileDescriptor(
+        name=f_desc_pb2.name,
+        package=f_desc_pb2.package,
+        serialized_pb=f_desc_pb2_encode)
+
+    for desc in f_desc.message_types_by_name.values():
+      desc_by_path[desc.full_name] = desc
+
+  return message_factory.MessageFactory().GetPrototype(desc_by_path[proto_type])
+
+
+def serialize_textproto_trace(trace_descriptor_path, text_proto_path,
+                              out_stream):
+  trace_message_factory = create_message_factory(trace_descriptor_path,
+                                                 'perfetto.protos.Trace')
+  proto = trace_message_factory()
+  with open(text_proto_path, 'r') as text_proto_file:
+    text_format.Merge(text_proto_file.read(), proto)
+  out_stream.write(proto.SerializeToString())
+  out_stream.flush()
+
+
+def serialize_python_trace(trace_descriptor_path, python_trace_path,
+                           out_stream):
+  python_cmd = ['python', python_trace_path, trace_descriptor_path]
+  subprocess.check_call(python_cmd, stdout=out_stream)
