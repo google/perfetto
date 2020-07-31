b1aa928bec08b21d18ab4e2c90bf701ce97c5842
Hector Dearman <hjd@google.com>
2020-07-31 11:46:32 +0100

    Support armv7l in system_probes_parser
    Bug: 162298532
    Change-Id: I51dce21660e88dbe1c708e6b99b8df3eec48e523

--- src/trace_processor/importers/proto/system_probes_parser.cc
+++ src/trace_processor/importers/proto/system_probes_parser.cc
@@ -381,6 +381,8 @@ void SystemProbesParser::ParseSystemInfo(ConstBytes blob) {
     SyscallTracker* syscall_tracker = SyscallTracker::GetOrCreate(context_);
     if (machine == "aarch64" || machine == "armv8l") {
       syscall_tracker->SetArchitecture(kAarch64);
+    } else if (machine == "armv7l") {
+      syscall_tracker->SetArchitecture(kAarch32);
     } else if (machine == "x86_64") {
       syscall_tracker->SetArchitecture(kX86_64);
     } else if (machine == "i686") {
