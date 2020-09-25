c2132df9b0d3a1a0c36aa991e407e9d365a7447f
Deepanjan Roy <dproy@google.com>
2020-09-25 18:06:27 -0400

    ui: Add a metrics page
    Adds a page that lists all available metrics. When run, it displays the
    metric output as prototext. Also displays errors, when that happens.
    
    Bug:161699653
    Change-Id: Icd281d334e8399dcfe3cd8a38cb08e176d038952

--- ui/src/common/metric_data.ts
+++ ui/src/common/metric_data.ts
@@ -1,4 +1,4 @@
-// Copyright (C) 2018 The Android Open Source Project
+// Copyright (C) 2020 The Android Open Source Project
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,13 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-@import 'typefaces';
-@import 'common';
-@import 'analyze_page';
-@import 'metrics_page';
-@import 'sidebar';
-@import 'topbar';
-@import 'record';
-@import 'modal';
-@import 'details';
-@import 'trace_info_page';
+export interface MetricResult {
+  name: string;
+  // Either result or error should be set.
+  resultString?: string;
+  error?: string;
+}
