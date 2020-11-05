e0ed212f173f7f80366e4dfa1b71d480bf46c585
Hector Dearman <hjd@google.com>
2020-11-05 13:19:14 +0000

    ui: Fix iteration on empty queries and null queries
    Fix two issues with iteration:
    1. If the first entry for a column is null, both the string and number
       columns are populated due to a quirk of the encoding. The validation
       code should handle this.
    2. Empty columns should match all column types.
    
    Also use iter to typecheck the thread_state track.
    
    Change-Id: I7f5355854159456d722b6fe3c043bb4c505ba19e

--- ui/src/tracks/thread_state/controller.ts
+++ ui/src/tracks/thread_state/controller.ts
@@ -13,7 +13,13 @@
 // limitations under the License.
 
 import {assertFalse} from '../../base/logging';
-import {slowlyCountRows} from '../../common/query_iterator';
+import {
+  iter,
+  NUM,
+  NUM_NULL,
+  slowlyCountRows,
+  STR
+} from '../../common/query_iterator';
 import {translateState} from '../../common/thread_state';
 import {fromNs, toNs} from '../../common/time';
 import {
@@ -69,7 +75,7 @@ class ThreadStateTrackController extends TrackController<Config, Data> {
         (ts + ${bucketNs / 2}) / ${bucketNs} * ${bucketNs} as tsq,
         ts,
         max(dur) as dur,
-        cpu,
+        cast(cpu as integer) as cpu,
         state,
         io_wait,
         id
@@ -107,7 +113,16 @@ class ThreadStateTrackController extends TrackController<Config, Data> {
       stringIndexes.set({shortState, ioWait}, idx);
       return idx;
     }
-
+    iter(
+        {
+          'ts': NUM,
+          'dur': NUM,
+          'cpu': NUM_NULL,
+          'state': STR,
+          'io_wait': NUM_NULL,
+          'id': NUM_NULL,
+        },
+        result);
     for (let row = 0; row < numRows; row++) {
       const cols = result.columns;
       const startNsQ = +cols[0].longValues![row];
