32e5d6080edfffaa4aa2fd55e6ab6c3fc088734c
Eric Seckler <eseckler@google.com>
2020-12-07 13:25:45 +0000

    processor: Fix memory leak in MemoryTrackerSnapshotParser
    std::map::emplace() will not construct the unique_ptr from the passed
    raw pointer if the key already exists in the map. Use insert() with a
    std::move()d unique_ptr instead to avoid the leak.
    
    Bug: oss-fuzz:28338
    Change-Id: I043cf6e50e1ec6dc728f3bfb907ed437bdd29550

--- src/trace_processor/trace_database_integrationtest.cc
+++ src/trace_processor/trace_database_integrationtest.cc
@@ -265,12 +265,14 @@ TEST_F(TraceProcessorIntegrationTest, Clusterfuzz17805) {
 #define MAYBE_Clusterfuzz21178 DISABLED_Clusterfuzz21178
 #define MAYBE_Clusterfuzz21890 DISABLED_Clusterfuzz21890
 #define MAYBE_Clusterfuzz23053 DISABLED_Clusterfuzz23053
+#define MAYBE_Clusterfuzz28338 DISABLED_Clusterfuzz28338
 #else  // PERFETTO_DCHECK_IS_ON()
 #define MAYBE_Clusterfuzz20215 Clusterfuzz20215
 #define MAYBE_Clusterfuzz20292 Clusterfuzz20292
 #define MAYBE_Clusterfuzz21178 Clusterfuzz21178
 #define MAYBE_Clusterfuzz21890 Clusterfuzz21890
 #define MAYBE_Clusterfuzz23053 Clusterfuzz23053
+#define MAYBE_Clusterfuzz28338 Clusterfuzz28338
 #endif  // PERFETTO_DCHECK_IS_ON()
 
 TEST_F(TraceProcessorIntegrationTest, MAYBE_Clusterfuzz20215) {
@@ -291,6 +293,10 @@ TEST_F(TraceProcessorIntegrationTest, MAYBE_Clusterfuzz21890) {
 
 TEST_F(TraceProcessorIntegrationTest, MAYBE_Clusterfuzz23053) {
   ASSERT_FALSE(LoadTrace("clusterfuzz_23053", 4096).ok());
+}
+
+TEST_F(TraceProcessorIntegrationTest, MAYBE_Clusterfuzz28338) {
+  ASSERT_TRUE(LoadTrace("clusterfuzz_28338", 4096).ok());
 }
 
 TEST_F(TraceProcessorIntegrationTest, RestoreInitialTables) {
