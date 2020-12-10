6d36a0e9267e64c00e07d664040df88ef0767674
Lalit Maganti <lalitm@google.com>
2020-12-10 17:15:22 +0000

    perfetto: add empty trigger name to match change to proto
    Change-Id: Ie9ddc5a7e3cc4fb2ea3360831562dd4cf1e01164

--- src/android_internal/statsd_logging.cc
+++ src/android_internal/statsd_logging.cc
@@ -27,7 +27,7 @@ void StatsdLogUploadEvent(PerfettoStatsdAtom atom,
                           int64_t uuid_lsb,
                           int64_t uuid_msb) {
   stats_write(PERFETTO_UPLOADED, static_cast<int32_t>(atom), uuid_lsb,
-              uuid_msb);
+              uuid_msb, "");
 }
 
 }  // namespace android_internal
