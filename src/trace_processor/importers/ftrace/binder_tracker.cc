9aca5984e20799b7342c9810c8692ccfeffa1ea7
Lalit Maganti <lalitm@google.com>
2021-03-01 13:26:37 +0000

    tp: don't pass category/name for binder end events
    Because of b/178578713, we cannot rely on binder events being well
    ordered with respect to userspace events. Thus, just end whichever slice
    is at the top of the stack meaning we no longer will get out-of-order
    event errors anymore
    
    Bug: 178578713
    Change-Id: I87260f981973d824137d5729d4c5a288213de575

--- src/trace_processor/importers/ftrace/binder_tracker.cc
+++ src/trace_processor/importers/ftrace/binder_tracker.cc
@@ -125,7 +125,7 @@ void BinderTracker::Transaction(int64_t ts,
     };
     context_->slice_tracker->AddArgs(track_id, binder_category_id_, reply_id_,
                                      dest_args_inserter);
-    context_->slice_tracker->End(ts, track_id, binder_category_id_, reply_id_,
+    context_->slice_tracker->End(ts, track_id, kNullStringId, kNullStringId,
                                  args_inserter);
     awaiting_rcv_for_reply_.insert(transaction_id);
     return;
@@ -152,8 +152,7 @@ void BinderTracker::TransactionReceived(int64_t ts,
   StringId thread_name = thread_table.name()[utid];
   TrackId track_id = context_->track_tracker->InternThreadTrack(utid);
   if (awaiting_rcv_for_reply_.count(transaction_id) > 0) {
-    context_->slice_tracker->End(ts, track_id, binder_category_id_,
-                                 transaction_slice_id_);
+    context_->slice_tracker->End(ts, track_id);
     awaiting_rcv_for_reply_.erase(transaction_id);
     return;
   }
@@ -214,8 +213,7 @@ void BinderTracker::Locked(int64_t ts, uint32_t pid) {
     return;
 
   TrackId track_id = context_->track_tracker->InternThreadTrack(utid);
-  context_->slice_tracker->End(ts, track_id, binder_category_id_,
-                               lock_waiting_id_);
+  context_->slice_tracker->End(ts, track_id);
   context_->slice_tracker->Begin(ts, track_id, binder_category_id_,
                                  lock_held_id_);
 
