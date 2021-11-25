79d7eebbc848e3eb3ec10733974f82ed2c665faa
Andrew Shulaev <ddrone@google.com>
2021-11-25 10:14:08 +0000

    Remove indentation in aggregation columns of pivot table
    Indentation of group columns already shows nesting level of the whole
    row, and adding indentation for aggregation columns only increases
    amount of whitespace on a line without helping to understand the
    structure.
    
    Code is simplified a bit to avoid string modification in case it's
    not necessary.
    
    Change-Id: Ib3eaf6828333e85d9f51145d8b2337d6e1c16624

--- ui/src/frontend/pivot_table.ts
+++ ui/src/frontend/pivot_table.ts
@@ -213,21 +213,17 @@ class PivotTableRow implements m.ClassComponent<PivotTableRowAttrs> {
               {pivotTableId, row, column, rowIndices, expandedRowColumns}));
         continue;
       }
-      let indentationLevel = 0;
-      let expandIconSpace = 0;
-      if (column.aggregation !== undefined) {
-        indentationLevel = rowIndices.length - 1;
-      } else {
-        indentationLevel = row.depth;
-        if (row.depth > 0 && column.isStackColumn) {
-          expandIconSpace = 3;
-        }
-      }
-      // For each indentation level add 2 spaces, if we have an expansion button
-      // add 3 spaces to cover the icon size.
+
       let value = row.row[column.name]!.toString();
-      value = value.padStart(
-          (indentationLevel * 2) + expandIconSpace + value.length, ' ');
+      if (column.aggregation === undefined) {
+        // For each indentation level add 2 spaces, if we have an expansion
+        // button add 3 spaces to cover the icon size.
+        let padding = 2 * row.depth;
+        if (row.depth > 0 && column.isStackColumn) {
+          padding += 3;
+        }
+        value = value.padStart(padding + value.length, ' ');
+      }
       cells.push(m('td.allow-white-space', value));
     }
     return m('tr', cells);
