cadd5ee9d333b2e89e09c90ea20a3ad81b1f21a5
Andrew Shulaev <ddrone@google.com>
2021-12-01 11:23:45 +0000

    Use template strings in pivot table unit tests for readability
    Change-Id: I2f26ca797100085901e46d39046e72fa7cf48414

--- ui/src/common/pivot_table_query_generator_unittest.ts
+++ ui/src/common/pivot_table_query_generator_unittest.ts
@@ -41,31 +41,34 @@ test('Generate query with pivots and aggregations', () => {
   const selectedAggregations: AggregationAttrs[] = [
     {aggregation: 'SUM', tableName: 'slice', columnName: 'dur', order: 'DESC'}
   ];
-  const expectedQuery = '\nSELECT\n' +
-      '"slice type",\n' +
-      '  "slice id",\n' +
-      '  "slice dur (SUM)",\n' +
-      '  "slice dur (SUM) (total)"\n' +
-      'FROM (\n' +
-      'SELECT\n' +
-      '"slice type",\n' +
-      '  "slice id",\n' +
-      '  SUM("slice dur (SUM)") OVER () AS "slice dur (SUM) (total)",\n' +
-      '  SUM("slice dur (SUM)") OVER (PARTITION BY "slice type",  "slice id")' +
-      ' AS "slice dur (SUM)"\n' +
-      'FROM (\n' +
-      'SELECT\n' +
-      'slice.type AS "slice type",\n' +
-      '  slice.id AS "slice id",\n' +
-      '  slice.dur AS "slice dur (SUM)"\n' +
-      'FROM\n' +
-      'slice\n' +
-      'WHERE\n' +
-      'slice.dur != -1\n' +
-      ')\n' +
-      ')\n' +
-      'GROUP BY "slice type",  "slice id",  "slice dur (SUM)"\n' +
-      'ORDER BY "slice dur (SUM)" DESC\n';
+  const expectedQuery = `
+    SELECT
+      "slice type",
+      "slice id",
+      "slice dur (SUM)",
+      "slice dur (SUM) (total)"
+    FROM (
+      SELECT
+        "slice type",
+        "slice id",
+        SUM("slice dur (SUM)") OVER () AS "slice dur (SUM) (total)",
+        SUM("slice dur (SUM)")
+          OVER (PARTITION BY "slice type",  "slice id")
+          AS "slice dur (SUM)"
+      FROM (
+        SELECT
+          slice.type AS "slice type",
+          slice.id AS "slice id",
+          slice.dur AS "slice dur (SUM)"
+        FROM
+          slice
+        WHERE
+          slice.dur != -1
+      )
+    )
+    GROUP BY "slice type",  "slice id",  "slice dur (SUM)"
+    ORDER BY "slice dur (SUM)" DESC
+  `;
   expectQueryEqual(
       pivotTableQueryGenerator.generateQuery(
           selectedPivots, selectedAggregations, WHERE_FILTERS, TABLES),
@@ -79,19 +82,21 @@ test('Generate query with pivots', () => {
     {tableName: 'slice', columnName: 'id', isStackPivot: false}
   ];
   const selectedAggregations: AggregationAttrs[] = [];
-  const expectedQuery = '\nSELECT\n' +
-      '"slice type",\n' +
-      '  "slice id"\n' +
-      'FROM (\n' +
-      'SELECT\n' +
-      'slice.type AS "slice type",\n' +
-      '  slice.id AS "slice id"\n' +
-      'FROM\n' +
-      'slice\n' +
-      'WHERE\n' +
-      'slice.dur != -1\n' +
-      ')\n' +
-      'GROUP BY "slice type",  "slice id"\n';
+  const expectedQuery = `
+    SELECT
+      "slice type",
+      "slice id"
+    FROM (
+      SELECT
+        slice.type AS "slice type",
+        slice.id AS "slice id"
+      FROM
+        slice
+      WHERE
+        slice.dur != -1
+    )
+    GROUP BY "slice type",  "slice id"
+  `;
   expectQueryEqual(
       pivotTableQueryGenerator.generateQuery(
           selectedPivots, selectedAggregations, WHERE_FILTERS, TABLES),
@@ -105,25 +110,27 @@ test('Generate query with aggregations', () => {
     {aggregation: 'SUM', tableName: 'slice', columnName: 'dur', order: 'DESC'},
     {aggregation: 'MAX', tableName: 'slice', columnName: 'dur', order: 'ASC'}
   ];
-  const expectedQuery = '\nSELECT\n' +
-      '"slice dur (SUM)",\n' +
-      '  "slice dur (MAX)"\n' +
-      'FROM (\n' +
-      'SELECT\n' +
-      'SUM("slice dur (SUM)") AS "slice dur (SUM)",\n' +
-      '  MAX("slice dur (MAX)") AS "slice dur (MAX)"\n' +
-      'FROM (\n' +
-      'SELECT\n' +
-      'slice.dur AS "slice dur (SUM)",\n' +
-      '  slice.dur AS "slice dur (MAX)"\n' +
-      'FROM\n' +
-      'slice\n' +
-      'WHERE\n' +
-      'slice.dur != -1\n' +
-      ')\n' +
-      ')\n' +
-      'GROUP BY "slice dur (SUM)",  "slice dur (MAX)"\n' +
-      'ORDER BY "slice dur (SUM)" DESC,  "slice dur (MAX)" ASC\n';
+  const expectedQuery = `
+    SELECT
+      "slice dur (SUM)",
+      "slice dur (MAX)"
+    FROM (
+      SELECT
+        SUM("slice dur (SUM)") AS "slice dur (SUM)",
+        MAX("slice dur (MAX)") AS "slice dur (MAX)"
+      FROM (
+        SELECT
+          slice.dur AS "slice dur (SUM)",
+          slice.dur AS "slice dur (MAX)"
+        FROM
+          slice
+        WHERE
+          slice.dur != -1
+      )
+    )
+    GROUP BY "slice dur (SUM)",  "slice dur (MAX)"
+    ORDER BY "slice dur (SUM)" DESC,  "slice dur (MAX)" ASC
+  `;
   expectQueryEqual(
       pivotTableQueryGenerator.generateQuery(
           selectedPivots, selectedAggregations, WHERE_FILTERS, TABLES),
@@ -139,42 +146,48 @@ test('Generate a query with stack pivot', () => {
   const selectedAggregations: AggregationAttrs[] = [
     {aggregation: 'COUNT', tableName: 'slice', columnName: 'id', order: 'DESC'},
   ];
-  const expectedQuery = '\nSELECT\n' +
-      '"slice name (stack)",\n' +
-      '  "slice depth (hidden)",\n' +
-      '  "slice stack_id (hidden)",\n' +
-      '  "slice parent_stack_id (hidden)",\n' +
-      '  "slice category",\n' +
-      '  "slice id (COUNT)",\n' +
-      '  "slice id (COUNT) (total)"\n' +
-      'FROM (\n' +
-      'SELECT\n' +
-      '"slice name (stack)",\n' +
-      '  "slice depth (hidden)",\n' +
-      '  "slice stack_id (hidden)",\n' +
-      '  "slice parent_stack_id (hidden)",\n' +
-      '  "slice category",\n' +
-      '  COUNT("slice id (COUNT)") OVER () AS "slice id (COUNT) (total)",\n' +
-      '  COUNT("slice id (COUNT)") OVER (PARTITION BY' +
-      ' "slice stack_id (hidden)",  "slice category") AS "slice id (COUNT)"\n' +
-      'FROM (\n' +
-      'SELECT\n' +
-      'slice.name AS "slice name (stack)",\n' +
-      '  slice.depth AS "slice depth (hidden)",\n' +
-      '  slice.stack_id AS "slice stack_id (hidden)",\n' +
-      '  slice.parent_stack_id AS "slice parent_stack_id (hidden)",\n' +
-      '  slice.category AS "slice category",\n' +
-      '  slice.id AS "slice id (COUNT)"\n' +
-      'FROM\n' +
-      'slice\n' +
-      'WHERE\n' +
-      'slice.dur != -1\n' +
-      ')\n' +
-      ')\n' +
-      'GROUP BY "slice name (stack)",  "slice depth (hidden)",  ' +
-      '"slice stack_id (hidden)",  "slice parent_stack_id (hidden)",  ' +
-      '"slice category",  "slice id (COUNT)"\n' +
-      'ORDER BY "slice id (COUNT)" DESC\n';
+  const expectedQuery = `
+    SELECT
+      "slice name (stack)",
+      "slice depth (hidden)",
+      "slice stack_id (hidden)",
+      "slice parent_stack_id (hidden)",
+      "slice category",
+      "slice id (COUNT)",
+      "slice id (COUNT) (total)"
+    FROM (
+      SELECT
+        "slice name (stack)",
+        "slice depth (hidden)",
+        "slice stack_id (hidden)",
+        "slice parent_stack_id (hidden)",
+        "slice category",
+        COUNT("slice id (COUNT)") OVER () AS "slice id (COUNT) (total)",
+        COUNT("slice id (COUNT)")
+          OVER (PARTITION BY "slice stack_id (hidden)",  "slice category")
+          AS "slice id (COUNT)"
+      FROM (
+        SELECT
+          slice.name AS "slice name (stack)",
+          slice.depth AS "slice depth (hidden)",
+          slice.stack_id AS "slice stack_id (hidden)",
+          slice.parent_stack_id AS "slice parent_stack_id (hidden)",
+          slice.category AS "slice category",
+          slice.id AS "slice id (COUNT)"
+        FROM
+          slice
+        WHERE
+          slice.dur != -1
+      )
+    )
+    GROUP BY "slice name (stack)",
+             "slice depth (hidden)",
+             "slice stack_id (hidden)",
+             "slice parent_stack_id (hidden)",
+             "slice category",
+             "slice id (COUNT)"
+    ORDER BY "slice id (COUNT)" DESC
+  `;
   expectQueryEqual(
       pivotTableQueryGenerator.generateQuery(
           selectedPivots, selectedAggregations, WHERE_FILTERS, TABLES),
@@ -187,25 +200,29 @@ test('Generate a descendant stack query', () => {
     {tableName: 'slice', columnName: SLICE_STACK_COLUMN, isStackPivot: true},
   ];
   const selectedAggregations: AggregationAttrs[] = [];
-  const expectedQuery = '\nSELECT\n' +
-      '"slice name (stack)",\n' +
-      '  "slice depth (hidden)",\n' +
-      '  "slice stack_id (hidden)",\n' +
-      '  "slice parent_stack_id (hidden)"\n' +
-      'FROM (\n' +
-      'SELECT\n' +
-      'slice.name AS "slice name (stack)",\n' +
-      '  slice.depth AS "slice depth (hidden)",\n' +
-      '  slice.stack_id AS "slice stack_id (hidden)",\n' +
-      '  slice.parent_stack_id AS "slice parent_stack_id (hidden)"\n' +
-      'FROM\n' +
-      'descendant_slice_by_stack(stack_id) AS slice\n' +
-      'WHERE\n' +
-      'slice.dur != -1\n' +
-      ')\n' +
-      'GROUP BY "slice name (stack)",  "slice depth (hidden)",  ' +
-      '"slice stack_id (hidden)",  "slice parent_stack_id (hidden)"\n' +
-      'ORDER BY "slice depth (hidden)" ASC\n';
+  const expectedQuery = `
+    SELECT
+      "slice name (stack)",
+      "slice depth (hidden)",
+      "slice stack_id (hidden)",
+      "slice parent_stack_id (hidden)"
+    FROM (
+      SELECT
+        slice.name AS "slice name (stack)",
+        slice.depth AS "slice depth (hidden)",
+        slice.stack_id AS "slice stack_id (hidden)",
+        slice.parent_stack_id AS "slice parent_stack_id (hidden)"
+      FROM
+        descendant_slice_by_stack(stack_id) AS slice
+      WHERE
+        slice.dur != -1
+    )
+    GROUP BY "slice name (stack)",
+             "slice depth (hidden)",
+             "slice stack_id (hidden)",
+             "slice parent_stack_id (hidden)"
+    ORDER BY "slice depth (hidden)" ASC
+  `;
 
   const table = ['descendant_slice_by_stack(stack_id) AS slice'];
   expectQueryEqual(
@@ -225,51 +242,57 @@ test('Generate a descendant stack query with another pivot', () => {
     {tableName: 'slice', columnName: 'category', isStackPivot: false}
   ];
   const selectedAggregations: AggregationAttrs[] = [];
-  const expectedQuery = '\nSELECT\n' +
-      '"slice name (stack)",\n' +
-      '  "slice depth (hidden)",\n' +
-      '  "slice stack_id (hidden)",\n' +
-      '  "slice parent_stack_id (hidden)",\n' +
-      '  "slice category"\n' +
-      'FROM (\n' +
-      'SELECT\n' +
-      'slice.name AS "slice name (stack)",\n' +
-      '  slice.depth AS "slice depth (hidden)",\n' +
-      '  slice.stack_id AS "slice stack_id (hidden)",\n' +
-      '  slice.parent_stack_id AS "slice parent_stack_id (hidden)",\n' +
-      '  slice.category AS "slice category"\n' +
-      'FROM\n' +
-      'slice\n' +
-      'WHERE\n' +
-      'slice.dur != -1 AND\n' +
-      '  slice.stack_id = stack_id\n' +
-      ')\n' +
-      'GROUP BY "slice name (stack)",  "slice depth (hidden)",  ' +
-      '"slice stack_id (hidden)",  "slice parent_stack_id (hidden)",  ' +
-      '"slice category"\n' +
-      ' UNION ALL \n' +
-      'SELECT\n' +
-      '"slice name (stack)",\n' +
-      '  "slice depth (hidden)",\n' +
-      '  "slice stack_id (hidden)",\n' +
-      '  "slice parent_stack_id (hidden)",\n' +
-      '  "slice category"\n' +
-      'FROM (\n' +
-      'SELECT\n' +
-      'slice.name AS "slice name (stack)",\n' +
-      '  slice.depth AS "slice depth (hidden)",\n' +
-      '  slice.stack_id AS "slice stack_id (hidden)",\n' +
-      '  slice.parent_stack_id AS "slice parent_stack_id (hidden)",\n' +
-      '  slice.category AS "slice category"\n' +
-      'FROM\n' +
-      'descendant_slice_by_stack(stack_id) AS slice\n' +
-      'WHERE\n' +
-      'slice.dur != -1\n' +
-      ')\n' +
-      'GROUP BY "slice name (stack)",  "slice depth (hidden)",  ' +
-      '"slice stack_id (hidden)",  "slice parent_stack_id (hidden)",  ' +
-      '"slice category"\n' +
-      'ORDER BY "slice depth (hidden)" ASC\n';
+  const expectedQuery = `
+    SELECT
+      "slice name (stack)",
+      "slice depth (hidden)",
+      "slice stack_id (hidden)",
+      "slice parent_stack_id (hidden)",
+      "slice category"
+    FROM (
+      SELECT
+        slice.name AS "slice name (stack)",
+        slice.depth AS "slice depth (hidden)",
+        slice.stack_id AS "slice stack_id (hidden)",
+        slice.parent_stack_id AS "slice parent_stack_id (hidden)",
+        slice.category AS "slice category"
+      FROM
+        slice
+      WHERE
+        slice.dur != -1 AND
+        slice.stack_id = stack_id
+    )
+    GROUP BY "slice name (stack)",
+             "slice depth (hidden)",
+             "slice stack_id (hidden)",
+             "slice parent_stack_id (hidden)",
+             "slice category"
+    UNION ALL
+    SELECT
+      "slice name (stack)",
+      "slice depth (hidden)",
+      "slice stack_id (hidden)",
+      "slice parent_stack_id (hidden)",
+      "slice category"
+    FROM (
+      SELECT
+        slice.name AS "slice name (stack)",
+        slice.depth AS "slice depth (hidden)",
+        slice.stack_id AS "slice stack_id (hidden)",
+        slice.parent_stack_id AS "slice parent_stack_id (hidden)",
+        slice.category AS "slice category"
+      FROM
+        descendant_slice_by_stack(stack_id) AS slice
+      WHERE
+        slice.dur != -1
+    )
+    GROUP BY "slice name (stack)",
+             "slice depth (hidden)",
+             "slice stack_id (hidden)",
+             "slice parent_stack_id (hidden)",
+             "slice category"
+    ORDER BY "slice depth (hidden)" ASC
+  `;
 
   const table = ['descendant_slice_by_stack(stack_id) AS slice'];
   expectQueryEqual(
