f53a262d12018b440e820aa6722fff5598d4fe03
Lalit Maganti <lalitm@google.com>
2022-02-01 15:01:08 +0000

    cts: add user build tracing flag to reporter test
    Bug: 217206179
    Change-Id: Idf938e56e8db2a3d4f657932a0df56acd912cf02

--- test/cts/reporter_test_cts.cc
+++ test/cts/reporter_test_cts.cc
@@ -41,6 +41,7 @@ TEST(PerfettoReporterTest, TestEndToEndReport) {
   TraceConfig trace_config;
   trace_config.add_buffers()->set_size_kb(1024);
   trace_config.set_duration_ms(200);
+  trace_config.set_allow_user_build_tracing(true);
 
   auto* ds_config = trace_config.add_data_sources()->mutable_config();
   ds_config->set_name("android.perfetto.FakeProducer");
