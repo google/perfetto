4ec9099c8e353e68bb77539604de8a9e94fd94ea
Primiano Tucci <primiano@google.com>
2022-02-08 22:10:28 +0000

    Infra: Revert LUCI changes for Mac-arm64
    The Bots run a version of MacOS too old to support
    the more recent versions of Xcode required to cross-compile.
    
    Bug: 218384527
    Change-Id: I849ad02f4bb13d3bb42cc1359c1a6a7e02247d35

--- infra/luci/recipe_modules/macos_sdk/__init__.py
+++ infra/luci/recipe_modules/macos_sdk/__init__.py
@@ -44,7 +44,7 @@ PROPERTIES = {
         ),
         default={
             'sdk_version':
- '12D4e',
+ '12B5025f',
             'tool_package':
  'infra/tools/mac_toolchain/${platform}',
             'tool_version':
