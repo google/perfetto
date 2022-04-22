5b49d52cc42d307112ab5ad4e90c2ba9dc17a2d5
Daniele Di Proietto <ddiproietto@google.com>
2022-04-22 15:59:40 +0100

    Move around comments in TraceWriterBase hierarchy
    TraceWriterImpl inherits from TraceWriter, which inherits from
    TraceWriterBase.
    
    This commit moves most of the comments to TraceWriterBase (in the spirit
    of documenting the interface, not the implementation).
    
    This also removes redundant pure virtual method declarations from
    TraceWriter. This makes it easier to add methods in future commits (work
    in progress).
    
    Change-Id: I70735894007e19eb69a83592045496b6731db0bd

--- src/tracing/core/trace_writer_for_testing.h
+++ src/tracing/core/trace_writer_for_testing.h
@@ -28,7 +28,7 @@ namespace perfetto {
 
 // A specialization of TraceWriter for testing which writes into memory
 // allocated by the ScatteredHeapBuffer.
-// See //include/perfetto/tracing/core/trace_writer.h for docs.
+// See //include/perfetto/ext/tracing/core/trace_writer.h for docs.
 class TraceWriterForTesting : public TraceWriter {
  public:
   TraceWriterForTesting();
