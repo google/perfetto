2b7c6f86dbaf67bb2231b2b36eb81c58714fa079
annaoz <annaoz@google.com>
2022-04-27 08:48:42 +0100

    tp: Convert all usage of instants to the slice table
    Bug: 153522250
    Change-Id: I3dd0ad0242f58418f897ae8fbf843e7c31d5d16f

--- test/trace_processor/parsing/sched_blocked_reason_function.sql
+++ test/trace_processor/parsing/sched_blocked_reason_function.sql
@@ -2,6 +2,7 @@ select
   ts,
   thread.tid as pid,
   EXTRACT_ARG(arg_set_id, 'function') as func
-from instant
-join thread on instant.ref = thread.utid
-where instant.name = 'sched_blocked_reason';
+from legacy_instant
+join thread USING (utid)
+where legacy_instant.name = 'sched_blocked_reason'
+order by ts
