ce6d5f37fcb290f877e74d6b385c5eb4a4d2becf
Tuchila Octavian <octaviant@google.com>
2022-05-11 10:36:38 +0100

    ui: Add adb and webusb logic for recording
    This is CL is part of the recording refactoring. It adds:
    - adb and webusb specific logic
    - an implementation of a tracing session
    
    Bug: 209947455
    Change-Id: I94dae3e371d810abac234142379b49bcf7e05aa7

---
+++ ui/src/common/recordingV2/targets/android_webusb_target.ts
@@ -0,0 +1,59 @@
+// Copyright (C) 2022 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import {assertExists} from '../../../base/logging';
+
+import {AdbConnectionOverWebusb} from '../adb_connection_over_webusb';
+import {
+  RecordingTargetV2,
+  TargetInfo,
+  TracingSession,
+  TracingSessionListener
+} from '../recording_interfaces_v2';
+import {TracedTracingSession} from '../traced_tracing_session';
+
+export class AndroidWebusbTarget implements RecordingTargetV2 {
+  private adbConnection: AdbConnectionOverWebusb;
+
+  constructor(private device: USBDevice) {
+    this.adbConnection = new AdbConnectionOverWebusb(device);
+  }
+
+  getInfo(): TargetInfo {
+    const name = assertExists(this.device.productName) + ' ' +
+        assertExists(this.device.serialNumber) + ' WebUsb';
+    // TODO(octaviant): fetch the OS from the adb connection
+    return {targetType: 'ANDROID', osVersion: 'Q', name};
+  }
+
+  // This is called when a usb USBConnectionEvent of type 'disconnect' event is
+  // emitted. This event is emitted when the USB connection is lost(example:
+  // when the user unplugged the connecting cable).
+  async disconnect(disconnectMessage?: string): Promise<void> {
+    await this.adbConnection.disconnect(disconnectMessage);
+  }
+
+  async createTracingSession(tracingSessionListener: TracingSessionListener):
+      Promise<TracingSession> {
+    this.adbConnection.onStatus = tracingSessionListener.onStatus;
+    this.adbConnection.onDisconnect = tracingSessionListener.onDisconnect;
+    const adbStream =
+        await this.adbConnection.connectSocket('/dev/socket/traced_consumer');
+
+    const tracingSession =
+        new TracedTracingSession(adbStream, tracingSessionListener);
+    await tracingSession.initConnection();
+    return tracingSession;
+  }
+}
