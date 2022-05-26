c15b9a45ad0a9a3f8e71a6e9cdd9b4c13d4a9e29
Lalit Maganti <lalitm@google.com>
2022-05-26 22:36:51 +0000

    Revert "Take Delegate instead of BoundInsterter in json_utils"
    This reverts commit 1f9c74704b71b8ea5ba6e426cd481bd63638ce97.
    
    Reason for revert: With aosp/2106670 this should no longer be necessary
    
    Change-Id: I81c0c98a578a5fc137f6c0d6a38b9e3501ccdf54

--- src/trace_processor/importers/proto/track_event_parser.cc
+++ src/trace_processor/importers/proto/track_event_parser.cc
@@ -121,7 +121,8 @@ class TrackEventArgsParser : public util::ProtoToArgsParser::Delegate {
     if (!json_value)
       return false;
     return json::AddJsonValueToArgs(*json_value, base::StringView(key.flat_key),
-                                    base::StringView(key.key), &storage_, this);
+                                    base::StringView(key.key), &storage_,
+                                    &inserter_);
   }
   void AddNull(const Key& key) final {
     inserter_.AddArg(storage_.InternString(base::StringView(key.flat_key)),
