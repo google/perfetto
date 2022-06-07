1b741f4f46ec3976e2647d74a065f6af1fd382c9
Mohit Saini <mohitms@google.com>
2022-06-07 12:20:30 +0100

    Ensure event name is not omitted in end-events
    event name should be omitted with `TRACE_EVENT_BEGIN` macros
    but not with `TRACE_EVENT_END`. This is taken care already at the
    public macro level. However it still makes sense to ensure trace
    implemention doesn't omit event-names in end-events. Which was also
    supported for StaticString event-names. However, this was not taken
    care if event name is DynamicString (aosp/2111624)
    
    Change-Id: I9cb6bc157f3bae47115eefe178d6f366d9826b18

--- src/tracing/test/api_integrationtest.cc
+++ src/tracing/test/api_integrationtest.cc
@@ -3232,15 +3232,26 @@ TEST_P(PerfettoApiTest, TrackEventEventNameDynamicString) {
   tracing_session->get()->StartBlocking();
   TRACE_EVENT_BEGIN("foo", perfetto::DynamicString{std::string("Event1")});
   TRACE_EVENT_BEGIN("foo", perfetto::DynamicString{std::string("Event2")});
+
   TRACE_EVENT0("foo", TRACE_STR_COPY(std::string("Event3")));
   const char* event4 = "Event4";
   TRACE_EVENT0("foo", event4);
+
+  // Ensure that event-name is not emitted in case of `_END` events.
+  PERFETTO_INTERNAL_TRACK_EVENT(
+      "foo", perfetto::DynamicString{std::string("Event5")},
+      ::perfetto::protos::pbzero::TrackEvent::TYPE_SLICE_END);
+  PERFETTO_INTERNAL_TRACK_EVENT(
+      "foo", "Event6", ::perfetto::protos::pbzero::TrackEvent::TYPE_SLICE_END);
+
   auto slices = StopSessionAndReadSlicesFromTrace(tracing_session);
-  ASSERT_EQ(4u, slices.size());
+  ASSERT_EQ(6u, slices.size());
   EXPECT_EQ("B:foo.Event1", slices[0]);
   EXPECT_EQ("B:foo.Event2", slices[1]);
   EXPECT_EQ("B:foo.Event3", slices[2]);
   EXPECT_EQ("B:foo.Event4", slices[3]);
+  EXPECT_EQ("E", slices[4]);
+  EXPECT_EQ("E", slices[5]);
 }
 
 TEST_P(PerfettoApiTest, TrackEventDynamicStringInDebugArgs) {
