4bb5961cef54519a5e7591ae4b6e7a94bf737f6a
Hector Dearman <hjd@google.com>
2022-07-05 20:44:18 +0100

    ui: Combine all tracks into single files
    Historically tracks were split into four files:
    - controller: the code for fetching the data from TP
    - frontend: the code for rendering the track
    - common: the track, name, config and intermediate data format
    - index: imported frontend and controller
    
    To mangle a quote: "This has made a lot of people confused, and
    been widely regarded as a bad move.". It made a *little* more sense
    when the controller was a separate Worker but even then they should
    have been combined in retrospect.
    
    For each track this combines the four files into the index file
    with minimal import reorganization as needed. We also take this
    opportunity to move track {,controller} registration to happen
    at plugin activation time:
    
      export function activate() {
        trackRegistry.register(AndroidLogTrack);
        trackControllerRegistry.register(AndroidLogTrackController);
      }
    
    rather than at file import time:
    
      trackRegistry.register(AndroidLogTrack);
      trackControllerRegistry.register(AndroidLogTrackController);
      export function activate() {}
    
    Activation happens only the tiniest bit later than import anyway at
    the moment so no impact is expected.
    
    It's -800 lines overall, mostly copyright headers but there was also
    a bunch of now unnecessary imports to remove.
    
    Change-Id: Ic32fc0d433b6a7dad019a6d79f81d6dfbbed4a00

--- ui/src/tracks/expected_frames/index.ts
+++ ui/src/tracks/expected_frames/index.ts
@@ -12,10 +12,155 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import './controller';
-import './frontend';
+import {NUM, NUM_NULL, STR} from '../../common/query_result';
+import {fromNs, toNs} from '../../common/time';
+import {TrackData} from '../../common/track_data';
+import {
+  TrackController,
+  trackControllerRegistry,
+} from '../../controller/track_controller';
+import {NewTrackArgs, Track} from '../../frontend/track';
+import {trackRegistry} from '../../frontend/track_registry';
+import {ChromeSliceTrack} from '../chrome_slices';
+
+export const EXPECTED_FRAMES_SLICE_TRACK_KIND = 'ExpectedFramesSliceTrack';
+
+export interface Config {
+  maxDepth: number;
+  trackIds: number[];
+}
+
+export interface Data extends TrackData {
+  // Slices are stored in a columnar fashion. All fields have the same length.
+  strings: string[];
+  sliceIds: Float64Array;
+  starts: Float64Array;
+  ends: Float64Array;
+  depths: Uint16Array;
+  titles: Uint16Array;   // Index in |strings|.
+  colors?: Uint16Array;  // Index in |strings|.
+  isInstant: Uint16Array;
+  isIncomplete: Uint16Array;
+}
+
+class ExpectedFramesSliceTrackController extends TrackController<Config, Data> {
+  static readonly kind = EXPECTED_FRAMES_SLICE_TRACK_KIND;
+  private maxDurNs = 0;
+
+  async onBoundsChange(start: number, end: number, resolution: number):
+      Promise<Data> {
+    const startNs = toNs(start);
+    const endNs = toNs(end);
+
+    const pxSize = this.pxSize();
+
+    // ns per quantization bucket (i.e. ns per pixel). /2 * 2 is to force it to
+    // be an even number, so we can snap in the middle.
+    const bucketNs = Math.max(Math.round(resolution * 1e9 * pxSize / 2) * 2, 1);
+
+    if (this.maxDurNs === 0) {
+      const maxDurResult = await this.query(`
+        select max(iif(dur = -1, (SELECT end_ts FROM trace_bounds) - ts, dur))
+          as maxDur
+        from experimental_slice_layout
+        where filter_track_ids = '${this.config.trackIds.join(',')}'
+      `);
+      this.maxDurNs = maxDurResult.firstRow({maxDur: NUM_NULL}).maxDur || 0;
+    }
+
+    const queryRes = await this.query(`
+      SELECT
+        (ts + ${bucketNs / 2}) / ${bucketNs} * ${bucketNs} as tsq,
+        ts,
+        max(iif(dur = -1, (SELECT end_ts FROM trace_bounds) - ts, dur)) as dur,
+        layout_depth as layoutDepth,
+        name,
+        id,
+        dur = 0 as isInstant,
+        dur = -1 as isIncomplete
+      from experimental_slice_layout
+      where
+        filter_track_ids = '${this.config.trackIds.join(',')}' and
+        ts >= ${startNs - this.maxDurNs} and
+        ts <= ${endNs}
+      group by tsq, layout_depth
+      order by tsq, layout_depth
+    `);
+
+    const numRows = queryRes.numRows();
+    const slices: Data = {
+      start,
+      end,
+      resolution,
+      length: numRows,
+      strings: [],
+      sliceIds: new Float64Array(numRows),
+      starts: new Float64Array(numRows),
+      ends: new Float64Array(numRows),
+      depths: new Uint16Array(numRows),
+      titles: new Uint16Array(numRows),
+      colors: new Uint16Array(numRows),
+      isInstant: new Uint16Array(numRows),
+      isIncomplete: new Uint16Array(numRows),
+    };
+
+    const stringIndexes = new Map<string, number>();
+    function internString(str: string) {
+      let idx = stringIndexes.get(str);
+      if (idx !== undefined) return idx;
+      idx = slices.strings.length;
+      slices.strings.push(str);
+      stringIndexes.set(str, idx);
+      return idx;
+    }
+    const greenIndex = internString('#4CAF50');
+
+    const it = queryRes.iter({
+      tsq: NUM,
+      ts: NUM,
+      dur: NUM,
+      layoutDepth: NUM,
+      id: NUM,
+      name: STR,
+      isInstant: NUM,
+      isIncomplete: NUM,
+    });
+    for (let row = 0; it.valid(); it.next(), ++row) {
+      const startNsQ = it.tsq;
+      const startNs = it.ts;
+      const durNs = it.dur;
+      const endNs = startNs + durNs;
+
+      let endNsQ = Math.floor((endNs + bucketNs / 2 - 1) / bucketNs) * bucketNs;
+      endNsQ = Math.max(endNsQ, startNsQ + bucketNs);
+
+      slices.starts[row] = fromNs(startNsQ);
+      slices.ends[row] = fromNs(endNsQ);
+      slices.depths[row] = it.layoutDepth;
+      slices.titles[row] = internString(it.name);
+      slices.sliceIds[row] = it.id;
+      slices.isInstant[row] = it.isInstant;
+      slices.isIncomplete[row] = it.isIncomplete;
+      slices.colors![row] = greenIndex;
+    }
+    return slices;
+  }
+}
+
+
+export class ExpectedFramesSliceTrack extends ChromeSliceTrack {
+  static readonly kind = EXPECTED_FRAMES_SLICE_TRACK_KIND;
+  static create(args: NewTrackArgs): Track {
+    return new ExpectedFramesSliceTrack(args);
+  }
+}
+
+function activate() {
+  trackControllerRegistry.register(ExpectedFramesSliceTrackController);
+  trackRegistry.register(ExpectedFramesSliceTrack);
+}
 
 export const plugin = {
   pluginId: 'perfetto.ExpectedFrames',
-  activate: () => {},
+  activate,
 };
