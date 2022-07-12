4a17c4c0e25ab5ae91c3a3d3c5c0497838b4e036
Marcin Oczeretko <marcinoc@google.com>
2022-07-12 16:47:04 +0100

    Change android_frame_timeline_metric to extract more frame metrics
    Add total_frames, missed_sf_frames, and percentiles of frame durations
    
    Change-Id: Ia853b98dd61a05f7292c5a71ca9630881b8c3a0d

--- src/trace_processor/metrics/sql/android/android_frame_timeline_metric.sql
+++ src/trace_processor/metrics/sql/android/android_frame_timeline_metric.sql
@@ -13,33 +13,56 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 
-DROP VIEW IF EXISTS app_deadline_missed_view;
+SELECT RUN_METRIC('android/process_metadata.sql');
 
-CREATE VIEW app_deadline_missed_view
-AS
-SELECT p.name AS process, COUNT(jank_type) AS 'jank_count'
+DROP VIEW IF EXISTS android_frame_timeline_metric_per_process;
+CREATE VIEW android_frame_timeline_metric_per_process AS
+SELECT
+  process.upid,
+  process.name AS process_name,
+  process_metadata.metadata AS process_metadata,
+  COUNT(1) AS total_frames,
+  SUM(jank_type GLOB '*App Deadline Missed*') AS missed_app_frames,
+  SUM(
+    jank_type GLOB '*SurfaceFlinger*'
+    OR jank_type GLOB '*Prediction Error*'
+    OR jank_type GLOB '*Display HAL*') AS missed_sf_frames,
+  SUM(jank_type GLOB '*App Deadline Missed*'
+    OR jank_type GLOB '*SurfaceFlinger*'
+    OR jank_type GLOB '*Prediction Error*'
+    OR jank_type GLOB '*Display HAL*') AS missed_frames,
+  CAST(PERCENTILE(dur, 50) AS INTEGER) AS frame_dur_p50,
+  CAST(PERCENTILE(dur, 90) AS INTEGER) AS frame_dur_p90,
+  CAST(PERCENTILE(dur, 95) AS INTEGER) AS frame_dur_p95,
+  CAST(PERCENTILE(dur, 99) AS INTEGER) AS frame_dur_p99,
+  CAST(AVG(dur) AS INTEGER) AS frame_dur_avg,
+  MAX(dur) AS frame_dur_max
 FROM actual_frame_timeline_slice
-LEFT JOIN process AS p
-  USING (upid)
-WHERE jank_type GLOB '*App Deadline Missed*'
-GROUP BY process;
+JOIN process USING (upid)
+JOIN process_metadata USING (upid)
+GROUP BY process.upid, process.name;
 
 DROP VIEW IF EXISTS android_frame_timeline_metric_output;
-
 CREATE VIEW android_frame_timeline_metric_output
 AS
 SELECT
-    AndroidFrameTimelineMetric(
-    'app_deadline_missed_total_count',
-    (SELECT SUM(jank_count) FROM app_deadline_missed_view),
-    'process',
-    (
+  AndroidFrameTimelineMetric(
+    'total_frames', SUM(total_frames),
+    'missed_app_frames', SUM(missed_app_frames),
+    'process', (
       SELECT
         RepeatedField(
-            AndroidFrameTimelineMetric_ProcessBreakdown(
-            'process_name',
-            process,
-            'app_deadline_missed_count',
-            jank_count))
-      FROM app_deadline_missed_view
-    ));
+          AndroidFrameTimelineMetric_ProcessBreakdown(
+            'process', process_metadata,
+            'total_frames', total_frames,
+            'missed_frames', missed_frames,
+            'missed_app_frames', missed_app_frames,
+            'missed_sf_frames', missed_sf_frames,
+            'frame_dur_max', frame_dur_max,
+            'frame_dur_avg', frame_dur_avg,
+            'frame_dur_p50', frame_dur_p50,
+            'frame_dur_p90', frame_dur_p90,
+            'frame_dur_p95', frame_dur_p95,
+            'frame_dur_p99', frame_dur_p99))
+      FROM android_frame_timeline_metric_per_process))
+FROM android_frame_timeline_metric_per_process;
