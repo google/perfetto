4602c5cba9ffa467628382c1b4a91ce70a6b3bc6
Igor Kraskevich <kraskevich@google.com>
2022-07-28 17:05:25 +0000

    Factor SizeProfileComputer out of protoprofile tool
    - This separates Trace proto content breakdown and pprof profile generation into two distinct classes. The former can be reused for other output formats (for example, SQL tables). This is the main goal of this change.
    
    - This also replaces std::map's with base::FlatHashMap's - the new version is ~30-40% faster on the traces I've checked.
    
    - I've compared generated pprof profile protos for the current vs the new version - the field ordering in the proto is different, but the resulting profiles look the same for a few traces I've checked, so the protoprofile tool behavior should not change.
    
    Bug: b/238062136
    Change-Id: Ic07b0da53640f15eb2b48d820b6fe565f0ec67eb

--- src/tools/protoprofile/main.cc
+++ src/tools/protoprofile/main.cc
@@ -22,12 +22,14 @@
 #include <google/protobuf/io/zero_copy_stream_impl.h>
 
 #include "perfetto/ext/base/file_utils.h"
+#include "perfetto/ext/base/flat_hash_map.h"
 #include "perfetto/ext/base/scoped_file.h"
 #include "perfetto/protozero/field.h"
 #include "perfetto/protozero/packed_repeated_fields.h"
 #include "perfetto/protozero/proto_decoder.h"
 #include "perfetto/protozero/proto_utils.h"
 #include "perfetto/protozero/scattered_heap_buffer.h"
+#include "src/trace_processor/util/proto_profiler.h"
 
 #include "protos/third_party/pprof/profile.pbzero.h"
 
@@ -77,69 +79,28 @@ void MultiFileErrorCollectorImpl::AddWarning(const std::string& filename,
                 message.c_str());
 }
 
-class SizeProfileComputer {
+class PprofProfileComputer {
  public:
   std::string Compute(const uint8_t* ptr,
                       size_t size,
                       const Descriptor* descriptor);
 
  private:
-  struct StackInfo {
-    std::vector<size_t> samples;
-    std::vector<int> locations;
-  };
-
-  void ComputeInner(const uint8_t* ptr,
-                    size_t size,
-                    const Descriptor* descriptor);
-  void Sample(size_t size);
   int InternString(const std::string& str);
   int InternLocation(const std::string& str);
-  size_t GetFieldSize(const protozero::Field& f);
-
-  // Convert the current stack into a string:
-  // {"Foo", "#bar", "Bar", "#baz", "int"} -> "Foo$#bar$Bar$#baz$int$"
-  std::string StackToKey();
-
-  // The current 'stack' we're considering as we parse the protobuf.
-  // For example if we're currently looking at the varint field baz which is
-  // nested inside message Bar which is in turn a field named bar on the message
-  // Foo. Then the stack would be: Foo, #bar, Bar, #baz, int
-  // We keep track of both the field names (#bar, #baz) and the field types
-  // (Foo, Bar, int) as sometimes we are intrested in which fields are big
-  // and sometimes which types are big.
-  std::vector<std::string> stack_;
-
-  // Information about each stack seen. Keyed by a unique string for each stack.
-  std::map<std::string, StackInfo> stack_info_;
 
   // Interned strings:
   std::vector<std::string> strings_;
-  std::map<std::string, int> string_to_id_;
+  base::FlatHashMap<std::string, int> string_to_id_;
 
   // Interned 'locations', each location is a single frame of the stack.
-  std::map<std::string, int> locations_;
+  base::FlatHashMap<std::string, int> locations_;
 };
 
-size_t SizeProfileComputer::GetFieldSize(const protozero::Field& f) {
-  uint8_t buf[10];
-  switch (f.type()) {
-    case protozero::proto_utils::ProtoWireType::kVarInt:
-      return static_cast<size_t>(
-          protozero::proto_utils::WriteVarInt(f.as_uint64(), buf) - buf);
-    case protozero::proto_utils::ProtoWireType::kLengthDelimited:
-      return f.size();
-    case protozero::proto_utils::ProtoWireType::kFixed32:
-      return 4;
-    case protozero::proto_utils::ProtoWireType::kFixed64:
-      return 8;
-  }
-  PERFETTO_FATAL("unexpected field type");  // for gcc
-}
-
-int SizeProfileComputer::InternString(const std::string& s) {
-  if (string_to_id_.count(s)) {
-    return string_to_id_[s];
+int PprofProfileComputer::InternString(const std::string& s) {
+  auto val = string_to_id_.Find(s);
+  if (val) {
+    return *val;
   }
   strings_.push_back(s);
   int id = static_cast<int>(strings_.size() - 1);
@@ -147,43 +108,24 @@ int SizeProfileComputer::InternString(const std::string& s) {
   return id;
 }
 
-int SizeProfileComputer::InternLocation(const std::string& s) {
-  if (locations_.count(s)) {
-    return locations_[s];
+int PprofProfileComputer::InternLocation(const std::string& s) {
+  auto val = locations_.Find(s);
+  if (val) {
+    return *val;
   }
   int id = static_cast<int>(locations_.size()) + 1;
   locations_[s] = id;
   return id;
 }
 
-std::string SizeProfileComputer::StackToKey() {
-  std::string key;
-  for (const std::string& part : stack_) {
-    key += part;
-    key += "$";
-  }
-  return key;
-}
-
-void SizeProfileComputer::Sample(size_t size) {
-  const std::string& key = StackToKey();
-
-  if (!stack_info_.count(key)) {
-    StackInfo& info = stack_info_[key];
-    info.locations.resize(stack_.size());
-    for (size_t i = 0; i < stack_.size(); i++) {
-      info.locations[i] = InternLocation(stack_[i]);
-    }
-  }
-
-  stack_info_[key].samples.push_back(size);
-}
-
-std::string SizeProfileComputer::Compute(const uint8_t* ptr,
-                                         size_t size,
-                                         const Descriptor* descriptor) {
+std::string PprofProfileComputer::Compute(const uint8_t* ptr,
+                                          size_t size,
+                                          const Descriptor* descriptor) {
   PERFETTO_CHECK(InternString("") == 0);
-  ComputeInner(ptr, size, descriptor);
+
+  trace_processor::util::SizeProfileComputer computer;
+  auto field_path_to_samples = computer.Compute(ptr, size, descriptor);
+
   protozero::HeapBuffered<third_party::perftools::profiles::pbzero::Profile>
       profile;
 
@@ -207,25 +149,27 @@ std::string SizeProfileComputer::Compute(const uint8_t* ptr,
   sample_type->set_type(InternString("total_size"));
   sample_type->set_unit(InternString("bytes"));
 
-  // For each unique stack we've seen write out the stats:
-  for (auto& id_info : stack_info_) {
-    StackInfo& info = id_info.second;
+  // For each unique field path we've seen write out the stats:
+  for (auto it = field_path_to_samples.GetIterator(); it; ++it) {
+    const auto& field_path = it.key();
+    auto& samples = it.value();
 
     protozero::PackedVarInt location_ids;
     auto* sample = profile->add_sample();
-    for (auto it = info.locations.rbegin(); it != info.locations.rend(); ++it) {
-      location_ids.Append(static_cast<uint64_t>(*it));
+    for (auto loc_it = field_path.rbegin(); loc_it != field_path.rend();
+         ++loc_it) {
+      location_ids.Append(InternLocation(*loc_it));
     }
     sample->set_location_id(location_ids);
 
-    std::sort(info.samples.begin(), info.samples.end());
-    size_t count = info.samples.size();
+    std::sort(samples.begin(), samples.end());
+    size_t count = samples.size();
     size_t total_size = 0;
-    size_t max_size = info.samples[count - 1];
-    size_t min_size = info.samples[0];
-    size_t median_size = info.samples[count / 2];
+    size_t max_size = samples[count - 1];
+    size_t min_size = samples[0];
+    size_t median_size = samples[count / 2];
     for (size_t i = 0; i < count; ++i)
-      total_size += info.samples[i];
+      total_size += samples[i];
     // These have to be in the same order as the sample types above:
     protozero::PackedVarInt values;
     values.Append(static_cast<int64_t>(count));
@@ -240,81 +184,23 @@ std::string SizeProfileComputer::Compute(const uint8_t* ptr,
   // locations which in turn are associated to functions. We don't currently
   // distinguish them so we make a 1:1 mapping between the locations and the
   // functions:
-  for (const auto& location_id : locations_) {
+  for (auto it = locations_.GetIterator(); it; ++it) {
     auto* location = profile->add_location();
-    location->set_id(static_cast<uint64_t>(location_id.second));
+    location->set_id(static_cast<uint64_t>(it.value()));
+
     auto* line = location->add_line();
-    line->set_function_id(static_cast<uint64_t>(location_id.second));
-  }
+    line->set_function_id(static_cast<uint64_t>(it.value()));
 
-  for (const auto& location_id : locations_) {
     auto* function = profile->add_function();
-    function->set_id(static_cast<uint64_t>(location_id.second));
-    function->set_name(InternString(location_id.first));
+    function->set_id(static_cast<uint64_t>(it.value()));
+    function->set_name(InternString(it.key()));
   }
-
   // Finally the string table. We intern more strings above, so this has to be
   // last.
-  for (int i = 0; i < static_cast<int>(strings_.size()); i++) {
-    profile->add_string_table(strings_[static_cast<size_t>(i)]);
+  for (size_t i = 0; i < strings_.size(); i++) {
+    profile->add_string_table(strings_[i]);
   }
   return profile.SerializeAsString();
-}
-
-void SizeProfileComputer::ComputeInner(const uint8_t* ptr,
-                                       size_t size,
-                                       const Descriptor* descriptor) {
-  size_t overhead = size;
-  size_t unknown = 0;
-  protozero::ProtoDecoder decoder(ptr, size);
-
-  stack_.push_back(descriptor->name());
-
-  // Compute the size of each sub-field of this message, subtracting it
-  // from overhead and possible adding it to unknown.
-  for (;;) {
-    if (decoder.bytes_left() == 0)
-      break;
-    protozero::Field field = decoder.ReadField();
-    if (!field.valid()) {
-      PERFETTO_ELOG("Field not valid (can mean field id >1000)");
-      break;
-    }
-
-    int id = field.id();
-    ProtoWireType type = field.type();
-    size_t field_size = GetFieldSize(field);
-
-    overhead -= field_size;
-    const FieldDescriptor* field_descriptor = descriptor->FindFieldByNumber(id);
-    if (!field_descriptor) {
-      unknown += field_size;
-      continue;
-    }
-
-    stack_.push_back("#" + field_descriptor->name());
-    bool is_message_type =
-        field_descriptor->type() == FieldDescriptor::TYPE_MESSAGE;
-    if (type == ProtoWireType::kLengthDelimited && is_message_type) {
-      ComputeInner(field.data(), field.size(),
-                   field_descriptor->message_type());
-    } else {
-      stack_.push_back(field_descriptor->type_name());
-      Sample(field_size);
-      stack_.pop_back();
-    }
-    stack_.pop_back();
-  }
-
-  if (unknown) {
-    stack_.push_back("#:unknown:");
-    Sample(unknown);
-    stack_.pop_back();
-  }
-
-  // Anything not blamed on a child is overhead for this message.
-  Sample(overhead);
-  stack_.pop_back();
 }
 
 int PrintUsage(int, const char** argv) {
@@ -362,7 +248,7 @@ int Main(int argc, const char** argv) {
     PERFETTO_ELOG("Could not open output path (%s)", output_path);
     return 1;
   }
-  SizeProfileComputer computer;
+  PprofProfileComputer computer;
   std::string out = computer.Compute(start, size, descriptor);
   base::WriteAll(output_fd.get(), out.data(), out.size());
   base::FlushFile(output_fd.get());
---
+++ src/trace_processor/util/proto_profiler.h
@@ -0,0 +1,84 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef SRC_TRACE_PROCESSOR_UTIL_PROTO_PROFILER_H_
+#define SRC_TRACE_PROCESSOR_UTIL_PROTO_PROFILER_H_
+
+#include <algorithm>
+#include <vector>
+
+#include <google/protobuf/compiler/importer.h>
+#include <google/protobuf/dynamic_message.h>
+#include <google/protobuf/io/zero_copy_stream_impl.h>
+
+#include "perfetto/ext/base/flat_hash_map.h"
+
+#include "perfetto/protozero/field.h"
+
+namespace perfetto {
+namespace trace_processor {
+namespace util {
+
+class SizeProfileComputer {
+ public:
+  using FieldPath = std::vector<std::string>;
+  using SizeSamples = std::vector<size_t>;
+
+  struct FieldPathHasher {
+    using argument_type = std::vector<std::string>;
+    using result_type = size_t;
+
+    result_type operator()(const argument_type& p) const {
+      size_t h = 0u;
+      for (auto v : p)
+        h ^= std::hash<std::string>{}(v);
+      return h;
+    }
+  };
+
+  // Returns a list of samples (i.e. all encountered field sizes) for each
+  // field path in trace proto.
+  // TODO(kraskevich): consider switching to internal DescriptorPool.
+  base::FlatHashMap<FieldPath, SizeSamples, FieldPathHasher> Compute(
+      const uint8_t* ptr,
+      size_t size,
+      const google::protobuf::Descriptor* descriptor);
+
+ private:
+  void ComputeInner(const uint8_t* ptr,
+                    size_t size,
+                    const google::protobuf::Descriptor* descriptor);
+  void Sample(size_t size);
+  size_t GetFieldSize(const protozero::Field& f);
+
+  // The current 'stack' we're considering as we parse the protobuf.
+  // For example if we're currently looking at the varint field baz which is
+  // nested inside message Bar which is in turn a field named bar on the message
+  // Foo. Then the stack would be: Foo, #bar, Bar, #baz, int
+  // We keep track of both the field names (#bar, #baz) and the field types
+  // (Foo, Bar, int) as sometimes we are intrested in which fields are big
+  // and sometimes which types are big.
+  std::vector<std::string> stack_;
+
+  // Information about each field path seen.
+  base::FlatHashMap<FieldPath, SizeSamples, FieldPathHasher> path_to_samples_;
+};
+
+}  // namespace util
+}  // namespace trace_processor
+}  // namespace perfetto
+
+#endif  // SRC_TRACE_PROCESSOR_UTIL_PROTO_PROFILER_H_
