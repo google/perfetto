bd675abf0fdeb68762da06f9bdecaee92a9d4fda
Simon MacMullen <simonmacm@google.com>
2022-08-25 11:56:18 +0000

    Add some extra metric tracks for screen state and doze state.
    Add a "merged" version of global_counter_span_view which merges together spans with the same value (since we get that a lot for screen state).
    
    Screenshot: https://screenshot.googleplex.com/WtC4TcEwkruWjkB
    Bug: 243667311
    Change-Id: Ief9fb7a332bdcc597c34118b0529ab064813608c

--- src/trace_processor/metrics/sql/android/android_batt.sql
+++ src/trace_processor/metrics/sql/android/android_batt.sql
@@ -99,9 +99,17 @@ WHERE
     AND dur != -1
 ;
 
-SELECT RUN_METRIC('android/global_counter_span_view.sql',
+SELECT RUN_METRIC('android/global_counter_span_view_merged.sql',
   'table_name', 'screen_state',
   'counter_name', 'ScreenState');
+
+SELECT RUN_METRIC('android/process_counter_span_view.sql',
+  'table_name', 'doze_light_state',
+  'counter_name', 'DozeLightState');
+
+SELECT RUN_METRIC('android/process_counter_span_view.sql',
+  'table_name', 'doze_deep_state',
+  'counter_name', 'DozeDeepState');
 
 DROP TABLE IF EXISTS screen_state_span_with_suspend;
 CREATE VIRTUAL TABLE screen_state_span_with_suspend
@@ -115,10 +123,56 @@ SELECT
        'Suspended' AS slice_name,
        'Suspend / resume' AS track_name,
        'slice' AS track_type
-FROM suspend_slice_;
+FROM suspend_slice_
+UNION ALL
+SELECT ts,
+       dur,
+       CASE screen_state_val
+         WHEN 1 THEN 'Screen off'
+         WHEN 2 THEN 'Screen on'
+         WHEN 3 THEN 'Always-on display (doze)'
+         ELSE 'unknown'
+       END AS slice_name,
+       'Screen state' AS track_name,
+       'slice' AS track_type
+FROM screen_state_span
+UNION ALL
+-- See DeviceIdleController.java for where these states come from and how
+-- they transition.
+SELECT ts,
+       dur,
+       CASE doze_light_state_val
+         WHEN 0 THEN 'active'
+         WHEN 1 THEN 'inactive'
+         WHEN 4 THEN 'idle'
+         WHEN 5 THEN 'waiting_for_network'
+         WHEN 6 THEN 'idle_maintenance'
+         WHEN 7 THEN 'override'
+         ELSE 'unknown'
+       END AS slice_name,
+       'Doze light state' AS track_name,
+       'slice' AS track_type
+FROM doze_light_state_span
+UNION ALL
+SELECT ts,
+       dur,
+       CASE doze_deep_state_val
+         WHEN 0 THEN 'active'
+         WHEN 1 THEN 'inactive'
+         WHEN 2 THEN 'idle_pending'
+         WHEN 3 THEN 'sensing'
+         WHEN 4 THEN 'locating'
+         WHEN 5 THEN 'idle'
+         WHEN 6 THEN 'idle_maintenance'
+         WHEN 7 THEN 'quick_doze_delay'
+         ELSE 'unknown'
+       END AS slice_name,
+       'Doze deep state' AS track_name,
+       'slice' AS track_type
+FROM doze_deep_state_span;
 
 DROP VIEW IF EXISTS android_batt_output;
-CREATE VIEW android_batt_output AS
+CREATE VIEW android_batt_output AS 
 SELECT AndroidBatteryMetric(
   'battery_counters', (
     SELECT RepeatedField(
