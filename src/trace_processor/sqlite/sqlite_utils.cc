9d3de5c93770cf6a45b078bc09958e2447fb34c4
Joseph Koshy <jkoshy@chromium.org>
2022-11-02 16:05:19 +0000

    Un-inline a function that is used from a single location.
    This change list does not change code behavior.
    
    Bug: 256691616
    Change-Id: I1b383273429c410aab3650184ee028c036e6c50a

--- src/trace_processor/sqlite/sqlite_utils.cc
+++ src/trace_processor/sqlite/sqlite_utils.cc
@@ -29,6 +29,69 @@ std::wstring SqliteValueToWString(sqlite3_value* value) {
       reinterpret_cast<const wchar_t*>(sqlite3_value_text16(value)), count);
 }
 
+base::Status GetColumnsForTable(sqlite3* db,
+                                const std::string& raw_table_name,
+                                std::vector<SqliteTable::Column>& columns) {
+  PERFETTO_DCHECK(columns.empty());
+  char sql[1024];
+  const char kRawSql[] = "SELECT name, type from pragma_table_info(\"%s\")";
+
+  // Support names which are table valued functions with arguments.
+  std::string table_name = raw_table_name.substr(0, raw_table_name.find('('));
+  size_t n = base::SprintfTrunc(sql, sizeof(sql), kRawSql, table_name.c_str());
+  PERFETTO_DCHECK(n > 0);
+
+  sqlite3_stmt* raw_stmt = nullptr;
+  int err =
+      sqlite3_prepare_v2(db, sql, static_cast<int>(n), &raw_stmt, nullptr);
+  if (err != SQLITE_OK) {
+    return base::ErrStatus("Preparing database failed");
+  }
+  ScopedStmt stmt(raw_stmt);
+  PERFETTO_DCHECK(sqlite3_column_count(*stmt) == 2);
+
+  for (;;) {
+    err = sqlite3_step(raw_stmt);
+    if (err == SQLITE_DONE)
+      break;
+    if (err != SQLITE_ROW) {
+      return base::ErrStatus("Querying schema of table %s failed",
+                             raw_table_name.c_str());
+    }
+
+    const char* name =
+        reinterpret_cast<const char*>(sqlite3_column_text(*stmt, 0));
+    const char* raw_type =
+        reinterpret_cast<const char*>(sqlite3_column_text(*stmt, 1));
+    if (!name || !raw_type || !*name) {
+      return base::ErrStatus("Schema for %s has invalid column values",
+                             raw_table_name.c_str());
+    }
+
+    SqlValue::Type type;
+    if (base::CaseInsensitiveEqual(raw_type, "STRING") ||
+        base::CaseInsensitiveEqual(raw_type, "TEXT")) {
+      type = SqlValue::Type::kString;
+    } else if (base::CaseInsensitiveEqual(raw_type, "DOUBLE")) {
+      type = SqlValue::Type::kDouble;
+    } else if (base::CaseInsensitiveEqual(raw_type, "BIG INT") ||
+               base::CaseInsensitiveEqual(raw_type, "UNSIGNED INT") ||
+               base::CaseInsensitiveEqual(raw_type, "INT") ||
+               base::CaseInsensitiveEqual(raw_type, "BOOLEAN") ||
+               base::CaseInsensitiveEqual(raw_type, "INTEGER")) {
+      type = SqlValue::Type::kLong;
+    } else if (!*raw_type) {
+      PERFETTO_DLOG("Unknown column type for %s %s", raw_table_name.c_str(),
+                    name);
+      type = SqlValue::Type::kNull;
+    } else {
+      return base::ErrStatus("Unknown column type '%s' on table %s", raw_type,
+                             raw_table_name.c_str());
+    }
+    columns.emplace_back(columns.size(), name, type);
+  }
+  return base::OkStatus();
+}
 }  // namespace sqlite_utils
 }  // namespace trace_processor
 }  // namespace perfetto
