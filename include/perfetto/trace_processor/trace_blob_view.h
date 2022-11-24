d937514e584fb3a51f4af21fffec21d1b5c88860
Lalit Maganti <lalitm@google.com>
2022-11-24 15:26:45 +0000

    tp: add minimal compression for TracePacketData inside sorter queues
    Change-Id: Iff3314ddfe55b73ea6c40a95b00573391dd4a7ab

--- include/perfetto/trace_processor/trace_blob_view.h
+++ include/perfetto/trace_processor/trace_blob_view.h
@@ -48,7 +48,7 @@ namespace trace_processor {
 class TraceBlobView {
  public:
   // Takes ownership of the passed |blob|.
-  static constexpr size_t kWholeBlob = std::numeric_limits<size_t>::max();
+  static constexpr uint32_t kWholeBlob = std::numeric_limits<uint32_t>::max();
   explicit TraceBlobView(TraceBlob blob,
                          size_t offset = 0,
                          size_t length = kWholeBlob) {
@@ -62,6 +62,18 @@ class TraceBlobView {
       length_ = static_cast<uint32_t>(length);
     }
     blob_.reset(new TraceBlob(std::move(blob)));
+  }
+
+  TraceBlobView(RefPtr<TraceBlob> blob_ptr, uint32_t offset, uint32_t length) {
+    PERFETTO_DCHECK(offset <= std::numeric_limits<uint32_t>::max());
+    data_ = blob_ptr->data() + offset;
+    if (length == kWholeBlob) {
+      length_ = static_cast<uint32_t>(blob_ptr->size() - offset);
+    } else {
+      PERFETTO_DCHECK(offset + length_ <= blob_ptr->size());
+      length_ = length;
+    }
+    blob_ = blob_ptr;
   }
 
   // Trivial empty ctor.
@@ -98,6 +110,8 @@ class TraceBlobView {
 
   TraceBlobView copy() const { return slice(data_, length_); }
 
+  explicit operator bool() const { return data_ != nullptr; }
+
   bool operator==(const TraceBlobView& rhs) const {
     return (data_ == rhs.data_) && (length_ == rhs.length_) &&
            (blob_ == rhs.blob_);
@@ -106,8 +120,13 @@ class TraceBlobView {
 
   const uint8_t* data() const { return data_; }
   // TODO(primiano): normalize length() vs size() usage.
-  size_t length() const { return length_; }
-  size_t size() const { return length_; }
+  uint32_t length() const { return length_; }
+  uint32_t size() const { return length_; }
+  uint32_t offset() const {
+    return static_cast<uint32_t>(data_ - blob_->data());
+  }
+
+  const RefPtr<TraceBlob>& blob() const { return blob_; }
 
  private:
   TraceBlobView(RefPtr<TraceBlob> blob, const uint8_t* data, uint32_t length)
