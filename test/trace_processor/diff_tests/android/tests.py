efdc7c84c237a2d925b494e6966b66859f877686
Zim <zezeozue@google.com>
2023-02-02 12:28:51 +0000

    [metrics]: Add unaggregated binder_txn metrics
    We already had android_binder metrics but these were very coarse stats
    on per-process binder txn counts.
    
    We now add a metric per binder-txn including an aggregation of the
    thread_states and kernel blocked_functions on the client and server side
    of the txn.
    
    This will provide insight into why binder txns vary so widely and
    anomalies like really long txns with short replies.
    
    The change first adds some foundational SQL views as part of the stdlib.
    These views are then used to implement the additional metrics.
    
    Test: tools/diff_test_trace_processor.py \ out/android/trace_processor_shell \
    --name-filter '.*binder.*'
    Bug: 267312501
    Change-Id: I5651308177ed17918be94e6fe538257eb428c562

--- test/trace_processor/diff_tests/android/tests.py
+++ test/trace_processor/diff_tests/android/tests.py
@@ -110,3 +110,89 @@ class Android(TestSuite):
         WHERE t.name = 'DeviceStateChanged';
         """,
         out=Path('android_system_property_slice.out'))
+
+  def test_binder_sync_binder_metrics(self):
+    return DiffTestBlueprint(
+        trace=DataPath('android_binder_metric_trace.atr'),
+        query="""
+        SELECT IMPORT('android.binder');
+        SELECT
+          aidl_name,
+          binder_txn_id,
+          client_process,
+          client_thread,
+          client_upid,
+          client_utid,
+          is_main_thread,
+          client_ts,
+          client_dur,
+          binder_reply_id,
+          server_process,
+          server_thread,
+          server_upid,
+          server_utid,
+          server_ts,
+          server_dur
+        FROM android_sync_binder_metrics_by_txn
+        WHERE binder_txn_id = 34382
+        ORDER BY client_ts
+        LIMIT 1;
+      """,
+        out=Csv("""
+      "aidl_name","binder_txn_id","client_process","client_thread","client_upid","client_utid","is_main_thread","client_ts","client_dur","binder_reply_id","server_process","server_thread","server_upid","server_utid","server_ts","server_dur"
+      "AIDL::java::ISensorPrivacyManager::isSensorPrivacyEnabled::server",34382,"/system/bin/audioserver","audioserver",281,281,1,25505818197,3125407,34383,"system_server","binder:641_4",311,539,25505891588,3000749
+      """))
+
+  def test_binder_sync_binder_thread_state(self):
+    return DiffTestBlueprint(
+        trace=DataPath('android_binder_metric_trace.atr'),
+        query="""
+      SELECT IMPORT('android.binder');
+      SELECT
+        binder_txn_id,
+        binder_reply_id,
+        thread_state_type,
+        thread_state,
+        thread_state_dur,
+        thread_state_count
+      FROM android_sync_binder_thread_state_by_txn
+      WHERE binder_txn_id = 34382
+      ORDER BY thread_state_dur;
+      """,
+        out=Csv("""
+      "binder_txn_id","binder_reply_id","thread_state_type","thread_state","thread_state_dur","thread_state_count"
+      34382,34383,"binder_reply","R+",10030,1
+      34382,34383,"binder_txn","Running",26597,2
+      34382,34383,"binder_txn","R",38947,1
+      34382,34383,"binder_reply","Running",533663,3
+      34382,34383,"binder_reply","D",864664,1
+      34382,34383,"binder_reply","R",1592392,1
+      34382,34383,"binder_txn","S",3059863,1
+      """))
+
+  def test_binder_sync_binder_blocked_function(self):
+    return DiffTestBlueprint(
+        trace=DataPath('android_binder_metric_trace.atr'),
+        query="""
+      SELECT IMPORT('android.binder');
+      SELECT
+        binder_txn_id,
+        binder_reply_id,
+        thread_state_type,
+        blocked_function,
+        blocked_function_dur,
+        blocked_function_count
+      FROM android_sync_binder_blocked_functions_by_txn
+      WHERE binder_txn_id = 34382
+      ORDER BY blocked_function_dur;
+      """,
+        out=Csv("""
+      "binder_txn_id","binder_reply_id","thread_state_type","blocked_function","blocked_function_dur","blocked_function_count"
+      34382,34383,"binder_reply","filemap_fault",864664,1
+      """))
+
+  def test_binder_metric(self):
+    return DiffTestBlueprint(
+        trace=DataPath('android_binder_metric_trace.atr'),
+        query=Metric('android_binder'),
+        out=Path('android_binder_metric.out'))
