842d3611bb12c3efbdc9d48527f7efe77f61080a
Lalit Maganti <lalitm@google.com>
2023-05-26 01:30:37 +0100

    tp: introduce PerfettoSqlEngine intermediary
    This CL introduces PerfettoSqlEngine as an intermediary engine before
    SqliteEngine and consolidates anything which belongs there into the
    class (e.g. registering SqlFunction instances, SqliteTable etc).
    
    Bug: 261195778
    Change-Id: I6b4dbd27e5b80fb4a41cd536c49b3d4b5ce89706

--- src/trace_processor/trace_processor_impl.cc
+++ src/trace_processor/trace_processor_impl.cc
@@ -74,6 +74,7 @@
 #include "src/trace_processor/prelude/table_functions/table_function.h"
 #include "src/trace_processor/prelude/table_functions/view.h"
 #include "src/trace_processor/prelude/tables_views/tables_views.h"
+#include "src/trace_processor/sqlite/perfetto_sql_engine.h"
 #include "src/trace_processor/sqlite/scoped_db.h"
 #include "src/trace_processor/sqlite/sql_stats_table.h"
 #include "src/trace_processor/sqlite/sqlite_table.h"
@@ -107,7 +108,7 @@ const char kAllTablesQuery[] =
     "* FROM sqlite_temp_master)";
 
 template <typename SqlFunction, typename Ptr = typename SqlFunction::Context*>
-void RegisterFunction(SqliteEngine* engine,
+void RegisterFunction(PerfettoSqlEngine* engine,
                       const char* name,
                       int argc,
                       Ptr context = nullptr,
@@ -239,7 +240,7 @@ std::vector<std::string> SanitizeMetricMountPaths(
 }
 
 void SetupMetrics(TraceProcessor* tp,
-                  SqliteEngine* engine,
+                  PerfettoSqlEngine* engine,
                   std::vector<metrics::SqlMetricFile>* sql_metrics,
                   const std::vector<std::string>& extension_paths) {
   const std::vector<std::string> sanitized_extension_paths =
@@ -281,8 +282,9 @@ void SetupMetrics(TraceProcessor* tp,
   // functions are supported.
   {
     auto ret = sqlite3_create_function_v2(
-        engine->db(), "RepeatedField", 1, SQLITE_UTF8, nullptr, nullptr,
-        metrics::RepeatedFieldStep, metrics::RepeatedFieldFinal, nullptr);
+        engine->sqlite_engine()->db(), "RepeatedField", 1, SQLITE_UTF8, nullptr,
+        nullptr, metrics::RepeatedFieldStep, metrics::RepeatedFieldFinal,
+        nullptr);
     if (ret)
       PERFETTO_FATAL("Error initializing RepeatedField");
   }
@@ -298,137 +300,6 @@ void InsertIntoTraceMetricsTable(sqlite3* db, const std::string& metric_name) {
     PERFETTO_ELOG("Error registering table: %s", insert_error);
     sqlite3_free(insert_error);
   }
-}
-
-void IncrementCountForStmt(sqlite3_stmt* stmt,
-                           IteratorImpl::StmtMetadata* metadata) {
-  metadata->statement_count++;
-
-  // If the stmt is already done, it clearly didn't have any output.
-  if (sqlite_utils::IsStmtDone(stmt))
-    return;
-
-  if (sqlite3_column_count(stmt) == 1) {
-    sqlite3_value* value = sqlite3_column_value(stmt, 0);
-
-    // If the "VOID" pointer associated to the return value is not null,
-    // that means this is a function which is forced to return a value
-    // (because all functions in SQLite have to) but doesn't actually
-    // wait to (i.e. it wants to be treated like CREATE TABLE or similar).
-    // Because of this, ignore the return value of this function.
-    // See |WrapSqlFunction| for where this is set.
-    if (sqlite3_value_pointer(value, "VOID") != nullptr) {
-      return;
-    }
-
-    // If the statement only has a single column and that column is named
-    // "suppress_query_output", treat it as a statement without output for
-    // accounting purposes. This allows an escape hatch for cases where the
-    // user explicitly wants to ignore functions as having output.
-    if (strcmp(sqlite3_column_name(stmt, 0), "suppress_query_output") == 0) {
-      return;
-    }
-  }
-
-  // Otherwise, the statement has output and so increment the count.
-  metadata->statement_count_with_output++;
-}
-
-base::Status PrepareAndStepUntilLastValidStmt(
-    sqlite3* db,
-    const std::string& sql,
-    ScopedStmt* output_stmt,
-    IteratorImpl::StmtMetadata* metadata) {
-  ScopedStmt prev_stmt;
-  // A sql string can contain several statements. Some of them might be comment
-  // only, e.g. "SELECT 1; /* comment */; SELECT 2;". Here we process one
-  // statement on each iteration. SQLite's sqlite_prepare_v2 (wrapped by
-  // PrepareStmt) returns on each iteration a pointer to the unprocessed string.
-  //
-  // Unfortunately we cannot call PrepareStmt and tokenize all statements
-  // upfront because sqlite_prepare_v2 also semantically checks the statement
-  // against the schema. In some cases statements might depend on the execution
-  // of previous ones (e.e. CREATE VIEW x; SELECT FROM x; DELETE VIEW x;).
-  //
-  // Also, unfortunately, we need to PrepareStmt to find out if a statement is a
-  // comment or a real statement.
-  //
-  // The logic here is the following:
-  //  - We invoke PrepareStmt on each statement.
-  //  - If the statement is a comment we simply skip it.
-  //  - If the statement is valid, we step once to make sure side effects take
-  //    effect.
-  //  - If we encounter a valid statement afterwards, we step internally through
-  //    all rows of the previous one. This ensures that any further side effects
-  //    take hold *before* we step into the next statement.
-  //  - Once no further non-comment statements are encountered, we return an
-  //    iterator to the last valid statement.
-  for (const char* rem_sql = sql.c_str(); rem_sql && rem_sql[0];) {
-    ScopedStmt cur_stmt;
-    {
-      PERFETTO_TP_TRACE(metatrace::Category::QUERY, "QUERY_PREPARE");
-      const char* tail = nullptr;
-      RETURN_IF_ERROR(sqlite_utils::PrepareStmt(db, rem_sql, &cur_stmt, &tail));
-      rem_sql = tail;
-    }
-
-    // The only situation where we'd have an ok status but also no prepared
-    // statement is if the statement of SQL we parsed was a pure comment. In
-    // this case, just continue to the next statement.
-    if (!cur_stmt)
-      continue;
-
-    // Before stepping into |cur_stmt|, we need to finish iterating through
-    // the previous statement so we don't have two clashing statements (e.g.
-    // SELECT * FROM v and DROP VIEW v) partially stepped into.
-    if (prev_stmt) {
-      PERFETTO_TP_TRACE(metatrace::Category::QUERY, "STMT_STEP_UNTIL_DONE",
-                        [&prev_stmt](metatrace::Record* record) {
-                          auto expanded_sql =
-                              sqlite_utils::ExpandedSqlForStmt(*prev_stmt);
-                          record->AddArg("SQL", expanded_sql.get());
-                        });
-      RETURN_IF_ERROR(sqlite_utils::StepStmtUntilDone(prev_stmt.get()));
-    }
-
-    PERFETTO_DLOG("Executing statement: %s", sqlite3_sql(*cur_stmt));
-
-    {
-      PERFETTO_TP_TRACE(metatrace::Category::TOPLEVEL, "STMT_FIRST_STEP",
-                        [&cur_stmt](metatrace::Record* record) {
-                          auto expanded_sql =
-                              sqlite_utils::ExpandedSqlForStmt(*cur_stmt);
-                          record->AddArg("SQL", expanded_sql.get());
-                        });
-
-      // Now step once into |cur_stmt| so that when we prepare the next statment
-      // we will have executed any dependent bytecode in this one.
-      int err = sqlite3_step(*cur_stmt);
-      if (err != SQLITE_ROW && err != SQLITE_DONE) {
-        return base::ErrStatus(
-            "%s", sqlite_utils::FormatErrorMessage(
-                      prev_stmt.get(), base::StringView(sql), db, err)
-                      .c_message());
-      }
-    }
-
-    // Increment the neecessary counts for the statement.
-    IncrementCountForStmt(cur_stmt.get(), metadata);
-
-    // Propogate the current statement to the next iteration.
-    prev_stmt = std::move(cur_stmt);
-  }
-
-  // If we didn't manage to prepare a single statment, that means everything
-  // in the SQL was treated as a comment.
-  if (!prev_stmt)
-    return base::ErrStatus("No valid SQL to run");
-
-  // Update the output statment and column count.
-  *output_stmt = std::move(prev_stmt);
-  metadata->column_count =
-      static_cast<uint32_t>(sqlite3_column_count(output_stmt->get()));
-  return base::OkStatus();
 }
 
 const char* TraceTypeToString(TraceType trace_type) {
@@ -456,7 +327,7 @@ const char* TraceTypeToString(TraceType trace_type) {
 }
 
 // Register SQL functions only used in local development instances.
-void RegisterDevFunctions(SqliteEngine* engine) {
+void RegisterDevFunctions(PerfettoSqlEngine* engine) {
   RegisterFunction<WriteFile>(engine, "WRITE_FILE", 2);
 }
 
@@ -513,7 +384,7 @@ TraceProcessorImpl::TraceProcessorImpl(const Config& cfg)
     context_.content_analyzer.reset(new ProtoContentAnalyzer(&context_));
   }
 
-  sqlite3_str_split_init(engine_.db());
+  sqlite3_str_split_init(engine_.sqlite_engine()->db());
   RegisterAdditionalModules(&context_);
 
   // New style function registration.
@@ -537,10 +408,11 @@ TraceProcessorImpl::TraceProcessorImpl(const Config& cfg)
   RegisterFunction<CreateViewFunction>(
       &engine_, "CREATE_VIEW_FUNCTION", 3,
       std::unique_ptr<CreateViewFunction::Context>(
-          new CreateViewFunction::Context{engine_.db()}));
-  RegisterFunction<Import>(&engine_, "IMPORT", 1,
-                           std::unique_ptr<Import::Context>(new Import::Context{
-                               engine_.db(), this, &sql_modules_}));
+          new CreateViewFunction::Context{engine_.sqlite_engine()->db()}));
+  RegisterFunction<Import>(
+      &engine_, "IMPORT", 1,
+      std::unique_ptr<Import::Context>(new Import::Context{
+          engine_.sqlite_engine()->db(), this, &sql_modules_}));
   RegisterFunction<ToFtrace>(
       &engine_, "TO_FTRACE", 1,
       std::unique_ptr<ToFtrace::Context>(new ToFtrace::Context{
@@ -549,20 +421,22 @@ TraceProcessorImpl::TraceProcessorImpl(const Config& cfg)
   // Old style function registration.
   // TODO(lalitm): migrate this over to using RegisterFunction once aggregate
   // functions are supported.
-  RegisterLastNonNullFunction(engine_.db());
-  RegisterValueAtMaxTsFunction(engine_.db());
+  RegisterLastNonNullFunction(engine_.sqlite_engine()->db());
+  RegisterValueAtMaxTsFunction(engine_.sqlite_engine()->db());
   {
     base::Status status = RegisterStackFunctions(&engine_, &context_);
     if (!status.ok())
       PERFETTO_ELOG("%s", status.c_message());
   }
   {
-    base::Status status = PprofFunctions::Register(engine_.db(), &context_);
+    base::Status status =
+        PprofFunctions::Register(engine_.sqlite_engine()->db(), &context_);
     if (!status.ok())
       PERFETTO_ELOG("%s", status.c_message());
   }
   {
-    base::Status status = LayoutFunctions::Register(engine_.db(), &context_);
+    base::Status status =
+        LayoutFunctions::Register(engine_.sqlite_engine()->db(), &context_);
     if (!status.ok())
       PERFETTO_ELOG("%s", status.c_message());
   }
@@ -575,20 +449,20 @@ TraceProcessorImpl::TraceProcessorImpl(const Config& cfg)
   const TraceStorage* storage = context_.storage.get();
 
   // Operator tables.
-  engine_.RegisterVirtualTableModule<SpanJoinOperatorTable>(
+  engine_.sqlite_engine()->RegisterVirtualTableModule<SpanJoinOperatorTable>(
       "span_join", storage, SqliteTable::TableType::kExplicitCreate, false);
-  engine_.RegisterVirtualTableModule<SpanJoinOperatorTable>(
+  engine_.sqlite_engine()->RegisterVirtualTableModule<SpanJoinOperatorTable>(
       "span_left_join", storage, SqliteTable::TableType::kExplicitCreate,
       false);
-  engine_.RegisterVirtualTableModule<SpanJoinOperatorTable>(
+  engine_.sqlite_engine()->RegisterVirtualTableModule<SpanJoinOperatorTable>(
       "span_outer_join", storage, SqliteTable::TableType::kExplicitCreate,
       false);
-  engine_.RegisterVirtualTableModule<WindowOperatorTable>(
+  engine_.sqlite_engine()->RegisterVirtualTableModule<WindowOperatorTable>(
       "window", storage, SqliteTable::TableType::kExplicitCreate, true);
-  RegisterCreateViewFunctionModule(&engine_);
+  RegisterCreateViewFunctionModule(engine_.sqlite_engine());
 
   // Initalize the tables and views in the prelude.
-  InitializePreludeTablesViews(engine_.db());
+  InitializePreludeTablesViews(engine_.sqlite_engine()->db());
 
   auto stdlib_modules = GetStdlibModules();
   for (auto module_it = stdlib_modules.GetIterator(); module_it; ++module_it) {
@@ -601,9 +475,9 @@ TraceProcessorImpl::TraceProcessorImpl(const Config& cfg)
   SetupMetrics(this, &engine_, &sql_metrics_, cfg.skip_builtin_metric_paths);
 
   // Legacy tables.
-  engine_.RegisterVirtualTableModule<SqlStatsTable>(
+  engine_.sqlite_engine()->RegisterVirtualTableModule<SqlStatsTable>(
       "sqlstats", storage, SqliteTable::TableType::kEponymousOnly, false);
-  engine_.RegisterVirtualTableModule<StatsTable>(
+  engine_.sqlite_engine()->RegisterVirtualTableModule<StatsTable>(
       "stats", storage, SqliteTable::TableType::kEponymousOnly, false);
 
   // Tables dynamically generated at query time.
@@ -750,7 +624,8 @@ void TraceProcessorImpl::Flush() {
       context_.storage->InternString(TraceTypeToString(context_.trace_type));
   context_.metadata_tracker->SetMetadata(metadata::trace_type,
                                          Variadic::String(trace_type_id));
-  BuildBoundsTable(engine_.db(), context_.storage->GetTraceTimestampBoundsNs());
+  BuildBoundsTable(engine_.sqlite_engine()->db(),
+                   context_.storage->GetTraceTimestampBoundsNs());
 }
 
 void TraceProcessorImpl::NotifyEndOfFile() {
@@ -788,7 +663,8 @@ void TraceProcessorImpl::NotifyEndOfFile() {
   // TraceProcessorStorageImpl::NotifyEndOfFile, this will be counted in
   // trace bounds: this is important for parsers like ninja which wait until
   // the end to flush all their data.
-  BuildBoundsTable(engine_.db(), context_.storage->GetTraceTimestampBoundsNs());
+  BuildBoundsTable(engine_.sqlite_engine()->db(),
+                   context_.storage->GetTraceTimestampBoundsNs());
 
   TraceProcessorStorageImpl::DestroyContext();
 }
@@ -832,23 +708,18 @@ Iterator TraceProcessorImpl::ExecuteQuery(const std::string& sql) {
       context_.storage->mutable_sql_stats()->RecordQueryBegin(
           sql, base::GetWallTimeNs().count());
 
-  ScopedStmt stmt;
-  IteratorImpl::StmtMetadata metadata;
-  base::Status status =
-      PrepareAndStepUntilLastValidStmt(engine_.db(), sql, &stmt, &metadata);
-  PERFETTO_DCHECK((status.ok() && stmt) || (!status.ok() && !stmt));
-
+  base::StatusOr<PerfettoSqlEngine::ExecutionResult> result =
+      engine_.ExecuteUntilLastStatement(sql);
   std::unique_ptr<IteratorImpl> impl(
-      new IteratorImpl(this, engine_.db(), status, std::move(stmt),
-                       std::move(metadata), sql_stats_row));
+      new IteratorImpl(this, std::move(result), sql_stats_row));
   return Iterator(std::move(impl));
 }
 
 void TraceProcessorImpl::InterruptQuery() {
-  if (!engine_.db())
+  if (!engine_.sqlite_engine()->db())
     return;
   query_interrupted_.store(true);
-  sqlite3_interrupt(engine_.db());
+  sqlite3_interrupt(engine_.sqlite_engine()->db());
 }
 
 bool TraceProcessorImpl::IsRootMetricField(const std::string& metric_name) {
@@ -940,7 +811,7 @@ base::Status TraceProcessorImpl::RegisterMetric(const std::string& path,
           prev_path.c_str(), path.c_str(), metric.proto_field_name->c_str());
     }
 
-    InsertIntoTraceMetricsTable(engine_.db(), no_ext_name);
+    InsertIntoTraceMetricsTable(engine_.sqlite_engine()->db(), no_ext_name);
   }
 
   sql_metrics_.emplace_back(metric);
