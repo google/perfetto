e922fa2ab7222d077d6bfd90f32a6e57ffadb961
Arun Johnson <arunjohnson@google.com>
2023-06-09 00:30:40 +0000

    Android codec_metrics and tests to perfetto
    Bug: 286081370
    Change-Id: I153dead30ea86501699b010634a20c35140571bf

---
+++ test/trace_processor/diff_tests/codecs/tests.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# Copyright (C) 2023 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License a
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from python.generators.diff_tests.testing import Path, DataPath, Metric
+from python.generators.diff_tests.testing import Csv, Json, TextProto
+from python.generators.diff_tests.testing import DiffTestBlueprint
+from python.generators.diff_tests.testing import TestSuite
+
+
+class Codecs(TestSuite):
+
+  def test_codecs_framedecoder_metric(self):
+    return DiffTestBlueprint(
+        trace=DataPath('codec-framedecoder-trace.pftrace'),
+        query=Metric('codec_metrics'),
+        out=Path('codec-framedecoder-trace.out'))
