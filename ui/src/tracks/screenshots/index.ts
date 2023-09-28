ec8171e40c5b560abca2b5fd85c6bd8fc324ee7f
Kartar Singh <kartarsingh@google.com>
2023-09-28 12:06:44 +0100

    [ui] Add screenshot track
    Add ui support for visualizing screenshots in traces. In followup
    change we will add a python script for capturing trace and screencapture
    video simultaneously and then merging into a single trace. Using the
    traces captured using the script and this UI feature we should be able
    to see screenshots in perfetto like this: http://screen/BcWEtzCfAt7kb6m.
    
    Bug: b/287934064
    Change-Id: I01e528a9e12b8ceab72fdbec1f1ece9ce977a5cc

---
+++ ui/src/tracks/screenshots/index.ts
@@ -0,0 +1,94 @@
+// Copyright (C) 2023 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import {v4 as uuidv4} from 'uuid';
+
+import {AddTrackArgs} from '../../common/actions';
+import {Engine} from '../../common/engine';
+import {PrimaryTrackSortKey} from '../../common/state';
+import {
+  NamedSliceTrackTypes,
+} from '../../frontend/named_slice_track';
+import {NewTrackArgs, Track} from '../../frontend/track';
+import {Plugin, PluginContext, PluginInfo} from '../../public';
+import {
+  CustomSqlDetailsPanelConfig,
+  CustomSqlTableDefConfig,
+  CustomSqlTableSliceTrack,
+} from '../custom_sql_table_slices';
+
+import {
+  ScreenshotTab,
+} from './screenshot_panel';
+
+export {Data} from '../chrome_slices';
+
+class ScreenshotsTrack extends CustomSqlTableSliceTrack<NamedSliceTrackTypes> {
+  static readonly kind = 'dev.perfetto.ScreenshotsTrack';
+  static create(args: NewTrackArgs): Track {
+    return new ScreenshotsTrack(args);
+  }
+
+  getSqlDataSource(): CustomSqlTableDefConfig {
+    return {
+      sqlTableName: 'android_screenshots',
+      columns: ['*'],
+    };
+  }
+
+  getDetailsPanel(): CustomSqlDetailsPanelConfig {
+    return {
+      kind: ScreenshotTab.kind,
+      config: {
+        sqlTableName: this.tableName,
+        title: 'Screenshots',
+      },
+    };
+  }
+}
+
+export type DecideTracksResult = {
+  tracksToAdd: AddTrackArgs[],
+};
+
+export async function decideTracks(engine: Engine):
+    Promise<DecideTracksResult> {
+  const result: DecideTracksResult = {
+    tracksToAdd: [],
+  };
+
+  await engine.query(`SELECT IMPORT('android.screenshots')`);
+
+  result.tracksToAdd.push({
+    id: uuidv4(),
+    engineId: engine.id,
+    kind: ScreenshotsTrack.kind,
+    trackSortKey: PrimaryTrackSortKey.ASYNC_SLICE_TRACK,
+    name: 'Screenshots',
+    config: {},
+    trackGroup: undefined,
+  });
+  return result;
+}
+
+class ScreenshotsPlugin implements Plugin {
+  onActivate(ctx: PluginContext): void {
+    ctx.registerTrack(ScreenshotsTrack);
+  }
+}
+
+export const plugin: PluginInfo = {
+  pluginId: 'perfetto.Screenshots',
+  plugin: ScreenshotsPlugin,
+};
