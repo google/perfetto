fcfd5b8d2dad240e0690ed5ecfc0d44844fcfd02
Steve Golton <stevegolton@google.com>
2023-12-04 14:53:37 +0000

    [ui] Merge expected and actual frames plugins into one
    Change-Id: I3211d241059e42443b847babfb45dd2ad55dbc3e

--- ui/src/controller/aggregation/frame_aggregation_controller.ts
+++ ui/src/controller/aggregation/frame_aggregation_controller.ts
@@ -17,7 +17,7 @@ import {pluginManager} from '../../common/plugins';
 import {Area, Sorting} from '../../common/state';
 import {globals} from '../../frontend/globals';
 import {Engine} from '../../trace_processor/engine';
-import {ACTUAL_FRAMES_SLICE_TRACK_KIND} from '../../tracks/actual_frames';
+import {ACTUAL_FRAMES_SLICE_TRACK_KIND} from '../../tracks/frames';
 
 import {AggregationController} from './aggregation_controller';
 
