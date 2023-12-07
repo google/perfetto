3dd34fb3db1e9eae843a821bb148ccfb504b1b9f
Andrew Levans <121060410+ALevansSamsung@users.noreply.github.com>
2023-12-07 13:52:06 -0600

    Add some exceptions to a hack in Perfetto (#93)

--- ui/src/controller/record_controller.ts
+++ ui/src/controller/record_controller.ts
@@ -129,7 +129,8 @@ export function toPbtxt(configBuffer: Uint8Array): string {
         value.startsWith('STAT_') || value.startsWith('LID_') ||
         value.startsWith('BATTERY_COUNTER_') || value === 'DISCARD' ||
         value === 'RING_BUFFER' || value === 'BACKGROUND' ||
-        value === 'USER_INITIATED';
+        value === 'USER_INITIATED' ||
+        value === 'START_TRACING';
   }
   // Since javascript doesn't have 64 bit numbers when converting protos to
   // json the proto library encodes them as strings. This is lossy since
@@ -144,6 +145,7 @@ export function toPbtxt(configBuffer: Uint8Array): string {
       'samplingIntervalBytes',
       'shmemSizeBytes',
       'timestampUnitMultiplier',
+      'counterPeriodNs',
     ].includes(key);
   }
   function* message(msg: {}, indent: number): IterableIterator<string> {
