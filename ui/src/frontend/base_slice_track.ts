b3d37b3bf7e5a31b13513f0a5c6b1446c2b636b7
Steve Golton <stevegolton@google.com>
2024-01-08 13:09:53 +0000

    [ui] Fix flag grammar
    Change-Id: I52a16b6de7ccf0c48b46cb45e8bf03fff00bb948

--- ui/src/frontend/base_slice_track.ts
+++ ui/src/frontend/base_slice_track.ts
@@ -56,8 +56,8 @@ const INCOMPLETE_SLICE_WIDTH_PX = 20;
 
 export const CROP_INCOMPLETE_SLICE_FLAG = featureFlags.register({
   id: 'cropIncompleteSlice',
-  name: 'Crop incomplete Slice',
-  description: 'Display incomplete slice in short form',
+  name: 'Crop incomplete slices',
+  description: 'Display incomplete slices in short form',
   defaultValue: false,
 });
 
