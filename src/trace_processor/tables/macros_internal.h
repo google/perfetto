1a4ac4b15ec2115dbc8787ecda4fe31118383e5e
Lalit Maganti <lalitm@google.com>
2024-02-13 16:24:25 +0000

    tp: attempt binary size reductions in python tables
    Try and push more work (i.e. creation of vectors) to the superclass
    instead which should hopefully save some binary size.
    
    Change-Id: I763f1b4c6acf26da2772901b64cd213b1312c31a

--- src/trace_processor/tables/macros_internal.h
+++ src/trace_processor/tables/macros_internal.h
@@ -19,15 +19,20 @@
 
 #include <cstddef>
 #include <cstdint>
+#include <initializer_list>
 #include <type_traits>
 #include <utility>
 #include <vector>
 
+#include "perfetto/base/compiler.h"
 #include "perfetto/base/logging.h"
+#include "perfetto/trace_processor/ref_counted.h"
 #include "src/trace_processor/containers/bit_vector.h"
 #include "src/trace_processor/containers/row_map.h"
 #include "src/trace_processor/containers/string_pool.h"
 #include "src/trace_processor/db/column.h"
+#include "src/trace_processor/db/column/data_layer.h"
+#include "src/trace_processor/db/column/selector_overlay.h"
 #include "src/trace_processor/db/column_storage.h"
 #include "src/trace_processor/db/column_storage_overlay.h"
 #include "src/trace_processor/db/table.h"
@@ -100,17 +105,17 @@ class MacroTable : public Table {
 
   ~MacroTable() override;
 
-  void UpdateOverlaysAfterParentInsert() {
+  PERFETTO_NO_INLINE void UpdateOverlaysAfterParentInsert() {
     CopyLastInsertFrom(parent_->overlays());
   }
 
-  void UpdateSelfOverlayAfterInsert() {
+  PERFETTO_NO_INLINE void UpdateSelfOverlayAfterInsert() {
     IncrementRowCountAndAddToLastOverlay();
   }
 
-  static std::vector<ColumnLegacy> CopyColumnsFromParentOrAddRootColumns(
-      MacroTable* self,
-      const MacroTable* parent) {
+  PERFETTO_NO_INLINE static std::vector<ColumnLegacy>
+  CopyColumnsFromParentOrAddRootColumns(MacroTable* self,
+                                        const MacroTable* parent) {
     std::vector<ColumnLegacy> columns;
     if (parent) {
       for (const ColumnLegacy& col : parent->columns()) {
@@ -121,6 +126,20 @@ class MacroTable : public Table {
       columns.emplace_back("type", &self->type_, ColumnLegacy::kNonNull, 1, 0);
     }
     return columns;
+  }
+
+  PERFETTO_NO_INLINE void OnConstructionCompletedRegularConstructor(
+      std::initializer_list<RefPtr<column::DataLayer>> storage_layers,
+      std::initializer_list<RefPtr<column::DataLayer>> null_layers) {
+    std::vector<RefPtr<column::DataLayer>> overlay_layers(
+        OverlayCount(parent_) + 1);
+    for (uint32_t i = 0; i < overlay_layers.size() - 1; ++i) {
+      PERFETTO_CHECK(overlays()[i].row_map().IsBitVector());
+      overlay_layers[i].reset(new column::SelectorOverlay(
+          overlays()[i].row_map().GetIfBitVector()));
+    }
+    Table::OnConstructionCompleted(storage_layers, null_layers,
+                                   std::move(overlay_layers));
   }
 
   static uint32_t OverlayCount(const MacroTable* parent) {
@@ -147,8 +166,8 @@ class MacroTable : public Table {
   ColumnStorage<StringPool::Id> type_;
 
  private:
-  static std::vector<ColumnStorageOverlay> EmptyOverlaysFromParent(
-      const MacroTable* parent) {
+  PERFETTO_NO_INLINE static std::vector<ColumnStorageOverlay>
+  EmptyOverlaysFromParent(const MacroTable* parent) {
     std::vector<ColumnStorageOverlay> overlays(
         parent ? parent->overlays().size() : 0);
     for (auto& overlay : overlays) {
@@ -157,9 +176,9 @@ class MacroTable : public Table {
     overlays.emplace_back();
     return overlays;
   }
-  static std::vector<ColumnStorageOverlay> SelectedOverlaysFromParent(
-      const macros_internal::MacroTable& parent,
-      const RowMap& rm) {
+  PERFETTO_NO_INLINE static std::vector<ColumnStorageOverlay>
+  SelectedOverlaysFromParent(const macros_internal::MacroTable& parent,
+                             const RowMap& rm) {
     std::vector<ColumnStorageOverlay> overlays;
     for (const auto& overlay : parent.overlays()) {
       overlays.emplace_back(overlay.SelectRows(rm));
