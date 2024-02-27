eda90924eb07cb7bc23403367c9d28f6e08f17be
Primiano Tucci <primiano@google.com>
2024-02-27 16:41:50 +0000

    CI: remove android-arm-asan bot
    - We have asan coverage on linux.
    - This bot has never caught anything interesting.
    - Because it runs on an old arm-on-x86 emulator,
      with asan on top, it's extremely slow and causes
      flakes.
    
    Change-Id: I465b4611f09c012d1cbf39f52ee6129f20734480

--- infra/ci/frontend/static/script.js
+++ infra/ci/frontend/static/script.js
@@ -28,7 +28,6 @@ const JOB_TYPES = [
   { id: 'linux-clang-x86_64-bazel', label: 'bazel' },
   { id: 'ui-clang-x86_64-release', label: 'rel' },
   { id: 'android-clang-arm-release', label: 'rel' },
-  { id: 'android-clang-arm-asan', label: 'asan' },
 ];
 
 const STATS_LINK =
@@ -193,7 +192,7 @@ var CLsPageRenderer = {
                 m('td[rowspan=4]', 'Status'),
                 m('td[rowspan=4]', 'Owner'),
                 m('td[rowspan=4]', 'Updated'),
-                m('td[colspan=11]', 'Bots'),
+                m('td[colspan=10]', 'Bots'),
               ),
               m('tr',
                 m('td[colspan=9]', 'linux'),
@@ -203,7 +202,7 @@ var CLsPageRenderer = {
                 m('td', 'gcc7'),
                 m('td[colspan=7]', 'clang'),
                 m('td[colspan=1]', 'ui'),
-                m('td[colspan=2]', 'clang-arm'),
+                m('td[colspan=1]', 'clang-arm'),
               ),
               m('tr#cls_header',
                 JOB_TYPES.map(job => m(`td#${job.id}`, job.label))
