2ee90c75c99f8059632b77ee481662c84badc0fb
Steve Golton <stevegolton@google.com>
2024-03-21 16:00:53 +0000

    ui: Add getEventSet() to track API
    Change-Id: If8e51ef5936a39f2437c56813ad818faa2b0cdd4

--- ui/src/public/index.ts
+++ ui/src/public/index.ts
@@ -21,6 +21,7 @@ import {ColorScheme} from '../core/colorizer';
 import {LegacySelection} from '../common/state';
 import {PanelSize} from '../frontend/panel';
 import {EngineProxy} from '../trace_processor/engine';
+import {UntypedEventSet} from '../common/event_set';
 
 export {EngineProxy} from '../trace_processor/engine';
 export {
@@ -208,6 +209,11 @@ export interface Track {
   onMouseMove?(position: {x: number; y: number}): void;
   onMouseClick?(position: {x: number; y: number}): boolean;
   onMouseOut?(): void;
+
+  /**
+   * Optional: Get the event set that represents this track's data.
+   */
+  getEventSet?(): UntypedEventSet;
 }
 
 // A definition of a track, including a renderer implementation and metadata.
