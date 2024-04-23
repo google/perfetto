98fb7faa6b27a2f6304d83a361d19b1075760ff6
Kean Mariotti <keanmariotti@google.com>
2024-04-23 08:40:32 +0000

    protozero: allow top-level extension blocks
    Bug: 276433199
    Change-Id: Ica04a07f45662cac0ee5fa35767d3cc8baef86a6

--- src/protozero/protoc_plugin/protozero_plugin.cc
+++ src/protozero/protoc_plugin/protozero_plugin.cc
@@ -393,8 +393,9 @@ class GeneratorJob {
     for (int i = 0; i < source_->enum_type_count(); ++i)
       enums_.push_back(source_->enum_type(i));
 
-    if (source_->extension_count() > 0)
-      Abort("top-level extension blocks are not supported");
+    if (source_->extension_count() > 0) {
+      // TODO(b/336524288): emit field numbers
+    }
 
     for (const Descriptor* message : messages_) {
       for (int i = 0; i < message->enum_type_count(); ++i) {
