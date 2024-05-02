7adde2d700d5478570b69080c219731bb9ec92c4
Vilas Bhat <vilasbhat@google.com>
2024-05-02 09:44:35 -0700

    perfetto/ftrace: Introduce device_pm_callback_[start|end] ftrace events
    This event logs the start and end of device power management (PM)
    callback invocation and is useful for analyzing system suspend / resume
    latency in the kernel. Device in this context is any hardware component
    managed by a Linux kernel driver.
    
    Bug: 330333710
    Change-Id: Ifd46f940ec82095fd6fa867d0560b0ed79a9750f

--- src/trace_processor/importers/ftrace/ftrace_descriptors.cc
+++ src/trace_processor/importers/ftrace/ftrace_descriptors.cc
@@ -24,7 +24,7 @@ namespace perfetto {
 namespace trace_processor {
 namespace {
 
-std::array<FtraceMessageDescriptor, 504> descriptors{{
+std::array<FtraceMessageDescriptor, 506> descriptors{{
     {nullptr, 0, {}},
     {nullptr, 0, {}},
     {nullptr, 0, {}},
@@ -5563,6 +5563,28 @@ std::array<FtraceMessageDescriptor, 504> descriptors{{
             {},
             {"event", ProtoSchemaType::kString},
             {"timestamp", ProtoSchemaType::kUint64},
+        },
+    },
+    {
+        "device_pm_callback_start",
+        5,
+        {
+            {},
+            {"device", ProtoSchemaType::kString},
+            {"driver", ProtoSchemaType::kString},
+            {"parent", ProtoSchemaType::kString},
+            {"pm_ops", ProtoSchemaType::kString},
+            {"event", ProtoSchemaType::kInt32},
+        },
+    },
+    {
+        "device_pm_callback_end",
+        3,
+        {
+            {},
+            {"device", ProtoSchemaType::kString},
+            {"driver", ProtoSchemaType::kString},
+            {"error", ProtoSchemaType::kInt32},
         },
     },
 }};
