045bc1df0de9be365c7d0932246601ed758dc323
Steve Golton <stevegolton@google.com>
2024-05-03 14:52:25 +0100

    ui: Don't render incomplete slices in the middle of the trace
    In other words: only draw incomplete slices if they're the last slice
    at their depth
    
    Bug: 338028952
    Change-Id: Iade0c86e801c4eec36bbf91c63857470ffbffebf

--- ui/src/frontend/base_slice_track.ts
+++ ui/src/frontend/base_slice_track.ts
@@ -356,8 +356,8 @@ export abstract class BaseSliceTrack<
           id
           ${extraCols ? ',' + extraCols : ''}
         from (${this.getSqlSource()})
-        where dur = -1
         group by 1
+        having dur = -1
       `);
     }
     const incomplete = new Array<CastInternal<T['slice']>>(queryRes.numRows());
