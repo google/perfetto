a512570839b8d94664c7c71bcde46141666d8a1d
Carlos Caballero <carlscab@google.com>
2024-05-20 08:48:15 +0000

    Reapply "Refactor perf parsing"
    This reverts commit 1fe050dc3c3506d7d6ffa99b0f81cbee03364195.
    
    It is impossible that this patch could have broken that test. The new
    code is not run by the test and the code does not even run on device.
    
    Bug: b/341360168
    Bug: b/334978369
    Change-Id: I65cd1b9f54a4aa03a518a9f5baff9b8bb6bb0cc6

--- src/trace_processor/importers/perf/perf_session.h
+++ src/trace_processor/importers/perf/perf_session.h
@@ -20,7 +20,6 @@
 #include <sys/types.h>
 #include <cstddef>
 #include <cstdint>
-#include <optional>
 
 #include "perfetto/ext/base/flat_hash_map.h"
 #include "perfetto/ext/base/status_or.h"
@@ -29,15 +28,19 @@
 #include "src/trace_processor/importers/perf/perf_event.h"
 #include "src/trace_processor/importers/perf/perf_event_attr.h"
 
-namespace perfetto::trace_processor::perf_importer {
+namespace perfetto::trace_processor {
+
+class TraceProcessorContext;
+
+namespace perf_importer {
 
 // Helper to deal with perf_event_attr instances in a perf file.
 class PerfSession : public RefCounted {
  public:
   class Builder {
    public:
-    explicit Builder(uint32_t perf_session_id)
-        : perf_session_id_(perf_session_id) {}
+    Builder(TraceProcessorContext* context, uint32_t perf_session_id)
+        : context_(context), perf_session_id_(perf_session_id) {}
     base::StatusOr<RefPtr<PerfSession>> Build();
     Builder& AddAttrAndIds(perf_event_attr attr, std::vector<uint64_t> ids) {
       attr_with_ids_.push_back({std::move(attr), std::move(ids)});
@@ -50,6 +53,7 @@ class PerfSession : public RefCounted {
       std::vector<uint64_t> ids;
     };
 
+    TraceProcessorContext* const context_;
     uint32_t perf_session_id_;
     std::vector<PerfEventAttrWithIds> attr_with_ids_;
   };
@@ -83,6 +87,7 @@ class PerfSession : public RefCounted {
   bool has_single_perf_event_attr_;
 };
 
-}  // namespace perfetto::trace_processor::perf_importer
+}  // namespace perf_importer
+}  // namespace perfetto::trace_processor
 
 #endif  // SRC_TRACE_PROCESSOR_IMPORTERS_PERF_PERF_SESSION_H_
