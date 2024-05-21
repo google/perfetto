a3d7ede7c1ecc5eb76115e9427f147776d8688e7
Lalit Maganti <lalitm@google.com>
2024-05-21 14:31:50 +0100

    tp: improve handling of !usable constraints in BestIndex
    This CL changes how !usable constraints are handling to respect the
    SQLite documentation better: the documentation has changed since this
    behaviour was originally implemented.
    
    Specifically, we return errors more eagerly if !usable constraint is
    present for a required argument. This allows SQLite to know that we
    need the column to be present. We also return proper error messages
    if a required constraint is completely missing.
    
    See
    https://www.sqlite.org/vtab.html#enforcing_required_parameters_on_table_valued_functions
    for the documentation on this.
    
    Change-Id: If927e0a890e62cde7dd9c2114144028f25081589

--- src/trace_processor/perfetto_sql/intrinsics/operators/slice_mipmap_operator.cc
+++ src/trace_processor/perfetto_sql/intrinsics/operators/slice_mipmap_operator.cc
@@ -151,14 +151,15 @@ int SliceMipmapOperator::Disconnect(sqlite3_vtab* vtab) {
   return SQLITE_OK;
 }
 
-int SliceMipmapOperator::BestIndex(sqlite3_vtab*, sqlite3_index_info* info) {
-  base::Status status =
-      sqlite::utils::ValidateFunctionArguments(info, kArgCount, IsArgColumn);
-  if (!status.ok()) {
-    return SQLITE_CONSTRAINT;
-  }
-  if (info->nConstraint != kArgCount) {
-    return SQLITE_CONSTRAINT;
+int SliceMipmapOperator::BestIndex(sqlite3_vtab* vtab,
+                                   sqlite3_index_info* info) {
+  base::Status status;
+  int ret = sqlite::utils::ValidateFunctionArguments(info, kArgCount,
+                                                     IsArgColumn, status);
+  if (ret != SQLITE_OK) {
+    PERFETTO_CHECK(!status.ok());
+    sqlite::utils::SetError(vtab, status.c_message());
+    return ret;
   }
   return SQLITE_OK;
 }
