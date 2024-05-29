b59776f96022da51ef648d8ec052a634f00f3c06
Aaron Vaage <vaage@google.com>
2024-05-29 15:59:48 -0700

    Trace Redaction - Redact compact sched waking
    Goes through all the waking events in the compact sched and
    
      1. filter them (optionally adds the event to the output)
      2. modify the (optionally changes the pid and comm value)
    
    For some reason, this implementation is not compatible with thread
    merging. The problem is mostly in the thread merging implementation,
    so it has been disabled.
    
    Bug: 339504266
    Change-Id: I2f7f619a0dd7e975e37eb3ae14a783428b1cb3ab

--- src/trace_redaction/redact_sched_events.cc
+++ src/trace_redaction/redact_sched_events.cc
@@ -385,7 +385,8 @@ base::Status RedactSchedEvents::OnCompSched(
   };
 
   if (std::any_of(has_waking_fields.begin(), has_waking_fields.end(), IsTrue)) {
-    // TODO(vaage): Create and call TransformCompSchedWaking().
+    RETURN_IF_ERROR(
+        OnCompactSchedWaking(context, comp_sched, &intern_table, message));
   }
 
   // IMPORTANT: The intern table can only be added after switch and waking
@@ -516,6 +517,116 @@ base::Status RedactSchedEvents::OnCompSchedSwitch(
   return base::OkStatus();
 }
 
+base::Status RedactSchedEvents::OnCompactSchedWaking(
+    const Context& context,
+    protos::pbzero::FtraceEventBundle::CompactSched::Decoder& compact_sched,
+    InternTable* intern_table,
+    protos::pbzero::FtraceEventBundle::CompactSched* compact_sched_message)
+    const {
+  protozero::PackedVarInt var_comm_index;
+  protozero::PackedVarInt var_common_flags;
+  protozero::PackedVarInt var_pid;
+  protozero::PackedVarInt var_prio;
+  protozero::PackedVarInt var_target_cpu;
+  protozero::PackedVarInt var_timestamp;
+
+  // Time is expressed as delta time, for example:
+  //
+  //         Event: A          B     C      D
+  // Absolute Time: 20         30    35     41
+  //                |          |     |      |
+  //    Delta Time: 20         10    5      6
+  //
+  // When an event is removed, for example, event B, delta times are off:
+  //
+  //               Event:  A          *     C      D
+  //       Absolute Time: 20         30    35     41
+  //                       |          |     |      |
+  //          Delta Time: 20          *     5      6
+  //                       |                |      |
+  // Effective Abs. Time: 20               25     31
+  //               Error:  0               10     10
+  //
+  // To address this issue, delta times are added into a bucket. The bucket is
+  // drained each time an event is retained. If an event is dropped, its time
+  // is added to the bucket, but the bucket won't be drained until a retained
+  // event drains it.
+  uint64_t ts_bucket = 0;
+  uint64_t ts_absolute = 0;
+
+  std::string comm;
+
+  std::array<bool, 7> parse_errors = {!compact_sched.has_intern_table(),
+                                      false,
+                                      false,
+                                      false,
+                                      false,
+                                      false,
+                                      false};
+
+  // A note on readability, because the waking iterators are the primary focus,
+  // they won't have a "waking" prefix.
+  auto it_comm_index = compact_sched.waking_comm_index(&parse_errors.at(1));
+  auto it_common_flags = compact_sched.waking_common_flags(&parse_errors.at(2));
+  auto it_pid = compact_sched.waking_pid(&parse_errors.at(3));
+  auto it_prio = compact_sched.waking_prio(&parse_errors.at(4));
+  auto it_target_cpu = compact_sched.waking_target_cpu(&parse_errors.at(5));
+  auto it_timestamp = compact_sched.waking_timestamp(&parse_errors.at(6));
+
+  while (it_comm_index && it_common_flags && it_pid && it_prio &&
+         it_target_cpu && it_timestamp) {
+    ts_bucket += *it_timestamp;  // add time to the bucket
+    ts_absolute += *it_timestamp;
+
+    if (filter_->Includes(context, ts_absolute, *it_pid)) {
+      // Now that the waking event will be kept, it can be modified using the
+      // same rules as switch events.
+      auto pid = *it_pid;
+      comm.assign(intern_table->Find(*it_comm_index));
+      modifier_->Modify(context, ts_absolute, *it_target_cpu, &pid, &comm);
+
+      auto comm_it = intern_table->Push(comm.data(), comm.size());
+
+      var_comm_index.Append(comm_it);
+      var_common_flags.Append(*it_common_flags);
+      var_pid.Append(pid);
+      var_prio.Append(*it_prio);
+      var_target_cpu.Append(*it_target_cpu);
+      var_timestamp.Append(ts_bucket);
+
+      ts_bucket = 0;  // drain the whole bucket.
+    }
+
+    ++it_comm_index;
+    ++it_common_flags;
+    ++it_pid;
+    ++it_prio;
+    ++it_target_cpu;
+    ++it_timestamp;
+  }
+
+  if (std::any_of(parse_errors.begin(), parse_errors.end(), IsTrue)) {
+    return base::ErrStatus(
+        "RedactSchedEvents: failed to parse FtraceEventBundle::CompactSched.");
+  }
+
+  if (it_comm_index || it_common_flags || it_pid || it_prio || it_target_cpu ||
+      it_timestamp) {
+    return base::ErrStatus(
+        "RedactSchedEvents: uneven associative arrays in "
+        "FtraceEventBundle::CompactSched (waking).");
+  }
+
+  compact_sched_message->set_waking_comm_index(var_comm_index);
+  compact_sched_message->set_waking_common_flags(var_common_flags);
+  compact_sched_message->set_waking_pid(var_pid);
+  compact_sched_message->set_waking_prio(var_prio);
+  compact_sched_message->set_waking_target_cpu(var_target_cpu);
+  compact_sched_message->set_waking_timestamp(var_timestamp);
+
+  return base::OkStatus();
+}
+
 // Switch event transformation: Clear the comm value if the thread/process is
 // not part of the target packet.
 base::Status ClearComms::Modify(const Context& context,
@@ -530,6 +641,14 @@ base::Status ClearComms::Modify(const Context& context,
     comm->clear();
   }
 
+  return base::OkStatus();
+}
+
+base::Status DoNothing::Modify(const Context&,
+                               uint64_t,
+                               int32_t,
+                               int32_t*,
+                               std::string*) const {
   return base::OkStatus();
 }
 
