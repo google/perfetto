a069586e36b694d741cf4aff16408e8210db6c99
John Bates <jbates@google.com>
2024-05-29 23:31:45 +0000

    Add kgsl/gpu_frequency ftrace event support
    Bug: 343520578
    Change-Id: I4aec4992bdab11d69614f9a1264ae3057b139a58

--- src/tools/ftrace_proto_gen/ftrace_proto_gen.cc
+++ src/tools/ftrace_proto_gen/ftrace_proto_gen.cc
@@ -59,7 +59,8 @@ std::string EventNameToProtoFieldName(const std::string& group,
   // These groups have events where the name alone conflicts with an existing
   // proto:
   if (group == "sde" || group == "g2d" || group == "dpu" || group == "mali" ||
-      group == "lwis" || group == "samsung") {
+      group == "lwis" || group == "samsung" ||
+      (group == "kgsl" && name == "gpu_frequency")) {
     event_name = group + "_" + event_name;
   }
   return event_name;
