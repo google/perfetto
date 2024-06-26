56760ea45692c7119cc02a3526c515d573008fa7
Ivan Chong <ivankc@google.com>
2024-06-26 17:09:55 +0000

    bigtrace: Prototype multiple workers on a single machine
    - Use gRPC's round robin load balancing to distribute requests to a set
      of workers
    
    Change-Id: Ie2f8d0872b8cdfeaa545721f06bc5fe478e22149

--- src/bigtrace/orchestrator_main.cc
+++ src/bigtrace/orchestrator_main.cc
@@ -18,9 +18,12 @@
 #include <memory>
 #include <mutex>
 
+#include <grpcpp/channel.h>
 #include <grpcpp/client_context.h>
 #include <grpcpp/grpcpp.h>
 #include <grpcpp/impl/service_type.h>
+#include <grpcpp/security/credentials.h>
+#include <grpcpp/support/channel_arguments.h>
 #include <grpcpp/support/status.h>
 
 #include "perfetto/base/status.h"
@@ -31,6 +34,7 @@
 #include "protos/perfetto/bigtrace/orchestrator.pb.h"
 #include "protos/perfetto/bigtrace/worker.grpc.pb.h"
 #include "protos/perfetto/bigtrace/worker.pb.h"
+#include "src/cpp/server/thread_pool_interface.h"
 
 namespace perfetto {
 namespace bigtrace {
@@ -38,20 +42,28 @@ namespace {
 
 struct CommandLineOptions {
   std::string worker_address;
+  uint64_t worker_count;
 };
 
 CommandLineOptions ParseCommandLineOptions(int argc, char** argv) {
   CommandLineOptions command_line_options;
-  static option long_options[] = {{"worker", required_argument, nullptr, 'w'},
-                                  {nullptr, 0, nullptr, 0}};
+  static option long_options[] = {
+      {"worker", required_argument, nullptr, 'w'},
+      {"num_workers", required_argument, nullptr, 'n'},
+      {nullptr, 0, nullptr, 0}};
   int c;
-  while ((c = getopt_long(argc, argv, "w:", long_options, nullptr)) != -1) {
+  while ((c = getopt_long(argc, argv, "w:n:", long_options, nullptr)) != -1) {
     switch (c) {
       case 'w':
         command_line_options.worker_address = optarg;
         break;
+      case 'n':
+        command_line_options.worker_count = static_cast<uint64_t>(atoi(optarg));
+        break;
       default:
-        PERFETTO_ELOG("Usage: %s --worker=worker_address", argv[0]);
+        PERFETTO_ELOG(
+            "Usage: %s --worker=worker_address --worker_count=worker_count",
+            argv[0]);
         break;
     }
   }
@@ -114,13 +126,24 @@ base::Status OrchestratorMain(int argc, char** argv) {
   CommandLineOptions options = ParseCommandLineOptions(argc, argv);
 
   std::string server_address("localhost:5051");
-  std::string worker_address = !options.worker_address.empty()
-                                   ? options.worker_address
-                                   : "localhost:5052";
+  std::string worker_address =
+      !options.worker_address.empty() ? options.worker_address : "localhost";
 
-  // Setup the Orchestrator Client
-  auto channel =
-      grpc::CreateChannel(worker_address, grpc::InsecureChannelCredentials());
+  uint64_t worker_count = options.worker_count;
+  PERFETTO_CHECK(worker_count > 0);
+
+  // TODO(ivankc) Replace with DNS resolver
+  std::string target_address = "ipv4:";
+
+  for (uint64_t i = 0; i < worker_count; ++i) {
+    std::string address = worker_address + ":" + std::to_string(5052 + i) + ",";
+    target_address += address;
+  }
+
+  grpc::ChannelArguments args;
+  args.SetLoadBalancingPolicyName("round_robin");
+  auto channel = grpc::CreateCustomChannel(
+      target_address, grpc::InsecureChannelCredentials(), args);
   bool connected = channel->WaitForConnected(std::chrono::system_clock::now() +
                                              std::chrono::milliseconds(5000));
 
