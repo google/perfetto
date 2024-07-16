f106cb781b4821b30f32b5f80a7bace7257ac0ce
Primiano Tucci <primiano@google.com>
2024-07-16 15:14:46 -0700

    ci: fix setting of PERFETTO_TEST_DATA_CACHE
    Follow up to aosp/3178119 which added the setting of the
    PERFETTO_TEST_DATA_CACHE too late. The download happens on
    tools/install-build-deps which was few lines above.
    
    
    Change-Id: Ib5f5e941588702505e66e0910423c5703dee5a39

--- test/ci/common.sh
+++ test/ci/common.sh
@@ -20,6 +20,11 @@ OUT_PATH="out/dist"
 
 export PYTHONUNBUFFERED=1
 
+# Avoids re-downloading test data files over and over from network.
+# tools/test_data looks at the PERFETTO_TEST_DATA_CACHE env var.
+export PERFETTO_TEST_DATA_CACHE=/ci/cache/test_data
+mkdir -p $PERFETTO_TEST_DATA_CACHE
+
 tools/install-build-deps $PERFETTO_INSTALL_BUILD_DEPS_ARGS
 
 # Assumes Linux. Windows should use /win/clang instead.
@@ -27,11 +32,6 @@ if [[ -e buildtools/linux64/clang/bin/llvm-symbolizer ]]; then
   export ASAN_SYMBOLIZER_PATH="$(readlink -f buildtools/linux64/clang/bin/llvm-symbolizer)"
   export MSAN_SYMBOLIZER_PATH="$(readlink -f buildtools/linux64/clang/bin/llvm-symbolizer)"
 fi
-
-# Avoids re-downloading test data files over and over from network.
-# tools/test_data looks at the PERFETTO_TEST_DATA_CACHE env var.
-export PERFETTO_TEST_DATA_CACHE=/ci/cache/test_data
-mkdir -p $PERFETTO_TEST_DATA_CACHE
 
 # Performs checks on generated protos and build files.
 tools/gn gen out/tmp.protoc --args="is_debug=false cc_wrapper=\"ccache\""
