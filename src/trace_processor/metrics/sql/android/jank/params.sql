20d803432f31e21f34636b3bcd4098ae6dd0e9fa
Nicolo' Mazzucato <nicomazz@google.com>
2024-08-07 13:16:57 +0000

    Automatically infer UI thread for each cuj
    This deprecates the hardcoded list of ui threads per CUJs in favour of a new detection logic.
    In recent Android versions, once a CUJ is emitted, its UIThread will have a slice with format "J<cuj_name>#UIThread".
    
    This slice is used to detect the utid of the cuj main thread.
    
    Note that the old list is still working when this instant event is not available, but the instant event takes priority.
    
    Tests have been added both for the new instant event behaviour, and for the old hardcoded list.
    
    Bug: 337803466
    Test: tools/ninja -C out/linux_clang_release && tools/diff_test_trace_processor.py out/linux_clang_release/trace_processor_shell --name-filter AndroidMetrics:android_blocking_calls_cuj_different
    Change-Id: If81fe60c6ed5a43ae1ad3889ce04555a43f848c0

--- src/trace_processor/metrics/sql/android/jank/params.sql
+++ src/trace_processor/metrics/sql/android/jank/params.sql
@@ -1,4 +1,26 @@
--- Table to store parameters that will be matched with CUJs using the CUJ name.
+--
+-- Copyright 2024 The Android Open Source Project
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     https://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+-- (Deprecated) Table to store parameters that will be matched with CUJs using
+-- the CUJ name.
+--
+-- Note that this list should not be used anymore: in recent android versions
+-- each CUJ reports an instant event on their ui thread that can be used to
+-- automatically find the thread id.
+-- This is kept for compatibility with old android versions, as a fallback only.
+-- TODO: b/358038927 - Remove this list in 2025
 DROP TABLE IF EXISTS android_jank_cuj_param_set;
 CREATE TABLE android_jank_cuj_param_set (cuj_name_glob STRING, main_thread_override STRING);
 INSERT INTO android_jank_cuj_param_set (cuj_name_glob, main_thread_override)
