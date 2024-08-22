b165ef34e5fac10ebab5bfc283bf1c2e1a29dc2e
Kyle Holm <kholm@google.com>
2024-08-22 16:33:00 -0700

    Adds an Android input event plugin
    This plugin creates a new track which shows the start and duration of
    input events (e.g. ACTION_MOVE, ACTION_UP)
    
    Adds some columns to android.input and android.frames.timeline tables.
    
    Updates the android_frames_choreographer_do_frame test to account for
    the new columns.
    
    Bug: 359075178
    Change-Id: Ied9b81d47413f7ecd299917837be2b0724a9eff7

---
+++ ui/src/plugins/com.android.InputEvents/index.ts
@@ -0,0 +1,71 @@
+// Copyright (C) 2024 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import {LONG} from '../../trace_processor/query_result';
+import {PerfettoPlugin, PluginDescriptor} from '../../public/plugin';
+import {Trace} from '../../public/trace';
+import {
+  SimpleSliceTrack,
+  SimpleSliceTrackConfig,
+} from '../../frontend/simple_slice_track';
+import {TrackNode} from '../../public/workspace';
+import {getOrCreateUserInteractionGroup} from '../../public/standard_groups';
+
+class InputEvents implements PerfettoPlugin {
+  private readonly SQL_SOURCE = `
+    SELECT
+      read_time as ts,
+      end_to_end_latency_dur as dur,
+      CONCAT(event_type, ' ', event_action, ': ', process_name, ' (', input_event_id, ')') as name
+    FROM android_input_events
+    WHERE end_to_end_latency_dur IS NOT NULL
+    `;
+
+  async onTraceLoad(ctx: Trace): Promise<void> {
+    const cnt = await(ctx.engine.query(`
+      SELECT
+        count(*) as cnt
+      FROM slice
+      WHERE name GLOB 'UnwantedInteractionBlocker::notifyMotion*'
+    `));
+    if (cnt.firstRow({cnt: LONG}).cnt == 0n) {
+      return;
+    }
+
+    const config: SimpleSliceTrackConfig = {
+      data: {
+        sqlSource: this.SQL_SOURCE,
+        columns: ['ts', 'dur', 'name'],
+      },
+      columns: {ts: 'ts', dur: 'dur', name: 'name'},
+      argColumns: [],
+    };
+    await ctx.engine.query("INCLUDE PERFETTO MODULE android.input;");
+    const uri = 'com.android.InputEvents#InputEventsTrack';
+    const title = 'Input Events';
+    ctx.tracks.registerTrack({
+      uri,
+      title: title,
+      track: new SimpleSliceTrack(ctx, {trackUri: uri}, config)
+    });
+    const node = new TrackNode({uri, title});
+    const group = getOrCreateUserInteractionGroup(ctx.workspace);
+    group.addChildInOrder(node);
+  }
+}
+
+export const plugin: PluginDescriptor = {
+  pluginId: 'com.android.InputEvents',
+  plugin: InputEvents,
+};
