a06de91a734a3fd9d3132412123ba79406fbaa8c
Mohamed Elrakad <elrakadm@google.com>
2024-11-05 11:51:33 +0000

    tp: Add device manufacturer to TraceProcessor
    Bug: 364587875
    Test: captured trace locally
    Change-Id: Id2515d5630601700362a7905a36c776c0e74fb13

--- src/trace_processor/storage/metadata.h
+++ src/trace_processor/storage/metadata.h
@@ -31,6 +31,7 @@ namespace metadata {
   F(all_data_source_flushed_ns,        KeyType::kMulti,   Variadic::kInt),    \
   F(all_data_source_started_ns,        KeyType::kSingle,  Variadic::kInt),    \
   F(android_build_fingerprint,         KeyType::kSingle,  Variadic::kString), \
+  F(android_device_manufacturer,       KeyType::kSingle,  Variadic::kString), \
   F(android_sdk_version,               KeyType::kSingle,  Variadic::kInt),    \
   F(android_soc_model,                 KeyType::kSingle,  Variadic::kString), \
   F(android_guest_soc_model,           KeyType::kSingle,  Variadic::kString), \
