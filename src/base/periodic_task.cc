61f4b96c505d81a298376358b231add8ba681679
Jahdiel Alvarez <jahdiel@google.com>
2024-12-11 08:20:59 -0800

    Updated perfetto's GN build files to support traced compilation in QNX
    Created new argument 'is_qnx' to handle all QNX-specific compilation
    steps in GN build system. Currently, compilation for QNX is only
    supported via perfetto's standalone builds. Also, updated the perfetto's
    codebase to avoid running Linux-specific code in QNX. When compiling for
    QNX the PERFETTO_OS_LINUX flag is still set to keep the QNX build as
    compatible as possible with the Linux build.
    
    With this change, one can build the QNX target of traced by compiling
    with the following args.gn file:
    '''
    target_os = "qnx"
    target_cpu = "arm64"
    '''
    
    Only arm64 architecture is supported and one needs to setup the QNX
    developer environment before being able to compile.
    
    NOTE: traced_relay compiles but still does not run with this change.
    
    Test: tools/ninja -C out/qnx traced traced_relay perfetto
    Bug: 382283537
    Change-Id: I08bd3672a60b69cb92ba87eaeda9cec5b893afbc

--- src/base/periodic_task.cc
+++ src/base/periodic_task.cc
@@ -24,7 +24,7 @@
 #include "perfetto/base/time.h"
 #include "perfetto/ext/base/file_utils.h"
 
-#if PERFETTO_BUILDFLAG(PERFETTO_OS_LINUX) || \
+#if PERFETTO_BUILDFLAG(PERFETTO_OS_LINUX_BUT_NOT_QNX) || \
     (PERFETTO_BUILDFLAG(PERFETTO_OS_ANDROID) && __ANDROID_API__ >= 19)
 #include <sys/timerfd.h>
 #endif
@@ -44,7 +44,7 @@ uint32_t GetNextDelayMs(const TimeMillis& now_ms,
 }
 
 ScopedPlatformHandle CreateTimerFd(const PeriodicTask::Args& args) {
-#if PERFETTO_BUILDFLAG(PERFETTO_OS_LINUX) || \
+#if PERFETTO_BUILDFLAG(PERFETTO_OS_LINUX_BUT_NOT_QNX) || \
     (PERFETTO_BUILDFLAG(PERFETTO_OS_ANDROID) && __ANDROID_API__ >= 19)
   ScopedPlatformHandle tfd(
       timerfd_create(CLOCK_BOOTTIME, TFD_CLOEXEC | TFD_NONBLOCK));
