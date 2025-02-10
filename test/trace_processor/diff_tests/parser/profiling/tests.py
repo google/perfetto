4a3cf8440fde0d8fd11b4ccb878e343f025929ec
Ioannis Ilkos <ilkos@google.com>
2025-02-10 17:13:16 +0000

    Make android_user_id a first-class citizen in the process tables
    Bug: 394253503
    Change-Id: I529ab59a762c5897d96b9691d80046a5d7a46de8

--- test/trace_processor/diff_tests/parser/profiling/tests.py
+++ test/trace_processor/diff_tests/parser/profiling/tests.py
@@ -117,6 +117,7 @@ class Profiling(TestSuite):
               name: "system_server"
               uid: 1000
               pid: 2
+              android_user_id: 0
             }
             mappings {
               path: "[anon: libc_malloc]"
