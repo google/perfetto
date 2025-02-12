e8b72318bc3600d146661964c2521d296d29337e
Devarshi Bhatt <devarshimb@google.com>
2025-02-12 04:12:26 +0000

    Add debug track for frame with max dur blocking call in per-frame metric
    For the per-frame metric, apart from pinning the CUJ and blocking calls,
    it is useful to pin the frame which contains the blocking call with the
    max duration. For the aggregation case 'max_dur_per_frame_ns', added
    support to pin the frame during which the blocking call with the max
    duration occurred in a separate debug track.
    
    Test: Manual trace and metric check
    Bug: 394866952

--- ui/src/plugins/dev.perfetto.PinAndroidPerfMetrics/handlers/pinBlockingCall.ts
+++ ui/src/plugins/dev.perfetto.PinAndroidPerfMetrics/handlers/pinBlockingCall.ts
@@ -23,6 +23,7 @@ import {
   addLatencyCUJDebugTrack,
 } from '../../dev.perfetto.AndroidCujs';
 import {addDebugSliceTrack} from '../../../components/tracks/debug_tracks';
+import {LONG} from '../../../trace_processor/query_result';
 
 class BlockingCallMetricHandler implements MetricHandler {
   /**
@@ -59,6 +60,14 @@ class BlockingCallMetricHandler implements MetricHandler {
     this.pinSingleCuj(ctx, metricData);
     const config = this.blockingCallTrackConfig(metricData);
     addDebugSliceTrack({trace: ctx, ...config});
+    // Only trigger adding track for frame when the aggregation is for max duration per frame.
+    if (metricData.aggregation === 'max_dur_per_frame_ns') {
+      this.frameWithMaxDurBlockingCallTrackConfig(ctx, metricData).then(
+        (frameConfigArgs) => {
+          addDebugSliceTrack({trace: ctx, ...frameConfigArgs});
+        },
+      );
+    }
   }
 
   private async pinSingleCuj(ctx: Trace, metricData: BlockingCallMetricData) {
@@ -102,6 +111,101 @@ class BlockingCallMetricHandler implements MetricHandler {
       argColumns: ['name', 'ts', 'dur'],
       title: trackName,
     };
+  }
+
+  private async frameWithMaxDurBlockingCallTrackConfig(
+    ctx: Trace,
+    metricData: BlockingCallMetricData,
+  ) {
+    const cuj = metricData.cujName;
+    const processName = metricData.process;
+    const blockingCallName = metricData.blockingCallName;
+
+    // Fetch the frame_id of the frame with the max duration blocking call.
+    const result = await ctx.engine.query(`
+        INCLUDE PERFETTO MODULE android.slices;
+        INCLUDE PERFETTO MODULE android.binder;
+        INCLUDE PERFETTO MODULE android.critical_blocking_calls;
+        INCLUDE PERFETTO MODULE android.frames.timeline;
+        INCLUDE PERFETTO MODULE android.cujs.sysui_cujs;
+
+        WITH extended_frame_boundary AS (
+          SELECT frame_ts as ts,
+          ui_thread_utid,
+          frame_id,
+          layer_id,
+          -- Calculate the end timestamp (ts_end) by taking the start time (frame_ts) of the next frame in the session.
+          -- For the last frame, fall back to the default ts_end.
+          COALESCE(LEAD(frame_ts) OVER (PARTITION BY cuj_id ORDER BY frame_id ASC), ts_end) AS ts_end,
+          frame_id
+        FROM _android_frames_in_cuj order by frame_id
+        ),
+        blocking_calls_per_frame AS (
+          SELECT
+            MIN(
+                bc.dur,
+                frame.ts_end - bc.ts,
+                bc.ts_end - frame.ts
+            ) AS dur,
+            MAX(frame.ts, bc.ts) AS ts,
+            bc.upid,
+            bc.name,
+            bc.process_name,
+            bc.utid,
+            frame.frame_id,
+            frame.layer_id
+          FROM _android_critical_blocking_calls bc
+          JOIN extended_frame_boundary frame
+          ON bc.utid = frame.ui_thread_utid
+          -- The following condition to accommodate blocking call crossing frame boundary. The blocking
+          -- call starts in a frame and ends in a frame. It can either be the same frame or a different
+          -- frame.
+          WHERE (bc.ts >= frame.ts AND bc.ts <= frame.ts_end) -- Blocking call starts within the frame.
+            OR (bc.ts_end >= frame.ts AND bc.ts_end <= frame.ts_end)
+        ),
+        blocking_calls_frame_cuj AS (
+          SELECT
+            b.frame_id,
+            b.layer_id,
+            b.name,
+            frame_cuj.cuj_name,
+            b.ts,
+            b.dur,
+            b.process_name
+          FROM _android_frames_in_cuj frame_cuj
+          JOIN blocking_calls_per_frame b
+          USING (upid, frame_id, layer_id)
+          )
+        SELECT
+          frame_id
+        FROM blocking_calls_frame_cuj
+        WHERE
+          process_name = "${processName}"
+          AND name = "${blockingCallName}"
+          AND cuj_name = "${cuj}"
+        ORDER BY dur DESC limit 1`);
+    const row = result.firstRow({frame_id: LONG});
+    // Fetch the ts and dur of the frame corresponding to the above frame_id.
+    const frameWithMaxDurBlockingCallQuery = `
+        SELECT
+          cast_string!(frame_id) as frame_id,
+          ts,
+          dur
+        FROM android_frames_layers
+        WHERE frame_id = ${row.frame_id}
+      `;
+
+    const trackName = 'Frame with max duration blocking call';
+    const config = {
+      data: {
+        sqlSource: frameWithMaxDurBlockingCallQuery,
+        columns: ['frame_id', 'ts', 'dur'],
+      },
+      columns: {ts: 'ts', dur: 'dur', name: 'frame_id'},
+      argColumns: ['frame_id', 'ts', 'dur'],
+      title: trackName,
+    };
+    return Promise.resolve(config);
   }
 }
 
