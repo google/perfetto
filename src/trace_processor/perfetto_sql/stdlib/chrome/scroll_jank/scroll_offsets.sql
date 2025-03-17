31bf9915cf5f9c562dee2e2cc0d458ba425cf8d6
Perfetto Team <perfetto-dev@google.com>
2025-03-17 12:03:48 -0700

    COPYBARA_IMPORT=Project import generated by Copybara.
    GitOrigin-RevId: e30216cb04bfda7e9bf796d862626024da4f7c70
    Change-Id: I03b070782cf36de29da765060b9c4722b1b8c5f0

--- src/trace_processor/perfetto_sql/stdlib/chrome/scroll_jank/scroll_offsets.sql
+++ src/trace_processor/perfetto_sql/stdlib/chrome/scroll_jank/scroll_offsets.sql
@@ -2,7 +2,7 @@
 -- Use of this source code is governed by a BSD-style license that can be
 -- found in the LICENSE file.
 
--- sqlformat file off
+--- sqlformat file off
 
 -- This file creates two public views:
 --     - chrome_scroll_input_offsets and
