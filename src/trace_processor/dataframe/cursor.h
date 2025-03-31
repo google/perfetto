d2761b60f2e192b79794ab8dd93db3088f38228f
Lalit Maganti <lalitm@google.com>
2025-03-31 21:45:37 +0100

    Fix compile

--- src/trace_processor/dataframe/cursor.h
+++ src/trace_processor/dataframe/cursor.h
@@ -101,17 +101,17 @@ class Cursor {
       case ColumnType::GetTypeIndex<Id>():
         cell_callback_impl.OnCell(idx);
         break;
-      case C::GetTypeIndex<Uint32>():
-        callback.OnCell(c.storage.unchecked_data<Uint32>()[idx]);
+      case ColumnType::GetTypeIndex<Uint32>():
+        cell_callback_impl.OnCell(c.storage.unchecked_data<Uint32>()[idx]);
         break;
-      case C::GetTypeIndex<Int32>():
-        callback.OnCell(c.storage.unchecked_data<Int32>()[idx]);
+      case ColumnType::GetTypeIndex<Int32>():
+        cell_callback_impl.OnCell(c.storage.unchecked_data<Int32>()[idx]);
         break;
-      case C::GetTypeIndex<Int64>():
-        callback.OnCell(c.storage.unchecked_data<Int64>()[idx]);
+      case ColumnType::GetTypeIndex<Int64>():
+        cell_callback_impl.OnCell(c.storage.unchecked_data<Int64>()[idx]);
         break;
-      case C::GetTypeIndex<Double>():
-        callback.OnCell(c.storage.unchecked_data<Double>()[idx]);
+      case ColumnType::GetTypeIndex<Double>():
+        cell_callback_impl.OnCell(c.storage.unchecked_data<Double>()[idx]);
         break;
       default:
         PERFETTO_FATAL("Invalid storage spec");
