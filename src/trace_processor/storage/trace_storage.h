3af721daafd754366df7ab160d357f4750853a3f
Anna Bauza <annabauza@google.com>
2025-04-11 07:32:13 +0000

    Create android Users table
    Change-Id: I922e866376fb2419fff87f21729268b32e808dc6

--- src/trace_processor/storage/trace_storage.h
+++ src/trace_processor/storage/trace_storage.h
@@ -540,6 +540,11 @@ class TraceStorage {
     return &package_list_table_;
   }
 
+  const tables::UserListTable& user_list_table() const {
+    return user_list_table_;
+  }
+  tables::UserListTable* mutable_user_list_table() { return &user_list_table_; }
+
   const tables::AndroidGameInterventionListTable&
   android_game_intervention_list_table() const {
     return android_game_intervention_list_table_;
@@ -1179,6 +1184,7 @@ class TraceStorage {
   tables::PerfSampleTable perf_sample_table_{&string_pool_};
   tables::InstrumentsSampleTable instruments_sample_table_{&string_pool_};
   tables::PackageListTable package_list_table_{&string_pool_};
+  tables::UserListTable user_list_table_{&string_pool_};
   tables::AndroidGameInterventionListTable
       android_game_intervention_list_table_{&string_pool_};
   tables::ProfilerSmapsTable profiler_smaps_table_{&string_pool_};
