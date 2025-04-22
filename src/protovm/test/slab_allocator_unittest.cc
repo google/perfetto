6e8a7a59eda31c54db741a0a4235f045f9a82e0e
Kean Mariotti <keanmariotti@google.com>
2025-04-22 14:51:45 +0000

    protovm: slab allocator towards production quality
    - Make slabs 4KB-aligned (manual memory page allocation).
    - Keep a map of 4KB-aligned blocks to corresponding slab. Used to
      map an element to be deallocated to the corresponding slab in O(1).
    - Keep a list of non-full slabs. Used to access a slab ready for
      allocations in O(1).
    - Deallocate slabs once they become empty.
    
    Change-Id: Iaff73ffae5feba574f7d6975dadd433f6638c9e9

--- src/protovm/test/slab_allocator_unittest.cc
+++ src/protovm/test/slab_allocator_unittest.cc
@@ -24,10 +24,50 @@ namespace test {
 
 class SlabAllocatorTest : public ::testing::Test {
  protected:
+  std::vector<void*> AllocateAll() {
+    std::vector<void*> allocated;
+
+    for (size_t i = 0; i < kCapacity; ++i) {
+      auto* p = allocator_.Allocate();
+      EXPECT_NE(p, nullptr);
+      if (!allocated.empty()) {
+        EXPECT_NE(p, allocated.front());
+      }
+      allocated.push_back(p);
+    }
+
+    return allocated;
+  }
+
+  void FreeAll(const std::vector<void*>& allocated) {
+    for (auto* p : allocated) {
+      allocator_.Free(p);
+    }
+  }
+
+  void CheckPointersAreDistinct(const std::vector<void*>& pointers) const {
+    auto sorted = std::vector<uintptr_t>{};
+    for (const void* p : pointers) {
+      sorted.push_back(reinterpret_cast<uintptr_t>(p));
+    }
+
+    std::sort(sorted.begin(), sorted.end());
+    auto new_end = std::unique(sorted.begin(), sorted.end());
+    bool has_duplicates = new_end != sorted.cend();
+    EXPECT_FALSE(has_duplicates);
+  }
+
+  void CheckPointersAlignment(const std::vector<void*> pointers) const {
+    for (const void* p : pointers) {
+      uintptr_t i = reinterpret_cast<uintptr_t>(p);
+      EXPECT_TRUE(i % alignof(ElementType) == 0);
+    }
+  }
+
   // Slab capacity and total capacity chosen to internally allocate two slabs
   // in order to test allocations across multiple slabs.
   static constexpr size_t kSlabCapacity = 64;
-  static constexpr size_t kMaxSlabs = 2;
+  static constexpr size_t kMaxSlabs = 4;
   static constexpr size_t kCapacity = kMaxSlabs * kSlabCapacity;
 
   struct ElementType {
@@ -40,38 +80,25 @@ class SlabAllocatorTest : public ::testing::Test {
 };
 
 TEST_F(SlabAllocatorTest, AllocatesDeallocates) {
-  std::vector<void*> allocated;
-  for (size_t i = 0; i < kCapacity; ++i) {
-    auto* p = allocator_.Allocate();
-    ASSERT_NE(p, nullptr);
-    if (!allocated.empty()) {
-      ASSERT_NE(p, allocated.front());
-    }
-    allocated.push_back(p);
-  }
+  std::vector<void*> allocated = AllocateAll();
+  CheckPointersAreDistinct(allocated);
+  CheckPointersAlignment(allocated);
 
-  for (auto* p : allocated) {
-    allocator_.Free(p);
-  }
+  FreeAll(allocated);
 
-  std::vector<void*> reallocated;
-  for (size_t i = 0; i < kCapacity; ++i) {
-    auto* p = allocator_.Allocate();
-    ASSERT_NE(p, nullptr);
-    reallocated.push_back(p);
-  }
-  std::reverse(reallocated.begin(), reallocated.end());
+  allocated = AllocateAll();
+  CheckPointersAreDistinct(allocated);
+  CheckPointersAlignment(allocated);
 
-  ASSERT_EQ(allocated, reallocated);
-}
+  // Free in different (reverse) order
+  std::reverse(allocated.begin(), allocated.end());
+  FreeAll(allocated);
 
-TEST_F(SlabAllocatorTest, RespectsAlignment) {
-  for (size_t i = 0; i < kCapacity; ++i) {
-    auto* p = allocator_.Allocate();
-    ASSERT_NE(p, nullptr);
-    ASSERT_EQ(reinterpret_cast<size_t>(p) % alignof(ElementType),
-              static_cast<size_t>(0));
-  }
+  allocated = AllocateAll();
+  CheckPointersAreDistinct(allocated);
+  CheckPointersAlignment(allocated);
+
+  FreeAll(allocated);
 }
 
 }  // namespace test
