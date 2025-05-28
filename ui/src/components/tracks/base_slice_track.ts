google/perfetto

commit 504c0b8061ac73bd97f8282c3bacbd64615177f7

Migrate to using DatasetSliceTrack for ftrace tracks (#1644)
This adds basic functionality for the ftrace tracks including:
- Tooltips
- Selection
- Details panels

ui/src/components/tracks/base_slice_track.ts
@@ -268,6 +268,7 @@ export abstract class BaseSliceTrack<
     sliceLayout: Partial<SliceLayout> = {},
     protected readonly depthGuess: number = 0,
     protected readonly instantWidthPx: number = CHEVRON_WIDTH_PX,
+    protected readonly forceTimestampRenderOrder: boolean = false,
   ) {
     // Work out the extra columns.
     // This is the union of the embedder-defined columns and the base columns
@@ -513,12 +514,8 @@ export abstract class BaseSliceTrack<
     }
 
     // Second pass: fill slices by color.
-    const vizSlicesByColor = vizSlices.slice();
-    vizSlicesByColor.sort((a, b) =>
-      colorCompare(a.colorScheme.base, b.colorScheme.base),
-    );
     let lastColor = undefined;
-    for (const slice of vizSlicesByColor) {
+    for (const slice of vizSlices) {
       const color = slice.isHighlighted
         ? slice.colorScheme.variant.cssString
         : slice.colorScheme.base.cssString;
@@ -526,6 +523,7 @@ export abstract class BaseSliceTrack<
         lastColor = color;
         ctx.fillStyle = color;
       }
+      ctx.fillStyle = color;
       const y = padding + slice.depth * (sliceHeight + rowSpacing);
       if (slice.flags & SLICE_FLAGS_INSTANT) {
         this.drawChevron(ctx, slice.x, y, sliceHeight);
@@ -554,7 +552,7 @@ export abstract class BaseSliceTrack<
 
     // Pass 2.5: Draw fillRatio light section.
     ctx.fillStyle = `#FFFFFF50`;
-    for (const slice of vizSlicesByColor) {
+    for (const slice of vizSlices) {
       // Can't draw fill ratio on incomplete or instant slices.
       if (slice.flags & (SLICE_FLAGS_INCOMPLETE | SLICE_FLAGS_INSTANT)) {
         continue;
@@ -741,6 +739,17 @@ export abstract class BaseSliceTrack<
 
     this.slicesKey = slicesKey;
     this.onUpdatedSlices(slices);
+
+    // Sort slices by color - this can make rendering more efficient as it
+    // reduces the number of times we need to switch colors. For slice heavy
+    // tracks the result looks identical, however on tracks that have a lot of
+    // instant slices the result can look very different, so we might want to
+    // force timestamp render order for these tracks.
+    if (!this.forceTimestampRenderOrder) {
+      slices.sort((a, b) =>
+        colorCompare(a.colorScheme.base, b.colorScheme.base),
+      );
+    }
     this.slices = slices;
 
     raf.scheduleCanvasRedraw();

test/data/ui-screenshots/arcvm_trace.test.ts/ftrace-events/ftrace-events.png.sha256
@@ -1,1 +1,1 @@
-ffceeb538188fb32881643427fa0e0bdb89c06c3edb9aa73f63f146e27aa5845
+b79c82a4d2ea9df943e83fcdd627e5d3909f7aca217a05369102858311498746

test/data/ui-screenshots/ftrace_tracks_and_tab.test.ts/ftrace-tracks/ftrace-events.png.sha256
@@ -1,1 +1,1 @@
-3d5c417981266a6cd88eaced8d8f9bc487692c86a59475d13b4ff903077d9876
+25545f443f948339ce860c31f77765a7b3a00fb7f7bea08e51039f2a01339e48

ui/src/components/details/slice_args.ts
@@ -28,7 +28,10 @@ import {Trace} from '../../public/trace';
 import {extensions} from '../extensions';
 
 // Renders slice arguments (key/value pairs) as a subtree.
-export function renderArguments(trace: Trace, args: Arg[]): m.Children {
+export function renderArguments(
+  trace: Trace,
+  args: ReadonlyArray<Arg>,
+): m.Children {
   if (args.length > 0) {
     const tree = convertArgsToTree(args);
     return renderArgTreeNodes(trace, tree);

ui/src/components/details/slice_args_parser.ts
@@ -39,7 +39,7 @@ export interface ArgNode<T> {
 //
 // See unit tests for examples.
 export function convertArgsToTree<T extends {key: string}>(
-  input: T[],
+  input: ReadonlyArray<T>,
 ): ArgNode<T>[] {
   const result: ArgNode<T>[] = [];
   for (const arg of input) {

ui/src/components/sql_utils/args.ts
@@ -23,7 +23,7 @@ import {
 import {ArgSetId, ArgsId, asArgId} from './core_types';
 
 export type ArgValue = bigint | string | number | boolean | null;
-type ArgValueType =
+export type ArgValueType =
   | 'int'
   | 'uint'
   | 'pointer'

ui/src/components/tracks/dataset_slice_track.ts
@@ -129,6 +129,14 @@ export interface DatasetSliceTrackAttrs<T extends DatasetSchema> {
   readonly instantStyle?: InstantStyle;
 
   /**
+   * Events are usually rendered in color order for performance. However for
+   * tracks that have a lot of overlapping event such as those full of instant
+   * events, this can look odd, so this setting forces events to be rendered in
+   * timestamp order, potentially at the cost of a bit of performance.
+   */
+  readonly forceTsRenderOrder?: boolean;
+
+  /**
    * An optional function to override the color scheme for each event.
    * If omitted, the default slice color scheme is used.
    */
@@ -203,6 +211,7 @@ export class DatasetSliceTrack<T extends ROW_SCHEMA> extends BaseSliceTrack<
       attrs.sliceLayout,
       attrs.initialMaxDepth,
       attrs.instantStyle?.width,
+      attrs.forceTsRenderOrder ?? false,
     );
     this.rootTableName = attrs.rootTableName;
   }

ui/src/plugins/dev.perfetto.Ftrace/ftrace_details_panel.ts
@@ -1,0 +1,166 @@
+// Copyright (C) 2025 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import m from 'mithril';
+import {assertUnreachable} from '../../base/logging';
+import {Time} from '../../base/time';
+import {renderArguments} from '../../components/details/slice_args';
+import {Arg, ArgValue, ArgValueType} from '../../components/sql_utils/args';
+import {asArgId} from '../../components/sql_utils/core_types';
+import {Timestamp} from '../../components/widgets/timestamp';
+import {TrackEventDetailsPanel} from '../../public/details_panel';
+import {Trace} from '../../public/trace';
+import {
+  LONG_NULL,
+  NUM,
+  NUM_NULL,
+  STR,
+  STR_NULL,
+} from '../../trace_processor/query_result';
+import {DetailsShell} from '../../widgets/details_shell';
+import {GridLayout, GridLayoutColumn} from '../../widgets/grid_layout';
+import {Section} from '../../widgets/section';
+import {Tree, TreeNode} from '../../widgets/tree';
+
+export class FtraceEventDetailsPanel implements TrackEventDetailsPanel {
+  private args?: ReadonlyArray<Arg>;
+
+  constructor(
+    readonly trace: Trace,
+    readonly row: Readonly<{
+      id: number;
+      ts: bigint;
+      name: string;
+      cpu: number;
+    }>,
+  ) {}
+
+  async load() {
+    await this.loadArgs();
+  }
+
+  render() {
+    return m(
+      DetailsShell,
+      {
+        title: `Ftrace Event`,
+        description: this.row.name,
+      },
+      m(
+        GridLayout,
+        m(
+          GridLayoutColumn,
+          m(
+            Section,
+            {title: 'Details'},
+            m(
+              Tree,
+              m(TreeNode, {
+                left: 'ID',
+                right: this.row.id,
+              }),
+              m(TreeNode, {
+                left: 'Name',
+                right: this.row.name,
+              }),
+              m(TreeNode, {
+                left: 'Timestamp',
+                right: m(Timestamp, {ts: Time.fromRaw(this.row.ts)}),
+              }),
+              m(TreeNode, {
+                left: 'CPU',
+                right: this.row.cpu,
+              }),
+            ),
+          ),
+        ),
+        m(
+          GridLayoutColumn,
+          m(
+            Section,
+            {title: 'Arguments'},
+            m(Tree, this.args && renderArguments(this.trace, this.args)),
+          ),
+        ),
+      ),
+    );
+  }
+
+  private async loadArgs() {
+    const queryRes = await this.trace.engine.query(`
+      SELECT
+        args.id as id,
+        flat_key as flatKey,
+        key,
+        int_value as intValue,
+        string_value as stringValue,
+        real_value as realValue,
+        value_type as valueType,
+        display_value as displayValue
+      FROM ftrace_event
+      JOIN args USING(arg_set_id)
+      WHERE ftrace_event.id = ${this.row.id}
+    `);
+
+    const it = queryRes.iter({
+      id: NUM,
+      flatKey: STR,
+      key: STR,
+      intValue: LONG_NULL,
+      stringValue: STR_NULL,
+      realValue: NUM_NULL,
+      valueType: STR,
+      displayValue: STR_NULL,
+    });
+
+    const args: Arg[] = [];
+    for (; it.valid(); it.next()) {
+      const value = parseArgValue(it);
+      args.push({
+        id: asArgId(it.id),
+        flatKey: it.flatKey,
+        key: it.key,
+        value,
+        displayValue: it.displayValue ?? 'NULL',
+      });
+    }
+    this.args = args;
+  }
+}
+
+function parseArgValue(it: {
+  valueType: string;
+  intValue: bigint | null;
+  stringValue: string | null;
+  realValue: number | null;
+}): ArgValue {
+  const valueType = it.valueType as ArgValueType;
+  switch (valueType) {
+    case 'int':
+    case 'uint':
+      return it.intValue;
+    case 'pointer':
+      return it.intValue === null ? null : `0x${it.intValue.toString(16)}`;
+    case 'string':
+      return it.stringValue;
+    case 'bool':
+      return Boolean(it.intValue);
+    case 'real':
+      return it.realValue;
+    case 'null':
+      return null;
+    default:
+      assertUnreachable(valueType);
+  }
+}

commit 8d77f0bbd174f8fb90cd2553b01241c4327ca5ae

Fix disappearing slices on zoom bug (#1650)
Fix bug introduced in #1644 arising from sorting the slices at load time
rather than at render time. It turns out there was a very good reason
for doing this - slices are required to be sorted by timestamp for other
logic to work, so we cannot sort by color at load time.

ui/src/components/tracks/base_slice_track.ts
@@ -514,6 +514,12 @@ export abstract class BaseSliceTrack<
     }
 
     // Second pass: fill slices by color.
+    const vizSlicesByColor = vizSlices.slice();
+    if (!this.forceTimestampRenderOrder) {
+      vizSlicesByColor.sort((a, b) =>
+        colorCompare(a.colorScheme.base, b.colorScheme.base),
+      );
+    }
     let lastColor = undefined;
     for (const slice of vizSlices) {
       const color = slice.isHighlighted
@@ -523,7 +529,6 @@ export abstract class BaseSliceTrack<
         lastColor = color;
         ctx.fillStyle = color;
       }
-      ctx.fillStyle = color;
       const y = padding + slice.depth * (sliceHeight + rowSpacing);
       if (slice.flags & SLICE_FLAGS_INSTANT) {
         this.drawChevron(ctx, slice.x, y, sliceHeight);
@@ -552,7 +557,7 @@ export abstract class BaseSliceTrack<
 
     // Pass 2.5: Draw fillRatio light section.
     ctx.fillStyle = `#FFFFFF50`;
-    for (const slice of vizSlices) {
+    for (const slice of vizSlicesByColor) {
       // Can't draw fill ratio on incomplete or instant slices.
       if (slice.flags & (SLICE_FLAGS_INCOMPLETE | SLICE_FLAGS_INSTANT)) {
         continue;
@@ -739,17 +744,6 @@ export abstract class BaseSliceTrack<
 
     this.slicesKey = slicesKey;
     this.onUpdatedSlices(slices);
-
-    // Sort slices by color - this can make rendering more efficient as it
-    // reduces the number of times we need to switch colors. For slice heavy
-    // tracks the result looks identical, however on tracks that have a lot of
-    // instant slices the result can look very different, so we might want to
-    // force timestamp render order for these tracks.
-    if (!this.forceTimestampRenderOrder) {
-      slices.sort((a, b) =>
-        colorCompare(a.colorScheme.base, b.colorScheme.base),
-      );
-    }
     this.slices = slices;
 
     raf.scheduleCanvasRedraw();

.github/workflows/ui-tests.yml
@@ -77,7 +77,7 @@ jobs:
         run: |
           mkdir /tmp/chrome
           cd /tmp/chrome
-          CHROME_VERSION=135.0.7049.52
+          CHROME_VERSION=136.0.7103.113
           curl -Ls -o chrome.deb https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_${CHROME_VERSION}-1_amd64.deb
           dpkg-deb -x chrome.deb  .
 

test/data/ui-screenshots/aggregation.test.ts/slices/slice-aggregation.png.sha256
@@ -1,1 +1,1 @@
-9b8ac3ba398743f47ffb583cff8094fe0314e314fe247d9f1ee430514b6d2b90
+fb23556d95d908220e813a04c0b31e95a186edc65b060277599dc6d2d1e22c7d

test/data/ui-screenshots/debug_tracks.test.ts/debug-tracks-pivot/debug-track-pivot.png.sha256
@@ -1,1 +1,1 @@
-92a4f052e91634de4887f31c85023865bb99fd8318d9b8169de36d177c5e0729
+cae758d141ef8c740465b77a425e23ad80c5a039cdcc7e52d779641304a7abcf

test/data/ui-screenshots/debug_tracks.test.ts/debug-tracks/debug-slice-clicked.png.sha256
@@ -1,1 +1,1 @@
-5ea124dcb71297ebacba792fc5fd4ef6ee8d85ec313b43a40da171afe6576b4c
+08793195f66d60b1f0641ee3588a2b4eb418d206e692ba97d6563321440c308d

test/data/ui-screenshots/debug_tracks.test.ts/debug-tracks/debug-track-added.png.sha256
@@ -1,1 +1,1 @@
-bbbfad9596f5b0a0180af71feaa5eab2b6b03608b05806e7ad8b44bcdc68b459
+b93da51a2288b5b93abcaccd21b3a9360c8aa5a02bae0934576941631e9a7eb1

test/data/ui-screenshots/load_and_tracks.test.ts/omnibox-search/process-details.png.sha256
@@ -1,1 +1,1 @@
-7fe029da7cb33832cd18bbf1baa638e593e87111cfecbbf2df0b79ac2fa42538
+3a9a170fa980eac6cc092aa6d2e8b1cf638ccfd57ce0c5f562de18fe9b8a6db0

test/data/ui-screenshots/load_and_tracks.test.ts/omnibox-search/search-slice.png.sha256
@@ -1,1 +1,1 @@
-d992e57a9240506221b56668baef6e646e00733442373a6cef87f7c8deea65f8
+e6c30082c6415cb3e68652a5f7fdb5cf08145b28bb0c28a17f381516d45693d1

test/data/ui-screenshots/zoom.test.ts/zoom-out/zoomed-out.png.sha256
@@ -1,1 +1,1 @@
-7dda5abe7422545125109a097ab1c9cd5814d605265b2b3659f1ce989ff12c4f
+ec44db3f67464e46b73c194bd66a38774c0923e5abf69c98a5ffeac43fbece21