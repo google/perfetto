2885645e7bc3101239571acf62b612a941cea01a
Steve Golton <stevegolton@google.com>
2025-05-28 11:09:43 +0100

    Remove direct dependency from aggregations to thread states, and re-introduce bar chart support for thread state aggregations
    Change-Id: I1463ab17d54aa5aefca19920b623928556353205

--- ui/src/plugins/dev.perfetto.Sched/common.ts
+++ ui/src/plugins/dev.perfetto.Sched/common.ts
@@ -12,9 +12,46 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import {HSLColor} from '../../base/color';
+import {ColorScheme} from '../../base/color_scheme';
+import {GRAY, GRAY_COLOR, makeColorScheme} from '../../components/colorizer';
+
 export const CPU_SLICE_URI_PREFIX = '/sched_cpu';
 
 // Helper function moved here as it's only used by the overlay.
 export function uriForSchedTrack(cpu: number): string {
   return `${CPU_SLICE_URI_PREFIX}${cpu}`;
+}
+
+const DESAT_RED = makeColorScheme(new HSLColor([3, 30, 49]));
+const DARK_GREEN = makeColorScheme(new HSLColor([120, 44, 34]));
+const LIME_GREEN = makeColorScheme(new HSLColor([75, 55, 47]));
+const TRANSLUCENT_GRAY = {
+  base: new HSLColor([0, 1, 50], 0),
+  variant: new HSLColor([0, 1, 50], 0.2),
+  disabled: GRAY_COLOR,
+  // Make the text invisible
+  textBase: new HSLColor([0, 0, 0], 0),
+  textVariant: new HSLColor([0, 0, 0], 0),
+  textDisabled: new HSLColor([0, 0, 0], 0),
+};
+const ORANGE = makeColorScheme(new HSLColor([36, 100, 50]));
+const INDIGO = makeColorScheme(new HSLColor([231, 48, 48]));
+
+export function colorForThreadState(state: string): ColorScheme {
+  if (state === 'Running') {
+    return DARK_GREEN;
+  } else if (state.startsWith('Runnable')) {
+    return LIME_GREEN;
+  } else if (state.includes('Uninterruptible Sleep')) {
+    if (state.includes('non-IO')) {
+      return DESAT_RED;
+    }
+    return ORANGE;
+  } else if (state.includes('Dead')) {
+    return GRAY;
+  } else if (state.includes('Sleeping') || state.includes('Idle')) {
+    return TRANSLUCENT_GRAY;
+  }
+  return INDIGO;
 }
