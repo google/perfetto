b4ffdd1c3d8af6ce6eecdb5bd10d668b622969ed
Edgar Arriaga <edgararriaga@google.com>
2025-05-29 00:16:18 +0000

    python formatting fixes for bigtrace protos

--- python/perfetto/bigtrace/protos/perfetto/trace_processor/metatrace_categories_pb2.py
+++ python/perfetto/bigtrace/protos/perfetto/trace_processor/metatrace_categories_pb2.py
@@ -9,28 +9,25 @@ from google.protobuf import descriptor_pool as _descriptor_pool
 from google.protobuf import runtime_version as _runtime_version
 from google.protobuf import symbol_database as _symbol_database
 from google.protobuf.internal import builder as _builder
+
 _runtime_version.ValidateProtobufRuntimeVersion(
-    _runtime_version.Domain.PUBLIC,
-    5,
-    29,
-    0,
-    '',
-    'protos/perfetto/trace_processor/metatrace_categories.proto'
-)
+    _runtime_version.Domain.PUBLIC, 5, 29, 0, '',
+    'protos/perfetto/trace_processor/metatrace_categories.proto')
 # @@protoc_insertion_point(imports)
 
 _sym_db = _symbol_database.Default()
 
-
-
-
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n:protos/perfetto/trace_processor/metatrace_categories.proto\x12\x0fperfetto.protos*}\n\x13MetatraceCategories\x12\x12\n\x0eQUERY_TIMELINE\x10\x01\x12\x12\n\x0eQUERY_DETAILED\x10\x02\x12\x11\n\rFUNCTION_CALL\x10\x04\x12\x06\n\x02\x44\x42\x10\x08\x12\x10\n\x0c\x41PI_TIMELINE\x10\x10\x12\x08\n\x04NONE\x10\x00\x12\x07\n\x03\x41LL\x10\x1f')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(
+    b'\n:protos/perfetto/trace_processor/metatrace_categories.proto\x12\x0fperfetto.protos*}\n\x13MetatraceCategories\x12\x12\n\x0eQUERY_TIMELINE\x10\x01\x12\x12\n\x0eQUERY_DETAILED\x10\x02\x12\x11\n\rFUNCTION_CALL\x10\x04\x12\x06\n\x02\x44\x42\x10\x08\x12\x10\n\x0c\x41PI_TIMELINE\x10\x10\x12\x08\n\x04NONE\x10\x00\x12\x07\n\x03\x41LL\x10\x1f'
+)
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
-_builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'protos.perfetto.trace_processor.metatrace_categories_pb2', _globals)
+_builder.BuildTopDescriptorsAndMessages(
+    DESCRIPTOR, 'protos.perfetto.trace_processor.metatrace_categories_pb2',
+    _globals)
 if not _descriptor._USE_C_DESCRIPTORS:
   DESCRIPTOR._loaded_options = None
-  _globals['_METATRACECATEGORIES']._serialized_start=79
-  _globals['_METATRACECATEGORIES']._serialized_end=204
+  _globals['_METATRACECATEGORIES']._serialized_start = 79
+  _globals['_METATRACECATEGORIES']._serialized_end = 204
 # @@protoc_insertion_point(module_scope)
