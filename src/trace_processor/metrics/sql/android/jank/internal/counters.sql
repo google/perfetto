2ea46d6146bf5c592ccf603e6e0f07fdb35d0526
devianb <devarshimb@google.com>
2025-06-19 07:09:05 +0000

    metric changes

--- src/trace_processor/metrics/sql/android/jank/internal/counters.sql
+++ src/trace_processor/metrics/sql/android/jank/internal/counters.sql
@@ -12,6 +12,7 @@
 -- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
+INCLUDE PERFETTO MODULE android.cujs.sysui_cujs;
 
 DROP TABLE IF EXISTS android_jank_cuj_counter;
 CREATE PERFETTO TABLE android_jank_cuj_counter AS
@@ -24,7 +25,7 @@ WITH cuj_counter_track AS (
     -- take the name of the counter after #
     STR_SPLIT(track.name, '#', 1) AS counter_name
   FROM process_counter_track track
-  JOIN android_jank_cuj USING (upid)
+  JOIN android_sysui_jank_cujs USING (upid)
   WHERE track.name GLOB 'J<*>#*'
 )
 SELECT
@@ -50,32 +51,6 @@ WHERE
   AND ($ts_max IS NULL OR ts <= $ts_max)
 ORDER BY ts ASC LIMIT 1;
 
-DROP TABLE IF EXISTS cuj_marker_missed_callback;
-CREATE PERFETTO TABLE cuj_marker_missed_callback AS
-SELECT
-  marker_track.name AS cuj_slice_name,
-  marker.ts,
-  marker.name AS marker_name
-FROM slice marker
-JOIN track marker_track on  marker_track.id = marker.track_id
-WHERE marker.name GLOB '*FT#Missed*';
-
-CREATE OR REPLACE PERFETTO FUNCTION android_missed_vsyncs_for_callback(
-  cuj_slice_name STRING,
-  ts_min INT,
-  ts_max INT,
-  callback_missed STRING
-)
-RETURNS INT AS
-SELECT IFNULL(SUM(marker_name GLOB $callback_missed), 0)
-FROM cuj_marker_missed_callback
-WHERE
-  cuj_slice_name = $cuj_slice_name
-  AND ts >= $ts_min
-  AND ($ts_max IS NULL OR ts <= $ts_max)
-ORDER BY ts ASC
-LIMIT 1;
-
 DROP TABLE IF EXISTS android_jank_cuj_counter_metrics;
 CREATE PERFETTO TABLE android_jank_cuj_counter_metrics AS
 -- Order CUJs to get the ts of the next CUJ with the same name.
@@ -96,7 +71,7 @@ WITH cujs_ordered AS (
       ELSE MAX(ts, ts_end - 4000000)
     END AS ts_earliest_allowed_counter,
     LEAD(ts_end) OVER (PARTITION BY cuj_name ORDER BY ts_end ASC) AS ts_end_next_cuj
-  FROM android_jank_cuj
+  FROM android_sysui_jank_cujs
 )
 SELECT
   cuj_id,
@@ -110,6 +85,6 @@ SELECT
   android_jank_cuj_counter_value(cuj_name, 'maxSuccessiveMissedFrames', ts_earliest_allowed_counter, ts_end_next_cuj) AS missed_frames_max_successive,
   -- convert ms to nanos to align with the unit for `dur` in the other tables
   android_jank_cuj_counter_value(cuj_name, 'maxFrameTimeMillis', ts_earliest_allowed_counter, ts_end_next_cuj) * 1000000 AS frame_dur_max,
-  android_missed_vsyncs_for_callback(cuj_slice_name, ts_earliest_allowed_counter, ts_end_next_cuj, '*SF*') AS sf_callback_missed_frames,
-  android_missed_vsyncs_for_callback(cuj_slice_name, ts_earliest_allowed_counter, ts_end_next_cuj, '*HWUI*') AS hwui_callback_missed_frames
+  _android_missed_vsyncs_for_callback(cuj_slice_name, ts_earliest_allowed_counter, ts_end_next_cuj, '*SF*') AS sf_callback_missed_frames,
+  _android_missed_vsyncs_for_callback(cuj_slice_name, ts_earliest_allowed_counter, ts_end_next_cuj, '*HWUI*') AS hwui_callback_missed_frames
 FROM cujs_ordered cuj;
