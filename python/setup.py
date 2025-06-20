93610093e1e7e1d8ee11744fef4cd9416496ff91
Lalit Maganti <lalitm@google.com>
2025-06-20 23:55:21 +0000

    Revert "python: fix setup.py script"
    This reverts commit c0378897539640f6c028cd7e431f83562a052443.

--- python/setup.py
+++ python/setup.py
@@ -6,7 +6,7 @@ setup(
         'perfetto',
         'perfetto.batch_trace_processor',
         'perfetto.common',
-        'perfetto.protos.perfetto.trace',
+        'perfetto.protos',
         'perfetto.trace_builder',
         'perfetto.trace_processor',
         'perfetto.trace_uri_resolver',
@@ -15,7 +15,7 @@ setup(
         'perfetto.trace_processor': ['*.descriptor'],
     },
     include_package_data=True,
-    version='0.13.0',
+    version='0.12.0',
     license='apache-2.0',
     description='Python APIs and bindings for Perfetto (perfetto.dev)',
     author='Perfetto',
@@ -26,12 +26,13 @@ setup(
     install_requires=[
         'protobuf',
     ],
-    extras_require={
-        'numpy': ['numpy'],
-        'pandas': ['pandas'],
-    },
+    extras_requires=[
+        'numpy',
+        'pandas',
+    ],
     classifiers=[
         'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: Apache Software License',
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
