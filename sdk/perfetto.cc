332f018d1fcf2bc52e262d9700a31ba4529d147e
KananSu <149131467+KananSu@users.noreply.github.com>
2025-06-25 14:40:05 +0800

    Amalgamated source for v50.1

--- sdk/perfetto.cc
+++ sdk/perfetto.cc
@@ -31467,6 +31467,8 @@ void TrackEvent_LegacyEvent::Serialize(::protozero::Message* msg) const {
 // Intentionally empty (crbug.com/998165)
 // gen_amalgamated begin source: gen/protos/perfetto/trace/ftrace/workqueue.pbzero.cc
 // Intentionally empty (crbug.com/998165)
+// gen_amalgamated begin source: gen/protos/perfetto/trace/js_profile/js_profile.pbzero.cc
+// Intentionally empty (crbug.com/998165)
 // gen_amalgamated begin source: gen/protos/perfetto/trace/perfetto/perfetto_metatrace.pbzero.cc
 // Intentionally empty (crbug.com/998165)
 // gen_amalgamated begin source: gen/protos/perfetto/trace/perfetto/tracing_service_event.pbzero.cc
@@ -44208,8 +44210,8 @@ const char* GetVersionCode();
 #ifndef GEN_PERFETTO_VERSION_GEN_H_
 #define GEN_PERFETTO_VERSION_GEN_H_
 
-#define PERFETTO_VERSION_STRING() "v50.1-da5fcf015"
-#define PERFETTO_VERSION_SCM_REVISION() "da5fcf015ddda87445f1a359604d7c6f4077e10a"
+#define PERFETTO_VERSION_STRING() "v50.1-9e57a2f04"
+#define PERFETTO_VERSION_SCM_REVISION() "9e57a2f0406127b5ce7513646e246396a16cb7b3"
 
 #endif  // GEN_PERFETTO_VERSION_GEN_H_
 /*
@@ -60062,63 +60064,6 @@ class PERFETTO_EXPORT_COMPONENT UnixSocket {
 }  // namespace perfetto
 
 #endif  // INCLUDE_PERFETTO_EXT_BASE_UNIX_SOCKET_H_
-// gen_amalgamated begin header: src/base/vm_sockets.h
-/*
- * Copyright (C) 2023 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-#ifndef SRC_BASE_VM_SOCKETS_H_
-#define SRC_BASE_VM_SOCKETS_H_
-
-// gen_amalgamated expanded: #include "perfetto/base/build_config.h"
-
-#if PERFETTO_BUILDFLAG(PERFETTO_OS_LINUX) || \
-    PERFETTO_BUILDFLAG(PERFETTO_OS_ANDROID)
-
-#include <sys/socket.h>
-
-#if PERFETTO_BUILDFLAG(PERFETTO_OS_QNX)
-// Requires the QNX Advanced Virtualization Framework
-#include <vm_sockets.h>
-#elif defined(AF_VSOCK)
-// Use system vm_socket.h if available.
-#include <linux/vm_sockets.h>
-#else  // defined(AF_SOCK)
-// Fallback and use the stripped copy from the UAPI vm_sockets.h.
-
-#include <stdint.h>  // For uint8_t.
-
-#define AF_VSOCK 40
-
-struct sockaddr_vm {
-  sa_family_t svm_family;
-  unsigned short svm_reserved1;
-  unsigned int svm_port;
-  unsigned int svm_cid;
-  uint8_t svm_flags;
-  unsigned char svm_zero[sizeof(struct sockaddr) - sizeof(sa_family_t) -
-                         sizeof(unsigned short) - sizeof(unsigned int) -
-                         sizeof(unsigned int) - sizeof(uint8_t)];
-};
-
-#endif  // defined(AF_SOCK)
-
-#endif  // PERFETTO_BUILDFLAG(PERFETTO_OS_LINUX) ||
-        // PERFETTO_BUILDFLAG(PERFETTO_OS_ANDROID)
-
-#endif  // SRC_BASE_VM_SOCKETS_H_
 /*
  * Copyright (C) 2017 The Android Open Source Project
  *
