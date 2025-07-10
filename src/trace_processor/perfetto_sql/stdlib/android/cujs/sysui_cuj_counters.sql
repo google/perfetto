d92008ad12cb90fc9bcbba06f5d6791f4d553d59
devianb <devarshimb@google.com>
2025-07-10 09:52:49 +0000

    Improve documentation.

--- src/trace_processor/perfetto_sql/stdlib/android/cujs/sysui_cuj_counters.sql
+++ src/trace_processor/perfetto_sql/stdlib/android/cujs/sysui_cuj_counters.sql
@@ -13,9 +13,15 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 
+-- This module captures the various counter values associated with Jank CUJs. These counters can be
+-- missedFrames, missedSFFrames, missedCallbacks etc. These tables are beneficial while querying
+-- for jank related metrics.
 INCLUDE PERFETTO MODULE android.cujs.sysui_cujs;
 
--- Extract CUJ and missed callback slice names.
+-- A missed callback in a perfetto trace means the ftrace was unable to capture callback events
+-- for an operation. For eg. a SFMissedCallback indicates that events related to surfaceflinger
+-- operations, such as vsync synchronization, transaction/presentation callbacks were not captured.
+-- This view captures all such missed callback slices.
 CREATE PERFETTO VIEW _marker_missed_callback AS
 SELECT
   marker_track.name AS cuj_slice_name,
@@ -27,13 +33,13 @@ JOIN track AS marker_track
 WHERE
   marker.name GLOB '*FT#Missed*';
 
--- Extract count of a missed callback for a specific CUJ.
+-- Extract count of a given missed callback for a specific CUJ.
 CREATE PERFETTO FUNCTION _android_cuj_missed_vsyncs_for_callback(
     -- name of the cuj slice.
     cuj_slice_name STRING,
-    -- min ts.
+    -- Min timestamp after which the missed callback value should be considered.
     ts_min TIMESTAMP,
-    -- max ts.
+    -- Max timestamp before which the missed callback value should be considered.
     ts_max TIMESTAMP,
     -- missed callback.
     callback_missed STRING
@@ -83,7 +89,9 @@ JOIN cuj_counter_track
 CREATE PERFETTO FUNCTION _android_jank_cuj_counter_value(
     cuj_name STRING,
     counter_name STRING,
+    -- Min timestamp after which the CUJ counter value should be considered.
     ts_min TIMESTAMP,
+    -- Max timestamp before which the CUJ counter value should be considered.
     ts_max TIMESTAMP
 )
 RETURNS LONG AS
