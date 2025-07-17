ba94cbcf90fec0bd3371d023279977f3617e7551
Harrison Teplitz <hteplitz@google.com>
2025-07-17 11:18:22 -0700

    test change for perfetto

--- ui/src/frontend/globals.ts
+++ ui/src/frontend/globals.ts
@@ -26,6 +26,7 @@ class Globals {
 
   // WARNING: do not change/rename/move without considering impact on the
   // internal_user script.
+  // Test test
   get extraSqlPackages() {
     return AppImpl.instance.extraSqlPackages;
   }
