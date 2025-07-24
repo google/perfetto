f6e1d1dddc7116b6322fd993d1cd2cca2747e6f5
Nikita Ioffe <ioffe@google.com>
2025-07-24 14:35:58 +0000

    Re-run tools/{run_ftrace_proto_gen,gen_all}

--- src/trace_processor/importers/ftrace/ftrace_descriptors.cc
+++ src/trace_processor/importers/ftrace/ftrace_descriptors.cc
@@ -24,7 +24,7 @@ namespace perfetto {
 namespace trace_processor {
 namespace {
 
-std::array<FtraceMessageDescriptor, 579> descriptors{{
+std::array<FtraceMessageDescriptor, 580> descriptors{{
     {nullptr, 0, {}},
     {nullptr, 0, {}},
     {nullptr, 0, {}},
@@ -6438,6 +6438,16 @@ std::array<FtraceMessageDescriptor, 579> descriptors{{
             {"res_a4", ProtoSchemaType::kUint64},
             {"handled", ProtoSchemaType::kInt32},
             {"err", ProtoSchemaType::kInt32},
+        },
+    },
+    {
+        "iommu_idmap",
+        3,
+        {
+            {},
+            {"from", ProtoSchemaType::kUint64},
+            {"to", ProtoSchemaType::kUint64},
+            {"prot", ProtoSchemaType::kInt32},
         },
     },
 }};
