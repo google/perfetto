46b9552dfe89e2525d0d2c6c05335afc70305b10
omarelfouly <fouly@google.com>
2025-07-25 12:58:37 +0000

    tp: stdlib: adds __linux_perf_etm_metadata to get file_name and rel_pc

---
+++ src/trace_processor/perfetto_sql/stdlib/linux/perf/etm.sql
@@ -0,0 +1,41 @@
+--
+-- Copyright 2025 The Android Open Source Project
+--
+-- Licensed under the Apache License, Version 2.0 (the 'License');
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     https://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an 'AS IS' BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+-- Given a trace_id, returns the file_name and relative program counter for
+-- each instruction in that trace.
+CREATE PERFETTO FUNCTION __linux_perf_etm_metadata(
+    -- ID of the trace.
+    trace_id INTEGER
+)
+RETURNS TABLE (
+  -- Name of the file containing the instruction.
+  file_name STRING,
+  -- Relative program counter of the instruction.
+  rel_pc INTEGER
+) AS
+SELECT
+  __intrinsic_file.name AS file_name,
+  __intrinsic_etm_iterate_instruction_range.address - stack_profile_mapping.start + stack_profile_mapping.exact_offset + __intrinsic_elf_file.load_bias AS rel_pc
+FROM __intrinsic_etm_decode_trace
+JOIN __intrinsic_etm_iterate_instruction_range
+  ON __intrinsic_etm_decode_trace.instruction_range = __intrinsic_etm_iterate_instruction_range.instruction_range
+JOIN stack_profile_mapping
+  ON __intrinsic_etm_decode_trace.mapping_id = stack_profile_mapping.id
+JOIN __intrinsic_elf_file
+  ON stack_profile_mapping.build_id = __intrinsic_elf_file.build_id
+JOIN __intrinsic_file
+  ON __intrinsic_elf_file.file_id = __intrinsic_file.id
+WHERE
+  __intrinsic_etm_decode_trace.trace_id = $trace_id;
