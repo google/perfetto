google/perfetto

commit 5f140065223c88ef4755057dc4fa16d24768f5a5

fix mingw builds (#2397)
Upstreaming patches from JuliaPackaging/Yggdrasil#11770. One remaining
issue is that `NetworkPacketEvent_Decoder` in the sdk header defines a
member function with the name `interface`, which conflicts with a mingw
macro, but I wasn't able to figure out from where that definition was
generated

src/base/ctrl_c_handler.cc
@@ -21,8 +21,8 @@
 #include "perfetto/base/logging.h"
 
 #if PERFETTO_BUILDFLAG(PERFETTO_OS_WIN)
-#include <Windows.h>
 #include <io.h>
+#include <windows.h>
 #else
 #include <signal.h>
 #include <unistd.h>

src/base/event_fd.cc
@@ -20,8 +20,8 @@
 #include <stdint.h>
 
 #if PERFETTO_BUILDFLAG(PERFETTO_OS_WIN)
-#include <Windows.h>
 #include <synchapi.h>
+#include <windows.h>
 #elif PERFETTO_BUILDFLAG(PERFETTO_OS_QNX)
 #include <unistd.h>
 #elif PERFETTO_BUILDFLAG(PERFETTO_OS_LINUX) || \

src/base/file_utils.cc
@@ -36,10 +36,10 @@
 #include "perfetto/ext/base/utils.h"
 
 #if PERFETTO_BUILDFLAG(PERFETTO_OS_WIN)
-#include <Windows.h>
 #include <direct.h>
 #include <io.h>
 #include <stringapiset.h>
+#include <windows.h>
 #else
 #include <dirent.h>
 #include <unistd.h>

src/base/pipe.cc
@@ -21,8 +21,8 @@
 #include <fcntl.h>  // For O_BINARY (Windows) and F_SETxx (UNIX)
 
 #if PERFETTO_BUILDFLAG(PERFETTO_OS_WIN)
-#include <Windows.h>
 #include <namedpipeapi.h>
+#include <windows.h>
 #else
 #include <sys/types.h>
 #include <unistd.h>

src/base/temp_file.cc
@@ -23,10 +23,10 @@
 #include <string.h>
 
 #if PERFETTO_BUILDFLAG(PERFETTO_OS_WIN)
-#include <Windows.h>
 #include <direct.h>
 #include <fileapi.h>
 #include <io.h>
+#include <windows.h>
 #else
 #include <unistd.h>
 #endif

src/base/unix_task_runner.cc
@@ -23,8 +23,8 @@
 #include <stdlib.h>
 
 #if PERFETTO_BUILDFLAG(PERFETTO_OS_WIN)
-#include <Windows.h>
 #include <synchapi.h>
+#include <windows.h>
 #else
 #include <unistd.h>
 #endif

src/base/utils.cc
@@ -57,9 +57,9 @@
 #endif  // OS_LINUX | OS_ANDROID
 
 #if PERFETTO_BUILDFLAG(PERFETTO_OS_WIN)
-#include <Windows.h>
 #include <io.h>
 #include <malloc.h>  // For _aligned_malloc().
+#include <windows.h>
 #endif
 
 #if PERFETTO_BUILDFLAG(PERFETTO_OS_ANDROID)

src/base/test/vm_test_utils.cc
@@ -28,7 +28,7 @@
 #include <vector>
 
 // clang-format off
-#include <Windows.h>
+#include <windows.h>
 #include <Psapi.h>
 // clang-format on
 #else  // PERFETTO_BUILDFLAG(PERFETTO_OS_WIN)

commit 7632e6685852f7fb21c848a1f5fdf21ce9bcd04d

windows: restore include order
windows.h has to be included before headers like synchapi.h. Restore the
ordering before the commit below, introducing extra newlines as
separators to prevent autoformatters from reordering it back.

Fixes: 5f14006522 fix mingw builds (#2397)

src/base/ctrl_c_handler.cc
@@ -21,8 +21,9 @@
 #include "perfetto/base/logging.h"
 
 #if PERFETTO_BUILDFLAG(PERFETTO_OS_WIN)
-#include <io.h>
 #include <windows.h>
+
+#include <io.h>
 #else
 #include <signal.h>
 #include <unistd.h>

src/base/event_fd.cc
@@ -20,8 +20,9 @@
 #include <stdint.h>
 
 #if PERFETTO_BUILDFLAG(PERFETTO_OS_WIN)
-#include <synchapi.h>
 #include <windows.h>
+
+#include <synchapi.h>
 #elif PERFETTO_BUILDFLAG(PERFETTO_OS_QNX)
 #include <unistd.h>
 #elif PERFETTO_BUILDFLAG(PERFETTO_OS_LINUX) || \

src/base/file_utils.cc
@@ -36,10 +36,11 @@
 #include "perfetto/ext/base/utils.h"
 
 #if PERFETTO_BUILDFLAG(PERFETTO_OS_WIN)
+#include <windows.h>
+
 #include <direct.h>
 #include <io.h>
 #include <stringapiset.h>
-#include <windows.h>
 #else
 #include <dirent.h>
 #include <unistd.h>

src/base/pipe.cc
@@ -21,8 +21,9 @@
 #include <fcntl.h>  // For O_BINARY (Windows) and F_SETxx (UNIX)
 
 #if PERFETTO_BUILDFLAG(PERFETTO_OS_WIN)
-#include <namedpipeapi.h>
 #include <windows.h>
+
+#include <namedpipeapi.h>
 #else
 #include <sys/types.h>
 #include <unistd.h>

src/base/temp_file.cc
@@ -23,10 +23,11 @@
 #include <string.h>
 
 #if PERFETTO_BUILDFLAG(PERFETTO_OS_WIN)
+#include <windows.h>
+
 #include <direct.h>
 #include <fileapi.h>
 #include <io.h>
-#include <windows.h>
 #else
 #include <unistd.h>
 #endif

src/base/unix_task_runner.cc
@@ -23,8 +23,9 @@
 #include <stdlib.h>
 
 #if PERFETTO_BUILDFLAG(PERFETTO_OS_WIN)
-#include <synchapi.h>
 #include <windows.h>
+
+#include <synchapi.h>
 #else
 #include <unistd.h>
 #endif

src/base/utils.cc
@@ -57,9 +57,10 @@
 #endif  // OS_LINUX | OS_ANDROID
 
 #if PERFETTO_BUILDFLAG(PERFETTO_OS_WIN)
+#include <windows.h>
+
 #include <io.h>
 #include <malloc.h>  // For _aligned_malloc().
-#include <windows.h>
 #endif
 
 #if PERFETTO_BUILDFLAG(PERFETTO_OS_ANDROID)

src/base/test/vm_test_utils.cc
@@ -29,6 +29,7 @@
 
 // clang-format off
 #include <windows.h>
+
 #include <Psapi.h>
 // clang-format on
 #else  // PERFETTO_BUILDFLAG(PERFETTO_OS_WIN)

src/base/threading/channel_unittest.cc
@@ -26,8 +26,9 @@
 #include "test/gtest_and_gmock.h"
 
 #if PERFETTO_BUILDFLAG(PERFETTO_OS_WIN)
-#include <synchapi.h>
 #include <windows.h>
+
+#include <synchapi.h>
 #else
 #include <poll.h>
 #endif