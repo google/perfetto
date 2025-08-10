afba77081f73e0c0a2c2af3fb48543327699a5ff
Peng Jiang <anything.j@gmail.com>
2025-08-10 11:01:02 +0100

    add unit test for getParamValues

--- ui/src/core/plugin_manager_unittest.ts
+++ ui/src/core/plugin_manager_unittest.ts
@@ -13,34 +13,77 @@
 // limitations under the License.
 
 import {parseAndSplitParams} from './plugin_manager';
+import {getParamValues} from './plugin_manager';
 
+test('Basic splitting', () => {
+  const pluginParams = 'plugin-1:"something", plugin-2:"something"';
 
-test("Basic splitting", () => {
-  const pluginParams = 'plugin-1:"something", plugin-2:"something"'
-
-  expect(parseAndSplitParams(pluginParams)).toEqual(['plugin-1:"something"', 'plugin-2:"something"']);
+  expect(parseAndSplitParams(pluginParams)).toEqual([
+    'plugin-1:"something"',
+    'plugin-2:"something"',
+  ]);
 });
 
-test("Basic splitting without quotes", () => {
-  const pluginParams = 'plugin-1:something, plugin-2:something'
+test('Basic splitting without quotes', () => {
+  const pluginParams = 'plugin-1:something, plugin-2:something';
 
-  expect(parseAndSplitParams(pluginParams)).toEqual(['plugin-1:something', 'plugin-2:something']);
+  expect(parseAndSplitParams(pluginParams)).toEqual([
+    'plugin-1:something',
+    'plugin-2:something',
+  ]);
 });
 
-test("Comma inside quotes", () => {
-  const pluginParams = 'plugin-1:"some,thing", plugin-2:"other"'
+test('Comma inside quotes', () => {
+  const pluginParams = 'plugin-1:"some,thing", plugin-2:"other"';
 
-  expect(parseAndSplitParams(pluginParams)).toEqual(['plugin-1:"some,thing"', 'plugin-2:"other"']);
+  expect(parseAndSplitParams(pluginParams)).toEqual([
+    'plugin-1:"some,thing"',
+    'plugin-2:"other"',
+  ]);
 });
 
-test("Comma inside for both quotes", () => {
-  const pluginParams = 'plugin-1:"some,thing", plugin-2:"other, thing"'
+test('Comma inside for both quotes', () => {
+  const pluginParams = 'plugin-1:"some,thing", plugin-2:"other, thing"';
 
-  expect(parseAndSplitParams(pluginParams)).toEqual(['plugin-1:"some,thing"', 'plugin-2:"other, thing"']);
+  expect(parseAndSplitParams(pluginParams)).toEqual([
+    'plugin-1:"some,thing"',
+    'plugin-2:"other, thing"',
+  ]);
 });
 
-test("Empty pluginParams", () => {
-  const pluginParams = ''
+test('Empty pluginParams', () => {
+  const pluginParams = '';
 
   expect(parseAndSplitParams(pluginParams)).toEqual([]);
+});
+
+test('Returns values for matching paramName', () => {
+  const pluginParams = [
+    'parameter-1=(value1--value2--value3)',
+    'parameter-2=(other1--other2)',
+  ];
+  expect(getParamValues(pluginParams, 'parameter-1')).toEqual([
+    'value1',
+    'value2',
+    'value3',
+  ]);
+});
+test('Trims whitespace around values', () => {
+  const pluginParams = ['parameter-1=( value1 -- value2 -- value3 )'];
+  expect(getParamValues(pluginParams, 'parameter-1')).toEqual([
+    'value1',
+    'value2',
+    'value3',
+  ]);
+});
+test('Returns empty array if paramName not found', () => {
+  const pluginParams = ['parameter-1=(value1--value2)'];
+  expect(getParamValues(pluginParams, 'parameter-2')).toEqual([]);
+});
+test('Returns empty array if no matching pattern', () => {
+  const pluginParams = ['parameter-1=value1--value2'];
+  expect(getParamValues(pluginParams, 'parameter-1')).toEqual([]);
+});
+test('Returns empty array for empty input', () => {
+  expect(getParamValues([], 'parameter-1')).toEqual([]);
 });
