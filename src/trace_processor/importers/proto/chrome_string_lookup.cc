af6879d3e8cc9833fbc50712654d1f4c191df325
Joe Mason <joenotcharles@google.com>
2025-08-12 21:13:42 +0000

    Use a switch instead of maps and arrays

--- src/trace_processor/importers/proto/chrome_string_lookup.cc
+++ src/trace_processor/importers/proto/chrome_string_lookup.cc
@@ -16,8 +16,6 @@
 
 #include "src/trace_processor/importers/proto/chrome_string_lookup.h"
 
-#include <map>
-
 #include "perfetto/ext/base/utils.h"
 #include "protos/perfetto/trace/track_event/chrome_legacy_ipc.pbzero.h"
 #include "protos/perfetto/trace/track_event/chrome_thread_descriptor.pbzero.h"
@@ -32,168 +30,235 @@ namespace trace_processor {
 
 namespace {
 
-struct ProcessName {
-  chrome_enums::ProcessType type;
-  const char* name;
-};
+// By design, these switches handle unrecognized enum values with a default
+// fallback, so that new entries can be added to
+// protos/third_party/chromium_chrome_enums.proto without immediately updating
+// the list of names.
+#if defined(__GNUC__)  // clang also supports GCC syntax.
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wcovered-switch-default"
+#pragma GCC diagnostic ignored "-Wswitch-enum"
+// -Wswitch includes all warnings from -Wswitch-enum except "enumeration value
+// not explicitly handled in switch".
+#pragma GCC diagnostic error "-Wswitch"
+#endif  // __GNUC__
 
-constexpr ProcessName kProcessNames[] = {
-    {chrome_enums::PROCESS_UNSPECIFIED, nullptr},
-    {chrome_enums::PROCESS_BROWSER, "Browser"},
-    {chrome_enums::PROCESS_RENDERER, "Renderer"},
-    {chrome_enums::PROCESS_UTILITY, "Utility"},
-    {chrome_enums::PROCESS_ZYGOTE, "SandboxHelper"},
-    {chrome_enums::PROCESS_GPU, "Gpu"},
-    {chrome_enums::PROCESS_PPAPI_PLUGIN, "PpapiPlugin"},
-    {chrome_enums::PROCESS_PPAPI_BROKER, "PpapiBroker"},
-    {chrome_enums::PROCESS_SERVICE_NETWORK,
-     "Service: network.mojom.NetworkService"},
-    {chrome_enums::PROCESS_SERVICE_TRACING,
-     "Service: tracing.mojom.TracingService"},
-    {chrome_enums::PROCESS_SERVICE_STORAGE,
-     "Service: storage.mojom.StorageService"},
-    {chrome_enums::PROCESS_SERVICE_AUDIO, "Service: audio.mojom.AudioService"},
-    {chrome_enums::PROCESS_SERVICE_DATA_DECODER,
-     "Service: data_decoder.mojom.DataDecoderService"},
-    {chrome_enums::PROCESS_SERVICE_UTIL_WIN, "Service: chrome.mojom.UtilWin"},
-    {chrome_enums::PROCESS_SERVICE_PROXY_RESOLVER,
-     "Service: proxy_resolver.mojom.ProxyResolverFactory"},
-    {chrome_enums::PROCESS_SERVICE_CDM,
-     "Service: media.mojom.CdmServiceBroker"},
-    {chrome_enums::PROCESS_SERVICE_MEDIA_FOUNDATION,
-     "Service: media.mojom.MediaFoundationServiceBroker"},
-    {chrome_enums::PROCESS_SERVICE_VIDEO_CAPTURE,
-     "Service: video_capture.mojom.VideoCaptureService"},
-    {chrome_enums::PROCESS_SERVICE_UNZIPPER, "Service: unzip.mojom.Unzipper"},
-    {chrome_enums::PROCESS_SERVICE_MIRRORING,
-     "Service: mirroring.mojom.MirroringService"},
-    {chrome_enums::PROCESS_SERVICE_FILEPATCHER,
-     "Service: patch.mojom.FilePatcher"},
-    {chrome_enums::PROCESS_SERVICE_TTS,
-     "Service: chromeos.tts.mojom.TtsService"},
-    {chrome_enums::PROCESS_SERVICE_PRINTING,
-     "Service: printing.mojom.PrintingService"},
-    {chrome_enums::PROCESS_SERVICE_QUARANTINE,
-     "Service: quarantine.mojom.Quarantine"},
-    {chrome_enums::PROCESS_SERVICE_CROS_LOCALSEARCH,
-     "Service: chromeos.local_search_service.mojom.LocalSearchService"},
-    {chrome_enums::PROCESS_SERVICE_CROS_ASSISTANT_AUDIO_DECODER,
-     "Service: chromeos.assistant.mojom.AssistantAudioDecoderFactory"},
-    {chrome_enums::PROCESS_SERVICE_FILEUTIL,
-     "Service: chrome.mojom.FileUtilService"},
-    {chrome_enums::PROCESS_SERVICE_PRINTCOMPOSITOR,
-     "Service: printing.mojom.PrintCompositor"},
-    {chrome_enums::PROCESS_SERVICE_PAINTPREVIEW,
-     "Service: paint_preview.mojom.PaintPreviewCompositorCollection"},
-    {chrome_enums::PROCESS_SERVICE_SPEECHRECOGNITION,
-     "Service: media.mojom.SpeechRecognitionService"},
-    {chrome_enums::PROCESS_SERVICE_XRDEVICE,
-     "Service: device.mojom.XRDeviceService"},
-    {chrome_enums::PROCESS_SERVICE_READICON,
-     "Service: chrome.mojom.UtilReadIcon"},
-    {chrome_enums::PROCESS_SERVICE_LANGUAGEDETECTION,
-     "Service: language_detection.mojom.LanguageDetectionService"},
-    {chrome_enums::PROCESS_SERVICE_SHARING, "Service: sharing.mojom.Sharing"},
-    {chrome_enums::PROCESS_SERVICE_MEDIAPARSER,
-     "Service: chrome.mojom.MediaParserFactory"},
-    {chrome_enums::PROCESS_SERVICE_QRCODEGENERATOR,
-     "Service: qrcode_generator.mojom.QRCodeService"},
-    {chrome_enums::PROCESS_SERVICE_PROFILEIMPORT,
-     "Service: chrome.mojom.ProfileImport"},
-    {chrome_enums::PROCESS_SERVICE_IME,
-     "Service: chromeos.ime.mojom.ImeService"},
-    {chrome_enums::PROCESS_SERVICE_RECORDING,
-     "Service: recording.mojom.RecordingService"},
-    {chrome_enums::PROCESS_SERVICE_SHAPEDETECTION,
-     "Service: shape_detection.mojom.ShapeDetectionService"},
-    {chrome_enums::PROCESS_RENDERER_EXTENSION, "Extension Renderer"},
-};
+// Returns a name, which may be null, for `process_type`.
+const char* GetProcessNameString(
+    chrome_enums::ProcessType process_type,
+    bool ignore_predefined_thread_types_for_testing) {
+  if (!ignore_predefined_thread_types_for_testing) {
+    switch (process_type) {
+      case chrome_enums::PROCESS_UNSPECIFIED:
+        return nullptr;
+      case chrome_enums::PROCESS_BROWSER:
+        return "Browser";
+      case chrome_enums::PROCESS_RENDERER:
+        return "Renderer";
+      case chrome_enums::PROCESS_UTILITY:
+        return "Utility";
+      case chrome_enums::PROCESS_ZYGOTE:
+        return "SandboxHelper";
+      case chrome_enums::PROCESS_GPU:
+        return "Gpu";
+      case chrome_enums::PROCESS_PPAPI_PLUGIN:
+        return "PpapiPlugin";
+      case chrome_enums::PROCESS_PPAPI_BROKER:
+        return "PpapiBroker";
+      case chrome_enums::PROCESS_SERVICE_NETWORK:
+        return "Service: network.mojom.NetworkService";
+      case chrome_enums::PROCESS_SERVICE_TRACING:
+        return "Service: tracing.mojom.TracingService";
+      case chrome_enums::PROCESS_SERVICE_STORAGE:
+        return "Service: storage.mojom.StorageService";
+      case chrome_enums::PROCESS_SERVICE_AUDIO:
+        return "Service: audio.mojom.AudioService";
+      case chrome_enums::PROCESS_SERVICE_DATA_DECODER:
+        return "Service: data_decoder.mojom.DataDecoderService";
+      case chrome_enums::PROCESS_SERVICE_UTIL_WIN:
+        return "Service: chrome.mojom.UtilWin";
+      case chrome_enums::PROCESS_SERVICE_PROXY_RESOLVER:
+        return "Service: proxy_resolver.mojom.ProxyResolverFactory";
+      case chrome_enums::PROCESS_SERVICE_CDM:
+        return "Service: media.mojom.CdmServiceBroker";
+      case chrome_enums::PROCESS_SERVICE_MEDIA_FOUNDATION:
+        return "Service: media.mojom.MediaFoundationServiceBroker";
+      case chrome_enums::PROCESS_SERVICE_VIDEO_CAPTURE:
+        return "Service: video_capture.mojom.VideoCaptureService";
+      case chrome_enums::PROCESS_SERVICE_UNZIPPER:
+        return "Service: unzip.mojom.Unzipper";
+      case chrome_enums::PROCESS_SERVICE_MIRRORING:
+        return "Service: mirroring.mojom.MirroringService";
+      case chrome_enums::PROCESS_SERVICE_FILEPATCHER:
+        return "Service: patch.mojom.FilePatcher";
+      case chrome_enums::PROCESS_SERVICE_TTS:
+        return "Service: chromeos.tts.mojom.TtsService";
+      case chrome_enums::PROCESS_SERVICE_PRINTING:
+        return "Service: printing.mojom.PrintingService";
+      case chrome_enums::PROCESS_SERVICE_QUARANTINE:
+        return "Service: quarantine.mojom.Quarantine";
+      case chrome_enums::PROCESS_SERVICE_CROS_LOCALSEARCH:
+        return "Service: "
+               "chromeos.local_search_service.mojom.LocalSearchService";
+      case chrome_enums::PROCESS_SERVICE_CROS_ASSISTANT_AUDIO_DECODER:
+        return "Service: chromeos.assistant.mojom.AssistantAudioDecoderFactory";
+      case chrome_enums::PROCESS_SERVICE_FILEUTIL:
+        return "Service: chrome.mojom.FileUtilService";
+      case chrome_enums::PROCESS_SERVICE_PRINTCOMPOSITOR:
+        return "Service: printing.mojom.PrintCompositor";
+      case chrome_enums::PROCESS_SERVICE_PAINTPREVIEW:
+        return "Service: paint_preview.mojom.PaintPreviewCompositorCollection";
+      case chrome_enums::PROCESS_SERVICE_SPEECHRECOGNITION:
+        return "Service: media.mojom.SpeechRecognitionService";
+      case chrome_enums::PROCESS_SERVICE_XRDEVICE:
+        return "Service: device.mojom.XRDeviceService";
+      case chrome_enums::PROCESS_SERVICE_READICON:
+        return "Service: chrome.mojom.UtilReadIcon";
+      case chrome_enums::PROCESS_SERVICE_LANGUAGEDETECTION:
+        return "Service: language_detection.mojom.LanguageDetectionService";
+      case chrome_enums::PROCESS_SERVICE_SHARING:
+        return "Service: sharing.mojom.Sharing";
+      case chrome_enums::PROCESS_SERVICE_MEDIAPARSER:
+        return "Service: chrome.mojom.MediaParserFactory";
+      case chrome_enums::PROCESS_SERVICE_QRCODEGENERATOR:
+        return "Service: qrcode_generator.mojom.QRCodeService";
+      case chrome_enums::PROCESS_SERVICE_PROFILEIMPORT:
+        return "Service: chrome.mojom.ProfileImport";
+      case chrome_enums::PROCESS_SERVICE_IME:
+        return "Service: chromeos.ime.mojom.ImeService";
+      case chrome_enums::PROCESS_SERVICE_RECORDING:
+        return "Service: recording.mojom.RecordingService";
+      case chrome_enums::PROCESS_SERVICE_SHAPEDETECTION:
+        return "Service: shape_detection.mojom.ShapeDetectionService";
+      case chrome_enums::PROCESS_RENDERER_EXTENSION:
+        return "Extension Renderer";
+      default:
+        // Fall through to the generated name.
+        break;
+    }
+  }
+  return chrome_enums::ProcessType_Name(process_type);
+}
 
-struct ThreadName {
-  ChromeThreadDescriptor::ThreadType type;
-  const char* name;
-};
+// Returns a name, which may be null, for `thread_type`.
+const char* GetThreadNameString(
+    ChromeThreadDescriptor::ThreadType thread_type,
+    bool ignore_predefined_thread_types_for_testing) {
+  if (!ignore_predefined_thread_types_for_testing) {
+    switch (thread_type) {
+      case ChromeThreadDescriptor::THREAD_UNSPECIFIED:
+        return nullptr;
+      case ChromeThreadDescriptor::THREAD_MAIN:
+        return "CrProcessMain";
+      case ChromeThreadDescriptor::THREAD_IO:
+        return "ChromeIOThread";
+      case ChromeThreadDescriptor::THREAD_NETWORK_SERVICE:
+        return "NetworkService";
+      case ChromeThreadDescriptor::THREAD_POOL_BG_WORKER:
+        return "ThreadPoolBackgroundWorker&";
+      case ChromeThreadDescriptor::THREAD_POOL_FG_WORKER:
+        return "ThreadPoolForegroundWorker&";
+      case ChromeThreadDescriptor::THREAD_POOL_BG_BLOCKING:
+        return "ThreadPoolSingleThreadBackgroundBlocking&";
+      case ChromeThreadDescriptor::THREAD_POOL_FG_BLOCKING:
+        return "ThreadPoolSingleThreadForegroundBlocking&";
+      case ChromeThreadDescriptor::THREAD_POOL_SERVICE:
+        return "ThreadPoolService";
+      case ChromeThreadDescriptor::THREAD_COMPOSITOR:
+        return "Compositor";
+      case ChromeThreadDescriptor::THREAD_VIZ_COMPOSITOR:
+        return "VizCompositorThread";
+      case ChromeThreadDescriptor::THREAD_COMPOSITOR_WORKER:
+        return "CompositorTileWorker&";
+      case ChromeThreadDescriptor::THREAD_SERVICE_WORKER:
+        return "ServiceWorkerThread&";
+      case ChromeThreadDescriptor::THREAD_MEMORY_INFRA:
+        return "MemoryInfra";
+      case ChromeThreadDescriptor::THREAD_SAMPLING_PROFILER:
+        return "StackSamplingProfiler";
 
-constexpr ThreadName kThreadNames[] = {
-    {ChromeThreadDescriptor::THREAD_UNSPECIFIED, nullptr},
-    {ChromeThreadDescriptor::THREAD_MAIN, "CrProcessMain"},
-    {ChromeThreadDescriptor::THREAD_IO, "ChromeIOThread"},
-    {ChromeThreadDescriptor::THREAD_NETWORK_SERVICE, "NetworkService"},
-    {ChromeThreadDescriptor::THREAD_POOL_BG_WORKER,
-     "ThreadPoolBackgroundWorker&"},
-    {ChromeThreadDescriptor::THREAD_POOL_FG_WORKER,
-     "ThreadPoolForegroundWorker&"},
-    {ChromeThreadDescriptor::THREAD_POOL_BG_BLOCKING,
-     "ThreadPoolSingleThreadBackgroundBlocking&"},
-    {ChromeThreadDescriptor::THREAD_POOL_FG_BLOCKING,
-     "ThreadPoolSingleThreadForegroundBlocking&"},
-    {ChromeThreadDescriptor::THREAD_POOL_SERVICE, "ThreadPoolService"},
-    {ChromeThreadDescriptor::THREAD_COMPOSITOR, "Compositor"},
-    {ChromeThreadDescriptor::THREAD_VIZ_COMPOSITOR, "VizCompositorThread"},
-    {ChromeThreadDescriptor::THREAD_COMPOSITOR_WORKER, "CompositorTileWorker&"},
-    {ChromeThreadDescriptor::THREAD_SERVICE_WORKER, "ServiceWorkerThread&"},
-    {ChromeThreadDescriptor::THREAD_MEMORY_INFRA, "MemoryInfra"},
-    {ChromeThreadDescriptor::THREAD_SAMPLING_PROFILER, "StackSamplingProfiler"},
+      case ChromeThreadDescriptor::THREAD_BROWSER_MAIN:
+        return "CrBrowserMain";
+      case ChromeThreadDescriptor::THREAD_RENDERER_MAIN:
+        return "CrRendererMain";
+      case ChromeThreadDescriptor::THREAD_CHILD_IO:
+        return "Chrome_ChildIOThread";
+      case ChromeThreadDescriptor::THREAD_BROWSER_IO:
+        return "Chrome_IOThread";
+      case ChromeThreadDescriptor::THREAD_UTILITY_MAIN:
+        return "CrUtilityMain";
+      case ChromeThreadDescriptor::THREAD_GPU_MAIN:
+        return "CrGpuMain";
+      case ChromeThreadDescriptor::THREAD_CACHE_BLOCKFILE:
+        return "CacheThread_BlockFile";
+      case ChromeThreadDescriptor::ChromeThreadDescriptor::THREAD_MEDIA:
+        return "Media";
+      case ChromeThreadDescriptor::THREAD_AUDIO_OUTPUTDEVICE:
+        return "AudioOutputDevice";
+      case ChromeThreadDescriptor::THREAD_GPU_MEMORY:
+        return "GpuMemoryThread";
+      case ChromeThreadDescriptor::THREAD_GPU_VSYNC:
+        return "GpuVSyncThread";
+      case ChromeThreadDescriptor::THREAD_DXA_VIDEODECODER:
+        return "DXVAVideoDecoderThread";
+      case ChromeThreadDescriptor::THREAD_BROWSER_WATCHDOG:
+        return "BrowserWatchdog";
+      case ChromeThreadDescriptor::THREAD_WEBRTC_NETWORK:
+        return "WebRTC_Network";
+      case ChromeThreadDescriptor::THREAD_WINDOW_OWNER:
+        return "Window owner thread";
+      case ChromeThreadDescriptor::THREAD_WEBRTC_SIGNALING:
+        return "WebRTC_Signaling";
+      case ChromeThreadDescriptor::THREAD_PPAPI_MAIN:
+        return "CrPPAPIMain";
+      case ChromeThreadDescriptor::THREAD_GPU_WATCHDOG:
+        return "GpuWatchdog";
+      case ChromeThreadDescriptor::THREAD_SWAPPER:
+        return "swapper";
+      case ChromeThreadDescriptor::THREAD_GAMEPAD_POLLING:
+        return "Gamepad polling thread";
+      case ChromeThreadDescriptor::THREAD_AUDIO_INPUTDEVICE:
+        return "AudioInputDevice";
+      case ChromeThreadDescriptor::THREAD_WEBRTC_WORKER:
+        return "WebRTC_Worker";
+      case ChromeThreadDescriptor::THREAD_WEBCRYPTO:
+        return "WebCrypto";
+      case ChromeThreadDescriptor::THREAD_DATABASE:
+        return "Database thread";
+      case ChromeThreadDescriptor::THREAD_PROXYRESOLVER:
+        return "Proxy Resolver";
+      case ChromeThreadDescriptor::THREAD_DEVTOOLSADB:
+        return "Chrome_DevToolsADBThread";
+      case ChromeThreadDescriptor::THREAD_NETWORKCONFIGWATCHER:
+        return "NetworkConfigWatcher";
+      case ChromeThreadDescriptor::THREAD_WASAPI_RENDER:
+        return "wasapi_render_thread";
+      case ChromeThreadDescriptor::THREAD_LOADER_LOCK_SAMPLER:
+        return "LoaderLockSampler";
+      case ChromeThreadDescriptor::THREAD_COMPOSITOR_GPU:
+        return "CompositorGpuThread";
+      default:
+        // Fall through to the generated name.
+        break;
+    }
+  }
+  return ChromeThreadDescriptor::ThreadType_Name(thread_type);
+}
 
-    {ChromeThreadDescriptor::THREAD_BROWSER_MAIN, "CrBrowserMain"},
-    {ChromeThreadDescriptor::THREAD_RENDERER_MAIN, "CrRendererMain"},
-    {ChromeThreadDescriptor::THREAD_CHILD_IO, "Chrome_ChildIOThread"},
-    {ChromeThreadDescriptor::THREAD_BROWSER_IO, "Chrome_IOThread"},
-    {ChromeThreadDescriptor::THREAD_UTILITY_MAIN, "CrUtilityMain"},
-    {ChromeThreadDescriptor::THREAD_GPU_MAIN, "CrGpuMain"},
-    {ChromeThreadDescriptor::THREAD_CACHE_BLOCKFILE, "CacheThread_BlockFile"},
-    {ChromeThreadDescriptor::ChromeThreadDescriptor::THREAD_MEDIA, "Media"},
-    {ChromeThreadDescriptor::THREAD_AUDIO_OUTPUTDEVICE, "AudioOutputDevice"},
-    {ChromeThreadDescriptor::THREAD_GPU_MEMORY, "GpuMemoryThread"},
-    {ChromeThreadDescriptor::THREAD_GPU_VSYNC, "GpuVSyncThread"},
-    {ChromeThreadDescriptor::THREAD_DXA_VIDEODECODER, "DXVAVideoDecoderThread"},
-    {ChromeThreadDescriptor::THREAD_BROWSER_WATCHDOG, "BrowserWatchdog"},
-    {
-        ChromeThreadDescriptor::THREAD_WEBRTC_NETWORK,
-        "WebRTC_Network",
-    },
-    {ChromeThreadDescriptor::THREAD_WINDOW_OWNER, "Window owner thread"},
-    {ChromeThreadDescriptor::THREAD_WEBRTC_SIGNALING, "WebRTC_Signaling"},
-    {ChromeThreadDescriptor::THREAD_PPAPI_MAIN, "CrPPAPIMain"},
-    {ChromeThreadDescriptor::THREAD_GPU_WATCHDOG, "GpuWatchdog"},
-    {ChromeThreadDescriptor::THREAD_SWAPPER, "swapper"},
-    {ChromeThreadDescriptor::THREAD_GAMEPAD_POLLING, "Gamepad polling thread"},
-    {ChromeThreadDescriptor::THREAD_AUDIO_INPUTDEVICE, "AudioInputDevice"},
-    {ChromeThreadDescriptor::THREAD_WEBRTC_WORKER, "WebRTC_Worker"},
-    {ChromeThreadDescriptor::THREAD_WEBCRYPTO, "WebCrypto"},
-    {ChromeThreadDescriptor::THREAD_DATABASE, "Database thread"},
-    {ChromeThreadDescriptor::THREAD_PROXYRESOLVER, "Proxy Resolver"},
-    {ChromeThreadDescriptor::THREAD_DEVTOOLSADB, "Chrome_DevToolsADBThread"},
-    {ChromeThreadDescriptor::THREAD_NETWORKCONFIGWATCHER,
-     "NetworkConfigWatcher"},
-    {ChromeThreadDescriptor::THREAD_WASAPI_RENDER, "wasapi_render_thread"},
-    {ChromeThreadDescriptor::THREAD_LOADER_LOCK_SAMPLER, "LoaderLockSampler"},
-    {ChromeThreadDescriptor::THREAD_COMPOSITOR_GPU, "CompositorGpuThread"},
-};
+#if defined(__GNUC__)  // clang also supports GCC syntax.
+#pragma GCC diagnostic pop
+#endif  // __GNUC__
 
 }  // namespace
 
 ChromeStringLookup::ChromeStringLookup(
     TraceStorage* storage,
     bool ignore_predefined_names_for_testing) {
-  std::map<chrome_enums::ProcessType, const char*> known_process_names;
-  std::map<ChromeThreadDescriptor::ThreadType, const char*> known_thread_names;
-  if (!ignore_predefined_names_for_testing) {
-    for (size_t i = 0; i < base::ArraySize(kProcessNames); i++) {
-      known_process_names.emplace(kProcessNames[i].type, kProcessNames[i].name);
-    }
-    for (size_t i = 0; i < base::ArraySize(kThreadNames); i++) {
-      known_thread_names.emplace(kThreadNames[i].type, kThreadNames[i].name);
-    }
-  }
-
   for (int32_t i = chrome_enums::ProcessType_MIN;
        i <= chrome_enums::ProcessType_MAX; ++i) {
     const auto type = static_cast<chrome_enums::ProcessType>(i);
-    const auto it = known_process_names.find(type);
-    const char* name = (it != known_process_names.end())
-                           ? it->second
-                           : chrome_enums::ProcessType_Name(type);
+    const char* name =
+        GetProcessNameString(type, ignore_predefined_names_for_testing);
     chrome_process_name_ids_[type] =
         name ? storage->InternString(name) : kNullStringId;
   }
@@ -203,10 +268,8 @@ ChromeStringLookup::ChromeStringLookup(
        i <= ::perfetto::protos::pbzero::ChromeThreadDescriptor_ThreadType_MAX;
        ++i) {
     const auto type = static_cast<ChromeThreadDescriptor::ThreadType>(i);
-    const auto it = known_thread_names.find(type);
-    const char* name = (it != known_thread_names.end())
-                           ? it->second
-                           : ChromeThreadDescriptor::ThreadType_Name(type);
+    const char* name =
+        GetThreadNameString(type, ignore_predefined_names_for_testing);
     chrome_thread_name_ids_[type] =
         name ? storage->InternString(name) : kNullStringId;
   }
