019de0d767798ef658f290b6803a2e88808c57b1
Kirill Timofeev <ktimofeev@google.com>
2025-08-12 18:38:12 +0000

    Trigger CI build.

--- src/tracing/service/tracing_service_impl.cc
+++ src/tracing/service/tracing_service_impl.cc
@@ -589,6 +589,7 @@ base::Status TracingServiceImpl::EnableTracing(ConsumerEndpointImpl* consumer,
   PERFETTO_DCHECK_THREAD(thread_checker_);
 
   // Trigger CI build.
+  // Trigger CI build.
 
   // If the producer is specifying a UUID, respect that (at least for the first
   // snapshot). Otherwise generate a new UUID.
