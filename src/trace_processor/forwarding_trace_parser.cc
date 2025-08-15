11fc51f39cc3665577444b5b565407d3ff210c2f
Lalit Maganti <lalitm@google.com>
2025-08-15 14:18:55 +0100

    tp: don't allow ninja traces to be non-root traces (#2565)
    They assume they are the only trace and do funky things. Disallow having
    them inside of traces.

--- src/trace_processor/forwarding_trace_parser.cc
+++ src/trace_processor/forwarding_trace_parser.cc
@@ -106,7 +106,20 @@ base::Status ForwardingTraceParser::Init(const TraceBlobView& blob) {
     return base::ErrStatus("Unknown trace type provided (ERR:fmt)");
   }
   PERFETTO_DLOG("%s trace detected", TraceTypeToString(trace_type_));
-  UpdateSorterForTraceType(trace_type_);
+
+  if (file_id_.value != 0 && trace_type_ == kNinjaLogTraceType) {
+    return base::ErrStatus(
+        "Ninja traces currently do not support being contained inside other "
+        "trace formats. Please file a bug at "
+        "https://github.com/google/perfetto/issues if this is important to "
+        "you.");
+  }
+
+  std::optional<TraceSorter::SortingMode> minimum_sorting_mode =
+      GetMinimumSortingMode(trace_type_, *input_context_);
+  if (minimum_sorting_mode) {
+    input_context_->sorter->SetSortingMode(*minimum_sorting_mode);
+  }
   input_context_->trace_file_tracker->StartParsing(file_id_, trace_type_);
 
   // TODO(b/334978369) Make sure kProtoTraceType and kSystraceTraceType are
@@ -119,15 +132,6 @@ base::Status ForwardingTraceParser::Init(const TraceBlobView& blob) {
   ASSIGN_OR_RETURN(reader_, input_context_->reader_registry->CreateTraceReader(
                                 trace_type_, trace_context_));
   return base::OkStatus();
-}
-
-void ForwardingTraceParser::UpdateSorterForTraceType(TraceType trace_type) {
-  std::optional<TraceSorter::SortingMode> minimum_sorting_mode =
-      GetMinimumSortingMode(trace_type, *input_context_);
-  if (!minimum_sorting_mode.has_value()) {
-    return;
-  }
-  input_context_->sorter->SetSortingMode(*minimum_sorting_mode);
 }
 
 base::Status ForwardingTraceParser::Parse(TraceBlobView blob) {
