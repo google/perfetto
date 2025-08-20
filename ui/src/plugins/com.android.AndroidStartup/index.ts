1ab95599b49ad8bd81e770cafe5c3905a86b5cb2
Lalit Maganti <lalitm@google.com>
2025-08-20 02:17:01 +0100

    ui: move android plugins from dev.perfetto -> com.android (#2613)

---
+++ ui/src/plugins/com.android.AndroidStartup/index.ts
@@ -0,0 +1,90 @@
+// Copyright (C) 2024 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import {LONG} from '../../trace_processor/query_result';
+import {Trace} from '../../public/trace';
+import {PerfettoPlugin} from '../../public/plugin';
+import {createQuerySliceTrack} from '../../components/tracks/query_slice_track';
+import {TrackNode} from '../../public/workspace';
+import {optimizationsTrack} from './optimizations';
+
+export default class implements PerfettoPlugin {
+  static readonly id = 'com.android.AndroidStartup';
+
+  async onTraceLoad(ctx: Trace): Promise<void> {
+    const e = ctx.engine;
+    await e.query(`
+          include perfetto module android.startup.startups;
+          include perfetto module android.startup.startup_breakdowns;
+         `);
+
+    const cnt = await e.query('select count() cnt from android_startups');
+    if (cnt.firstRow({cnt: LONG}).cnt === 0n) {
+      return;
+    }
+
+    const trackSource = `
+          SELECT l.ts AS ts, l.dur AS dur, l.package AS name
+          FROM android_startups l
+    `;
+    const trackBreakdownSource = `
+        SELECT
+          ts,
+          dur,
+          reason AS name
+          FROM android_startup_opinionated_breakdown
+    `;
+
+    const trackNode = await this.loadStartupTrack(
+      ctx,
+      trackSource,
+      `/android_startups`,
+      'Android App Startups',
+    );
+    const trackBreakdownNode = await this.loadStartupTrack(
+      ctx,
+      trackBreakdownSource,
+      `/android_startups_breakdown`,
+      'Android App Startups Breakdown',
+    );
+    const optimizations = await optimizationsTrack(ctx);
+    ctx.workspace.addChildInOrder(trackNode);
+    trackNode.addChildLast(trackBreakdownNode);
+    if (!!optimizations) {
+      trackNode.addChildLast(optimizations);
+    }
+  }
+
+  private async loadStartupTrack(
+    ctx: Trace,
+    sqlSource: string,
+    uri: string,
+    title: string,
+  ): Promise<TrackNode> {
+    const track = await createQuerySliceTrack({
+      trace: ctx,
+      uri,
+      data: {
+        sqlSource,
+        columns: ['ts', 'dur', 'name'],
+      },
+    });
+    ctx.tracks.registerTrack({
+      uri,
+      renderer: track,
+    });
+    // Needs a sort order lower than 'Ftrace Events' so that it is prioritized in the UI.
+    return new TrackNode({name: title, uri, sortOrder: -6});
+  }
+}
