b14da579fb5b4ccef1daf44dd1ad198ee25fad69
omarelfouly <fouly@google.com>
2025-08-20 18:00:52 +0000

    ui: etm: adds session track to etm plugin

---
+++ ui/src/plugins/org.chromium.ETM/index.ts
@@ -0,0 +1,66 @@
+// Copyright (C) 2023 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import {Trace} from '../../public/trace';
+import {App} from '../../public/app';
+import {MetricVisualisation} from '../../public/plugin';
+import {PerfettoPlugin} from '../../public/plugin';
+
+import {createQueryCounterTrack} from '../../components/tracks/query_counter_track';
+import {TrackNode} from '../../public/workspace';
+
+export default class implements PerfettoPlugin {
+  static readonly id = 'org.chromium.ETM';
+
+  static onActivate(app: App): void {
+    console.log('ETMPlugin::onActivate()', app.pluginId);
+  }
+
+  async onTraceLoad(trace: Trace) {
+    const title = 'ETM Session ID';
+    const uri = `${trace.pluginId}#ETMSessionID`;
+    const query =
+      'select ts, value from counter inner join ' +
+      'counter_track on counter_track.id = counter.track_id ' +
+      'where name = "ETMSession"';
+
+    const renderer = await createQueryCounterTrack({
+      trace,
+      uri,
+      data: {
+        sqlSource: query,
+      },
+    });
+
+    trace.tracks.registerTrack({
+      uri,
+      renderer,
+      description: 'Track to show current ETM session on timeline',
+    });
+
+    const group = new TrackNode({
+      name: 'ETM',
+      sortOrder: -25,
+      isSummary: true,
+    });
+
+    const trackNode = new TrackNode({uri, name: title});
+    group.addChildInOrder(trackNode);
+    trace.workspace.addChildInOrder(group);
+  }
+
+  static metricVisualisations(): MetricVisualisation[] {
+    return [];
+  }
+}
