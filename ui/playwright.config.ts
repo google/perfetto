bcde5171015fe624ca248070cb1817025b847b79
Steve Golton <stevegolton@google.com>
2025-08-21 10:41:02 +0000

    Use maxDiffPixelRatio rather than threshold to cater for minor rendering differences

--- ui/playwright.config.ts
+++ ui/playwright.config.ts
@@ -44,8 +44,8 @@ export default defineConfig({
     timeout: 5000,
     toHaveScreenshot: {
       // Rendering is not 100% identical on Mac. Be more tolerant.
-      maxDiffPixelRatio: isMac ? 0.05 : undefined,
-      threshold: 0.3,
+      // Otherwise, allow for small differences between rendering engines on Linux machines.
+      maxDiffPixelRatio: isMac ? 0.05 : 0.0001,
     },
   },
 
