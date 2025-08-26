618b07c00c89c9483af24d705787a152c9b22cf7
Gabriel-Andrei Ignat <gignat@google.com>
2025-08-26 08:50:37 +0000

    kswapd: Add system metric, refine CPU breakdown
    Adds a new, simpler system-wide kswapd metric. This metric calculates
    the total time spend by kswapd expressed as percentage of the total
    trace time when there was any CPU activity.
    
    The existing 'linux_kswapd_cpu_breakdown' metric has been modified. It
    now uses a modified table 'cpu_sched_awake_time' to calculate the
    percentage of time kswapd ran on a CPU. This new table will compute the
    duration each CPU core spent running *non-idle* tasks ONLY during
    periods when the device power state was 'awake'.
    
    Test:
    tools/diff_test_trace_processor.py
    out/linux_clang_release/trace_processor_shell --name-filter='.*kswapd.*'
    tools/diff_test_trace_processor.py
    out/linux_clang_release/trace_processor_shell
    --name-filter='.*cpu_sched_awake.*'

--- src/trace_processor/perfetto_sql/stdlib/linux/memory/kswapd.sql
+++ src/trace_processor/perfetto_sql/stdlib/linux/memory/kswapd.sql
@@ -15,14 +15,16 @@
 
 INCLUDE PERFETTO MODULE linux.cpu.sched_breakdown;
 
-CREATE PERFETTO TABLE _kswapd_sched_time AS
+-- Helper table that calculates the total duration 'kswapd0' kernel thread
+-- was running for each CPU within the trace.
+CREATE PERFETTO TABLE _linux_kswapd_sched_time AS
 SELECT
   -- CPU id
   cpu,
   -- Unique CPU id
   ucpu,
-  -- 'kswapd0' sched duration: the sum of 'dur' (duration) for scheduling slices ('sched' table entries)
-  -- where the 'kswapd0' thread was present on this specific CPU within the trace.
+  -- 'kswapd0' sched duration: sum of 'dur' (duration) for 'kswapd0' sched slices ('sched' table entries),
+  -- for each CPU within the trace.
   sum(dur) AS kswapd_sched_dur_ns
 FROM sched
 JOIN thread
@@ -33,42 +35,77 @@ GROUP BY
   cpu,
   ucpu;
 
--- This view quantifies the CPU time consumed by the 'kswapd0' thread on each CPU.
--- The durations provided are derived from the 'dur' column of scheduling slices
--- within the 'sched' table. It presents the total accumulated duration for each
--- CPU (from 'sched' entries) and the portion of that duration attributed
--- to 'kswapd0', expressed both in nanoseconds and as a percentage.
+-- This table quantifies the CPU time consumed by the 'kswapd0' kernel thread on each CPU core,
+-- relative to the total time the CPU running *non-idle* tasks and the power state was 'awake'(not suspended).
+--
+-- This metric relies on the 'cpu_sched_awake_time' table from module linux.cpu.sched_breakdown, which intersects
+-- sched slices with 'awake' power state slices from 'android_suspend_state' table.
+--
+-- For the 'kswapd0' thread, there is no need to intersect with power state slices, as it is almost
+-- certain that power state is 'awake' when 'kswapd0' is running.
 --
 -- Data sources that need to be enabled: linux.ftrace
-CREATE PERFETTO VIEW linux_kswapd_cpu_breakdown (
+CREATE PERFETTO TABLE linux_kswapd_cpu_breakdown (
   -- CPU id
   cpu LONG,
   -- Unique CPU id
   ucpu LONG,
-  -- CPU sched duration: the sum of 'dur' (duration) for all scheduling slices ('sched' table entries)
-  -- on this CPU within the trace.
-  cpu_sched_dur_ns LONG,
-  -- 'kswapd0' sched duration: the sum of 'dur' (duration) for scheduling slices ('sched' table entries)
-  -- where the 'kswapd0' thread was present on this specific CPU within the trace.
+  -- CPU sched duration while 'awake': sum of 'dur' (duration) where sched slices intersect with 'awake' power state slices,
+  -- for each CPU within the trace.
+  cpu_sched_awake_dur_ns LONG,
+  -- 'kswapd0' sched duration: sum of 'dur' (duration) for 'kswapd0' sched slices ('sched' table entries),
+  -- for each CPU within the trace.
   kswapd_sched_dur_ns LONG,
-  -- The percentage of time on a CPU attributed to 'kswapd0', relative to
-  -- the total CPU duration, derived from 'sched' table data.
-  -- Calculated as (kswapd_sched_dur_ns / cpu_sched_dur_ns) * 100.
-  -- Returns 0.0 if 'cpu_sched_dur_ns' is zero.
-  kswapd_cpu_dur_percentage DOUBLE
+  -- The percentage of time on a CPU attributed to 'kswapd0'.
+  -- Calculated as (kswapd_sched_dur_ns / cpu_sched_awake_dur_ns) * 100.
+  -- Returns 0.0 if 'cpu_sched_awake_dur_ns' is zero.
+  -- Returns values in the [0.0, 100.0] range.
+  kswapd_cpu_awake_dur_percentage DOUBLE
 ) AS
 SELECT
   ct.cpu,
   ucpu,
-  cpu_sched_dur_ns,
+  cpu_sched_awake_dur_ns,
   coalesce(kswapd_sched_dur_ns, 0) AS kswapd_sched_dur_ns,
   iif(
-    cpu_sched_dur_ns > 0,
+    cpu_sched_awake_dur_ns > 0,
     round((
       coalesce(kswapd_sched_dur_ns, 0) * 100.0
-    ) / cpu_sched_dur_ns, 2),
+    ) / cpu_sched_awake_dur_ns, 2),
     0.0
-  ) AS kswapd_cpu_dur_percentage
-FROM cpu_sched_time AS ct
-LEFT JOIN _kswapd_sched_time
+  ) AS kswapd_cpu_awake_dur_percentage
+FROM cpu_sched_awake_time AS ct
+LEFT JOIN _linux_kswapd_sched_time
   USING (ucpu);
+
+-- This table calculates a system-wide metric of 'kswapd0' utilization.
+--
+-- The utilization is calculated as total time spent by 'kswapd0' thread
+-- expressed as percentage of the total trace time when there was
+-- any CPU activity (i.e. running tasks and not in 'suspended' power state).
+--
+-- Data sources that need to be enabled: linux.ftrace
+CREATE PERFETTO TABLE linux_kswapd_utilization (
+  -- The percentage of time spent by 'kswapd0' thread relative to total trace time
+  -- when there was any CPU activity (i.e. running tasks and not in 'suspended' power state).
+  -- Returns values in the [0.0, 100.0] range.
+  kswapd_utilization_percentage DOUBLE
+) AS
+SELECT
+  (
+    SELECT
+      sum(s.dur)
+    FROM sched AS s
+    JOIN thread AS t
+      USING (utid)
+    WHERE
+      t.name = 'kswapd0' AND s.dur != -1
+  ) * 100.0 / (
+    -- TO_MONOTONIC(ts) gives the timestamp using a monotonic clock, which by definition
+    -- does not include the time when the device power state is 'suspended'.
+    SELECT
+      max(to_monotonic(ts) + dur) - min(to_monotonic(ts))
+    FROM sched
+    WHERE
+      dur != -1
+  ) AS kswapd_utilization_percentage;
