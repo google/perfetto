1aaaa1ca0adb345908c95e18c3e81a564ad2f419
Steve Golton <stevegolton@google.com>
2025-09-01 15:11:09 +0100

    ui: Added an optional close/dismiss button to callouts (#2783)
    <img width="589" height="100" alt="image"
    src="https://github.com/user-attachments/assets/009cb3b2-1957-4900-99d3-3754766d3c61"
    />

--- ui/src/plugins/dev.perfetto.WidgetsPage/widgets_page.ts
+++ ui/src/plugins/dev.perfetto.WidgetsPage/widgets_page.ts
@@ -1,4 +1,4 @@
-// Copyright (C) 2023 The Android Open Source Project
+// Copyright (C) 2025 The Android Open Source Project
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -1336,6 +1336,7 @@ export class WidgetsPage implements m.ClassComponent<{app: App}> {
           ),
         initialOpts: {
           intent: new EnumOption(Intent.None, Object.values(Intent)),
+          dismissable: false,
         },
       }),
       m(WidgetShowcase, {
