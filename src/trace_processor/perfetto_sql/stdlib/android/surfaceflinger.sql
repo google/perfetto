a4da5ddc3192be893e90bba3a21e6d08a74ae3b4
devianb <devarshimb@google.com>
2025-09-02 06:27:20 +0000

    Format surfaceflinger sql.

--- src/trace_processor/perfetto_sql/stdlib/android/surfaceflinger.sql
+++ src/trace_processor/perfetto_sql/stdlib/android/surfaceflinger.sql
@@ -99,26 +99,36 @@ GROUP BY
 -- Extract app and SF frame vsync scoped to CUJs.
 CREATE PERFETTO TABLE _android_jank_cuj_app_sf_frame_timeline_match AS
 SELECT
- cuj_id,
-   do_frame.upid AS app_upid,
-   app_vsync,
-   app_sf_match.sf_upid,
-   app_sf_match.sf_vsync
-FROM _android_jank_cuj_do_frames do_frame
-JOIN android_app_to_sf_frame_timeline_match app_sf_match
-  ON do_frame.vsync = app_sf_match.app_vsync
-    AND do_frame.upid = app_sf_match.app_upid;
+  cuj_id,
+  do_frame.upid AS app_upid,
+  app_vsync,
+  app_sf_match.sf_upid,
+  app_sf_match.sf_vsync
+FROM _android_jank_cuj_do_frames AS do_frame
+JOIN android_app_to_sf_frame_timeline_match AS app_sf_match
+  ON do_frame.vsync = app_sf_match.app_vsync AND do_frame.upid = app_sf_match.app_upid;
 
 -- Extract ts and dur for a given slice name from the SF process main thread track.
-CREATE OR REPLACE PERFETTO FUNCTION _find_android_jank_cuj_sf_main_thread_slice(
-  slice_name_glob STRING)
-RETURNS TABLE(
-  cuj_id INT, utid INT, vsync INT, id INT,
-  name STRING, ts LONG, dur LONG, ts_end LONG)
-AS
-WITH sf_vsync AS (
-  SELECT DISTINCT cuj_id, sf_vsync AS vsync
-  FROM _android_jank_cuj_app_sf_frame_timeline_match)
+CREATE PERFETTO FUNCTION _find_android_jank_cuj_sf_main_thread_slice(
+    slice_name_glob STRING
+)
+RETURNS TABLE (
+  cuj_id INTEGER,
+  utid INTEGER,
+  vsync INTEGER,
+  id INTEGER,
+  name STRING,
+  ts LONG,
+  dur LONG,
+  ts_end LONG
+) AS
+WITH
+  sf_vsync AS (
+    SELECT DISTINCT
+      cuj_id,
+      sf_vsync AS vsync
+    FROM _android_jank_cuj_app_sf_frame_timeline_match
+  )
 SELECT
   cuj_id,
   utid,
@@ -129,48 +139,73 @@ SELECT
   slice.dur,
   slice.ts + slice.dur AS ts_end
 FROM slice
-JOIN _android_sf_main_thread main_thread USING (track_id)
+JOIN _android_sf_main_thread AS main_thread
+  USING (track_id)
 JOIN sf_vsync
-  ON CAST(STR_SPLIT(slice.name, " ", 1) AS INTEGER) = sf_vsync.vsync
-WHERE slice.name GLOB $slice_name_glob AND slice.dur > 0
-ORDER BY cuj_id, vsync;
+  ON CAST(str_split(slice.name, " ", 1) AS INTEGER) = sf_vsync.vsync
+WHERE
+  slice.name GLOB $slice_name_glob AND slice.dur > 0
+ORDER BY
+  cuj_id,
+  vsync;
 
 CREATE PERFETTO TABLE _android_jank_cuj_sf_commit_slice AS
-SELECT * FROM _FIND_ANDROID_JANK_CUJ_SF_MAIN_THREAD_SLICE('commit *');
+SELECT
+  *
+FROM _find_android_jank_cuj_sf_main_thread_slice('commit *');
 
 CREATE PERFETTO TABLE _android_jank_cuj_sf_composite_slice AS
-SELECT * FROM _FIND_ANDROID_JANK_CUJ_SF_MAIN_THREAD_SLICE('composite *');
+SELECT
+  *
+FROM _find_android_jank_cuj_sf_main_thread_slice('composite *');
 
 CREATE PERFETTO TABLE _android_jank_cuj_sf_on_message_invalidate_slice AS
-SELECT * FROM _FIND_ANDROID_JANK_CUJ_SF_MAIN_THREAD_SLICE('onMessageInvalidate *');
+SELECT
+  *
+FROM _find_android_jank_cuj_sf_main_thread_slice('onMessageInvalidate *');
 
 -- Similar to `android_jank_cuj_main_thread_frame_boundary`, calculates the frame boundaries
 -- based on when we *expected* the work to start and we use the end of the `composite` slice
 -- as the end of the work on the frame.
-DROP TABLE IF EXISTS _android_jank_cuj_sf_main_thread_frame_boundary;
 CREATE PERFETTO TABLE _android_jank_cuj_sf_main_thread_frame_boundary AS
 -- Join `commit` and `composite` slices using vsync IDs.
 -- We treat the two slices as a single "fake slice" that starts when `commit` starts, and ends
 -- when `composite` ends.
-WITH combined_commit_composite_slice AS (
-  SELECT
-    cuj_id,
-    commit_slice.utid,
-    vsync,
-    commit_slice.ts,
-    composite_slice.ts_end,
-    composite_slice.ts_end - commit_slice.ts AS dur
-  FROM _android_jank_cuj_sf_commit_slice commit_slice
-  JOIN _android_jank_cuj_sf_composite_slice composite_slice USING(cuj_id, vsync)
-),
--- As older builds will not have separate commit/composite slices for each frame, but instead
--- a single `onMessageInvalidate`, we UNION ALL the two tables. Exactly one of them should
--- have data.
-main_thread_slice AS (
-  SELECT utid, cuj_id, vsync, ts, dur, ts_end FROM combined_commit_composite_slice
-  UNION ALL
-  SELECT utid, cuj_id, vsync, ts, dur, ts_end FROM _android_jank_cuj_sf_on_message_invalidate_slice
-)
+WITH
+  combined_commit_composite_slice AS (
+    SELECT
+      cuj_id,
+      commit_slice.utid,
+      vsync,
+      commit_slice.ts,
+      composite_slice.ts_end,
+      composite_slice.ts_end - commit_slice.ts AS dur
+    FROM _android_jank_cuj_sf_commit_slice AS commit_slice
+    JOIN _android_jank_cuj_sf_composite_slice AS composite_slice
+      USING (cuj_id, vsync)
+  ),
+  -- As older builds will not have separate commit/composite slices for each frame, but instead
+  -- a single `onMessageInvalidate`, we UNION ALL the two tables. Exactly one of them should
+  -- have data.
+  main_thread_slice AS (
+    SELECT
+      utid,
+      cuj_id,
+      vsync,
+      ts,
+      dur,
+      ts_end
+    FROM combined_commit_composite_slice
+    UNION ALL
+    SELECT
+      utid,
+      cuj_id,
+      vsync,
+      ts,
+      dur,
+      ts_end
+    FROM _android_jank_cuj_sf_on_message_invalidate_slice
+  )
 SELECT
   cuj_id,
   utid,
@@ -179,7 +214,8 @@ SELECT
   main_thread_slice.ts AS ts_main_thread_start,
   main_thread_slice.ts_end,
   main_thread_slice.ts_end - expected_timeline.ts AS dur
-FROM expected_frame_timeline_slice expected_timeline
-JOIN _android_sf_process USING (upid)
+FROM expected_frame_timeline_slice AS expected_timeline
+JOIN _android_sf_process
+  USING (upid)
 JOIN main_thread_slice
   ON main_thread_slice.vsync = CAST(expected_timeline.name AS INTEGER);
