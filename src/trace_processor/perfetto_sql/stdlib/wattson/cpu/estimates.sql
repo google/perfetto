9f732e2dc63dc4c696b90113b71ef63dd87377d4
Samuel Wu <wusamuel@google.com>
2025-09-04 23:59:54 +0000

    stdlib: optimize: Remove unnecessary table in CPU estimates
    This table is no longer needed, and gives a slight performance benefit.
    
    Test: tools/diff_test_trace_processor.py out/linux_clang_release/trace_processor_shell --name-filter '.*wattson*' --print-slowest-tests
    Bug: 443097308
    Signed-off-by: Samuel Wu <wusamuel@google.com>

--- src/trace_processor/perfetto_sql/stdlib/wattson/cpu/estimates.sql
+++ src/trace_processor/perfetto_sql/stdlib/wattson/cpu/estimates.sql
@@ -21,32 +21,35 @@ INCLUDE PERFETTO MODULE wattson.device_infos;
 
 INCLUDE PERFETTO MODULE wattson.utils;
 
--- Final table showing the curves per CPU per slice
-CREATE PERFETTO TABLE _system_state_curves AS
+-- The most basic components of Wattson, all normalized to be in mW on a per
+-- system state basis
+CREATE PERFETTO TABLE _cpu_estimates_mw AS
 SELECT
   base.ts,
   base.dur,
   -- base.cpu[0-3]_curve will be non-zero if CPU has 1D dependency
   -- base.cpu[0-3]_curve will be zero if device is suspended or deep idle
   -- base.cpu[0-3]_curve will be NULL if 2D dependency required
-  coalesce(base.cpu0_curve, lut0.curve_value) AS cpu0_curve,
-  coalesce(base.cpu1_curve, lut1.curve_value) AS cpu1_curve,
-  coalesce(base.cpu2_curve, lut2.curve_value) AS cpu2_curve,
-  coalesce(base.cpu3_curve, lut3.curve_value) AS cpu3_curve,
+  coalesce(base.cpu0_curve, lut0.curve_value) AS cpu0_mw,
+  coalesce(base.cpu1_curve, lut1.curve_value) AS cpu1_mw,
+  coalesce(base.cpu2_curve, lut2.curve_value) AS cpu2_mw,
+  coalesce(base.cpu3_curve, lut3.curve_value) AS cpu3_mw,
   -- base.cpu[4-7]_curve will be non-zero if CPU has 1D dependency
   -- base.cpu[4-7]_curve will be zero if device is suspended or deep idle
   -- base.cpu[4-7]_curve will be NULL if CPU doesn't exist on device
-  coalesce(base.cpu4_curve, 0.0) AS cpu4_curve,
-  coalesce(base.cpu5_curve, 0.0) AS cpu5_curve,
-  coalesce(base.cpu6_curve, 0.0) AS cpu6_curve,
-  coalesce(base.cpu7_curve, 0.0) AS cpu7_curve,
+  coalesce(base.cpu4_curve, 0.0) AS cpu4_mw,
+  coalesce(base.cpu5_curve, 0.0) AS cpu5_mw,
+  coalesce(base.cpu6_curve, 0.0) AS cpu6_mw,
+  coalesce(base.cpu7_curve, 0.0) AS cpu7_mw,
   iif(
     no_static = 1,
     0.0,
     iif(0 IN _device_policies, coalesce(lut0.static, 0), 0) + iif(1 IN _device_policies, coalesce(lut1.static, 0), 0) + iif(2 IN _device_policies, coalesce(lut2.static, 0), 0) + iif(3 IN _device_policies, coalesce(lut3.static, 0), 0) + iif(4 IN _device_policies, coalesce(lut4.static, 0), 0) + iif(5 IN _device_policies, coalesce(lut5.static, 0), 0) + iif(6 IN _device_policies, coalesce(lut6.static, 0), 0) + iif(7 IN _device_policies, coalesce(lut7.static, 0), 0) + static_1d
-  ) AS static_curve,
-  iif(all_cpu_deep_idle = 1, 0, base.l3_hit_count * l3_lut.l3_hit) AS l3_hit_value,
-  iif(all_cpu_deep_idle = 1, 0, base.l3_miss_count * l3_lut.l3_miss) AS l3_miss_value
+  ) + (
+    -- LUT for l3 is scaled by 10^6 to save resolution and in units of kWs. Scale
+    -- this by 10^3 so when divided by ns, result is in units of mW
+    coalesce(base.l3_hit_count * l3_lut.l3_hit, 0) + coalesce(base.l3_miss_count * l3_lut.l3_miss, 0)
+  ) * 1000 / dur AS dsu_scu_mw
 FROM _w_dependent_cpus_calc AS base
 -- LUT for 2D dependencies
 LEFT JOIN _filtered_curves_2d AS lut0
@@ -93,26 +96,3 @@ LEFT JOIN _filtered_curves_l3 AS l3_lut
   ON l3_lut.freq_khz = base.freq_0
   AND l3_lut.dep_policy = base.dep_policy_0
   AND l3_lut.dep_freq = base.dep_freq_0;
-
--- The most basic components of Wattson, all normalized to be in mW on a per
--- system state basis
-CREATE PERFETTO TABLE _cpu_estimates_mw AS
-SELECT
-  ts,
-  dur,
-  cpu0_curve AS cpu0_mw,
-  cpu1_curve AS cpu1_mw,
-  cpu2_curve AS cpu2_mw,
-  cpu3_curve AS cpu3_mw,
-  cpu4_curve AS cpu4_mw,
-  cpu5_curve AS cpu5_mw,
-  cpu6_curve AS cpu6_mw,
-  cpu7_curve AS cpu7_mw,
-  -- LUT for l3 is scaled by 10^6 to save resolution and in units of kWs. Scale
-  -- this by 10^3 so when divided by ns, result is in units of mW
-  (
-    (
-      coalesce(l3_hit_value, 0) + coalesce(l3_miss_value, 0)
-    ) * 1000 / dur
-  ) + static_curve AS dsu_scu_mw
-FROM _system_state_curves;
