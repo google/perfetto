049d26463a6a692676a38dca79fe638528e7454e
pqhu-google <pqhu@google.com>
2025-09-05 10:26:16 -0700

    Address multiple comments reviews.
    - try/catch and return early from the plugin if google3 modules are not available
    - Removed unnecessary comment and function.
    - Use better name for registered command.

--- ui/src/plugins/com.google.YouTubeTrace/index.ts
+++ ui/src/plugins/com.google.YouTubeTrace/index.ts
@@ -1,19 +1,4 @@
-// Copyright (C) 2023 The Android Open Source Project
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 import {Trace} from '../../public/trace';
-import {MetricVisualisation} from '../../public/plugin';
 import {PerfettoPlugin} from '../../public/plugin';
 
 export default class implements PerfettoPlugin {
@@ -29,48 +14,50 @@ export default class implements PerfettoPlugin {
    */
   async onTraceLoad(trace: Trace): Promise<void> {
     trace.commands.registerCommand({
-      id: `YT CUJ#pinJankTracksMainThread`,
+      id: `com.google.YouTubePinJankTracksMainThread`,
       name: 'YT CUJ: Pin Jank Tracks (main thread)',
       callback: async () => {
-        await this.includeCommonModule(trace);
-        this.pinDebugTrack(
-          trace,
-          'SELECT * FROM reconstruct_imp_duration_spans(1)',
-          'YT Jank: Imp Duration (main thread)',
-        );
-        this.pinDebugTrack(
-          trace,
-          'SELECT * FROM recreate_scroll_duration_spans(1)',
-          'YT Jank: Scroll Duration (main thread)',
-        );
-        this.pinDebugTrack(
-          trace,
-          'SELECT * FROM merged_local_directors(1)',
-          'YT Jank: Merged Local Director (main thread)',
-        );
+        if (await this.tryIncludeCommonModule(trace)) {
+          this.pinDebugTrack(
+            trace,
+            'SELECT * FROM reconstruct_imp_duration_spans(1)',
+            'YT Jank: Imp Duration (main thread)',
+          );
+          this.pinDebugTrack(
+            trace,
+            'SELECT * FROM recreate_scroll_duration_spans(1)',
+            'YT Jank: Scroll Duration (main thread)',
+          );
+          this.pinDebugTrack(
+            trace,
+            'SELECT * FROM merged_local_directors(1)',
+            'YT Jank: Merged Local Director (main thread)',
+          );
+        }
       },
     });
 
     trace.commands.registerCommand({
-      id: `YT CUJ#pinJankTracksAllThreads`,
+      id: `com.google.YouTubePinJankTracksAllThreads`,
       name: 'YT CUJ: Pin Jank Tracks (all threads)',
       callback: async () => {
-        await this.includeCommonModule(trace);
-        this.pinDebugTrack(
-          trace,
-          'SELECT * FROM reconstruct_imp_duration_spans(0)',
-          'YT Jank: Imp Duration (all threads)',
-        );
-        this.pinDebugTrack(
-          trace,
-          'SELECT * FROM recreate_scroll_duration_spans(0)',
-          'YT Jank: Scroll Duration (all threads)',
-        );
-        this.pinDebugTrack(
-          trace,
-          'SELECT * FROM merged_local_directors(0)',
-          'YT Jank: Merged Local Director (all threads)',
-        );
+        if (await this.tryIncludeCommonModule(trace)) {
+          this.pinDebugTrack(
+            trace,
+            'SELECT * FROM reconstruct_imp_duration_spans(0)',
+            'YT Jank: Imp Duration (all threads)',
+          );
+          this.pinDebugTrack(
+            trace,
+            'SELECT * FROM recreate_scroll_duration_spans(0)',
+            'YT Jank: Scroll Duration (all threads)',
+          );
+          this.pinDebugTrack(
+            trace,
+            'SELECT * FROM merged_local_directors(0)',
+            'YT Jank: Merged Local Director (all threads)',
+          );
+        }
       },
     });
   }
@@ -84,19 +71,20 @@ export default class implements PerfettoPlugin {
     }
   }
 
-  private async includeCommonModule(trace: Trace) {
-    await trace.engine.query(
-      `INCLUDE PERFETTO MODULE google3.video.youtube.analytics.client_apps.system_health.tools.trace.perfetto_module.imp`,
-    );
-    await trace.engine.query(
-      `INCLUDE PERFETTO MODULE google3.video.youtube.analytics.client_apps.system_health.tools.trace.perfetto_module.scroll`,
-    );
-    await trace.engine.query(
-      `INCLUDE PERFETTO MODULE google3.video.youtube.analytics.client_apps.system_health.tools.trace.perfetto_module.local_director`,
-    );
-  }
-
-  static metricVisualisations(): MetricVisualisation[] {
-    return [];
+  private async tryIncludeCommonModule(trace: Trace) {
+    try {
+      await trace.engine.query(
+        `INCLUDE PERFETTO MODULE google3.video.youtube.analytics.client_apps.system_health.tools.trace.perfetto_module.imp`,
+      );
+      await trace.engine.query(
+        `INCLUDE PERFETTO MODULE google3.video.youtube.analytics.client_apps.system_health.tools.trace.perfetto_module.scroll`,
+      );
+      await trace.engine.query(
+        `INCLUDE PERFETTO MODULE google3.video.youtube.analytics.client_apps.system_health.tools.trace.perfetto_module.local_director`,
+      );
+      return true;
+    } catch (e) {
+      return false;
+    }
   }
 }
