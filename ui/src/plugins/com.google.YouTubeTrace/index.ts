6180dee58eeab170013d86a9679961c531bdc950
pqhu-google <pqhu@google.com>
2025-09-05 10:53:13 -0700

    Remove unused comment and restore copyright header

--- ui/src/plugins/com.google.YouTubeTrace/index.ts
+++ ui/src/plugins/com.google.YouTubeTrace/index.ts
@@ -1,17 +1,22 @@
+// Copyright (C) 2023 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 import {Trace} from '../../public/trace';
 import {PerfettoPlugin} from '../../public/plugin';
 
 export default class implements PerfettoPlugin {
   static readonly id = 'com.google.YouTubeTrace';
 
-  /**
-   * This hook is called as the trace is loading. At this point the trace is
-   * loaded into trace processor and it's ready to process queries. This hook
-   * should be used for adding tracks and commands that depend on the trace.
-   *
-   * It should not be used for finding tracks from other plugins as there is no
-   * guarantee those tracks will have been added yet.
-   */
   async onTraceLoad(trace: Trace): Promise<void> {
     trace.commands.registerCommand({
       id: `com.google.YouTubePinJankTracksMainThread`,
