f10ea7121679b6339e0bf1bae6f932d1bb995327
Anna Mayzner <mayzner@google.com>
2025-09-15 12:40:39 +0000

    fix formatting error

--- ui/src/plugins/dev.perfetto.ExplorePage/json_handler.ts
+++ ui/src/plugins/dev.perfetto.ExplorePage/json_handler.ts
@@ -211,7 +211,10 @@ export function deserializeState(
     });
 
     // Backwards compatibility: if prevNodes is not in the JSON, infer it.
-    if (serializedNode.prevNodes && serializedNode.prevNodes.length > 0) {
+    if (
+      serializedNode.prevNodes !== undefined &&
+      serializedNode.prevNodes.length > 0
+    ) {
       node.prevNodes = serializedNode.prevNodes.map((id) => {
         const prevNode = nodes.get(id);
         if (prevNode == null) {
