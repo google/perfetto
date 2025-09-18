11aa2f15c48fd32767fafb3dd9dd8d565c0e91dc
Priyanka <57577698+PriyankaSPX@users.noreply.github.com>
2025-09-18 18:11:33 +0100

    tp: Update ViewCapture tables with eager property fields. (#2992)
    Bug: 438683146
    Test: tools/diff_test_trace_processor.py
    out/linux_clang_release/trace_processor_shell
    --name-filter="ViewCapture|PerfettoTable:winscope"

--- src/trace_processor/perfetto_sql/stdlib/android/winscope/viewcapture.sql
+++ src/trace_processor/perfetto_sql/stdlib/android/winscope/viewcapture.sql
@@ -17,15 +17,21 @@
 CREATE PERFETTO VIEW android_viewcapture (
   -- Snapshot id
   id LONG,
-  -- Timestamp when the snapshot was triggered
+  -- Timestamp when snapshot was triggered
   ts TIMESTAMP,
-  -- Extra args parsed from the proto message
-  arg_set_id ARGSETID
+  -- Extra args parsed from proto message
+  arg_set_id ARGSETID,
+  -- Package name deinterned from proto message
+  package_name STRING,
+  -- Window name deinterned from proto message
+  window_name STRING
 ) AS
 SELECT
   id,
   ts,
-  arg_set_id
+  arg_set_id,
+  package_name,
+  window_name
 FROM __intrinsic_viewcapture;
 
 -- Android viewcapture view (from android.viewcapture data source).
@@ -34,11 +40,32 @@ CREATE PERFETTO VIEW android_viewcapture_view (
   id LONG,
   -- Snapshot id
   snapshot_id LONG,
-  -- Extra args parsed from the proto message
-  arg_set_id ARGSETID
+  -- Extra args parsed from proto message
+  arg_set_id ARGSETID,
+  -- Id from proto message
+  node_id LONG,
+  -- Hashcode from proto message
+  hashcode LONG,
+  -- Is visible computed from proto message and view position
+  is_visible LONG,
+  -- Parent id from proto message
+  parent_id LONG,
+  -- View id deinterned from proto message
+  view_id STRING,
+  -- Class name deinterned from proto message
+  class_name STRING,
+  -- Trace rect id
+  trace_rect_id LONG
 ) AS
 SELECT
   id,
   snapshot_id,
-  arg_set_id
+  arg_set_id,
+  node_id,
+  hashcode,
+  is_visible,
+  parent_id,
+  view_id,
+  class_name,
+  trace_rect_id
 FROM __intrinsic_viewcapture_view;
