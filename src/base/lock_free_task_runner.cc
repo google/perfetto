992b845791c40fed43c4597d99fcaf86acffd992
Primiano Tucci <primiano@google.com>
2025-09-30 11:36:26 +0000

    LockFreeTaskRunner: fix race on Quit
    Some tests where hitting the following race:
     - Thread1 (!= main thread) invokes Quit, which in turn becomes a PostTask.
     - This function sees quit_=true and returns from Run().
     - The owner of the LFTR at that point is entitled to destroy LFTR.
     - Thread1 is still executing the epilogue of the PostTask, decrementing the
       refcount, and ends up operating on invalid memory.
    
    Fix it by waiting that the refcounts go to 0 before returning from Run().

--- src/base/lock_free_task_runner.cc
+++ src/base/lock_free_task_runner.cc
@@ -24,8 +24,8 @@
 // Keep the \n before to prevent clang-format reordering.
 #include <synchapi.h>
 #else
-#include <unistd.h>
 #include <poll.h>
+#include <unistd.h>
 #endif
 
 #include <thread>
@@ -206,6 +206,19 @@ void LockFreeTaskRunner::Run() {
       errno = 0;
       RunTaskWithWatchdogGuard(std::move(delayed_task));
     }
+  }
+
+  // Wait for all other threads to have finished the Quit's PostTask().
+  // This is to prevent the following race in tests:
+  // - Thread1 (!= main thread) invokes Quit, which in turn becomes a PostTask.
+  // - This function sees quit_=true and returns from Run().
+  // - The owner of the LFTR at that point is entitled to destroy LFTR.
+  // - Thread1 is still executing the epilogue of the PostTask, decrementing the
+  //   refcount, and ends up operating on invalid memory.
+  while (
+      std::any_of(refcounts_.begin(), refcounts_.end(),
+                  [](std::atomic<int32_t>& bucket) { return bucket.load(); })) {
+    std::this_thread::yield();
   }
 }
 
