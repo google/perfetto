04a02eb5f60c25daf3a07fbfa3aefeef366760d3
Samuel Wu <wusamuel@google.com>
2025-10-02 14:34:36 -0700

    stdlib: Add functionality to extrapolate cpu_idle state (#3178)
    Extrapolate or infer the cpuidle state based on the swapper thread (aka
    idle thread) when cpu_idle trace events are not available. This allows
    for a best effort power estimate of AoT traces and other traces that may
    not have the cpu_idle event.
    
    Additionally, make corresponding changes to metrics and UI that rely
    upon Wattson, so that there is an indication of when the cpuidle state
    is based on the real trace event or when it's extrapolated.
    
    Bug: 435479190
    Signed-off-by: Samuel Wu <wusamuel@google.com>

--- src/trace_processor/metrics/sql/android/wattson_trace_threads.sql
+++ src/trace_processor/metrics/sql/android/wattson_trace_threads.sql
@@ -33,8 +33,9 @@ SELECT RUN_METRIC(
 DROP VIEW IF EXISTS wattson_trace_threads_output;
 CREATE PERFETTO VIEW wattson_trace_threads_output AS
 SELECT AndroidWattsonTasksAttributionMetric(
-  'metric_version', 4,
-  'power_model_version', 1,
+  'metric_version', metric_version,
+  'power_model_version', power_model_version,
+  'is_crude_estimate', is_crude_estimate,
   'period_info', (
     SELECT RepeatedField(
       AndroidWattsonTaskPeriodInfo(
@@ -44,4 +45,5 @@ SELECT AndroidWattsonTasksAttributionMetric(
     )
     FROM _wattson_per_task
   )
-);
+)
+FROM _wattson_tasks_metric_metadata;
