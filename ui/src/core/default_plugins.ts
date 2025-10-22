1036e8a2de35f012f537384d626cf20be721e7d6
Nick Chameyev <nickchameyev@google.com>
2025-10-22 10:37:51 +0100

    Create a plugin to organize nested tracks by name
    Adds an action to organize tracks using the ' -> ' separator.
    
    For example, tracks with names:
    ```
      - Foo -> Bar -> Track1
      - Foo -> Bar -> Track2
      - Foo -> Track3
    ```
    
    Will be reorganized into:
    ```
      - Foo
         - Bar
            - Track1
            - Track2
         - Track3
    ```

--- ui/src/core/default_plugins.ts
+++ ui/src/core/default_plugins.ts
@@ -36,6 +36,7 @@ export const defaultPlugins = [
   'com.android.DayExplorer',
   'com.android.GpuWorkPeriod',
   'com.android.LargeScreensPerf',
+  'com.android.OrganizeNestedTracks',
   'com.android.PinAndroidPerfMetrics',
   'com.android.PinSysUITracks',
   'com.android.SysUIWorkspace',
