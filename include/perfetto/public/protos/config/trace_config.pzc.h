8812536bd56e03871ecef6f3542ae5824ff93b3f
Kirill Timofeev <ktimofeev@google.com>
2025-10-27 17:33:17 +0000

    WIP support persistent traces.

--- include/perfetto/public/protos/config/trace_config.pzc.h
+++ include/perfetto/public/protos/config/trace_config.pzc.h
@@ -297,6 +297,11 @@ PERFETTO_PB_FIELD(perfetto_protos_TraceConfig,
                   bool,
                   no_flush_before_write_into_file,
                   42);
+PERFETTO_PB_FIELD(perfetto_protos_TraceConfig,
+                  VARINT,
+                  bool,
+                  persist_trace_after_reboot,
+                  43);
 
 PERFETTO_PB_MSG(perfetto_protos_TraceConfig_SessionSemaphore);
 PERFETTO_PB_FIELD(perfetto_protos_TraceConfig_SessionSemaphore,
