9f5cca22653c64bbe4e7323977c8d533e7c4225f
Jahdiel Alvarez <jahdiel@google.com>
2025-11-11 10:33:55 -0800

    Use process table to identify machine ids in Android logs
    When ftrace isn't set as a data source during a multi-VM tracing
    session, the CPU table won't be populated. This means that if Android
    logs were indeed set as a data source, then the AndroidLog panel won't
    show the machine id column and filter button, even though there are logs
    from multiple machines, because it uses the CPU table to identify the
    machines involved in the table.
    
    Since every log has a matching UPID then we can iterate through the
    process table instead and capture the machine ids from there, assuming
    that we will have way less processes than logs in the trace.
    
    Bug: 459819089

--- ui/src/plugins/com.android.AndroidLog/index.ts
+++ ui/src/plugins/com.android.AndroidLog/index.ts
@@ -47,11 +47,11 @@ interface AndroidLogPluginState {
 
 async function getMachineIds(engine: Engine): Promise<number[]> {
   // A machine might not provide Android logs, even if configured to do so.
-  // Hence, the |cpu| table might have ids not present in the logs. Given this
+  // Hence, the |process| table might have ids not present in the logs. Given this
   // is highly unlikely and going through all logs is expensive, we will get
-  // the ids from |cpu|, even if filter shows ids not present in logs.
+  // the ids from |process|, even if filter shows ids not present in logs.
   const result = await engine.query(
-    `SELECT DISTINCT(machine_id) FROM cpu ORDER BY machine_id`,
+    `SELECT DISTINCT(machine_id) FROM process ORDER BY machine_id`,
   );
   const machineIds: number[] = [];
   const it = result.iter({machine_id: NUM_NULL});
