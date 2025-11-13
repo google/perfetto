a44823304069c7aa5c29ea2c3f5dc7731f871d20
Lalit Maganti <lalitm@google.com>
2025-11-13 22:58:58 +0000

    tp: refactor trace_processor_shell to extract library target
    In Google3, we want a version of shell which is tightly integrated with
    the rest of Google infra (e.g. the SQL modules, the internal filesystems
    etc). But we don't want to maintain a separate shell.
    
    The best way around this is to extract a thin wrapper interface for
    shell which allows restricted customization points of shell without
    needing to duplicate lots of code

--- src/trace_processor/rpc/rpc.h
+++ src/trace_processor/rpc/rpc.h
@@ -62,7 +62,10 @@ class Rpc {
   // The unique_ptr argument is optional. If non-null it will adopt the passed
   // instance and allow to directly query that. If null, a new instanace will be
   // created internally by calling Parse().
-  explicit Rpc(std::unique_ptr<TraceProcessor>, bool has_preloaded_eof);
+  explicit Rpc(std::unique_ptr<TraceProcessor>,
+               bool has_preloaded_eof,
+               Config default_config,
+               std::function<void(TraceProcessor*)> on_trace_processor_created);
   Rpc();
   ~Rpc();
 
@@ -148,7 +151,10 @@ class Rpc {
   void DisableAndReadMetatraceInternal(
       protos::pbzero::DisableAndReadMetatraceResult*);
 
-  Config trace_processor_config_;
+  Config default_config_;
+  std::function<void(TraceProcessor*)> on_trace_processor_created_;
+
+  Config current_config_;
   std::unique_ptr<TraceProcessor> trace_processor_;
   RpcResponseFunction rpc_response_fn_;
   protozero::ProtoRingBuffer rxbuf_;
--- src/trace_processor/trace_processor_shell.cc
+++ src/trace_processor/trace_processor_shell.cc
@@ -14,6 +14,8 @@
  * limitations under the License.
  */
 
+#include "perfetto/ext/trace_processor/trace_processor_shell.h"
+
 #include <algorithm>
 #include <cctype>
 #include <cerrno>
@@ -1266,16 +1268,17 @@ void ExtendPoolWithBinaryDescriptor(
 }
 
 base::Status LoadTrace(TraceProcessor* trace_processor,
+                       TraceProcessorShell::PlatformInterface* platform,
                        const std::string& trace_file_path,
                        double* size_mb) {
-  base::Status read_status = ReadTraceUnfinalized(
-      trace_processor, trace_file_path.c_str(), [&size_mb](size_t parsed_size) {
+  base::Status load_status = platform->LoadTrace(
+      trace_processor, trace_file_path, [&size_mb](size_t parsed_size) {
         *size_mb = static_cast<double>(parsed_size) / 1E6;
         fprintf(stderr, "\rLoading trace: %.2f MB\r", *size_mb);
       });
-  if (!read_status.ok()) {
+  if (!load_status.ok()) {
     return base::ErrStatus("Could not read trace file (path: %s): %s",
-                           trace_file_path.c_str(), read_status.c_message());
+                           trace_file_path.c_str(), load_status.c_message());
   }
 
   bool is_proto_trace = false;
@@ -1886,10 +1889,22 @@ TraceSummaryOutputSpec::Format GetSummaryOutputFormat(
   exit(1);
 }
 
-base::Status TraceProcessorMain(int argc, char** argv) {
+}  // namespace
+
+TraceProcessorShell::TraceProcessorShell(
+    std::unique_ptr<PlatformInterface> platform_interface)
+    : platform_interface_(std::move(platform_interface)) {}
+
+std::unique_ptr<TraceProcessorShell> TraceProcessorShell::Create(
+    std::unique_ptr<PlatformInterface> platform_interface) {
+  return std::unique_ptr<TraceProcessorShell>(
+      new TraceProcessorShell(std::move(platform_interface)));
+}
+
+base::Status TraceProcessorShell::Run(int argc, char** argv) {
   CommandLineOptions options = ParseCommandLineOptions(argc, argv);
 
-  Config config;
+  Config config = platform_interface_->DefaultConfig();
   config.sorting_mode = options.force_full_sort
                             ? SortingMode::kForceFullSort
                             : SortingMode::kDefaultHeuristics;
@@ -1925,6 +1940,7 @@ base::Status TraceProcessorMain(int argc, char** argv) {
   }
 
   std::unique_ptr<TraceProcessor> tp = TraceProcessor::CreateInstance(config);
+  platform_interface_->OnTraceProcessorCreated(tp.get());
   RETURN_IF_ERROR(MaybeUpdateSqlPackages(tp.get(), options));
 
   // Enable metatracing as soon as possible.
@@ -1960,7 +1976,8 @@ base::Status TraceProcessorMain(int argc, char** argv) {
   if (!options.trace_file_path.empty()) {
     base::TimeNanos t_load_start = base::GetWallTimeNs();
     double size_mb = 0;
-    RETURN_IF_ERROR(LoadTrace(tp.get(), options.trace_file_path, &size_mb));
+    RETURN_IF_ERROR(LoadTrace(tp.get(), platform_interface_.get(),
+                              options.trace_file_path, &size_mb));
     t_load = base::GetWallTimeNs() - t_load_start;
 
     double t_load_s = static_cast<double>(t_load.count()) / 1E9;
@@ -2073,7 +2090,10 @@ base::Status TraceProcessorMain(int argc, char** argv) {
 
   if (options.enable_httpd) {
 #if PERFETTO_BUILDFLAG(PERFETTO_TP_HTTPD)
-    Rpc rpc(std::move(tp), !options.trace_file_path.empty());
+    Rpc rpc(std::move(tp), !options.trace_file_path.empty(), config,
+            [this](TraceProcessor* tp) {
+              platform_interface_->OnTraceProcessorCreated(tp);
+            });
 
 #if PERFETTO_HAS_SIGNAL_H()
     static Rpc* g_rpc_for_signal_handler = &rpc;
@@ -2104,7 +2124,10 @@ base::Status TraceProcessorMain(int argc, char** argv) {
   }
 
   if (options.enable_stdiod) {
-    Rpc rpc(std::move(tp), !options.trace_file_path.empty());
+    Rpc rpc(std::move(tp), !options.trace_file_path.empty(), config,
+            [this](TraceProcessor* tp) {
+              platform_interface_->OnTraceProcessorCreated(tp);
+            });
 #if PERFETTO_HAS_SIGNAL_H()
     static Rpc* g_rpc_for_signal_handler = &rpc;
     g_tp_for_signal_handler = nullptr;
@@ -2128,15 +2151,6 @@ base::Status TraceProcessorMain(int argc, char** argv) {
   return base::OkStatus();
 }
 
-}  // namespace
+TraceProcessorShell::PlatformInterface::~PlatformInterface() = default;
 
 }  // namespace perfetto::trace_processor
-
-int main(int argc, char** argv) {
-  auto status = perfetto::trace_processor::TraceProcessorMain(argc, argv);
-  if (!status.ok()) {
-    fprintf(stderr, "%s\n", status.c_message());
-    return 1;
-  }
-  return 0;
-}
