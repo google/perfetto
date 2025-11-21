82b63fe400f65e5f952ec8f57f16a6f257d50c09
Edgar Arriaga <edgararriaga@google.com>
2025-11-21 23:30:28 +0000

    Fix for BTP queries not providing trace filename inside metadata

--- python/perfetto/trace_uri_resolver/path.py
+++ python/perfetto/trace_uri_resolver/path.py
@@ -27,7 +27,7 @@ class PathUriResolver(TraceUriResolver):
   def resolve(self) -> TraceUriResolver.Result:
     return [
         TraceUriResolver.Result(
-            trace=util.file_generator(self.path), metadata=dict())
+            trace=util.file_generator(self.path), metadata={'trace': self.path})
     ]
 
   @classmethod
