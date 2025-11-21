8412a1f4a41e2bf956fd09ded53cbd3a93bed6a0
Ella Su <haixiaosu@google.com>
2025-11-21 16:24:38 +0000

    changes to profile track.

--- ui/src/plugins/dev.perfetto.LinuxPerf/perf_samples_profile_track.ts
+++ ui/src/plugins/dev.perfetto.LinuxPerf/perf_samples_profile_track.ts
@@ -18,32 +18,24 @@ import {getColorForSample} from '../../components/colorizer';
 import {
   metricsFromTableOrSubquery,
   QueryFlamegraph,
-  QueryFlamegraphMetric,
 } from '../../components/query_flamegraph';
 import {DetailsShell} from '../../widgets/details_shell';
 import {Timestamp} from '../../components/widgets/timestamp';
 import {Time, time} from '../../base/time';
-import {
-  Flamegraph,
-  FlamegraphState,
-  FLAMEGRAPH_STATE_SCHEMA,
-} from '../../widgets/flamegraph';
+import {Flamegraph, FLAMEGRAPH_STATE_SCHEMA} from '../../widgets/flamegraph';
 import {Trace} from '../../public/trace';
 import {SliceTrack} from '../../components/tracks/slice_track';
 import {SourceDataset} from '../../trace_processor/dataset';
 import {Stack} from '../../widgets/stack';
-import {TrackEventDetailsPanelSerializeArgs} from '../../public/details_panel';
 
 // TODO(stevegolton): Dedupe this file with instruments_samples_profile_track.ts
 
 export function createPerfCallsitesTrack(
   trace: Trace,
   uri: string,
-  upid: number | undefined,
-  utid: number | undefined,
-  sessionId: number | undefined,
-  detailsPanelState: FlamegraphState | undefined,
-  onDetailsPanelStateChange: (state: FlamegraphState) => void,
+  upid?: number,
+  utid?: number,
+  sessionId?: number,
 ) {
   const constraints = [];
   if (upid !== undefined) {
@@ -82,98 +74,99 @@ export function createPerfCallsitesTrack(
     sliceName: () => 'Perf sample',
     colorizer: (row) => getColorForSample(row.callsiteId),
     detailsPanel: (row) => {
-      // TODO(lalitm): we should be able remove this around the 26Q2 timeframe
-      // We moved serialization from being attached to selections to instead being
-      // attached to the plugin that loaded the panel.
-      const serialization: TrackEventDetailsPanelSerializeArgs<
-        FlamegraphState | undefined
-      > = {
-        schema: FLAMEGRAPH_STATE_SCHEMA.optional(),
-        state: undefined,
+      // for callstack view when selecting a single sample
+      const metrics = metricsFromTableOrSubquery(
+        `
+          (
+            select
+              id,
+              parent_id as parentId,
+              name,
+              mapping_name,
+              source_file || ':' || line_number as source_location,
+              self_count
+            from _callstacks_for_callsites!((
+              select ps.callsite_id
+              from perf_sample ps
+              join thread t using (utid)
+              where ps.ts = ${row.ts}
+                and ${trackConstraints}
+            ))
+          )
+        `,
+        [
+          {
+            name: 'count',
+            unit: '',
+            columnName: 'self_count',
+          },
+        ],
+        'include perfetto module linux.perf.samples',
+        [{name: 'mapping_name', displayName: 'Mapping'}],
+        [
+          {
+            name: 'source_location',
+            displayName: 'Source location',
+            mergeAggregation: 'ONE_OR_SUMMARY',
+          },
+        ],
+      );
+      const serialization = {
+        schema: FLAMEGRAPH_STATE_SCHEMA,
+        state: Flamegraph.createDefaultState(metrics),
       };
-      let state = detailsPanelState;
-      const flamegraph = new QueryFlamegraph(trace);
-      const metrics: ReadonlyArray<QueryFlamegraphMetric> =
-        metricsFromTableOrSubquery(
-          `
-            (
-              select
-                id,
-                parent_id as parentId,
-                name,
-                mapping_name,
-                source_file || ':' || line_number as source_location,
-                self_count
-              from _callstacks_for_callsites!((
-                select ps.callsite_id
-                from perf_sample ps
-                join thread t using (utid)
-                where ps.ts = ${row.ts}
-                  and ${trackConstraints}
-              ))
-            )
-          `,
-          [
-            {
-              name: 'count',
-              unit: '',
-              columnName: 'self_count',
-            },
-          ],
-          'include perfetto module linux.perf.samples',
-          [{name: 'mapping_name', displayName: 'Mapping'}],
-          [
-            {
-              name: 'source_location',
-              displayName: 'Source location',
-              mergeAggregation: 'ONE_OR_SUMMARY',
-            },
-          ],
-        );
-
+      const flamegraph = new QueryFlamegraph(trace, metrics, serialization);
       return {
-        load: async () => {
-          // If the state in the serialization is not undefined, we should read from
-          // it.
-          // TODO(lalitm): remove this in 26Q2 - see comment on `serialization`.
-          if (serialization.state !== undefined) {
-            state = Flamegraph.updateState(serialization.state, metrics);
-            onDetailsPanelStateChange(state);
-            serialization.state = undefined;
-          }
-        },
         render: () =>
-          renderDetailsPanel(
-            trace,
-            flamegraph,
-            metrics,
-            Time.fromRaw(row.ts),
-            state,
-            (newState) => {
-              state = newState;
-              onDetailsPanelStateChange(newState);
-            },
-          ),
+          renderDetailsPanel(trace, flamegraph, Time.fromRaw(row.ts)),
         serialization,
       };
     },
   });
 }
 
+export function createSkippedCallsitesTrack(
+  trace: Trace,
+  uri: string,
+) {
+
+  return SliceTrack.create({
+    trace,
+    uri,
+    dataset: new SourceDataset({
+      schema: {
+        id: NUM,
+        ts: LONG,
+        callsiteId: NUM,
+      },
+      src: `
+       SELECT
+          p.id,
+          ts,
+          0 AS callsiteId,
+          upid
+        FROM perf_sample AS p
+        JOIN thread USING (utid)
+        WHERE callsite_id IS NULL
+        ORDER BY ts
+      `,
+    }),
+    sliceName: () => 'Perf sample (skipped)',
+    colorizer: (row) => getColorForSample(row.callsiteId),
+  });
+}
+
 function renderDetailsPanel(
   trace: Trace,
   flamegraph: QueryFlamegraph,
-  metrics: ReadonlyArray<QueryFlamegraphMetric>,
   ts: time,
-  state: FlamegraphState | undefined,
-  onStateChange: (state: FlamegraphState) => void,
 ) {
   return m(
     '.pf-flamegraph-profile',
     m(
       DetailsShell,
       {
-        fillHeight: true,
+        fillParent: true,
         title: 'Perf sample',
         buttons: m(Stack, {orientation: 'horizontal', spacing: 'large'}, [
           m('span', [
@@ -185,7 +178,7 @@ function renderDetailsPanel(
           ]),
         ]),
       },
-      flamegraph.render({metrics, state, onStateChange}),
+      flamegraph.render(),
     ),
   );
 }
