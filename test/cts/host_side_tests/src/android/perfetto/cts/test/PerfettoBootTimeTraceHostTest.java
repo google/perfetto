fba81154362aa91decad1d30d7ef3326128f940a
Kirill Timofeev <ktimofeev@google.com>
2025-11-28 12:14:58 +0000

    Perfetto init script: get rid of `wait_for_prop`.
    When changing the init script to support perfetto reboot traces (see b/382209797), the perfetto initialization process became two phased:
    1. `property:persist.traced.enable` is triggered first
    2. then, on other trigger, `traced` service is started
    
    Previously, the action was triggered immediately by step 1, but included a `wait_for_prop` command waiting for step 2. This blocked the boot sequence, preventing `traced` from ever starting, resulting in a deadlock.
    
    To fix this, we removed `wait_for_prop` and moved `sys.trace.traced_started` to the action trigger. The action is now triggered only after `traced` has successfully started.
    
    This CL also adds a host test to verify that boot tracing works correctly.
    
    Tested: atest CtsPerfettoBootTimeTraceHostTestCases

---
+++ test/cts/host_side_tests/src/android/perfetto/cts/test/PerfettoBootTimeTraceHostTest.java
@@ -0,0 +1,273 @@
+package android.perfetto.cts.test;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.android.tradefed.device.DeviceNotAvailableException;
+import com.android.tradefed.device.ITestDevice;
+import com.android.tradefed.testtype.DeviceJUnit4ClassRunner;
+import com.android.tradefed.testtype.junit4.BaseHostJUnit4Test;
+import com.android.tradefed.util.CommandResult;
+import com.android.tradefed.util.CommandStatus;
+import com.android.tradefed.util.FileUtil;
+import com.android.tradefed.util.ProcessInfo;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import perfetto.protos.AndroidLog.AndroidLogPacket;
+import perfetto.protos.TraceOuterClass.Trace;
+import perfetto.protos.TracePacketOuterClass.TracePacket;
+import perfetto.protos.TracingServiceStateOuterClass.TracingServiceState;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@RunWith(DeviceJUnit4ClassRunner.class)
+public class PerfettoBootTimeTraceHostTest extends BaseHostJUnit4Test {
+    private static final String BOOT_TRACE_ENABLE_PROPERTY = "persist.debug.perfetto.boottrace";
+    private static final String BOOT_TRACE_RESULT_ON_DEVICE_PATH =
+            "/data/misc/perfetto-traces/boottrace.perfetto-trace";
+    private static final String BOOT_TRACE_CONFIG_ON_DEVICE_PATH =
+            "/data/misc/perfetto-configs/boottrace.pbtxt";
+    private static final String BOOT_TRACE_STOP_TRIGGER_CONFIG_ON_DEVICE_PATH =
+            "/data/misc/perfetto-configs/stopboottracetrigger.pbtxt";
+
+    private static final String BOOT_TRACE_CONFIG_SESSION_NAME = "PerfettoBootTimeTraceHostTest";
+    private static final String BOOT_TRACE_CONFIG =
+            """
+            unique_session_name: "%s"
+            buffers {
+                size_kb: 1024
+                fill_policy: RING_BUFFER
+            }
+            data_sources: {
+                config {
+                    name: "android.log"
+                    target_buffer: 0
+                    android_log_config {
+                        log_ids: LID_DEFAULT
+                        log_ids: LID_EVENTS
+                        log_ids: LID_KERNEL
+                        log_ids: LID_SYSTEM
+                    }
+                }
+            }
+            trigger_config {
+              triggers: {
+                name: "stop-boot-trace-trigger"
+                stop_delay_ms: 0
+              }
+              trigger_mode: STOP_TRACING
+              trigger_timeout_ms: 600000 # can't be used together with 'duration_ms', 10 minutes
+            }
+            """
+                    .formatted(BOOT_TRACE_CONFIG_SESSION_NAME)
+                    .stripIndent();
+
+    private static final String BOOT_TRACE_STOP_TRIGGER_CONFIG =
+            """
+            activate_triggers: "stop-boot-trace-trigger"
+            """
+                    .stripIndent();
+
+    private static File mTraceConfigFile = null;
+    private static File mTraceStopTriggerConfigFile = null;
+
+    private ITestDevice mTestDevice = null;
+
+    @BeforeClass
+    public static void setUpClass() throws IOException {
+        mTraceConfigFile = FileUtil.createTempFile("boot_trace_config", ".pbtxt");
+        FileUtil.writeToFile(BOOT_TRACE_CONFIG, mTraceConfigFile);
+        mTraceStopTriggerConfigFile =
+                FileUtil.createTempFile("boot_trace_stop_trigger_config", ".pbtxt");
+        FileUtil.writeToFile(BOOT_TRACE_STOP_TRIGGER_CONFIG, mTraceStopTriggerConfigFile);
+    }
+
+    @Before
+    public void setUp() throws DeviceNotAvailableException {
+        mTestDevice = getDevice();
+        // Make sure device is in the expected state
+        // Traced is running
+        ProcessInfo traced = mTestDevice.getProcessByName("traced");
+        assertThat(traced).isNotNull();
+        // Boot trace was not triggered
+        assertThat(mTestDevice.getProperty(BOOT_TRACE_ENABLE_PROPERTY)).isNotEqualTo("1");
+        if (mTestDevice.doesFileExist(BOOT_TRACE_RESULT_ON_DEVICE_PATH)) {
+            // Boot trace is not running
+            CommandResult result =
+                    mTestDevice.executeShellV2Command(
+                            "lsof -t " + BOOT_TRACE_RESULT_ON_DEVICE_PATH);
+            if (result.getStatus() == CommandStatus.SUCCESS) {
+                String pid = result.getStdout().trim();
+                if (!pid.isEmpty()) {
+                    Assert.fail("Boot trace is running, pid: " + pid);
+                }
+            }
+            mTestDevice.deleteFile(BOOT_TRACE_RESULT_ON_DEVICE_PATH);
+        }
+        cleanUpOnDeviceBootTraceFiles();
+    }
+
+    @After
+    public void tearDown() throws DeviceNotAvailableException {
+        cleanUpOnDeviceBootTraceFiles();
+        mTestDevice = null;
+    }
+
+    @Test
+    public void testBootTraceStart() throws DeviceNotAvailableException, IOException {
+        assertThat(mTestDevice.pushFile(mTraceConfigFile, BOOT_TRACE_CONFIG_ON_DEVICE_PATH))
+                .isTrue();
+        assertThat(mTestDevice.setProperty(BOOT_TRACE_ENABLE_PROPERTY, "1")).isTrue();
+
+        // Assert tracing session is not started when property set
+        List<String> runningTracingSessionNames = assertGetRunningTracingSessionNames();
+        assertThat(runningTracingSessionNames).doesNotContain(BOOT_TRACE_CONFIG_SESSION_NAME);
+
+        mTestDevice.reboot();
+
+        // Assert tracing session is started on device reboot
+        runningTracingSessionNames = assertGetRunningTracingSessionNames();
+        assertThat(runningTracingSessionNames).contains(BOOT_TRACE_CONFIG_SESSION_NAME);
+
+        // Assert property is reset after reboot.
+        assertThat(mTestDevice.getProperty(BOOT_TRACE_ENABLE_PROPERTY)).isNotEqualTo("1");
+
+        // Now push the trigger config to stop the tracing session.
+        assertThat(
+                        mTestDevice.pushFile(
+                                mTraceStopTriggerConfigFile,
+                                BOOT_TRACE_STOP_TRIGGER_CONFIG_ON_DEVICE_PATH))
+                .isTrue();
+        CommandResult result =
+                mTestDevice.executeShellV2Command(
+                        "perfetto --txt -c " + BOOT_TRACE_STOP_TRIGGER_CONFIG_ON_DEVICE_PATH);
+        assertThat(result.getStatus()).isEqualTo(CommandStatus.SUCCESS);
+
+        // Assert boot tracing session stopped.
+        runningTracingSessionNames = assertGetRunningTracingSessionNames();
+        assertThat(runningTracingSessionNames).doesNotContain(BOOT_TRACE_CONFIG_SESSION_NAME);
+
+        assertThat(mTestDevice.doesFileExist(BOOT_TRACE_RESULT_ON_DEVICE_PATH)).isTrue();
+        File traceFile = mTestDevice.pullFile(BOOT_TRACE_RESULT_ON_DEVICE_PATH);
+        assertThat(traceFile).isNotNull();
+
+        // Tracing session was started at the early boot stage, so we expect that:
+        // 1. It has multiple logcat events with the "init" tag (issues by the init process)
+        // 2. It was started before system is booted, so it has multiple "processing action .*
+        // sys.boot_completed=1 .*" lines
+        List<String> messages = getInitLogcatMessages(traceFile);
+        assertThat(messages).isNotEmpty();
+        boolean hasBootCompletedLine = false;
+        for (String message : messages) {
+            if (message.startsWith("processing action")
+                    && message.contains("sys.boot_completed=1")) {
+                hasBootCompletedLine = true;
+                break;
+            }
+        }
+        assertThat(hasBootCompletedLine).isTrue();
+    }
+
+    @Test
+    public void testBootTraceStartAndStop() throws DeviceNotAvailableException, IOException {
+        assertThat(mTestDevice.pushFile(mTraceConfigFile, BOOT_TRACE_CONFIG_ON_DEVICE_PATH))
+                .isTrue();
+        assertThat(
+                        mTestDevice.pushFile(
+                                mTraceStopTriggerConfigFile,
+                                BOOT_TRACE_STOP_TRIGGER_CONFIG_ON_DEVICE_PATH))
+                .isTrue();
+        assertThat(mTestDevice.setProperty(BOOT_TRACE_ENABLE_PROPERTY, "1")).isTrue();
+
+        // Assert tracing session is not started when property set
+        List<String> runningTracingSessionNames = assertGetRunningTracingSessionNames();
+        assertThat(runningTracingSessionNames).doesNotContain(BOOT_TRACE_CONFIG_SESSION_NAME);
+
+        // Assert there is no trace file before reboot.
+        assertThat(mTestDevice.doesFileExist(BOOT_TRACE_RESULT_ON_DEVICE_PATH)).isFalse();
+
+        mTestDevice.reboot();
+
+        // Assert property is reset after reboot.
+        assertThat(mTestDevice.getProperty(BOOT_TRACE_ENABLE_PROPERTY)).isNotEqualTo("1");
+        // Assertf tracing session was already stopped
+        runningTracingSessionNames = assertGetRunningTracingSessionNames();
+        assertThat(runningTracingSessionNames).doesNotContain(BOOT_TRACE_CONFIG_SESSION_NAME);
+
+        assertThat(mTestDevice.doesFileExist(BOOT_TRACE_RESULT_ON_DEVICE_PATH)).isTrue();
+        File traceFile = mTestDevice.pullFile(BOOT_TRACE_RESULT_ON_DEVICE_PATH);
+        assertThat(traceFile).isNotNull();
+
+        // Tracing session was started at the early boot stage, so we expect that:
+        // 1. It has multiple logcat events with the "init" tag (issues by the init process)
+        // 2. It is stopped by a trigger on a system boot, so it may or may not has any
+        // "sys.boot_completed=1 .*" lines.
+        List<String> messages = getInitLogcatMessages(traceFile);
+        assertThat(messages).isNotEmpty();
+        boolean hasProcessingActionLine = false;
+        for (String message : messages) {
+            if (message.startsWith("processing action")) {
+                hasProcessingActionLine = true;
+                break;
+            }
+        }
+        assertThat(hasProcessingActionLine).isTrue();
+    }
+
+    List<String> getInitLogcatMessages(File traceFile) throws IOException {
+        ArrayList<String> messages = new ArrayList<>();
+        Trace trace = Trace.parseFrom(new FileInputStream(traceFile));
+        for (TracePacket packet : trace.getPacketList()) {
+            if (packet.hasAndroidLog()) {
+                AndroidLogPacket logPacket = packet.getAndroidLog();
+                for (AndroidLogPacket.LogEvent event : logPacket.getEventsList()) {
+                    if (event.getTag().equals("init")) {
+                        messages.add(event.getMessage());
+                    }
+                }
+            }
+        }
+        return messages;
+    }
+
+    private TracingServiceState assertGetTracingServiceState() throws DeviceNotAvailableException {
+        ByteArrayOutputStream rawOutput = new ByteArrayOutputStream();
+        CommandResult result = mTestDevice.executeShellV2Command("perfetto --query-raw", rawOutput);
+        assertThat(result.getStatus()).isEqualTo(CommandStatus.SUCCESS);
+        try {
+            return TracingServiceState.parseFrom(rawOutput.toByteArray());
+        } catch (InvalidProtocolBufferException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private List<String> assertGetRunningTracingSessionNames() throws DeviceNotAvailableException {
+        TracingServiceState tracingServiceState = assertGetTracingServiceState();
+        ArrayList<String> runningTracingSessionNames = new ArrayList<>();
+        for (TracingServiceState.TracingSession session :
+                tracingServiceState.getTracingSessionsList()) {
+            if (session.getIsStarted()) {
+                runningTracingSessionNames.add(session.getUniqueSessionName());
+            }
+        }
+        return runningTracingSessionNames;
+    }
+
+    private void cleanUpOnDeviceBootTraceFiles() throws DeviceNotAvailableException {
+        mTestDevice.deleteFile(BOOT_TRACE_STOP_TRIGGER_CONFIG_ON_DEVICE_PATH);
+        mTestDevice.deleteFile(BOOT_TRACE_CONFIG_ON_DEVICE_PATH);
+        mTestDevice.deleteFile(BOOT_TRACE_RESULT_ON_DEVICE_PATH);
+    }
+}
