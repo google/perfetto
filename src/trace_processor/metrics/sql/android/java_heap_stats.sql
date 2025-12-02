7baff3dc44512a85e46aca1261a1203049ba59ac
haixiaosugoogle <haixiaosu@google.com>
2025-12-02 14:16:09 +0000

    Update java_heap_stats.sql

--- src/trace_processor/metrics/sql/android/java_heap_stats.sql
+++ src/trace_processor/metrics/sql/android/java_heap_stats.sql
@@ -26,7 +26,7 @@ heap_graph_sample_protos AS (
     upid,
     RepeatedField(JavaHeapStats_Sample(
       'ts', graph_sample_ts,
-      'process_uptime_ms', process_uptime / 1e6,
+      'process_uptime_ms', CAST(process_uptime / 1e6 as INT64),
       'heap_size', total_heap_size,
       'heap_native_size', total_native_alloc_registry_size,
       'obj_count', total_obj_count,
