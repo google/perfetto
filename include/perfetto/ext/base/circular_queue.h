google/perfetto

commit 480bbd106393be590e294f0f13037f439892c9a8

core: Introduce TraceBufferV2 (#3092)
TraceBufferV2 is a major rewrite of the historical TraceBuffer.
The main reasons for the redesign are:
- Supporting ProtoVM and future interning improvements.
- Reducing space wasted for internal fragmentation when a
  chunk contains only few bytes of trace data.
- Making the trace output respect more the input ordering,
  rather than completely reordering packets by sequence.

This CL contains a design doc that discusses various aspects
and challenges of the new TraceBuffer architecture.

Benchmark results:
```
Apple Macbook (M4)
------------------
BM_TraceBuffer_WR_SingleWriter<TraceBufferV1>       bytes_per_second=9.77742G/s
BM_TraceBuffer_WR_SingleWriter<TraceBufferV2>       bytes_per_second=12.6395G/s
BM_TraceBuffer_WR_MultipleWriters<TraceBufferV1>    bytes_per_second=8.65385G/s
BM_TraceBuffer_WR_MultipleWriters<TraceBufferV2>    bytes_per_second=11.7582G/s
BM_TraceBuffer_RD_MixedPackets<TraceBufferV1>      bytes_per_second=4.27694G/s
BM_TraceBuffer_RD_MixedPackets<TraceBufferV2>      bytes_per_second=4.35475G/s

Pixel 7
-------
BM_TraceBuffer_WR_SingleWriter<TraceBufferV1>      bytes_per_second=4.4379G/s
BM_TraceBuffer_WR_SingleWriter<TraceBufferV2>      bytes_per_second=3.7931G/s
BM_TraceBuffer_WR_MultipleWriters<TraceBufferV1>   bytes_per_second=3.19148G/s
BM_TraceBuffer_WR_MultipleWriters<TraceBufferV2>   bytes_per_second=3.47354G/s
BM_TraceBuffer_RD_MixedPackets<TraceBufferV1>      bytes_per_second=1.26698G/s
BM_TraceBuffer_RD_MixedPackets<TraceBufferV2>      bytes_per_second=1.35394G/s
```

Bug: b/447426810


**Stack:**
- [#3091](https://github.com/google/perfetto/pull/3091) (tb_extract)
  - **[This PR] (tbv2)**

include/perfetto/ext/base/circular_queue.h
@@ -160,6 +160,8 @@ class CircularQueue {
     }
 
    private:
+    friend class CircularQueue<T>;
+
     inline void Add(difference_type offset) {
       pos_ = static_cast<uint64_t>(static_cast<difference_type>(pos_) + offset);
       PERFETTO_DCHECK(pos_ <= queue_->end_);
@@ -172,6 +174,11 @@ class CircularQueue {
     uint32_t generation_;
 #endif
   };
+
+  using ReverseIterator = std::reverse_iterator<Iterator>;
+  using value_type = T;
+  using const_iterator = Iterator;
+  using iterator = Iterator;
 
   explicit CircularQueue(size_t initial_capacity = 1024) {
     Grow(initial_capacity);
@@ -233,6 +240,38 @@ class CircularQueue {
 
   void pop_front() { erase_front(1); }
 
+  void InsertBefore(Iterator pos, T value) {
+    increment_generation();
+    PERFETTO_DCHECK(pos.queue_ == this);
+    PERFETTO_DCHECK(pos.pos_ >= begin_ && pos.pos_ <= end_);
+
+    // Convert to relative position before potential Grow(), which rebases
+    // begin_ to 0.
+    uint64_t relative_pos = pos.pos_ - begin_;
+
+    if (PERFETTO_UNLIKELY(size() >= capacity_))
+      Grow();
+
+    // Calculate the actual insertion position (relative_pos from current
+    // begin_).
+    uint64_t insert_pos = begin_ + relative_pos;
+
+    // Move all elements one step forward from end_ - 1 to insert_pos.
+    for (uint64_t i = end_++; i > insert_pos; --i) {
+      T* dst = Get(i);
+      T* src = Get(i - 1);
+      new (dst) T(std::move(*src));
+      src->~T();
+    }
+
+    // Place the new value at insert_pos.
+    new (Get(insert_pos)) T(std::move(value));
+  }
+
+  void InsertAfter(ReverseIterator pos, T value) {
+    InsertBefore(pos.base(), std::move(value));
+  }
+
   void clear() { erase_front(size()); }
 
   void shrink_to_fit() {
@@ -252,6 +291,21 @@ class CircularQueue {
 
   Iterator begin() { return Iterator(this, begin_, generation()); }
   Iterator end() { return Iterator(this, end_, generation()); }
+  Iterator begin() const {
+    return Iterator(const_cast<CircularQueue*>(this), begin_, generation());
+  }
+  Iterator end() const {
+    return Iterator(const_cast<CircularQueue*>(this), end_, generation());
+  }
+
+  ReverseIterator rbegin() { return ReverseIterator(end()); }
+  ReverseIterator rend() { return ReverseIterator(begin()); }
+
+  Iterator Find(const T& value) {
+    auto it = std::find(begin(), end(), value);
+    return it;
+  }
+
   T& front() { return *begin(); }
   T& back() { return *(end() - 1); }
 

Android.bp
@@ -17153,6 +17153,7 @@ filegroup {
         "src/tracing/service/packet_stream_validator.cc",
         "src/tracing/service/random.cc",
         "src/tracing/service/trace_buffer_v1.cc",
+        "src/tracing/service/trace_buffer_v2.cc",
         "src/tracing/service/tracing_service_impl.cc",
     ],
 }
@@ -17164,6 +17165,7 @@ filegroup {
         "src/tracing/service/histogram_unittest.cc",
         "src/tracing/service/packet_stream_validator_unittest.cc",
         "src/tracing/service/trace_buffer_v1_unittest.cc",
+        "src/tracing/service/trace_buffer_v2_unittest.cc",
         "src/tracing/service/tracing_service_impl_unittest.cc",
         "src/tracing/service/zlib_compressor_unittest.cc",
     ],

BUILD
@@ -4752,6 +4752,8 @@ perfetto_filegroup(
         "src/tracing/service/trace_buffer.h",
         "src/tracing/service/trace_buffer_v1.cc",
         "src/tracing/service/trace_buffer_v1.h",
+        "src/tracing/service/trace_buffer_v2.cc",
+        "src/tracing/service/trace_buffer_v2.h",
         "src/tracing/service/tracing_service_impl.cc",
         "src/tracing/service/tracing_service_impl.h",
     ],

include/perfetto/ext/tracing/core/shared_memory_abi.h
@@ -310,6 +310,7 @@ class SharedMemoryABI {
   // There is one Chunk header per chunk (hence PageLayout per page) at the
   // beginning of each chunk.
   struct ChunkHeader {
+    inline static constexpr uint8_t kFlagsMask = 0x7;
     enum Flags : uint8_t {
       // If set, the first TracePacket in the chunk is partial and continues
       // from |chunk_id| - 1 (within the same |writer_id|).

src/base/circular_queue_unittest.cc
@@ -24,6 +24,8 @@ namespace perfetto {
 namespace base {
 namespace {
 
+using testing::ElementsAre;
+
 TEST(CircularQueueTest, Int) {
   CircularQueue<int> queue(/*initial_capacity=*/1);
   ASSERT_EQ(queue.size(), 0u);
@@ -171,6 +173,152 @@ TEST(CircularQueueTest, Iterators) {
     ASSERT_TRUE(std::is_sorted(begin, mid));
     ASSERT_TRUE(std::is_sorted(mid, end));
   }
+}
+
+TEST(CircularQueueTest, ReverseIterators) {
+  CircularQueue<int> queue;
+  ASSERT_EQ(queue.rbegin(), queue.rend());
+  queue.emplace_back(1);
+
+  ASSERT_EQ(*queue.rbegin(), 1);
+  ASSERT_EQ(++queue.rbegin(), queue.rend());
+
+  queue.emplace_back(2);
+  queue.emplace_back(3);
+
+  auto it = queue.rbegin();
+  ASSERT_EQ(*(it++), 3);
+  ASSERT_EQ(*(it++), 2);
+  ASSERT_EQ(*(it++), 1);
+  ASSERT_EQ(it, queue.rend());
+}
+
+TEST(CircularQueueTest, InsertBefore) {
+  CircularQueue<int> queue;
+  queue.InsertBefore(queue.end(), 20);
+  ASSERT_THAT(queue, ElementsAre(20));
+
+  queue.InsertBefore(queue.begin(), 10);
+  ASSERT_THAT(queue, ElementsAre(10, 20));
+
+  queue.InsertBefore(queue.end(), 40);
+  ASSERT_THAT(queue, ElementsAre(10, 20, 40));
+
+  queue.InsertBefore(queue.Find(40), 30);
+  ASSERT_THAT(queue, ElementsAre(10, 20, 30, 40));
+
+  queue.InsertBefore(queue.begin(), 0);
+  ASSERT_THAT(queue, ElementsAre(0, 10, 20, 30, 40));
+
+  // Now test InsertAfter(reverse_iterator). There is a catch here,
+  // Insert on a reverse iterator actually inserts *after* the iterator.
+  // As bad as it sound, this is consistent with what other STL containers do.
+  // It's tied to the fact that std::reverse_iterator creates maps r == i-1.
+  // From STL docs:
+  // > For a reverse iterator r constructed from an iterator i, the relationship
+  // > &*r == &*(i - 1) is always true (as long as r is dereferenceable); thus a
+  // > reverse iterator constructed from a one-past-the-end iterator
+  // > dereferences to the last element in a sequence.
+  // ```
+  queue.InsertAfter(queue.rbegin(), 60);
+  ASSERT_THAT(queue, ElementsAre(0, 10, 20, 30, 40, 60));
+
+  for (auto it = queue.rbegin(); it != queue.rend(); ++it) {
+    if (*it == 40) {
+      queue.InsertAfter(it, 50);
+      break;
+    }
+  }
+  ASSERT_THAT(queue, ElementsAre(0, 10, 20, 30, 40, 50, 60));
+
+  // I know you don't believe me, so here's the proof.
+  std::vector<int> v{10, 20};
+  v.emplace(v.rbegin().base(), 40);
+  ASSERT_THAT(v, ElementsAre(10, 20, 40));
+  for (auto it = v.rbegin(); it != v.rend(); ++it) {
+    if (*it == 20) {
+      v.insert(it.base(), 30);
+      break;
+    }
+  }
+  ASSERT_THAT(v, ElementsAre(10, 20, 30, 40));
+}
+
+TEST(CircularQueueTest, InsertBeforeReverse) {
+  CircularQueue<int> queue;
+
+  std::array<int, 5> new_entries{4, 1, 5, 2, 3};
+  for (int n : new_entries) {
+    auto it = queue.rbegin();
+    while (it != queue.rend() && n < *it) {
+      ++it;
+    }
+    queue.InsertAfter(it, n);
+  }
+  ASSERT_THAT(queue, ElementsAre(1, 2, 3, 4, 5));
+}
+
+// Test that InsertBefore works correctly when it triggers a capacity growth.
+// This verifies that the iterator's pos_ (which is an abstract index) remains
+// valid after the internal storage is reallocated during Grow().
+TEST(CircularQueueTest, InsertBeforeWithGrow) {
+  // Use a small initial capacity so we can easily trigger growth.
+  CircularQueue<int> queue(/*initial_capacity=*/4);
+
+  // Fill the queue to capacity.
+  queue.emplace_back(10);
+  queue.emplace_back(20);
+  queue.emplace_back(30);
+  queue.emplace_back(40);
+  ASSERT_EQ(queue.size(), 4u);
+  ASSERT_EQ(queue.capacity(), 4u);
+
+  // Get the position where we want to insert (at element 30).
+  // Note: We can't use the iterator directly after InsertBefore because
+  // increment_generation() invalidates it. We use pos_ which is accessed
+  // directly by InsertBefore.
+  auto it = queue.begin() + 2;
+  ASSERT_EQ(*it, 30);
+
+  // This InsertBefore should trigger Grow() since we're at capacity.
+  // After Grow(), the iterator's pos_ (an abstract index) should still be
+  // valid because CircularQueue uses monotonic indices, not pointers.
+  queue.InsertBefore(it, 25);
+
+  // Verify the queue grew.
+  ASSERT_GT(queue.capacity(), 4u);
+  ASSERT_EQ(queue.size(), 5u);
+
+  // Verify all elements are in the correct order.
+  ASSERT_THAT(queue, ElementsAre(10, 20, 25, 30, 40));
+}
+
+// Test InsertBefore with growth when the queue has wrapped around internally.
+TEST(CircularQueueTest, InsertBeforeWithGrowAfterWrap) {
+  CircularQueue<int> queue(/*initial_capacity=*/4);
+
+  // Fill and then pop to move the internal begin_ forward.
+  queue.emplace_back(1);
+  queue.emplace_back(2);
+  queue.pop_front();
+  queue.pop_front();
+
+  // Now internal begin_ is at position 2. Add elements to wrap around.
+  queue.emplace_back(10);
+  queue.emplace_back(20);
+  queue.emplace_back(30);
+  queue.emplace_back(40);
+  ASSERT_EQ(queue.size(), 4u);
+  ASSERT_EQ(queue.capacity(), 4u);
+
+  // Insert in the middle, triggering growth while wrapped.
+  auto it = queue.begin() + 2;
+  ASSERT_EQ(*it, 30);
+  queue.InsertBefore(it, 25);
+
+  ASSERT_GT(queue.capacity(), 4u);
+  ASSERT_EQ(queue.size(), 5u);
+  ASSERT_THAT(queue, ElementsAre(10, 20, 25, 30, 40));
 }
 
 TEST(CircularQueueTest, ObjectLifetime) {

src/tracing/service/BUILD.gn
@@ -51,6 +51,8 @@ source_set("service") {
     "trace_buffer.h",
     "trace_buffer_v1.cc",
     "trace_buffer_v1.h",
+    "trace_buffer_v2.cc",
+    "trace_buffer_v2.h",
     "tracing_service_impl.cc",
     "tracing_service_impl.h",
   ]
@@ -105,6 +107,7 @@ perfetto_unittest_source_set("unittests") {
     "histogram_unittest.cc",
     "packet_stream_validator_unittest.cc",
     "trace_buffer_v1_unittest.cc",
+    "trace_buffer_v2_unittest.cc",
   ]
 
   if (enable_perfetto_zlib) {
@@ -129,8 +132,12 @@ if (enable_perfetto_benchmarks) {
       "../../../protos/perfetto/trace/ftrace:zero",
       "../../protozero",
       "../core",
+      "../test:test_support",
     ]
-    sources = [ "packet_stream_validator_benchmark.cc" ]
+    sources = [
+      "packet_stream_validator_benchmark.cc",
+      "trace_buffer_benchmark.cc",
+    ]
   }
 }
 

commit f3951539fede7be03fa252128431b67beff49d3b

Add missing include to fix perfetto->chrome roll (#4139)
This PR adds missing #include <algorithm>
The perfetto -> chromium roll broke in
https://github.com/google/perfetto/commit/480bbd106393be590e294f0f13037f439892c9a8
See
https://chromium-review.googlesource.com/c/chromium/src/+/7268747?tab=checks

include/perfetto/ext/base/circular_queue.h
@@ -20,6 +20,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+#include <algorithm>
 #include <cstddef>
 #include <iterator>
 