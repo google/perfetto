73718bb69e99782681cbb034115c2f51a55eacb6
Steve Golton <stevegolton@google.com>
2025-12-17 17:13:09 +0000

    ui: DataGrid: Fix bugs in sort order and sort arrow hinting (#4141)
    - Fix bug where sorting on a pivot column would not clear sorts on
    aggregate columns.
    - Fix bug where the sort hint of aggregate columns always shows
    'ascending' rather than echoing what the current sort order is.

--- ui/src/components/widgets/datagrid/datagrid.ts
+++ ui/src/components/widgets/datagrid/datagrid.ts
@@ -651,7 +651,17 @@ export class DataGrid implements m.ClassComponent<DataGridAttrs> {
       return {field, sort: undefined};
     });
 
-    const newPivot: Pivot = {...this.pivot, groupBy: newGroupBy};
+    // Clear sort on aggregate columns when sorting by groupBy
+    const newAggregates = this.pivot.aggregates?.map((agg) => ({
+      ...agg,
+      sort: undefined,
+    }));
+
+    const newPivot: Pivot = {
+      ...this.pivot,
+      groupBy: newGroupBy,
+      aggregates: newAggregates,
+    };
     this.pivot = newPivot;
     attrs.onPivotChanged?.(newPivot);
   }
@@ -984,7 +994,8 @@ export class DataGrid implements m.ClassComponent<DataGridAttrs> {
     } = ctx;
 
     // Find the current sort direction (if any column is sorted)
-    const currentSort = this.columns.find((c) => c.sort)?.sort ?? 'ASC';
+    const currentSortDirection =
+      this.columns.find((c) => c.sort)?.sort ?? 'ASC';
 
     const columns: GridColumn[] = this.columns.map((col, colIndex) => {
       const {field, sort, aggregate} = col;
@@ -1107,7 +1118,7 @@ export class DataGrid implements m.ClassComponent<DataGridAttrs> {
           GridHeaderCell,
           {
             sort,
-            hintSortDirection: currentSort,
+            hintSortDirection: currentSortDirection,
             onSort: (direction) => this.updateSort(field, direction, attrs),
             menuItems,
             subContent,
@@ -1381,6 +1392,7 @@ export class DataGrid implements m.ClassComponent<DataGridAttrs> {
           GridHeaderCell,
           {
             sort,
+            hintSortDirection: currentSortDirection,
             onSort: (direction) =>
               this.updatePivotAggregateSort(i, direction, attrs),
             menuItems,
