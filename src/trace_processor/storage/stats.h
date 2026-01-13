1edd49cba96ca28b036c2657f7a8aea7eedf16b1
Kean Mariotti <keanmariotti@google.com>
2026-01-13 15:50:52 +0000

    tp: integrate ProtoVM
    Change-Id: Ia5d69ed4f3f7314630c7a0217a0f4d3a43f9d1a8

--- src/trace_processor/storage/stats.h
+++ src/trace_processor/storage/stats.h
@@ -826,7 +826,12 @@ namespace perfetto::trace_processor::stats {
   F(primes_missing_parent_id, kSingle, kInfo, kAnalysis,                       \
     "The parent_id field was missing from an edge that requires it."),         \
   F(primes_malformed_timestamp, kSingle, kDataLoss, kAnalysis,                 \
-    "The timestamp for an edge or trace was not able to be parsed")  // clang-format on
+    "The timestamp for an edge or trace was not able to be parsed"),           \
+  F(protovm_abort, kSingle,  kError, kAnalysis,                                \
+      "A proto VM instance aborted the execution while applying the patch. "   \
+      "This might be due to inconsistencies between VM program logic and "     \
+      "actual patch format.")
+// clang-format on
 
 enum Type {
   kSingle,  // Single-value property, one value per key.
