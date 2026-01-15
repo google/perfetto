0d5ee7fdcd37e36e2d75d0c5c92700fa2bc6e00d
Lalit Maganti <lalitm@google.com>
2026-01-15 02:13:49 +0000

    protozero: refactor filtering tests to be based on descriptors
    Will work a lot better for upcoming changes to migrate to an annotation
    based proto system

--- src/protozero/filtering/filter_util.cc
+++ src/protozero/filtering/filter_util.cc
@@ -32,6 +32,7 @@
 
 #include <google/protobuf/compiler/importer.h>
 #include <google/protobuf/descriptor.h>
+#include <google/protobuf/descriptor.pb.h>
 
 #include "perfetto/base/build_config.h"
 #include "perfetto/base/logging.h"
@@ -97,11 +98,8 @@ bool FilterUtil::LoadMessageDefinition(
     const std::set<std::string>& string_filter_fields,
     const std::map<std::string, uint32_t>& filter_string_semantic_types) {
   passthrough_fields_ = passthrough_fields;
-  passthrough_fields_seen_.clear();
   filter_string_fields_ = string_filter_fields;
-  filter_string_fields_seen_.clear();
   filter_string_semantic_types_ = filter_string_semantic_types;
-  filter_string_semantic_types_seen_.clear();
 
   // The protobuf compiler doesn't like backslashes and prints an error like:
   // Error C:\it7mjanpw3\perfetto-a16500 -1:0: Backslashes, consecutive slashes,
@@ -149,6 +147,53 @@ bool FilterUtil::LoadMessageDefinition(
                   root_message.c_str(), proto_file.c_str());
     return false;
   }
+
+  return LoadFromDescriptor(root_msg);
+}
+
+bool FilterUtil::LoadFromDescriptorSet(
+    const uint8_t* descriptor_set_bytes,
+    size_t descriptor_set_size,
+    const std::string& root_message,
+    const std::set<std::string>& passthrough_fields,
+    const std::set<std::string>& string_filter_fields,
+    const std::map<std::string, uint32_t>& filter_string_semantic_types) {
+  passthrough_fields_ = passthrough_fields;
+  filter_string_fields_ = string_filter_fields;
+  filter_string_semantic_types_ = filter_string_semantic_types;
+
+  google::protobuf::FileDescriptorSet fds;
+  if (!fds.ParseFromArray(descriptor_set_bytes,
+                          static_cast<int>(descriptor_set_size))) {
+    PERFETTO_ELOG("Failed to parse FileDescriptorSet");
+    return false;
+  }
+
+  google::protobuf::DescriptorPool pool;
+  for (const auto& file : fds.file()) {
+    if (!pool.BuildFile(file)) {
+      PERFETTO_ELOG("Failed to build file descriptor for %s",
+                    file.name().c_str());
+      return false;
+    }
+  }
+
+  const google::protobuf::Descriptor* root_msg =
+      pool.FindMessageTypeByName(root_message);
+  if (!root_msg) {
+    PERFETTO_ELOG("Could not find the root message \"%s\"",
+                  root_message.c_str());
+    return false;
+  }
+
+  return LoadFromDescriptor(root_msg);
+}
+
+bool FilterUtil::LoadFromDescriptor(
+    const google::protobuf::Descriptor* root_msg) {
+  passthrough_fields_seen_.clear();
+  filter_string_fields_seen_.clear();
+  filter_string_semantic_types_seen_.clear();
 
   // |descriptors_by_full_name| is passed by argument rather than being a member
   // field so that we don't risk leaving it out of sync (and depending on it in
