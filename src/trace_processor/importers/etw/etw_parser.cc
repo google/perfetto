8a7935b2fd07d902bef3ad7762fd2460dc5e13dc
Jesse McKenna <jessemckenna@google.com>
2026-02-02 09:46:18 -0800

    Split file I/O events by thread (#4599)
    This change is a follow up to [Add file I/O ETW events to Perfetto
    UI](https://github.com/google/perfetto/pull/4116). This change splits
    the file I/O events into one row per thread, per a reviewer request on
    the original PR
    ([discussion](https://github.com/google/perfetto/pull/4116#discussion_r2624358608)).
    
    Before:
    <img width="1728" height="1117" alt="Screenshot 2026-01-27 at 4 21
    49 PM"
    src="https://github.com/user-attachments/assets/87851ae6-da11-4051-92e9-f2eed81222a4"
    />
    
    After:
    <img width="1728" height="1117" alt="Screenshot 2026-01-28 at 10 16
    58 AM"
    src="https://github.com/user-attachments/assets/0b4cb926-657b-42fb-9b27-b8143144e3b5"
    />

--- src/trace_processor/importers/etw/etw_parser.cc
+++ src/trace_processor/importers/etw/etw_parser.cc
@@ -88,23 +88,26 @@ base::Status EtwParser::ParseEtwEvent(uint32_t cpu,
   }
 
   const auto& file_io_tracker = context_->file_io_tracker;
+  const UniqueTid utid =
+      context_->process_tracker->GetOrCreateThread(decoder.thread_id());
   if (decoder.has_file_io_create()) {
-    file_io_tracker->ParseFileIoCreate(ts, decoder.file_io_create());
+    file_io_tracker->ParseFileIoCreate(ts, utid, decoder.file_io_create());
   }
   if (decoder.has_file_io_dir_enum()) {
-    file_io_tracker->ParseFileIoDirEnum(ts, decoder.file_io_dir_enum());
+    file_io_tracker->ParseFileIoDirEnum(ts, utid, decoder.file_io_dir_enum());
   }
   if (decoder.has_file_io_info()) {
-    file_io_tracker->ParseFileIoInfo(ts, decoder.file_io_info());
+    file_io_tracker->ParseFileIoInfo(ts, utid, decoder.file_io_info());
   }
   if (decoder.has_file_io_read_write()) {
-    file_io_tracker->ParseFileIoReadWrite(ts, decoder.file_io_read_write());
+    file_io_tracker->ParseFileIoReadWrite(ts, utid,
+                                          decoder.file_io_read_write());
   }
   if (decoder.has_file_io_simple_op()) {
-    file_io_tracker->ParseFileIoSimpleOp(ts, decoder.file_io_simple_op());
+    file_io_tracker->ParseFileIoSimpleOp(ts, utid, decoder.file_io_simple_op());
   }
   if (decoder.has_file_io_op_end()) {
-    file_io_tracker->ParseFileIoOpEnd(ts, decoder.file_io_op_end());
+    file_io_tracker->ParseFileIoOpEnd(ts, utid, decoder.file_io_op_end());
   }
 
   return base::OkStatus();
