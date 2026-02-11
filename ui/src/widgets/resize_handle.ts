7bd423a987a4c4ee686ae60d6883d1ed7778739a
Steve Golton <stevegolton@google.com>
2026-02-11 12:10:40 +0000

    ui: Resizable track shell (#4748)
    Add a resizable handle down the full height of the timeline which allows
    dragging to resize the track shell area.
    
    
    [9d22456a-c062-4a11-9b92-a12b631e0f4b.webm](https://github.com/user-attachments/assets/a73a8818-e859-424a-94cc-1082423ed965)

--- ui/src/widgets/resize_handle.ts
+++ ui/src/widgets/resize_handle.ts
@@ -17,13 +17,16 @@ import {HTMLAttrs} from './common';
 import {MithrilEvent} from '../base/mithril_utils';
 
 export interface ResizeHandleAttrs extends HTMLAttrs {
-  onResize(deltaPx: number): void;
+  // Called with delta (relative change)
+  onResize?(deltaPx: number): void;
+  // Called with absolute position relative to offsetParent
+  onResizeAbsolute?(positionPx: number): void;
   onResizeStart?(): void;
   onResizeEnd?(): void;
   // Direction of the resize handle:
   // - 'vertical' (default): horizontal bar that can be dragged up/down
   // - 'horizontal': vertical bar that can be dragged left/right
-  direction?: 'vertical' | 'horizontal';
+  readonly direction?: 'vertical' | 'horizontal';
 }
 
 export class ResizeHandle implements m.ClassComponent<ResizeHandleAttrs> {
@@ -47,6 +50,7 @@ export class ResizeHandle implements m.ClassComponent<ResizeHandleAttrs> {
   view({attrs}: m.CVnode<ResizeHandleAttrs>): m.Children {
     const {
       onResize: _onResize,
+      onResizeAbsolute: _onResizeAbsolute,
       onResizeStart: _onResizeStart,
       onResizeEnd: _onResizeEnd,
       direction = 'vertical',
@@ -75,6 +79,8 @@ export class ResizeHandle implements m.ClassComponent<ResizeHandleAttrs> {
 
         this.handleElement!.setPointerCapture(e.pointerId);
         attrs.onResizeStart?.();
+
+        e.stopPropagation();
       },
       onpointermove: (e: MithrilEvent<PointerEvent>) => {
         const offsetParent = this.handleElement?.offsetParent as HTMLElement;
@@ -92,7 +98,8 @@ export class ResizeHandle implements m.ClassComponent<ResizeHandleAttrs> {
           const mouseOffsetX = e.clientX - offsetLeft;
 
           if (this.previousX !== undefined) {
-            attrs.onResize(mouseOffsetX - this.previousX);
+            attrs.onResize?.(mouseOffsetX - this.previousX);
+            attrs.onResizeAbsolute?.(mouseOffsetX);
             this.previousX = mouseOffsetX;
           }
         } else {
@@ -100,7 +107,8 @@ export class ResizeHandle implements m.ClassComponent<ResizeHandleAttrs> {
           const mouseOffsetY = e.clientY - offsetTop;
 
           if (this.previousY !== undefined) {
-            attrs.onResize(mouseOffsetY - this.previousY);
+            attrs.onResize?.(mouseOffsetY - this.previousY);
+            attrs.onResizeAbsolute?.(mouseOffsetY);
             this.previousY = mouseOffsetY;
           }
         }
