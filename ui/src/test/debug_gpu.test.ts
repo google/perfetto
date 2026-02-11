61e20fd55ef15d2b31eef707401dfab462ee2faf
Steve Golton <stevegolton@google.com>
2026-02-11 17:53:17 +0000

    Add test to debug the webgl version

---
+++ ui/src/test/debug_gpu.test.ts
@@ -0,0 +1,42 @@
+// Copyright (C) 2026 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import {Page} from '@playwright/test';
+import {PerfettoTestHelper} from './perfetto_ui_test_helper';
+
+let pth: PerfettoTestHelper;
+let page: Page;
+
+test.beforeAll(async ({browser}, _testInfo) => {
+  page = await browser.newPage();
+  pth = new PerfettoTestHelper(page);
+  await pth.openTraceFile('api34_startup_colƒd.perfetto-trace');
+});
+
+test('debug GPU info', async () => {
+  const info = await page.evaluate(() => {
+    const canvas = document.createElement('canvas');
+    const gl = canvas.getContext('webgl2') ?? canvas.getContext('webgl');
+    if (!gl) {
+      return {renderer: 'N/A', vendor: 'N/A', version: 'N/A'};
+    }
+    const dbg = gl.getExtension('WEBGL_debug_renderer_info');
+    return {
+      renderer: dbg ? gl.getParameter(dbg.UNMASKED_RENDERER_WEBGL) : 'N/A',
+      vendor: dbg ? gl.getParameter(dbg.UNMASKED_VENDOR_WEBGL) : 'N/A',
+      version: gl.getParameter(gl.VERSION),
+    };
+  });
+  console.log('GPU INFO:', info);
+});
