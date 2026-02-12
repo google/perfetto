a7a0e5035836a4b1cd2261fcfb2b146ade7727a7
Ivan Chong <ivankc@google.com>
2026-02-12 15:28:31 +0000

    Fix formatting

--- src/trace_processor/perfetto_sql/stdlib/android/input.sql
+++ src/trace_processor/perfetto_sql/stdlib/android/input.sql
@@ -117,7 +117,8 @@ SELECT
   cast_int!(ui_thread_utid) AS utid,
   frame_id
 FROM android_frames
-WHERE dur > 0;
+WHERE
+  dur > 0;
 
 CREATE PERFETTO TABLE _clean_deliver_events AS
 SELECT
@@ -136,8 +137,7 @@ JOIN thread_slice AS t
 JOIN slice AS parent
   ON s.parent_id = parent.id
 WHERE
-  s.name GLOB 'deliverInputEvent src=*'
-  AND dur > 0;
+  s.name GLOB 'deliverInputEvent src=*' AND dur > 0;
 
 CREATE PERFETTO TABLE _input_event_frame_intersections AS
 SELECT
