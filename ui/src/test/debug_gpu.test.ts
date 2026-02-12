e8d93d0b60cdf45cdd1094ecc9c57620e6b59242
Steve Golton <stevegolton@google.com>
2026-02-12 08:30:21 +0000

    Add better debug information to gpu test

--- ui/src/test/debug_gpu.test.ts
+++ ui/src/test/debug_gpu.test.ts
@@ -13,21 +13,77 @@
 // limitations under the License.
 
 import {test} from '@playwright/test';
+import {PerfettoTestHelper} from './perfetto_ui_test_helper';
 
 test('debug GPU info', async ({browser}) => {
   const page = await browser.newPage();
+  const pth = new PerfettoTestHelper(page);
+  await pth.openTraceFile('api34_startup_cold.perfetto-trace');
+
   const info = await page.evaluate(() => {
-    const canvas = document.createElement('canvas');
-    const gl = canvas.getContext('webgl2') ?? canvas.getContext('webgl');
+    // Use separate canvases to accurately detect support for each WebGL version
+    const canvas1 = document.createElement('canvas');
+    const canvas2 = document.createElement('canvas');
+    const webgl2 = canvas2.getContext('webgl2');
+    const webgl1 = canvas1.getContext('webgl');
+    const gl = webgl2 ?? webgl1;
+
     if (!gl) {
-      return {renderer: 'N/A', vendor: 'N/A', version: 'N/A'};
+      return {
+        error: 'WebGL not supported',
+        webgl2Supported: false,
+        webgl1Supported: false,
+      };
     }
+
     const dbg = gl.getExtension('WEBGL_debug_renderer_info');
-    return {
+    const contextType = webgl2 ? 'webgl2' : 'webgl1';
+
+    // Gather all available debug parameters
+    const params: Record<string, unknown> = {
+      contextType,
+      webgl2Supported: !!webgl2,
+      webgl1Supported: !!webgl1,
+      debugExtensionAvailable: !!dbg,
+
+      // Renderer info (from debug extension if available)
       renderer: dbg ? gl.getParameter(dbg.UNMASKED_RENDERER_WEBGL) : 'N/A',
       vendor: dbg ? gl.getParameter(dbg.UNMASKED_VENDOR_WEBGL) : 'N/A',
+
+      // Standard WebGL parameters
       version: gl.getParameter(gl.VERSION),
+      shadingLanguageVersion: gl.getParameter(gl.SHADING_LANGUAGE_VERSION),
+      vendorString: gl.getParameter(gl.VENDOR),
+      rendererString: gl.getParameter(gl.RENDERER),
+
+      // Capabilities
+      maxTextureSize: gl.getParameter(gl.MAX_TEXTURE_SIZE),
+      maxViewportDims: gl.getParameter(gl.MAX_VIEWPORT_DIMS),
+      maxRenderbufferSize: gl.getParameter(gl.MAX_RENDERBUFFER_SIZE),
+      maxVertexAttribs: gl.getParameter(gl.MAX_VERTEX_ATTRIBS),
+      maxVertexUniformVectors: gl.getParameter(gl.MAX_VERTEX_UNIFORM_VECTORS),
+      maxFragmentUniformVectors: gl.getParameter(gl.MAX_FRAGMENT_UNIFORM_VECTORS),
+      maxVaryingVectors: gl.getParameter(gl.MAX_VARYING_VECTORS),
+      maxTextureImageUnits: gl.getParameter(gl.MAX_TEXTURE_IMAGE_UNITS),
+      maxCombinedTextureImageUnits: gl.getParameter(
+        gl.MAX_COMBINED_TEXTURE_IMAGE_UNITS,
+      ),
+
+      // Precision formats (for fragment shaders)
+      highFloatPrecision: gl.getShaderPrecisionFormat(
+        gl.FRAGMENT_SHADER,
+        gl.HIGH_FLOAT,
+      ),
+      mediumFloatPrecision: gl.getShaderPrecisionFormat(
+        gl.FRAGMENT_SHADER,
+        gl.MEDIUM_FLOAT,
+      ),
+
+      // Available extensions
+      supportedExtensions: gl.getSupportedExtensions(),
     };
+
+    return params;
   });
   console.log('GPU INFO:', info);
 });
