d18b5e4e8ab932c663c3c6b6410d712ba09eb948
zezeozue <zezeozue@google.com>
2026-02-12 12:32:23 +0000

    sdk: Add Android process lifecycle and broadcast track events (#4779)
    AndroidTrackEvents to capture:
    1. process starts
    2. process deaths
    3. process state changes
    4. broadcast deliveries
    5. freezer transitions
    
    We already have jobs. And we'll add service bindings in a follow up.
    
    We capture process names at start and death to allow TraceProcessor
    detect PID reuse without the overhead of ftrace events and complexity of
    interning. Other events will be keyed by pid. This is a pragmatic first
    provides the equivalent of task_new_task and sched_process_free for
    managed Java processes.
    
    Also enabled javastream generation for these protos so the Android
    framework can use generated constants instead of hard-coded IDs.
    
    Test: Manual with ag/38360511

--- include/perfetto/public/protos/config/data_source_config.pzc.h
+++ include/perfetto/public/protos/config/data_source_config.pzc.h
@@ -56,6 +56,7 @@ PERFETTO_PB_MSG_DECL(perfetto_protos_PixelModemConfig);
 PERFETTO_PB_MSG_DECL(perfetto_protos_PriorityBoostConfig);
 PERFETTO_PB_MSG_DECL(perfetto_protos_ProcessStatsConfig);
 PERFETTO_PB_MSG_DECL(perfetto_protos_ProtoLogConfig);
+PERFETTO_PB_MSG_DECL(perfetto_protos_ProtoVmConfig);
 PERFETTO_PB_MSG_DECL(perfetto_protos_StatsdTracingConfig);
 PERFETTO_PB_MSG_DECL(perfetto_protos_SurfaceFlingerLayersConfig);
 PERFETTO_PB_MSG_DECL(perfetto_protos_SurfaceFlingerTransactionsConfig);
@@ -142,6 +143,11 @@ PERFETTO_PB_FIELD(perfetto_protos_DataSourceConfig,
                   perfetto_protos_PriorityBoostConfig,
                   priority_boost,
                   10);
+PERFETTO_PB_FIELD(perfetto_protos_DataSourceConfig,
+                  MSG,
+                  perfetto_protos_ProtoVmConfig,
+                  protovm_config,
+                  12);
 PERFETTO_PB_FIELD(perfetto_protos_DataSourceConfig,
                   MSG,
                   perfetto_protos_FtraceConfig,
