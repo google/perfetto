c2a2d5a70b299bbebc83a8abca5b47c8aad9dc48
Petr Čermák <petrcermak@google.com>
2026-02-12 17:24:50 +0000

    Add links between the main tracks of the Chrome Scroll Jank plugin
    This commit adds links to the details panels of events in the "Chrome
    Scroll Input Latencies", "Chrome Scroll Timeline" and "Chrome Scroll
    Timeline v4" tracks, so that it would be easy to jump between them. It
    also adds links to the relevant Chrome Standard Library tables.
    
    Issue: #4658

--- ui/src/plugins/org.chromium.ChromeScrollJank/scroll_timeline_v4_details_panel.ts
+++ ui/src/plugins/org.chromium.ChromeScrollJank/scroll_timeline_v4_details_panel.ts
@@ -33,26 +33,33 @@ import {renderDetails} from '../../components/details/slice_details';
 import {hasArgs} from '../../components/details/args';
 import {renderSliceArguments} from '../../components/details/slice_args';
 import {SLICE_TABLE} from '../../components/widgets/sql/table_definitions';
-import {SCROLL_TIMELINE_V4_TRACK} from './tracks';
+import {
+  EVENT_LATENCY_TRACK,
+  SCROLL_TIMELINE_TRACK,
+  SCROLL_TIMELINE_V4_TRACK,
+} from './tracks';
 import {TrackEventRef} from '../../components/widgets/track_event_ref';
 import {SCROLL_TIMELINE_V4_TABLE_DEFINITION} from './scroll_timeline_v4_model';
 
 export class ScrollTimelineV4DetailsPanel implements TrackEventDetailsPanel {
   // Information about the frame.
   private frameData?: {
-    // SCROLL_TIMELINE_V4_TRACK.tableName[id=this.id].name.
+    // Values from `SCROLL_TIMELINE_V4_TRACK.tableName[id=this.id]`:
     name: string;
-    // SCROLL_TIMELINE_V4_TRACK.tableName[id=this.id].original_slice_id.
     originalSliceId: SliceSqlId;
-    // SCROLL_TIMELINE_V4_TRACK.tableName[id=this.id].parent_id.
     parentId: number | undefined;
+
+    // References to the corresponding slices in `EVENT_LATENCY_TRACK` and
+    // `SCROLL_TIMELINE_TRACK`.
+    firstEventLatencyPluginSliceId: number | undefined;
+    firstScrollUpdatePluginSliceId: number | undefined;
   };
 
   private sliceDetails?: SliceDetails;
 
   constructor(
     private readonly trace: Trace,
-    // ID of the timeline slice in SCROLL_TIMELINE_V4_TRACK.tableName.
+    // ID of the frame slice in `SCROLL_TIMELINE_V4_TRACK.tableName`.
     private readonly id: number,
   ) {}
 
@@ -64,21 +71,37 @@ export class ScrollTimelineV4DetailsPanel implements TrackEventDetailsPanel {
   private async queryFrameData(): Promise<void> {
     assertTrue(this.frameData === undefined);
     const queryResult = await this.trace.engine.query(`
+      INCLUDE PERFETTO MODULE chrome.scroll_jank_v4;
+
       SELECT
-        name,
-        original_slice_id,
-        parent_id
-      FROM ${SCROLL_TIMELINE_V4_TRACK.tableName}
-      WHERE id = ${this.id}`);
+        model.name,
+        model.original_slice_id,
+        model.parent_id,
+        ${EVENT_LATENCY_TRACK.eventIdSqlSubqueryForEventLatency(
+          'results.first_event_latency_id',
+        )} AS first_event_latency_plugin_slice_id,
+        ${SCROLL_TIMELINE_TRACK.eventIdSqlSubqueryForEventLatency(
+          'results.first_event_latency_id',
+        )} AS first_scroll_update_plugin_slice_id
+      FROM ${SCROLL_TIMELINE_V4_TRACK.tableName} AS model
+      LEFT JOIN chrome_scroll_jank_v4_results AS results
+        ON model.original_slice_id = results.id
+      WHERE model.id = ${this.id}`);
     const row = queryResult.firstRow({
       name: STR,
       original_slice_id: NUM,
       parent_id: NUM_NULL,
+      first_event_latency_plugin_slice_id: NUM_NULL,
+      first_scroll_update_plugin_slice_id: NUM_NULL,
     });
     this.frameData = {
       name: row.name,
       originalSliceId: asSliceSqlId(row.original_slice_id),
       parentId: row.parent_id ?? undefined,
+      firstEventLatencyPluginSliceId:
+        row.first_event_latency_plugin_slice_id ?? undefined,
+      firstScrollUpdatePluginSliceId:
+        row.first_scroll_update_plugin_slice_id ?? undefined,
     };
   }
 
@@ -132,87 +155,143 @@ export class ScrollTimelineV4DetailsPanel implements TrackEventDetailsPanel {
     }
     const frameData = this.frameData;
     const isFrame = frameData.parentId === undefined;
-    const children: m.Child[] = [
-      m(TreeNode, {
-        left: [
-          `This ${isFrame ? 'frame' : 'stage'} `,
-          infoTooltip(
-            'Slice on the "Chrome Scroll Timeline v4" track created by the ' +
-              `plugin. It represents ${isFrame ? 'a' : 'a stage of a'} ` +
-              'frame containing one or more scroll updates.',
-          ),
-        ],
-        right: renderSqlRef({
-          trace: this.trace,
-          tableName: SCROLL_TIMELINE_V4_TRACK.tableName,
-          tableDefinition: SCROLL_TIMELINE_V4_TABLE_DEFINITION,
-          id: this.id,
-        }),
-      }),
-      m(TreeNode, {
-        left: [
-          m(TrackEventRef, {
-            trace: this.trace,
-            table: 'slice',
-            id: frameData.originalSliceId,
-            name: 'Original slice',
+    const children: m.Children = [
+      isFrame ? this.renderFrameReferences() : this.renderStageReference(),
+      m(
+        TreeNode,
+        {
+          left: 'Self',
+          startsCollapsed: true,
+        },
+        [
+          m(TreeNode, {
+            left: [
+              `This ${isFrame ? 'frame' : 'stage'} `,
+              infoTooltip(
+                `Slice on the "${SCROLL_TIMELINE_V4_TRACK.name}" track created ` +
+                  `by the plugin. It represents ${isFrame ? 'a' : 'a stage of a'}` +
+                  ' frame containing one or more scroll updates.',
+              ),
+            ],
+            right: renderSqlRef({
+              trace: this.trace,
+              tableName: SCROLL_TIMELINE_V4_TRACK.tableName,
+              tableDefinition: SCROLL_TIMELINE_V4_TABLE_DEFINITION,
+              id: this.id,
+            }),
           }),
-          infoTooltip(
-            'The original slice which Chrome emitted in the trace, from ' +
-              'which the plugin derived this slice.',
-          ),
+          m(TreeNode, {
+            left: [
+              m(TrackEventRef, {
+                trace: this.trace,
+                table: 'slice',
+                id: frameData.originalSliceId,
+                name: 'Original slice',
+              }),
+              infoTooltip(
+                'The original slice which Chrome emitted in the trace, from ' +
+                  'which the plugin derived this slice.',
+              ),
+            ],
+            right: renderSqlRef({
+              trace: this.trace,
+              tableName: 'slice',
+              tableDefinition: SLICE_TABLE,
+              id: frameData.originalSliceId,
+            }),
+          }),
         ],
-        right: renderSqlRef({
-          trace: this.trace,
-          tableName: 'slice',
-          tableDefinition: SLICE_TABLE,
-          id: frameData.originalSliceId,
-        }),
-      }),
-      isFrame ? this.renderFrameReference() : this.renderStageReference(),
+      ),
     ];
     return m(Section, {title: 'References'}, m(Tree, children));
   }
 
-  private renderFrameReference(): m.Child {
+  private renderFrameReferences(): m.Children {
+    assertTrue(this.frameData!.parentId === undefined);
+    return [this.renderRelatedTrackReferences(), this.renderStdlibReferences()];
+  }
+
+  private renderRelatedTrackReferences(): m.Child {
     const frameData = assertExists(this.frameData);
-    assertTrue(frameData.parentId === undefined);
-    return m(TreeNode, {
-      left: 'Standard library tables',
-      right: [
-        stdlibRef({
+    const children: m.Children = [];
+    if (frameData.firstEventLatencyPluginSliceId !== undefined) {
+      children.push(
+        trackEventRefTreeNode({
           trace: this.trace,
-          table: 'chrome_scroll_jank_v4_results',
-          id: frameData.originalSliceId,
+          table: EVENT_LATENCY_TRACK.tableName,
+          id: frameData.firstEventLatencyPluginSliceId!,
+          name: 'First EventLatency in this frame',
         }),
-        ', ',
-        stdlibRef({
+      );
+    }
+    if (frameData.firstScrollUpdatePluginSliceId !== undefined) {
+      children.push(
+        trackEventRefTreeNode({
           trace: this.trace,
-          table: 'chrome_scroll_jank_v4_reasons',
-          id: frameData.originalSliceId,
+          table: SCROLL_TIMELINE_TRACK.tableName,
+          id: frameData.firstScrollUpdatePluginSliceId!,
+          name: 'First scroll update in this frame',
         }),
-        ', ',
-        stdlibRef({
-          trace: this.trace,
-          table: 'chrome_scroll_frame_info_v4',
-          id: frameData.originalSliceId,
+      );
+    }
+    if (children.length === 0) {
+      return undefined;
+    }
+    return m(
+      TreeNode,
+      {left: 'Related tracks', startsCollapsed: false},
+      children,
+    );
+  }
+
+  private renderStdlibReferences(): m.Child {
+    const frameData = assertExists(this.frameData);
+    return m(
+      TreeNode,
+      {
+        left: 'Standard library tables',
+        startsCollapsed: false,
+      },
+      [
+        m(TreeNode, {
+          right: stdlibRef({
+            trace: this.trace,
+            table: 'chrome_scroll_jank_v4_results',
+            id: frameData.originalSliceId,
+          }),
         }),
-        ', ',
-        stdlibRef({
-          trace: this.trace,
-          table: 'chrome_scroll_jank_tags_v4',
-          id: frameData.originalSliceId,
-          idColumnName: 'frame_id',
+        m(TreeNode, {
+          right: stdlibRef({
+            trace: this.trace,
+            table: 'chrome_scroll_jank_v4_reasons',
+            id: frameData.originalSliceId,
+          }),
         }),
-        ', ',
-        stdlibRef({
-          trace: this.trace,
-          table: 'chrome_tagged_janky_scroll_frames_v4',
-          id: frameData.originalSliceId,
-          idColumnName: 'frame_id',
+        m(TreeNode, {
+          right: stdlibRef({
+            trace: this.trace,
+            table: 'chrome_scroll_frame_info_v4',
+            id: frameData.originalSliceId,
+          }),
+        }),
+        m(TreeNode, {
+          right: stdlibRef({
+            trace: this.trace,
+            table: 'chrome_scroll_jank_tags_v4',
+            id: frameData.originalSliceId,
+            idColumnName: 'frame_id',
+          }),
+        }),
+        m(TreeNode, {
+          right: stdlibRef({
+            trace: this.trace,
+            table: 'chrome_tagged_janky_scroll_frames_v4',
+            id: frameData.originalSliceId,
+            idColumnName: 'frame_id',
+          }),
         }),
       ],
-    });
+    );
   }
 
   private renderStageReference(): m.Child {
