86509db5b500ce2f531184764b8d35828c027a9e
Lalit Maganti <lalitm@google.com>
2026-02-17 16:38:16 +0000

    Revert "Fix Windows compilation and enable libperfetto_c on Windows (#4800)"
    This reverts commit 51c6d658b5e18c75d5deeeb9df9cd9491e7cc8a5.

--- include/perfetto/ext/base/sys_types.h
+++ include/perfetto/ext/base/sys_types.h
@@ -31,19 +31,13 @@
 // MinGW has these. clang-cl and MSVC, which use just the Windows SDK, don't.
 using uid_t = int;
 using gid_t = int;
-
-#if defined(_WIN64)
-using pid_t = int64_t;
-#else
 using pid_t = int;
-#endif  // _WIN64
-
 #endif  // !GCC
 
 #if defined(_WIN64)
 using ssize_t = int64_t;
 #else
-using ssize_t = int;
+using ssize_t = long;
 #endif  // _WIN64
 
 #endif  // OS_WIN
