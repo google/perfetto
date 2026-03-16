ed6cb89a6174e4a8a75eeadab180775ec1c9b757
Lalit Maganti <lalitm@google.com>
2026-03-16 17:47:59 +0000

    tp: add interactive and server subcommands
    Add two new subcommands to trace_processor_shell using the
    declarative flag system:
    - interactive: interactive SQL shell (renamed from repl per RFC-0018)
    - server: RPC server with http/stdio modes (renamed from serve)
    
    All subcommand sources are folded into the single :subcommand
    BUILD.gn target.

--- BUILD
+++ BUILD
@@ -4149,8 +4149,12 @@ perfetto_filegroup(
     srcs = [
         "src/trace_processor/shell/common_flags.cc",
         "src/trace_processor/shell/common_flags.h",
+        "src/trace_processor/shell/interactive_subcommand.cc",
+        "src/trace_processor/shell/interactive_subcommand.h",
         "src/trace_processor/shell/query_subcommand.cc",
         "src/trace_processor/shell/query_subcommand.h",
+        "src/trace_processor/shell/server_subcommand.cc",
+        "src/trace_processor/shell/server_subcommand.h",
         "src/trace_processor/shell/subcommand.cc",
         "src/trace_processor/shell/subcommand.h",
     ],
---
+++ src/trace_processor/shell/server_subcommand.cc
@@ -0,0 +1,113 @@
+/*
+ * Copyright (C) 2026 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "src/trace_processor/shell/server_subcommand.h"
+
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "perfetto/base/build_config.h"
+#include "perfetto/base/logging.h"
+#include "perfetto/base/status.h"
+#include "perfetto/ext/base/status_macros.h"
+#include "perfetto/ext/base/string_utils.h"
+#include "perfetto/ext/base/utils.h"
+#include "perfetto/ext/trace_processor/trace_processor_shell.h"
+#include "src/trace_processor/rpc/rpc.h"
+#include "src/trace_processor/rpc/stdiod.h"
+#include "src/trace_processor/shell/common_flags.h"
+#include "src/trace_processor/shell/subcommand.h"
+
+#if PERFETTO_BUILDFLAG(PERFETTO_TP_HTTPD)
+#include "src/trace_processor/rpc/httpd.h"
+#endif
+
+namespace perfetto::trace_processor::shell {
+
+const char* ServerSubcommand::name() const {
+  return "server";
+}
+
+const char* ServerSubcommand::description() const {
+  return "Start an RPC server.";
+}
+
+std::vector<FlagSpec> ServerSubcommand::GetFlags() {
+  return {
+      StringFlag("port", '\0', "PORT", "HTTP port.", &port_number_),
+      StringFlag("ip-address", '\0', "IP", "HTTP bind address.", &listen_ip_),
+      StringFlag("additional-cors-origins", '\0', "O1,O2,...",
+                 "Additional CORS origins for HTTP mode.",
+                 &additional_cors_origins_str_),
+  };
+}
+
+base::Status ServerSubcommand::Run(const SubcommandContext& ctx) {
+  // First positional arg is the mode.
+  if (ctx.positional_args.empty()) {
+    return base::ErrStatus("server: must specify mode (http or stdio)");
+  }
+  const std::string& mode = ctx.positional_args[0];
+
+  // Optional trace file is second positional arg.
+  std::string trace_file;
+  if (ctx.positional_args.size() >= 2) {
+    trace_file = ctx.positional_args[1];
+  }
+
+  auto config = BuildConfig(*ctx.global, ctx.platform);
+  ASSIGN_OR_RETURN(auto tp,
+                   SetupTraceProcessor(*ctx.global, config, ctx.platform));
+
+  if (!trace_file.empty()) {
+    ASSIGN_OR_RETURN(auto t_load,
+                     LoadTraceFile(tp.get(), ctx.platform, trace_file));
+    base::ignore_result(t_load);
+  }
+
+  bool has_trace = !trace_file.empty();
+
+  if (mode == "stdio") {
+    Rpc rpc(std::move(tp), has_trace, config, [&ctx](TraceProcessor* new_tp) {
+      ctx.platform->OnTraceProcessorCreated(new_tp);
+    });
+    return RunStdioRpcServer(rpc);
+  }
+
+  if (mode == "http") {
+#if PERFETTO_BUILDFLAG(PERFETTO_TP_HTTPD)
+    std::vector<std::string> additional_cors_origins;
+    if (!additional_cors_origins_str_.empty()) {
+      additional_cors_origins =
+          base::SplitString(additional_cors_origins_str_, ",");
+    }
+    Rpc rpc(std::move(tp), has_trace, config, [&ctx](TraceProcessor* new_tp) {
+      ctx.platform->OnTraceProcessorCreated(new_tp);
+    });
+    RunHttpRPCServer(rpc, listen_ip_, port_number_, additional_cors_origins);
+    PERFETTO_FATAL("Should never return");
+#else
+    return base::ErrStatus("HTTP RPC module not supported in this build");
+#endif
+  }
+
+  return base::ErrStatus("server: unknown mode '%s' (expected http or stdio)",
+                         mode.c_str());
+}
+
+}  // namespace perfetto::trace_processor::shell
