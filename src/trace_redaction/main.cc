7bcccaf59010d78ca6a865284c8138d7adb01bd5
Edgar Arriaga <edgararriaga@google.com>
2026-04-23 20:25:55 +0000

    revert cmdline arg passing

--- src/trace_redaction/main.cc
+++ src/trace_redaction/main.cc
@@ -15,10 +15,8 @@
  */
 
 #include "perfetto/base/build_config.h"
-#include "perfetto/base/compiler.h"
 #include "perfetto/base/logging.h"
 #include "perfetto/base/status.h"
-#include "perfetto/ext/base/string_utils.h"
 #include "src/trace_redaction/trace_redaction_framework.h"
 #include "src/trace_redaction/trace_redactor.h"
 #include "src/trace_redaction/verify_integrity.h"
@@ -33,14 +31,15 @@ namespace perfetto::trace_redaction {
 // Builds and runs a trace redactor.
 static base::Status Main(std::string_view input,
                          std::string_view output,
-                         std::string_view package_name,
-                         int nice) {
+                         std::string_view package_name) {
+  // Redaction is a low priority task as users are asynchronously informed
+  // when redaction finishes so we can keep it as a lower priority for now, if
+  // this requirement ever changes, then consider modifying redactor to let
+  // caller specify process level thread priority.
 #if PERFETTO_BUILDFLAG(PERFETTO_OS_ANDROID) && \
     PERFETTO_BUILDFLAG(PERFETTO_ANDROID_BUILD)
   // This will only occur for builds from android repository.
-  setpriority(PRIO_PROCESS, 0, nice);
-#else
-  base::ignore_result(nice);
+  setpriority(PRIO_PROCESS, 0, 19);
 #endif
 
   TraceRedactor::Config config;
@@ -52,18 +51,6 @@ static base::Status Main(std::string_view input,
   return redactor->Redact(input, output, &context);
 }
 
-static int Usage(const char* argv0) {
-  fprintf(stderr, R"(
-Trace redaction tool.
-Usage: %s [OPTIONS] <input_file> <output_file> <package_name>
-
-OPTIONS:
-    --nice <nice_value>                 Setup nice value for the redaction process. (Android-only)
-)",
-          argv0);
-  return 1;
-}
-
 }  // namespace perfetto::trace_redaction
 
 int main(int argc, char** argv) {
@@ -71,35 +58,14 @@ int main(int argc, char** argv) {
   constexpr int kFailure = 1;
   constexpr int kInvalidArgs = 2;
 
-  std::vector<const char*> positional_args;
-  int nice = 0;
-  for (int i = 1; i < argc; i++) {
-    if (strcmp(argv[i], "--nice") == 0) {
-      if (i + 1 < argc) {
-        auto nice_opt = perfetto::base::CStringToInt32(argv[++i]);
-        if (nice_opt.has_value()) {
-          nice = nice_opt.value();
-        } else {
-          PERFETTO_ELOG("Invalid value for --nice: %s", argv[i]);
-          return kInvalidArgs;
-        }
-      } else {
-        PERFETTO_ELOG("--nice requires a value");
-        return kInvalidArgs;
-      }
-    } else {
-      positional_args.push_back(argv[i]);
-    }
-  }
-
-  if (positional_args.size() != 3) {
-    PERFETTO_ELOG("Invalid arguments provided to trace_redactor.");
-    perfetto::trace_redaction::Usage(argv[0]);
+  if (argc != 4) {
+    PERFETTO_ELOG(
+        "Invalid arguments: %s <input file> <output file> <package name>",
+        argv[0]);
     return kInvalidArgs;
   }
 
-  auto result = perfetto::trace_redaction::Main(
-      positional_args[0], positional_args[1], positional_args[2], nice);
+  auto result = perfetto::trace_redaction::Main(argv[1], argv[2], argv[3]);
 
   if (result.ok()) {
     return kSuccess;
