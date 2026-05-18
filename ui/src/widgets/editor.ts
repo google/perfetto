c40934edfc61b3029365ab01eb6aead1e6a6c17e
Steve Golton <stevegolton@google.com>
2026-05-18 16:16:35 +0100

    ui: Use css imports to move scss files close to their corresponding ts files

--- ui/src/widgets/editor.ts
+++ ui/src/widgets/editor.ts
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import './editor.scss';
 import {indentWithTab} from '@codemirror/commands';
 import {EditorState, type Transaction} from '@codemirror/state';
 import {oneDark} from '@codemirror/theme-one-dark';
--- ui/src/widgets/tag_input.ts
+++ ui/src/widgets/tag_input.ts
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import './tag_input.scss';
 import m from 'mithril';
 import {type HTMLFocusableAttrs, Intent} from './common';
 import {findRef} from '../base/dom_utils';
