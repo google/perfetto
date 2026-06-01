44131e8de536fb1708924a36a8348b6a1c28c687
Shin-Yu Wang <shinyuw@google.com>
2026-06-01 08:09:19 +0000

    Add disp_dpu_line_underrun ftrace
    Bug: 518567466
    Test: can use gfx to record, show event on ui correctly

--- src/traced/probes/ftrace/predefined_tracepoints.cc
+++ src/traced/probes/ftrace/predefined_tracepoints.cc
@@ -83,6 +83,7 @@ base::FlatSet<GroupAndName> GenerateGfxTracePoints(
   AddEventGroup(table, "dpu", &events);
   InsertEvent("dpu", "tracing_mark_write", &events);
   InsertEvent("dpu", "disp_dpu_underrun", &events);
+  InsertEvent("dpu", "disp_dpu_line_underrun", &events);
   InsertEvent("dpu", "disp_frame_start_timeout", &events);
   InsertEvent("dpu", "disp_frame_done_timeout", &events);
   InsertEvent("dpu", "disp_frame_start_missing", &events);
