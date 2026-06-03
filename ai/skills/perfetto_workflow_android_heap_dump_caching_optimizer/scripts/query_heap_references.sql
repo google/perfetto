e34354c97ef38fc6db06fa28bfbfb00f919bf16b
Edgar Arriaga <edgararriaga@google.com>
2026-06-03 19:29:52 +0000

    Add copyright headers

--- ai/skills/perfetto_workflow_android_heap_dump_caching_optimizer/scripts/query_heap_references.sql
+++ ai/skills/perfetto_workflow_android_heap_dump_caching_optimizer/scripts/query_heap_references.sql
@@ -1,3 +1,18 @@
+--
+-- Copyright 2026 The Android Open Source Project
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     https://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
 -- This query returns all objects that are referenced (owned) by objects of class <owner_classname>
 -- and are instances of class <owned_classname>
 -- This query is useful to find out details about objects dominated by a class
