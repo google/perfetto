efc9220727982a03c473762dc3c6f7f5e29ec20e
Etienne Pierre-doray <etiennep@chromium.org>
2026-06-27 12:14:53 +0000

    Fix _process_available_info_summary

--- src/trace_processor/perfetto_sql/stdlib/prelude/after_eof/tracks.sql
+++ src/trace_processor/perfetto_sql/stdlib/prelude/after_eof/tracks.sql
@@ -159,6 +159,47 @@ WHERE
   t.event_type = 'slice'
   AND a.key = 'upid';
 
+-- Tracks containing state events associated to a process.
+CREATE PERFETTO TABLE process_state_track(
+  -- Unique identifier for this process state track.
+  id ID(track.id),
+  -- Name of the track.
+  name STRING,
+  -- The type of a track indicates the type of data the track contains.
+  --
+  -- Every track is uniquely identified by the the combination of the
+  -- type and a set of dimensions: type allow identifying a set of tracks
+  -- with the same type of data within the whole universe of tracks while
+  -- dimensions allow distinguishing between different tracks in that set.
+  type STRING,
+  -- The track which is the "parent" of this track. Only non-null for tracks
+  -- created using Perfetto's track_event API.
+  parent_id JOINID(track.id),
+  -- Args for this track which store information about "source" of this track in
+  -- the trace. For example: whether this track orginated from atrace, Chrome
+  -- tracepoints etc.
+  source_arg_set_id ARGSETID,
+  -- Machine identifier
+  machine_id JOINID(machine.id),
+  -- The upid that the track is associated with.
+  upid JOINID(process.id)
+)
+AS
+SELECT
+  t.id,
+  t.name,
+  t.type,
+  t.parent_id,
+  t.source_arg_set_id,
+  t.machine_id,
+  a.int_value AS upid
+FROM __intrinsic_track AS t
+JOIN args AS a
+  ON t.dimension_arg_set_id = a.arg_set_id
+WHERE
+  t.event_type = 'state'
+  AND a.key = 'upid';
+
 -- Tracks which are associated to a single CPU.
 CREATE PERFETTO TABLE cpu_track(
   -- Unique identifier for this cpu track.
