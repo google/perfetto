1799775cf69a12a4fafd17a07a34847ba5a60ae8
Safayat Ullah <safayat@google.com>
2026-07-07 17:44:30 +0000

    ui: Optimize perf counter track query

--- ui/src/plugins/dev.perfetto.LinuxPerf/index.ts
+++ ui/src/plugins/dev.perfetto.LinuxPerf/index.ts
@@ -94,11 +94,7 @@ export default class LinuxPerfPlugin implements PerfettoPlugin {
     const result = await trace.engine.query(`
       SELECT pct.perf_session_id, pct.name, MAX(pct.is_timebase) as is_timebase
       FROM perf_counter_track pct
-      WHERE EXISTS (
-        SELECT 1 FROM counter c
-        WHERE c.track_id = pct.id AND c.value != 0
-        LIMIT 1
-      )
+      JOIN _counter_track_summary s ON pct.id = s.id
       GROUP BY pct.perf_session_id, pct.name
       ORDER BY pct.perf_session_id, is_timebase DESC, pct.name
     `);
