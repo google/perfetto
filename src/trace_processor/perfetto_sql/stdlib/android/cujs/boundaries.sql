d76a2ddb1f05cbb9c9fa3be5cc21d5694a740fc1
Nicolo' Mazzucato <nicomazz@google.com>
2026-07-08 14:05:19 +0000

    tp: Migrate CUJ SF boundaries to stdlib
    Change-Id: I0abb147c32add1bf4f3e8c5821ae92b333b2d4d3

--- src/trace_processor/perfetto_sql/stdlib/android/cujs/boundaries.sql
+++ src/trace_processor/perfetto_sql/stdlib/android/cujs/boundaries.sql
@@ -15,6 +15,8 @@
 
 INCLUDE PERFETTO MODULE android.cujs.base;
 
+INCLUDE PERFETTO MODULE android.surfaceflinger;
+
 INCLUDE PERFETTO MODULE android.cujs.relevant_slices;
 
 -- Stores the min and max vsync IDs for each of the CUJs which are extracted
@@ -323,3 +325,91 @@ FROM _android_jank_cuj_render_thread_frame_boundary
 GROUP BY
   cuj_id,
   utid;
+
+-- Similarly, extract the min/max vsync for the SF from
+-- commit/compose/onMessageInvalidate slices on its main thread.
+CREATE PERFETTO TABLE _android_jank_cuj_sf_vsync_boundary(
+  -- CUJ id.
+  cuj_id LONG,
+  -- Minimum vsync ID within the CUJ.
+  vsync_min LONG,
+  -- Maximum vsync ID within the CUJ.
+  vsync_max LONG
+)
+AS
+SELECT cuj_id, MIN(vsync) AS vsync_min, MAX(vsync) AS vsync_max
+FROM _android_jank_cuj_sf_root_slice
+GROUP BY
+  cuj_id;
+
+-- Compute the CUJ boundary on the main thread from the frame boundaries.
+CREATE PERFETTO TABLE _android_jank_cuj_sf_main_thread_cuj_boundary(
+  -- CUJ id.
+  cuj_id LONG,
+  -- Thread id of the SF main thread.
+  utid JOINID(thread.id),
+  -- Start timestamp of the CUJ on the SF main thread.
+  ts TIMESTAMP,
+  -- End timestamp of the CUJ on the SF main thread.
+  ts_end TIMESTAMP,
+  -- Duration of the CUJ on the SF main thread.
+  dur DURATION
+)
+AS
+SELECT
+  cuj_id,
+  utid,
+  MIN(ts) AS ts,
+  MAX(ts_end) AS ts_end,
+  MAX(ts_end) - MIN(ts) AS dur
+FROM _android_jank_cuj_sf_main_thread_frame_boundary
+GROUP BY
+  cuj_id,
+  utid;
+
+-- RenderEngine will only work on a frame if SF falls back to client composition.
+-- Because of that we do not calculate overall CUJ boundaries so there is no
+-- `_android_jank_cuj_sf_render_engine_cuj_boundary` table created.
+-- RenderEngine frame boundaries are calculated based on `composeSurfaces` slice start
+-- on the main thread (this calls into RenderEngine), and when `REThreaded::drawLayers`
+-- ends.
+CREATE PERFETTO TABLE _android_jank_cuj_sf_render_engine_frame_boundary(
+  -- CUJ id.
+  cuj_id LONG,
+  -- Thread id of the RenderEngine thread.
+  utid JOINID(thread.id),
+  -- Vsync ID of this frame.
+  vsync LONG,
+  -- Timestamp of the composeSurfaces slice bounding this drawLayers.
+  ts TIMESTAMP,
+  -- Start timestamp of the drawLayers slice.
+  ts_draw_layers_start TIMESTAMP,
+  -- End timestamp of the drawLayers slice.
+  ts_end TIMESTAMP,
+  -- Duration of the frame boundary.
+  dur DURATION
+)
+AS
+SELECT
+  cuj_id,
+  utid,
+  vsync,
+  draw_layers.ts_compose_surfaces AS ts,
+  draw_layers.ts AS ts_draw_layers_start,
+  draw_layers.ts_end,
+  draw_layers.ts_end - draw_layers.ts_compose_surfaces AS dur
+FROM _android_jank_cuj_sf_draw_layers_slice AS draw_layers;
+
+CREATE PERFETTO TABLE _android_jank_cuj_sf_boundary(
+  -- CUJ id.
+  cuj_id LONG,
+  -- Start timestamp of the CUJ.
+  ts TIMESTAMP,
+  -- End timestamp of the CUJ.
+  ts_end TIMESTAMP,
+  -- Duration of the CUJ.
+  dur DURATION
+)
+AS
+SELECT cuj_id, ts, ts_end, dur
+FROM _android_jank_cuj_sf_main_thread_cuj_boundary;
