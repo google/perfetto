2afb505cea6f582e2ae5c66368aa08af007e68ac
Lalit Maganti <lalitm@google.com>
2026-07-10 12:40:58 +0100

    [𝘀𝗽𝗿] initial version
    Created using spr 1.3.7

--- test/trace_processor/diff_tests/tables/tests_sched.py
+++ test/trace_processor/diff_tests/tables/tests_sched.py
@@ -124,23 +124,23 @@ class TablesSched(TestSuite):
     return DiffTestBlueprint(
         trace=DataPath('sched_wakeup_trace.atr'),
         query="""
-          SELECT id, ts, name, cpu, utid, arg_set_id, common_flags
+          SELECT id, ts, name, cpu, utid, common_flags
           FROM ftrace_event
           WHERE common_flags != 0
           ORDER BY ts LIMIT 10
         """,
         out=Csv("""
-          "id","ts","name","cpu","utid","arg_set_id","common_flags"
-          3,1735489788930,"sched_waking",0,300,21,1
-          4,1735489812571,"sched_waking",0,300,25,1
-          5,1735489833977,"sched_waking",1,305,29,1
-          8,1735489876788,"sched_waking",1,297,47,1
-          9,1735489879097,"sched_waking",0,304,51,1
-          12,1735489933912,"sched_waking",0,428,69,1
-          14,1735489972385,"sched_waking",1,232,80,1
-          17,1735489999987,"sched_waking",1,232,80,1
-          19,1735490039439,"sched_waking",1,298,98,1
-          20,1735490042084,"sched_waking",1,298,102,1
+          "id","ts","name","cpu","utid","common_flags"
+          3,1735489788930,"sched_waking",0,300,1
+          4,1735489812571,"sched_waking",0,300,1
+          5,1735489833977,"sched_waking",1,305,1
+          8,1735489876788,"sched_waking",1,297,1
+          9,1735489879097,"sched_waking",0,304,1
+          12,1735489933912,"sched_waking",0,428,1
+          14,1735489972385,"sched_waking",1,232,1
+          17,1735489999987,"sched_waking",1,232,1
+          19,1735490039439,"sched_waking",1,298,1
+          20,1735490042084,"sched_waking",1,298,1
         """))
 
   def test_thread_executing_span_graph(self):
