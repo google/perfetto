502412dfb6c6a70d99faddd8783cdc6ae072d4f2
Safayat Ullah <safayat@google.com>
2026-07-28 13:51:42 +0100

    tp: Handle empty CpuInfo packets without crashing (#6898)
    When a trace packet contains an empty CpuInfo message (0 CPUs),
    SystemProbesParser previously logged an error but did not return early.
    Because std::all_of on an empty container returns true, it attempted
    out-of-bounds indexing on the empty cpu_infos vector, leading to a
    SIGILL crash.
    This change adds an early return and increments a new cpu_info_empty
    analysis stat when an empty CpuInfo packet is encountered.
    
    Bug: 539414702

--- src/trace_processor/storage/stats.h
+++ src/trace_processor/storage/stats.h
@@ -193,6 +193,11 @@ namespace perfetto::trace_processor::stats {
   F(missing_disk_io_event_name,           kSingle,  kError,    kAnalysis, Scope::kMachineAndTrace,      \
        "ETW Disk IO tracker encountered an event with an opcode for which it " \
         "didn't have a name."),                                                \
+  F(cpu_info_empty,                       kSingle,  kError,    kAnalysis, Scope::kMachineAndTrace,      \
+       "CpuInfo packet in the trace contained no CPUs. This usually happens "  \
+       "when /proc/cpuinfo is unreadable or empty in the environment (e.g. "   \
+       "sandbox or container) where tracing occurred. Metadata in the cpu "    \
+       "table will be missing."),                                              \
   F(mismatched_sched_switch_tids,         kSingle,  kError,    kAnalysis, Scope::kMachineAndTrace, ""), \
   F(mm_unknown_type,                      kSingle,  kError,    kAnalysis, Scope::kMachineAndTrace, ""), \
   F(parse_trace_duration_ns,              kSingle,  kInfo,     kAnalysis, Scope::kGlobal, ""), \
