1ce64fcc239bd1c34a2fac2595b092a973e9474c
Lalit Maganti <lalitm@google.com>
2026-09-15 00:17:36 +0000

    tp: fix package_list lookup in art_process_metadata importer
    When looking up whether a package already exists in package_list, pass
    the null-terminated string view c_str() to the cursor filter rather than
    the numeric raw_id() of the interned StringId. Passing raw_id() causes a
    type mismatch against the string column, causing the lookup to always
    evaluate to false and unconditionally inserting duplicate rows with a
    null version_code.
    
    Fixes: b/561692181

--- src/trace_processor/plugins/art_process_metadata_importer/art_process_metadata_module.cc
+++ src/trace_processor/plugins/art_process_metadata_importer/art_process_metadata_module.cc
@@ -24,6 +24,7 @@
 #include "perfetto/protozero/field.h"
 #include "protos/perfetto/trace/profiling/art_process_metadata.pbzero.h"
 #include "protos/perfetto/trace/trace_packet.pbzero.h"
+#include "src/trace_processor/containers/null_term_string_view.h"
 #include "src/trace_processor/core/dataframe/specs.h"
 #include "src/trace_processor/importers/common/mapping_tracker.h"
 #include "src/trace_processor/importers/common/process_tracker.h"
@@ -73,7 +74,8 @@ void UpdatePackageList(TraceProcessorContext* context,
                        base::StringView package_name,
                        int64_t uid) {
   StringId package_name_id = context->storage->InternString(package_name);
-  cursor.SetFilterValueUnchecked(0, package_name_id.raw_id());
+  NullTermStringView pkg_str = context->storage->GetString(package_name_id);
+  cursor.SetFilterValueUnchecked(0, pkg_str.c_str());
   cursor.SetFilterValueUnchecked(1, uid);
   cursor.Execute();
   bool found = !cursor.Eof();
