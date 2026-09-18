d1863331df038a68e872f3c7e194eaf0be2c4b03
Lalit Maganti <lalitm@google.com>
2026-09-18 04:32:04 +0100

    tp: keep selection header includes in standard groups

--- src/trace_processor/core/exec/row_selection.h
+++ src/trace_processor/core/exec/row_selection.h
@@ -21,8 +21,8 @@
 #include <cstdint>
 #include <memory>
 #include <vector>
-#include "src/trace_processor/core/exec/buffer_pool.h"
 
+#include "src/trace_processor/core/exec/buffer_pool.h"
 #include "src/trace_processor/core/util/flex_vector.h"
 #include "src/trace_processor/core/util/span.h"
 
