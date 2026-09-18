f719a2d7212272eafa89c978ab8df57ce403aa0f
Nicolo' Mazzucato <nicomazz@google.com>
2026-09-18 14:52:46 +0000

    ui: revert debug_tracks changes
    Change-Id: Ifcc270066e99ed1888f63e791aaab0c3198f1d20

--- ui/src/components/tracks/debug_tracks.ts
+++ ui/src/components/tracks/debug_tracks.ts
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import {raf} from '../../core/raf_scheduler';
 import type {Trace} from '../../public/trace';
 import {TrackNode} from '../../public/workspace';
 import {SourceDataset} from '../../trace_processor/dataset';
@@ -136,7 +135,6 @@ export async function addDebugSliceTrack(args: DebugSliceTrackArgs) {
       args.colorColumn,
     );
   }
-  raf.scheduleFullRedraw();
 }
 
 async function createTableForSliceTrack(
@@ -359,7 +357,6 @@ export async function addDebugCounterTrack(args: DebugCounterTrackArgs) {
   } else {
     addSingleCounterTrack(args.trace, tableName, titleBase, uriBase);
   }
-  raf.scheduleFullRedraw();
 }
 
 async function createTableForCounterTrack(
