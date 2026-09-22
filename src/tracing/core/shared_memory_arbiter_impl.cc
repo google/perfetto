6e99f8c3ba7d6d546f4b0582cdc6c720c61c6f86
Etienne Pierre-doray <etiennep@chromium.org>
2026-09-22 14:38:33 +0000

    Add comment

--- src/tracing/core/shared_memory_arbiter_impl.cc
+++ src/tracing/core/shared_memory_arbiter_impl.cc
@@ -491,6 +491,8 @@ bool SharedMemoryArbiterImpl::TryDirectPatchLocked(
   for (auto ctm_it = chunks_to_move.rbegin(); ctm_it != chunks_to_move.rend();
        ++ctm_it) {
     uint32_t header_bitmap = shmem_abi_.GetPageHeaderBitmap(ctm_it->page());
+    // The page layout may have changed concurrently in shared memory.
+    // See b/513159433.
     if (ctm_it->chunk() >=
         SharedMemoryABI::GetNumChunksFromHeaderBitmap(header_bitmap)) {
       continue;
@@ -626,6 +628,8 @@ void SharedMemoryArbiterImpl::FlushPendingCommitDataRequests(
       // them to kChunkComplete - otherwise the service won't look at them.
       for (auto& ctm : *commit_data_req_->mutable_chunks_to_move()) {
         uint32_t header_bitmap = shmem_abi_.GetPageHeaderBitmap(ctm.page());
+        // The page layout may have changed concurrently in shared memory.
+        // See b/513159433.
         if (ctm.chunk() >=
             SharedMemoryABI::GetNumChunksFromHeaderBitmap(header_bitmap)) {
           continue;
